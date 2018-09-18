7d296db008b8e873a629abae5acdfa502b119988
quzha <Quanlu.Zhang@microsoft.com>
2018-09-18 16:53:28 +0800

    optimize import of tuners, modify nnictl accordingly

---
+++ src/sdk/pynni/nni/constants.py
@@ -0,0 +1,51 @@
+# Copyright (c) Microsoft Corporation
+# All rights reserved.
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge,
+# to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and
+# to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ModuleName = {
+    'TPE': 'nni.hyperopt_tuner.hyperopt_tuner',
+    'Random': 'nni.hyperopt_tuner.hyperopt_tuner',
+    'Anneal': 'nni.hyperopt_tuner.hyperopt_tuner',
+    'Evolution': 'nni.evolution_tuner.evolution_tuner',
+    'SMAC': 'nni.smac_tuner.smac_tuner',
+
+    'Medianstop': 'nni.medianstop_assessor.medianstop_assessor'
+}
+
+ClassName = {
+    'TPE': 'HyperoptTuner',
+    'Random': 'HyperoptTuner',
+    'Anneal': 'HyperoptTuner',
+    'Evolution': 'EvolutionTuner',
+    'SMAC': 'SMACTuner',
+
+    'Medianstop': 'MedianstopAssessor'
+}
+
+ClassArgs = {
+    'TPE': {
+        'algorithm_name': 'TPE'
+    },
+    'Random': {
+        'algorithm_name': 'Random'
+    },
+    'Anneal': {
+        'algorithm_name': 'Anneal'
+    }
+}
