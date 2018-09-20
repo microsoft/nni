9fe3578bad75f1a5a67badeee22ff8877e8fbe7a
SparkSnail <shinaiyang@pku.edu.cn>
2018-09-20 14:26:53 +0800

    upgrade version to v0.1.1 (#103)
    * upgrade NNI sdk and nnictl version from v0.1.0 to v0.1.1

--- tools/setup.py
+++ tools/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = 'nnictl',
-    version = '0.0.1',
+    version = '0.1.1',
     packages = setuptools.find_packages(),
 
     python_requires = '>=3.5',
