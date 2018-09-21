7230f12c2319be750d5a12541183163cd77d184e
Crysple <v-zejlin@microsoft.com>
2018-09-21 13:58:18 +0800

    test built-in tuner in CI

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Integration Test
-cd naive && python3 run.sh
+cd naive && python3 run.py
 # Built-in Tuner Test 
-cd ../test_builtin_tuner/ && python3 run.sh
+cd ../test_builtin_tuner/ && python3 run.py
