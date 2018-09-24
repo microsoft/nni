a5d4a2088f56e0cb5ac5e8875ca2766cf3da8b57
Shinai Yang (FA TALENT) <t-shya@microsoft.com>
2018-09-24 12:56:27 +0800

    fix repo

--- src/nni_manager/training_service/pai/paiData.ts
+++ src/nni_manager/training_service/pai/paiData.ts
@@ -46,7 +46,7 @@ export class PAITrialJobDetail implements TrialJobDetail {
 }
 
 export const PAI_TRIAL_COMMAND_FORMAT: string =
-`pip3 install -v --user git+https://github.com/yds05/nni.git@dev-pai 
+`pip3 install -v --user git+https://github.com/yds05/nni.git@dev-pai-t-shya2
 && export NNI_PLATFORM=pai NNI_SYS_DIR={0} NNI_TRIAL_JOB_ID={1} NNI_OUTPUT_DIR={0} 
 && cd $NNI_SYS_DIR && mkdir .nni 
 && python3 -m trial.trial_keeper --trial_command '{2}' --nnimanager_ip '{3}'`;
