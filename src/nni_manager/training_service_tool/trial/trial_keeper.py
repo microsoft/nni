4cba4d135e97814e0b090c5f2be0279a7a880ce6
Shinai Yang (FA TALENT) <t-shya@microsoft.com>
2018-09-24 20:39:05 +0800

    debug

--- src/nni_manager/training_service_tool/trial/trial_keeper.py
+++ src/nni_manager/training_service_tool/trial/trial_keeper.py
@@ -54,21 +54,21 @@ def main_loop(args):
                 local_directory = os.environ['NNI_OUTPUT_DIR']
                 hdfs_output_dir = os.environ['NNI_HDFS_OUTPUT_DIR']
                 nni_user_name = os.environ['NNI_USER_NAME']
-                #get hdfs_host and hdfs_directory
-                hdfs_host_pattern = 'hdfs://[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}:[0-9]{2,5}'
-                hdfs_host = re.findall(hdfs_host_pattern, hdfs_output_dir)
-                hdfs_directory = hdfs_output_dir.replace(hdfs_host[0], '')
-                #get url_host
-                url_host_pattern = '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}'
-                url_host = re.findall(url_host_pattern, hdfs_host[0])
-                #init hdfs client
-                if not os.path.isdir(local_directory):
-                    raise Exception('Local Directory Error!')
-                #get local folder name
-                local_folder_name = local_directory.replace(os.path.dirname(local_directory), '')[1:]
-                hdfs_output_dir_full = os.path.join(hdfs_directory, local_folder_name)
-                hdfs_client = HdfsClient(hosts='{0}:{1}'.format(url_host[0], '50070'), user_name=nni_user_name)
-                print(local_directory, hdfs_output_dir_full)
+                # #get hdfs_host and hdfs_directory
+                # hdfs_host_pattern = 'hdfs://[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}:[0-9]{2,5}'
+                # hdfs_host = re.findall(hdfs_host_pattern, hdfs_output_dir)
+                # hdfs_directory = hdfs_output_dir.replace(hdfs_host[0], '')
+                # #get url_host
+                # url_host_pattern = '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}'
+                # url_host = re.findall(url_host_pattern, hdfs_host[0])
+                # #init hdfs client
+                # if not os.path.isdir(local_directory):
+                #     raise Exception('Local Directory Error!')
+                # #get local folder name
+                # local_folder_name = local_directory.replace(os.path.dirname(local_directory), '')[1:]
+                # hdfs_output_dir_full = os.path.join(hdfs_directory, local_folder_name)
+                # hdfs_client = HdfsClient(hosts='{0}:{1}'.format(url_host[0], '50070'), user_name=nni_user_name)
+                # print(local_directory, hdfs_output_dir_full)
                 # if copyDirectoryToHdfs(local_directory, hdfs_output_dir_full, hdfs_client):
                 #     print('copy directory success!')
                 # else:
