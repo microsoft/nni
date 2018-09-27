f61ab3e8aa1a8c1aca3fac0d77ebc50324018bb9
Shinai Yang (FA TALENT) <t-shya@microsoft.com>
2018-09-27 21:56:18 +0800

    fix bug

--- examples/trials/sklearn-regression/regression.py
+++ examples/trials/sklearn-regression/regression.py
@@ -15,7 +15,7 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-# import nni
+import nni
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
