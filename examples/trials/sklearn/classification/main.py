c9d510a309228d324fd469ff3a193cb6b81ea4ad
Shinai Yang (FA TALENT) <t-shya@microsoft.com>
2018-10-07 20:05:52 +0800

    add sklearn classification

--- examples/trials/sklearn/classification/main.py
+++ examples/trials/sklearn/classification/main.py
@@ -16,74 +16,50 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import nni
-from sklearn.datasets import load_boston
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
-from sklearn import linear_model
+from sklearn.feature_extraction.text import TfidfVectorizer
 import logging
-from sklearn.model_selection import train_test_split
 import numpy as np
-from sklearn.metrics import r2_score
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.svm import SVR
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.tree import DecisionTreeRegressor
 
-LOG = logging.getLogger('sklearn_regression')
+
+LOG = logging.getLogger('sklearn_classification')
 
 def load_data():
-    '''Load dataset, use boston dataset'''
-    boston = load_boston()
-    X_train, X_test, y_train, y_test = train_test_split(boston.data, boston.target, random_state=99, test_size=0.25)
+    '''Load dataset, use 20newsgroups dataset'''
+    news = fetch_20newsgroups(subset='all')
+    X_train, X_test, y_train, y_test = train_test_split(news.data, news.target, random_state=99, test_size=0.25)
     #normalize data
-    ss_X = StandardScaler()
-    ss_y = StandardScaler()
-
-    X_train = ss_X.fit_transform(X_train)
-    X_test = ss_X.transform(X_test)
-    y_train = ss_y.fit_transform(y_train[:, None])[:,0]
-    y_test = ss_y.transform(y_test[:, None])[:,0]
+    vec = TfidfVectorizer(stop_words='english')
+    X_train = vec.fit_transform(X_train)
+    X_test = vec.transform(X_test)
 
     return X_train, X_test, y_train, y_test
 
 def get_default_parameters():
     '''get default parameters'''
     params = {
-        'model_name': 'LinearRegression'
+        'loss': 'squared_hinge',
+        'C': 1.0,
+        'max_iter': 1000
     }
     return params
 
 def get_model(PARAMS):
     '''Get model according to parameters'''
-    model_dict = {
-        'LinearRegression': LinearRegression(),
-        'SVR': SVR(),
-        'KNeighborsRegressor': KNeighborsRegressor(),
-        'DecisionTreeRegressor': DecisionTreeRegressor()
-    }
-    if not model_dict.get(PARAMS['model_name']):
-        LOG.exception('Not supported model!')
-        exit(1)
+    model = LinearSVC()
+    model.loss = PARAMS.get('loss')
+    model.C = PARAMS.get('C')
+    model.max_iter = PARAMS.get('max_iter')
     
-    model = model_dict[PARAMS['model_name']]
-    
-    try:
-        if PARAMS['model_name'] == 'SVR':
-            model.kernel = PARAMS['svr_kernel']
-        elif PARAMS['model_name'] == 'KNeighborsRegressor':
-            model.weights = PARAMS['knr_weights']
-    except Exception as exception:
-        LOG.exception(exception)
-        raise
     return model
-
 
 def run(X_train, X_test, y_train, y_test, PARAMS):
     '''Train model and predict result'''
     model.fit(X_train, y_train)
-    predict_y = model.predict(X_test)
-    score = r2_score(y_test, predict_y)
-    LOG.debug('r2 score: %s' % score)
+    score = model.score(X_test, y_test)
+    LOG.debug('score: %s' % score)
     nni.report_final_result(score)
 
 if __name__ == '__main__':
