59626ecee84804aff19dc97b5a56f2b0b2230301
Shinai Yang (FA TALENT) <t-shya@microsoft.com>
2018-10-16 16:03:36 +0800

    update

--- tools/nnicmd/common_utils.py
+++ tools/nnicmd/common_utils.py
@@ -70,8 +70,4 @@ def detect_port(port):
         socket_test.close()
         return True
     except:
-        try:
-            socket_test.close()
-        except:
-            return False
         return False
