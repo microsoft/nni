b29b7e557be954a0a5909a3fd25027652fd67e93
SparkSnail <shinaiyang@pku.edu.cn>
2018-10-18 12:01:35 +0800

    Add exception handler in trial_keeper (#235)
    * add exception handling in trial_keeper.py

--- tools/trial_tool/trial_keeper.py
+++ tools/trial_tool/trial_keeper.py
@@ -54,8 +54,8 @@ def main_loop(args):
             print('subprocess terminated. Exit code is {}. Quit'.format(retCode))
             #copy local directory to hdfs
             nni_local_output_dir = os.environ['NNI_OUTPUT_DIR']
-            hdfs_client = HdfsClient(hosts='{0}:{1}'.format(args.pai_hdfs_host, '50070'), user_name=args.pai_user_name, timeout=5)
             try:
+                hdfs_client = HdfsClient(hosts='{0}:{1}'.format(args.pai_hdfs_host, '50070'), user_name=args.pai_user_name, timeout=5)
                 if copyDirectoryToHdfs(nni_local_output_dir, args.pai_hdfs_output_dir, hdfs_client):
                     print('copy directory from {0} to {1} success!'.format(nni_local_output_dir, args.pai_hdfs_output_dir))
                 else:
