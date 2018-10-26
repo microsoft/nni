4ce0b4f88fcfc442a0c39e27b0b0212a848be554
Crysple <v-zejlin@microsoft.com>
2018-10-26 15:21:12 +0800

    test ssh key

--- test/initialize_remote.sh
+++ test/initialize_remote.sh
@@ -4,7 +4,7 @@ pw="nniNNI123nniNNI"
 ip="nni@104.210.63.241"
 key="id_rsa"
 echo "Initializing remote machine..."
-ssh -i $key $ip "rm -rf pynni"
+yes | ssh -i $key $ip "rm -rf pynni"
 echo "Copy nni sdk to remote machine..."
 scp -i $key -r ../src/sdk/pynni $ip:~
 echo "Install nni sdk in remote machine..."
