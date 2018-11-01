a4b6eedffea9ca321060324bce569cb3117676f6
v-liguo <v-liguo@microsoft.com>
2018-11-01 19:20:49 +0800

    update

--- src/webui/src/static/const.ts
+++ src/webui/src/static/const.ts
@@ -1,5 +1,4 @@
-// export const MANAGER_IP = `/api/v1/nni`;
-export const MANAGER_IP = 'http://10.150.144.121:8080/api/v1/nni';
+export const MANAGER_IP = `/api/v1/nni`;
 export const trialJobStatus = [
     'UNKNOWN',
     'WAITING',
