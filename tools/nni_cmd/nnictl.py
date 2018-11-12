0a8e3ab4586c764a7cacce9bf4a7c2a1eb60c5ab
Shinai Yang (FA TALENT) <t-shya@microsoft.com>
2018-11-12 12:05:41 +0800

    update

--- tools/nni_cmd/nnictl.py
+++ tools/nni_cmd/nnictl.py
@@ -73,7 +73,7 @@ def parse_args():
 
     #parse stop command
     parser_stop = subparsers.add_parser('stop', help='stop the experiment')
-    parser_stop.add_argument('id', nargs='?', help='the id of experiment')
+    parser_stop.add_argument('id', nargs='?', help='the id of experiment, use \'all\' to stop all of experiments')
     parser_stop.set_defaults(func=stop_experiment)
 
     #parse trial command
