c83f01b7841c6ecaf35bf37edf155e31129ae2c9
Crysple <v-zejlin@microsoft.com>
2018-11-12 14:33:47 +0800

    fix sdk's unittest and add medianstop, batchtuner to ci

--- test/sdk_test.py
+++ test/sdk_test.py
@@ -11,39 +11,37 @@ GREEN = '\33[32m'
 RED = '\33[31m'
 CLEAR = '\33[0m'
 
-TUNER_LIST = ['TPE', 'Random', 'Anneal', 'Evolution']
+TUNER_LIST = ['BatchTuner']
+ASSESSOR_LIST = ['Medianstop']
 EXPERIMENT_URL = 'http://localhost:8080/api/v1/nni/experiment'
 
 
-def switch_tuner(tuner_name):
-    '''Change tuner in config.yml'''
-    config_path = 'sdk_tuner_test/local.yml'
+def switch(dispatch_type, dispatch_name):
+    '''Change dispatch in config.yml'''
+    config_path = 'sdk_test/local.yml'
     experiment_config = get_yml_content(config_path)
-    experiment_config['tuner'] = {
-        'builtinTunerName': tuner_name,
+    print(dispatch_type.lower())
+    experiment_config[dispatch_type.lower()] = {
+        'builtin' + dispatch_type + 'Name': dispatch_name,
         'classArgs': {
             'optimize_mode': 'maximize'
         }
     }
+    print(experiment_config)
     dump_yml_content(config_path, experiment_config)
 
-def test_builtin_tuner(tuner_name):
-    remove_files(['sdk_tuner_test/nni_tuner_result.txt'])
-    switch_tuner(tuner_name)
+def test_builtin_dispatcher(dispatch_type, dispatch_name):
+    remove_files(['sdk_test/nni_tuner_result.txt'])
+    switch(dispatch_type, dispatch_name)
 
-    print('Testing %s...'%tuner_name)
-    proc = subprocess.run(['nnictl', 'create', '--config', 'sdk_tuner_test/local.yml'])
+    print('Testing %s...' % dispatch_name)
+    proc = subprocess.run(['nnictl', 'create', '--config', 'sdk_test/local.yml'])
     assert proc.returncode == 0, '`nnictl create` failed with code %d' % proc.returncode
 
     nnimanager_log_path = fetch_experiment_config(EXPERIMENT_URL)
     
     for _ in range(10):
         time.sleep(3)
-
-        # check if tuner exists with error
-        tuner_status = read_last_line('tuner_result.txt')
-        assert tuner_status != 'ERROR', 'Tuner exited with error'
-
         # check if experiment is done
         experiment_status = check_experiment_status(nnimanager_log_path)
         if experiment_status:
@@ -51,16 +49,17 @@ def test_builtin_tuner(tuner_name):
     
     assert experiment_status, 'Failed to finish in 30 sec'
 
-def run():
-    to_remove = ['tuner_search_space.json', 'tuner_result.txt', 'assessor_result.txt']
+def run(dispatch_type):
+    to_remove = ['sdk_test/tuner_search_space.json', 'sdk_test/tuner_result.txt', 'sdk_test/assessor_result.txt']
     remove_files(to_remove)
 
-    for tuner_name in TUNER_LIST:
+    LIST = TUNER_LIST if dispatch_type == 'Tuner' else ASSESSOR_LIST
+    for dispatcher_name in LIST:
         try:
-            test_builtin_tuner(tuner_name)
-            print(GREEN + 'Test ' +tuner_name+ ' tuner: TEST PASS' + CLEAR)
+            test_builtin_dispatcher(dispatch_type, dispatcher_name)
+            print(GREEN + 'Test %s %s: TEST PASS' % (dispatcher_name, dispatch_type) + CLEAR)
         except Exception as error:
-            print(GREEN + 'Test ' +tuner_name+ ' tuner: TEST FAIL' + CLEAR)
+            print(RED + 'Test %s %s: TEST FAIL' % (dispatcher_name, dispatch_type) + CLEAR)
             print('%r' % error)
             traceback.print_exc()
             raise error
@@ -72,4 +71,5 @@ if __name__ == '__main__':
     installed = (sys.argv[-1] != '--preinstall')
     setup_experiment(installed)
 
-    run()
+    run('Tuner')
+    run('Assessor')
