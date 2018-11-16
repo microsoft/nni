ae979c9ac50f6886cc3529d9c16c3d60d0b10afb
Yan Ni <yann@microsoft.com>
2018-11-16 17:56:10 +0800

    sync graph.py between tuners & trial of ga_squad

--- examples/tuners/ga_customer_tuner/graph.py
+++ examples/tuners/ga_customer_tuner/graph.py
@@ -1,12 +1,40 @@
-# -*- coding: utf-8 -*-
+# Copyright (c) Microsoft Corporation
+# All rights reserved.
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge,
+# to any person obtaining a copy of this software and associated
+# documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and
+# to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+Graph is customed-define class, this module contains related class and function about graph.
+'''
+
 
 import copy
 import json
 import random
 from enum import Enum, unique
 
+global_layer_id = 0
+
 @unique
 class LayerType(Enum):
+    '''
+    Layer type
+    '''
     attention = 0
     self_attention = 1
     rnn = 2
@@ -14,69 +42,91 @@ class LayerType(Enum):
     output = 4
 
 class Layer(object):
-    def __init__(self, type, input=None, output=None, size=None):
-        self.input = input if input is not None else []
+    '''
+    Layer class, which contains the information of graph.
+    '''
+    def __init__(self, graph_type, inputs=None, output=None, size=None):
+        global global_layer_id
+        self.global_id = global_layer_id
+        global_layer_id += 1
+        self.input = inputs if inputs is not None else []
         self.output = output if output is not None else []
-        self.type = type
+        self.graph_type = graph_type
         self.is_delete = False
         self.size = size
-        if type == LayerType.attention.value:
+        if graph_type == LayerType.attention.value:
             self.input_size = 2
             self.output_size = 1
-        elif type == LayerType.rnn.value:
+        elif graph_type == LayerType.rnn.value:
             self.input_size = 1
             self.output_size = 1
-        elif type == LayerType.self_attention.value:
+        elif graph_type == LayerType.self_attention.value:
             self.input_size = 1
             self.output_size = 1
-        elif type == LayerType.input.value:
+        elif graph_type == LayerType.input.value:
             self.input_size = 0
             self.output_size = 1
-        elif type == LayerType.output.value:
+        elif graph_type == LayerType.output.value:
             self.input_size = 1
             self.output_size = 0
         else:
-            print(type)
-    def set_size(self, id, size):
-        if self.type == LayerType.attention.value:
-            if self.input[0] == id:
+            print(graph_type)
+    def set_size(self, graph_id, size):
+        '''
+        Set size.
+        '''
+        if self.graph_type == LayerType.attention.value:
+            if self.input[0] == graph_id:
                 self.size = size
-        if self.type == LayerType.rnn.value:
+        if self.graph_type == LayerType.rnn.value:
             self.size = size
-        if self.type == LayerType.self_attention.value:
+        if self.graph_type == LayerType.self_attention.value:
             self.size = size
-        if self.type == LayerType.output.value:
+        if self.graph_type == LayerType.output.value:
             if self.size != size:
                 return False
         return True
 
     def clear_size(self):
-        if self.type == LayerType.attention.value or LayerType.rnn.value or LayerType.self_attention.value:
+        '''
+        Clear size
+        '''
+        if self.graph_type == LayerType.attention.value or \
+            LayerType.rnn.value or LayerType.self_attention.value:
             self.size = None
 
     def __str__(self):
-        return 'input:' + str(self.input) + ' output:' + str(self.output) + ' type:' + str(
-            self.type) + ' is_delete:' + str(self.is_delete) + ' size:' + str(self.size)
+        return 'id:' + str(self.global_id) +  'input:' + str(self.input) + ' output:' + str(self.output) + ' type:' + str(
+            self.graph_type) + ' is_delete:' + str(self.is_delete) + ' size:' + str(self.size)
 
 def graph_dumps(graph):
+    '''
+    Dump the graph.
+    '''
     return json.dumps(graph, default=lambda obj: obj.__dict__)
 
-def graph_loads(js):
+def graph_loads(graph_json):
+    '''
+    Load graph
+    '''
     layers = []
-    for layer in js['layers']:
-        p = Layer(layer['type'],layer['input'],layer['output'],layer['size'])
-        p.is_delete = layer['is_delete']
-        layers.append(p)
-    graph = Graph(js['max_layer_num'],[], [], [])
+    for layer in graph_json['layers']:
+        layer_info = Layer(layer['type'], layer['input'], layer['output'], layer['size'])
+        layer_info.is_delete = layer['is_delete']
+        layers.append(layer_info)
+    graph = Graph(graph_json['max_layer_num'], [], [], [])
     graph.layers = layers
     return graph
 
 class Graph(object):
-    def __init__(self, max_layer_num, input, output, hide):
+    '''
+    Customed Graph class.
+    '''
+    def __init__(self, max_layer_num, inputs, output, hide):
         self.layers = []
         self.max_layer_num = max_layer_num
 
-        for layer in input:
+        for layer in inputs:
             self.layers.append(layer)
         for layer in output:
             self.layers.append(layer)
@@ -86,12 +136,15 @@ class Graph(object):
         assert self.is_legal()
 
     def is_topology(self, layers=None):
-        if layers == None:
+        '''
+        valid the topology
+        '''
+        if layers is None:
             layers = self.layers
         layers_nodle = []
-        xx = []
-        for i in range(len(layers)):
-            if layers[i].is_delete == False:
+        result = []
+        for i, layer in enumerate(layers):
+            if layer.is_delete is False:
                 layers_nodle.append(i)
         while True:
             flag_break = True
@@ -101,37 +154,46 @@ class Graph(object):
                 for layer2 in layers[layer1].input:
                     if layer2 in layers_nodle:
                         flag_arrive = False
-                if flag_arrive == True:
+                if flag_arrive is True:
                     for layer2 in layers[layer1].output:
-                        if layers[layer2].set_size(layer1, layers[layer1].size) == False:  # Size is error
+                        # Size is error
+                        if layers[layer2].set_size(layer1, layers[layer1].size) is False:
                             return False
                     layers_toremove.append(layer1)
-                    xx.append(layer1)
+                    result.append(layer1)
                     flag_break = False
             for layer in layers_toremove:
                 layers_nodle.remove(layer)
-            xx.append('|')
-            if flag_break == True:
+            result.append('|')
+            if flag_break:
                 break
-        if len(layers_nodle) > 0:  # There is loop in graph || some layers can't to arrive
+        # There is loop in graph || some layers can't to arrive
+        if layers_nodle:
             return False
-        return xx
+        return result
 
     def layer_num(self, layers=None):
-        if layers == None:
+        '''
+        Reutn number of layer.
+        '''
+        if layers is None:
             layers = self.layers
         layer_num = 0
         for layer in layers:
-            if layer.is_delete == False and layer.type != LayerType.input.value and layer.type != LayerType.output.value:
+            if layer.is_delete is False and layer.graph_type != LayerType.input.value\
+                and layer.graph_type != LayerType.output.value:
                 layer_num += 1
         return layer_num
 
     def is_legal(self, layers=None):
-        if layers == None:
+        '''
+        Judge whether is legal for layers
+        '''
+        if layers is None:
             layers = self.layers
 
         for layer in layers:
-            if layer.is_delete == False:
+            if layer.is_delete is False:
                 if len(layer.input) != layer.input_size:
                     return False
                 if len(layer.output) < layer.output_size:
@@ -141,71 +203,76 @@ class Graph(object):
         if self.layer_num(layers) > self.max_layer_num:
             return False
 
-        if self.is_topology(layers) == False:  # There is loop in graph || some layers can't to arrive
+        # There is loop in graph || some layers can't to arrive
+        if self.is_topology(layers) is False:
             return False
 
         return True
 
     def mutation(self, only_add=False):
+        '''
+        Mutation for a graph
+        '''
         types = []
         if self.layer_num() < self.max_layer_num:
             types.append(0)
             types.append(1)
-        if self.layer_num() > 0:
+        if self.layer_num() > 5 and only_add is False:
             types.append(2)
             types.append(3)
         # 0 : add a layer , delete a edge
         # 1 : add a layer , change a edge
         # 2 : delete a layer, delete a edge
         # 3 : delete a layer, change a edge
-        type = random.choice(types)
-        layer_type = random.choice([LayerType.attention.value, LayerType.self_attention.value, LayerType.rnn.value])
+        graph_type = random.choice(types)
+        layer_type = random.choice([LayerType.attention.value,\
+            LayerType.self_attention.value, LayerType.rnn.value])
         layers = copy.deepcopy(self.layers)
         cnt_try = 0
         while True:
             layers_in = []
             layers_out = []
             layers_del = []
-            for layer1 in range(len(layers)):
-                layer = layers[layer1]
-                if layer.is_delete == False:
-                    if layer.type != LayerType.output.value:
-                        layers_in.append(layer1)
-                    if layer.type != LayerType.input.value:
-                        layers_out.append(layer1)
-                    if layer.type != LayerType.output.value and layer.type != LayerType.input.value:
-                        layers_del.append(layer1)
-            if type <= 1:
+            for i, layer in enumerate(layers):
+                if layer.is_delete is False:
+                    if layer.graph_type != LayerType.output.value:
+                        layers_in.append(i)
+                    if layer.graph_type != LayerType.input.value:
+                        layers_out.append(i)
+                    if layer.graph_type != LayerType.output.value\
+                            and layer.graph_type != LayerType.input.value:
+                        layers_del.append(i)
+            if graph_type <= 1:
                 new_id = len(layers)
                 out = random.choice(layers_out)
-                input = []
+                inputs = []
                 output = [out]
                 pos = random.randint(0, len(layers[out].input) - 1)
                 last_in = layers[out].input[pos]
                 layers[out].input[pos] = new_id
-                if type == 0:
+                if graph_type == 0:
                     layers[last_in].output.remove(out)
-                if type == 1:
+                if graph_type == 1:
                     layers[last_in].output.remove(out)
                     layers[last_in].output.append(new_id)
-                    input = [last_in]
-                lay = Layer(type=layer_type, input=input, output=output)
-                while len(input) < lay.input_size:
+                    inputs = [last_in]
+                lay = Layer(graph_type=layer_type, inputs=inputs, output=output)
+                while len(inputs) < lay.input_size:
                     layer1 = random.choice(layers_in)
-                    input.append(layer1)
+                    inputs.append(layer1)
                     layers[layer1].output.append(new_id)
-                lay.input = input
+                lay.input = inputs
                 layers.append(lay)
             else:
                 layer1 = random.choice(layers_del)
                 for layer2 in layers[layer1].output:
                     layers[layer2].input.remove(layer1)
-                    if type == 2:
-                        v2 = random.choice(layers_in)
+                    if graph_type == 2:
+                        random_in = random.choice(layers_in)
                     else:
-                        v2 = random.choice(layers[layer1].input)
-                    layers[layer2].input.append(v2)
-                    layers[v2].output.append(layer2)
+                        random_in = random.choice(layers[layer1].input)
+                    layers[layer2].input.append(random_in)
+                    layers[random_in].output.append(layer2)
                 for layer2 in layers[layer1].input:
                     layers[layer2].output.remove(layer1)
                 layers[layer1].is_delete = True
@@ -219,22 +286,7 @@ class Graph(object):
 
     def __str__(self):
         info = ""
-        for id, layer in enumerate(self.layers):
-            if layer.is_delete == False:
-                info += 'id:%d ' % id + str(layer) + '\n'
+        for l_id, layer in enumerate(self.layers):
+            if layer.is_delete is False:
+                info += 'id:%d ' % l_id + str(layer) + '\n'
         return info
-
-if __name__ == '__main__':
-    graph = Graph(10,
-                  input=[Layer(LayerType.input.value, output=[4, 5], size='x'), Layer(LayerType.input.value, output=[4, 5], size='y')],
-                  output=[Layer(LayerType.output.value, input=[4], size='x'), Layer(LayerType.output.value, input=[5], size='y')],
-                  hide=[Layer(LayerType.attention.value, input=[0, 1], output=[2]), Layer(LayerType.attention.value, input=[1, 0], output=[3])])
-
-    s = graph_dumps(graph)
-    g = graph_loads(json.loads(s))
-    print(g)
-    print(s)
-
-    s = '''{"count":2,"array":[{"input":%s,"output":{"output":0.7}}]}'''%s
-    print(len(s))
-    print(s)
