21a2bb0b0b7c81da725dfa04b940c4abdc10f4b1
SparkSnail <shinaiyang@pku.edu.cn>
2018-11-28 11:26:01 +0800

    Support Azure k8s (#383)
    Support aks of kuberflow training service
    Support nnictl set nniManagerIp

--- src/nni_manager/rest_server/restValidationSchemas.ts
+++ src/nni_manager/rest_server/restValidationSchemas.ts
@@ -70,8 +70,16 @@ export namespace ValidationSchemas {
                 nfs: joi.object({
                     server: joi.string().min(1).required(),
                     path: joi.string().min(1).required()
-                }).required(),
-                kubernetesServer: joi.string().min(1).required()
+                }),
+                kubernetesServer: joi.string().min(1),
+                keyVault: joi.object({
+                    vaultName: joi.string().regex(/^([0-9]|[a-z]|[A-Z]|-){1,127}$/),
+                    name: joi.string().regex(/^([0-9]|[a-z]|[A-Z]|-){1,127}$/)
+                }),
+                azureStorage: joi.object({
+                    accountName: joi.string().regex(/^([0-9]|[a-z]|[A-Z]|-){3,31}$/),
+                    azureShare: joi.string().regex(/^([0-9]|[a-z]|[A-Z]|-){3,63}$/)
+                })
             }),
             nni_manager_ip: joi.object({
                 nniManagerIp: joi.string().min(1) 
