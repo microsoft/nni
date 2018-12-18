064ebd76d119167d7579849bfa123a2281c1da33
Crysple <v-zejlin@microsoft.com>
2018-12-18 16:20:58 +0800

    update annotation

--- src/sdk/pynni/tests/test_smartparam.py
+++ src/sdk/pynni/tests/test_smartparam.py
@@ -29,23 +29,33 @@ import nni.trial
 from unittest import TestCase, main
 
 
-lineno1 = 51
-lineno2 = 61
+lineno1 = 61
+lineno2 = 75
 
 class SmartParamTestCase(TestCase):
     def setUp(self):
         params = {
-            'test_smartparam/choice1/choice': 'c',
+            'test_smartparam/choice1/choice': 'a',
+            'test_smartparam/choice2/choice': '3*2+1',
+            'test_smartparam/choice3/choice': '[1, 2]',
+            'test_smartparam/choice4/choice': '{"a", 2}',
             'test_smartparam/__line{:d}/uniform'.format(lineno1): '5',
             'test_smartparam/func/function_choice': 'bar',
-            'test_smartparam/__line{:d}/function_choice'.format(lineno2): 'max'
+            'test_smartparam/lambda_func/function_choice': "lambda: 2*3",
+            'test_smartparam/__line{:d}/function_choice'.format(lineno2): 'max(1, 2, 3)'
         }
         nni.trial._params = { 'parameter_id': 'test_trial', 'parameters': params }
 
 
     def test_specified_name(self):
-        val = nni.choice({'a': 'a', 'b': 'b', 'c': 'c'}, name = 'choice1')
-        self.assertEqual(val, 'c')
+        val = nni.choice({'a': 'a', '3*2+1': 3*2+1, '[1, 2]': [1, 2], '{"a", 2}': {"a", 2}}, name = 'choice1')
+        self.assertEqual(val, 'a')
+        val = nni.choice({'a': 'a', '3*2+1': 3*2+1, '[1, 2]': [1, 2], '{"a", 2}': {"a", 2}}, name = 'choice2')
+        self.assertEqual(val, 7)
+        val = nni.choice({'a': 'a', '3*2+1': 3*2+1, '[1, 2]': [1, 2], '{"a", 2}': {"a", 2}}, name = 'choice3')
+        self.assertEqual(val, [1, 2])
+        val = nni.choice({'a': 'a', '3*2+1': 3*2+1, '[1, 2]': [1, 2], '{"a", 2}': {"a", 2}}, name = 'choice4')
+        self.assertEqual(val, {"a", 2})
 
     def test_default_name(self):
         val = nni.uniform(1, 10)  # NOTE: assign this line number to lineno1
@@ -55,10 +65,14 @@ class SmartParamTestCase(TestCase):
         val = nni.function_choice({'foo': foo, 'bar': bar}, name = 'func')
         self.assertEqual(val, 'bar')
 
+    def test_lambda_func(self):
+        val = nni.function_choice({"lambda: 2*3": lambda: 2*3, "lambda: 3*4": lambda: 3*4}, name = 'lambda_func')
+        self.assertEqual(val, 6)
+
     def test_default_name_func(self):
         val = nni.function_choice({
-            'max': lambda: max(1, 2, 3),
-            'min': lambda: min(1, 2)  # NOTE: assign this line number to lineno2
+            'max(1, 2, 3)': lambda: max(1, 2, 3),
+            'min(1, 2)': lambda: min(1, 2)  # NOTE: assign this line number to lineno2
         })
         self.assertEqual(val, 3)
 
