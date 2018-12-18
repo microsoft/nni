57b2cdbd560c87b7a0df30fb32c095ea39df1ed0
Shinai Yang (FA TALENT) <t-shya@microsoft.com>
2018-12-18 16:50:09 +0800

    add cancel logic for frameworkcontroller

--- src/nni_manager/training_service/kubernetes/frameworkcontroller/frameworkcontrollerApiClient.ts
+++ src/nni_manager/training_service/kubernetes/frameworkcontroller/frameworkcontrollerApiClient.ts
@@ -30,7 +30,28 @@ abstract class FrameworkControllerClient extends KubernetesCRDClient{
         super();
     }
 
-        /**
+    public async deleteKubernetesJob(labels: Map<string, string>): Promise<boolean> {
+        let result: Promise<boolean>;
+        // construct match query from labels for deleting tfjob
+        const matchQuery: string = Array.from(labels.keys()).map(labelKey => `${labelKey}=${labels.get(labelKey)}`).join(',');
+        try {
+            const deleteResult : any = await this.operator().delete({ qs: { 
+                labelSelector: matchQuery,
+                propagationPolicy: "Foreground"
+            } });
+            if(deleteResult.statusCode && deleteResult.statusCode >= 200 && deleteResult.statusCode <= 299) {
+                result = Promise.resolve(true);
+            } else {
+                result = Promise.reject(`KubeflowOperatorClient, delete labels ${matchQuery} get wrong statusCode ${deleteResult.statusCode}`);
+            }
+        } catch(err) {
+            result = Promise.reject(err);
+        }
+
+        return result;
+    }
+
+    /**
      * Factory method to generate operator cliet
      */
     public static generateOperatorClient(): KubernetesCRDClient {
