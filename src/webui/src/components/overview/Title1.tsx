8e1e1c036bce56558df45b809e15ef03bf04aaff
Lijiao <35484733+lvybriage@users.noreply.github.com>
2018-12-18 17:23:43 +0800

    [WebUI] Show version and support download logfile (#485)
    * [WebUI]Show version and support download logfile
    
    * Update download style

--- src/webui/src/components/overview/Title1.tsx
+++ src/webui/src/components/overview/Title1.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 interface Title1Props {
     text: string;
     icon: string;
+    bgcolor?: string;
 }
 
 class Title1 extends React.Component<Title1Props, {}> {
@@ -12,10 +13,10 @@ class Title1 extends React.Component<Title1Props, {}> {
     }
 
     render() {
-        const { text, icon } = this.props;
+        const { text, icon, bgcolor } = this.props;
         return (
             <div>
-                <div className="panelTitle">
+                <div className="panelTitle" style={{backgroundColor: bgcolor}}>
                     <img src={require(`../../static/img/icon/${icon}`)} alt="icon" />
                     <span>{text}</span>
                 </div>
