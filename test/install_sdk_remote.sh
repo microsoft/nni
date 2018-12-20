c6c0f2907387a4457e7ecec01eec24a66c4a5f81
Shinai Yang (FA TALENT) <t-shya@microsoft.com>
2018-12-20 17:06:05 +0800

    add copy task

--- test/install_sdk_remote.sh
+++ test/install_sdk_remote.sh
@@ -1,9 +1,4 @@
  #!/bin/bash
- ip="nni@104.210.63.241"
- key="id_rsa"
- #chmod 600 $key
- echo "Initializing remote machine..."
- yes | ssh -i $key $ip "rm -rf pynni"
  echo "Copy nni sdk to remote machine..."
  scp -i $key -r ../src/sdk/pynni $ip:~
  echo "Install nni sdk in remote machine..."
