c82d36cb81b2d995bcb4f079eab0f7cf26604c52
Crysple <v-zejlin@microsoft.com>
2018-12-24 16:19:24 +0800

    test

--- test/scripts/nni_annotation.sh
+++ test/scripts/nni_annotation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ## ------Run annotation test------
+echo ""
 echo "===========================Testing: nni_annotation==========================="
 cd ../../tools/
 python3 -m unittest -v nni_annotation/test_annotation.py
--- test/scripts/nni_manager.sh
+++ test/scripts/nni_manager.sh
@@ -2,6 +2,7 @@
 
 # -------------For typescrip unittest-------------
 cd ../../src/nni_manager
+echo ""
 echo "===========================Testing: nni_manager==========================="
 sed -ie 's/NNI_VERSION/1.0.0/' package.json
 npm run test
