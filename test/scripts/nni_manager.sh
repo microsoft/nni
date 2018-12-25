4aa7417bf04c47c4eeee3974addcc60195b5d3df
Crysple <v-zejlin@microsoft.com>
2018-12-25 14:00:01 +0800

    refactor

--- test/scripts/nni_manager.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# -------------For typescrip unittest-------------
-cd ../../src/nni_manager
-echo ""
-echo "===========================Testing: nni_manager==========================="
-sed -ie 's/NNI_VERSION/1.0.0/' package.json
-npm run test
--- test/unittest.sh
+++ test/unittest.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-cd scripts
+CWD=${PWD}
 # -------------For python unittest-------------
 
-## ------Run sdk test------
-chmod +x nni_sdk.sh
-./nni_sdk.sh
-
 ## ------Run annotation test------
-chmod +x nni_annotation.sh
-./nni_annotation.sh
+echo ""
+echo "===========================Testing: nni_annotation==========================="
+cd ${CWD}/../tools/
+python3 -m unittest -v nni_annotation/test_annotation.py
+
+## Export certain environment variables for unittest code to work
+export NNI_TRIAL_JOB_ID=test_trial_job_id
+export NNI_PLATFORM=unittest
+
+## ------Run sdk test------
+echo ""
+echo "===========================Testing: nni_sdk==========================="
+cd ${CWD}/../src/sdk/pynni/
+python3 -m unittest discover -v tests
+
+
 
 # -------------For typescrip unittest-------------
-chmod +x nni_manager.sh
-./nni_manager.sh
+cd ${CWD}/../src/nni_manager
+echo ""
+echo "===========================Testing: nni_manager==========================="
+sed -ie 's/NNI_VERSION/1.0.0/' package.json
+npm run test
