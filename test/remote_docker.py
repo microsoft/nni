4c894b78bdb9252919942672b584d25f18609793
Shinai Yang (FA TALENT) <t-shya@microsoft.com>
2018-12-26 17:25:52 +0800

    update

--- test/remote_docker.py
+++ test/remote_docker.py
@@ -30,7 +30,7 @@ def start_container(image, name):
     commit_id = output.decode('utf-8')
     exec_cmds = ['docker', 'exec', name, 'python3', '-m', 'pip', 'install', '--user', '--no-cache-dir', '/root/src/sdk/pynni/']
     check_call(exec_cmds)
-    with open(source_dir + 'port', 'w') as file:
+    with open(source_dir + '/port', 'w') as file:
         file.write('    port: ' + str(port))
 
 def stop_container(name):
