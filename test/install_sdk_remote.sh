1fb16caf94b7cae24a438d3efdb3eea2ba315836
Shinai Yang (FA TALENT) <t-shya@microsoft.com>
2018-12-27 11:08:20 +0800

    update

--- test/install_sdk_remote.sh
+++
@@ -1,5 +0,0 @@
- #!/bin/bash
- echo "Copy nni sdk to remote machine..."
- scp -i $key -r ../src/sdk/pynni $ip:~
- echo "Install nni sdk in remote machine..."
- ssh -i $key $ip "cd pynni && python3 -m pip install --user ."
