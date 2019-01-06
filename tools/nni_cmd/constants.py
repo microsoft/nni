7ae657bd65742b23d48f2c673b8af45c90b7c213
Linjie Xu <708829300@qq.com>
2019-01-06 19:41:44 +0800

    delete non-tuner in constants/ModuleName

--- tools/nni_cmd/constants.py
+++ tools/nni_cmd/constants.py
@@ -90,8 +90,6 @@ ModuleName = {
     'Evolution': 'nni.evolution_tuner.evolution_tuner',
     'SMAC': 'nni.smac_tuner.smac_tuner',
     'BatchTuner': 'nni.batch_tuner.batch_tuner',
-    'Medianstop': 'nni.medianstop_assessor.medianstop_assessor',
     'GridSearch': 'nni.gridsearch_tuner.gridsearch_tuner',
-    'NetworkMorphism': 'nni.networkmorphism_tuner.networkmorphism_tuner',
-    'Curvefitting': 'nni.curvefitting_assessor.curvefitting_assessor'
+    'NetworkMorphism': 'nni.networkmorphism_tuner.networkmorphism_tuner'
 }
