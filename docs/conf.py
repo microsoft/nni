5bf6d91169a13f84e6e29fb3ff99ec3d24982bf4
Yan Ni <yann@microsoft.com>
2019-01-17 12:50:31 +0800

    Revert "update conf"
    This reverts commit 8de482fee042082007c575e0985e6280d14f0e2f.

--- docs/conf.py
+++ docs/conf.py
@@ -94,11 +94,6 @@ html_theme_options = {
     'logo_only': True,
 }
 
-html_context = {
-    "display_github": True,
-    "github_version": "dev-sphinx"
-}
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
