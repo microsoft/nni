77bfc5d6442d07ce3ccb672c573fc672ec768817
Shinai Yang (FA TALENT) <t-shya@microsoft.com>
2019-02-19 16:45:07 +0800

    copy tools folder to remote

--- test/remote_docker.py
+++ test/remote_docker.py
@@ -28,8 +28,10 @@ def start_container(image, name):
     run_cmds = ['docker', 'run', '-d', '-p', str(port) + ':22', '--name', name, '--mount', 'type=bind,source=' + source_dir + ',target=/tmp/nni', image]
     output = check_output(run_cmds)
     commit_id = output.decode('utf-8')
-    exec_cmds = ['docker', 'exec', name, 'python3', '-m', 'pip', 'install', '--user', '--no-cache-dir', '/tmp/nni/pynni/']
-    check_call(exec_cmds)
+    sdk_cmds = ['docker', 'exec', name, 'python3', '-m', 'pip', 'install', '--user', '--no-cache-dir', '/tmp/nni/pynni/']
+    check_call(sdk_cmds)
+    tools_cmds = ['docker', 'exec', name, 'python3', '-m', 'pip', 'install', '--user', '--no-cache-dir', '/tmp/nni/tools']
+    check_call(tools_cmds)
     with open(source_dir + '/port', 'w') as file:
         file.write(str(port))
 
