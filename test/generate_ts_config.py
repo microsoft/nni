6b144d1e207602c0389ace5274b29b6becc9037b
Chengmin Chi <chec@microsoft.com>
2019-02-22 12:20:42 +0800

    updates for kubeflow

--- test/generate_ts_config.py
+++ test/generate_ts_config.py
@@ -27,26 +27,36 @@ def update_training_service_config(args):
     config = get_yml_content(TRAINING_SERVICE_FILE)
 
     if args.ts == 'pai':
-        config[args.ts]['paiConfig']['userName'] = args.pai_user
-        config[args.ts]['paiConfig']['passWord'] = args.pai_pwd
-        config[args.ts]['paiConfig']['host'] = args.pai_host
-        config[args.ts]['trial']['image'] = args.nni_docker_image
-        config[args.ts]['trial']['dataDir'] = args.data_dir
-        config[args.ts]['trial']['outputDir'] = args.output_dir
+        if args.pai_user is not None:
+            config[args.ts]['paiConfig']['userName'] = args.pai_user
+        if args.pai_pwd is not None:
+            config[args.ts]['paiConfig']['passWord'] = args.pai_pwd
+        if args.pai_host is not None:
+            config[args.ts]['paiConfig']['host'] = args.pai_host
+        if args.nni_docker_image is not None:
+            config[args.ts]['trial']['image'] = args.nni_docker_image
+        if args.data_dir is not None:
+            config[args.ts]['trial']['dataDir'] = args.data_dir
+        if args.output_dir is not None:
+            config[args.ts]['trial']['outputDir'] = args.output_dir
     elif args.ts == 'kubeflow':
-        config[args.ts]['kubeflowConfig']['nfs']['server'] = args.nfs_server
-        config[args.ts]['kubeflowConfig']['nfs']['path'] = args.nfs_path
+        if args.nfs_server is not None:
+            config[args.ts]['kubeflowConfig']['nfs']['server'] = args.nfs_server
+        if args.nfs_path is not None:
+            config[args.ts]['kubeflowConfig']['nfs']['path'] = args.nfs_path
+        if args.nni_docker_image is not None:
+            config[args.ts]['trial']['worker']['image'] = args.nni_docker_image
 
     dump_yml_content(TRAINING_SERVICE_FILE, config)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--ts", type=str, choices=['pai', 'kubeflow'], default='pai')
+    parser.add_argument("--nni_docker_image", type=str)
     # args for PAI
     parser.add_argument("--pai_user", type=str)
     parser.add_argument("--pai_pwd", type=str)
     parser.add_argument("--pai_host", type=str)
-    parser.add_argument("--nni_docker_image", type=str)
     parser.add_argument("--data_dir", type=str)
     parser.add_argument("--output_dir", type=str)
     # args for kubeflow
