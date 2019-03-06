4054470343f428c55daf1cb7ac04f782462c01ff
Shinai Yang (FA TALENT) <t-shya@microsoft.com>
2019-03-06 11:10:38 +0800

    catch exception in nnictl.py

--- tools/nni_cmd/nnictl.py
+++ tools/nni_cmd/nnictl.py
@@ -36,7 +36,7 @@ def nni_info(*args):
     if args[0].version:
         try:
             print(pkg_resources.get_distribution('nni').version)
-        except:
+        except Exception as exception:
             print_error('Get version failed, please use `pip3 list | grep nni` to check nni version!')
     else:
         print('please run "nnictl {positional argument} --help" to see nnictl guidance')
