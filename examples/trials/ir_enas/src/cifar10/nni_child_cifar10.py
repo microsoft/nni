be1a22f445f268577265b119eef5f7611b19cee7
Crysple <871886504@qq.com>
2019-03-20 13:05:38 +0800

    test

--- examples/trials/ir_enas/src/cifar10/nni_child_cifar10.py
+++ examples/trials/ir_enas/src/cifar10/nni_child_cifar10.py
@@ -3,7 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = "2"
+os.environ['CUDA_VISIBLE_DEVICES'] = "3"
 import shutil
 import sys
 import time
