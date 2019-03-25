600dd96e995188ddda7c9a727c5a547204da70f1
Shinai Yang (FA TALENT) <t-shya@microsoft.com>
2019-03-25 11:27:21 +0800

    fix remote_docker.py

--- test/remote_docker.py
+++ test/remote_docker.py
@@ -28,7 +28,7 @@ def start_container(image, name):
     run_cmds = ['docker', 'run', '-d', '-p', str(port) + ':22', '--name', name, '--mount', 'type=bind,source=' + source_dir + ',target=/tmp/nni', image]
     output = check_output(run_cmds)
     commit_id = output.decode('utf-8')
-    sdk_cmds = ['docker', 'exec', name, 'python3', '-m', 'pip', 'install', '/tmp/nni/dist/*.whl']
+    sdk_cmds = ['docker', 'exec', name, 'python3', '-m', 'pip', 'install', '/tmp/nni/dist/nni-0.1-py3-none-manylinux1_x86_64.whl']
     check_call(sdk_cmds)
     with open(source_dir + '/port', 'w') as file:
         file.write(str(port))
