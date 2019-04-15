156be4317f072e8ff3bcaedcd404ad9a45ab49f4
Crysple <871886504@qq.com>
2019-04-15 10:32:40 +0800

    rename function

--- tools/nni_cmd/launcher.py
+++ tools/nni_cmd/launcher.py
@@ -132,7 +132,7 @@ def start_rest_server(port, platform, mode, config_file_name, experiment_id=None
     log_header = LOG_HEADER % str(time_now)
     stdout_file.write(log_header)
     stderr_file.write(log_header)
-    process = Popen(cmds, cwd=entry_dir)#, stdout=stdout_file, stderr=stderr_file)
+    process = Popen(cmds, cwd=entry_dir, stdout=stdout_file, stderr=stderr_file)
     return process, str(time_now)
 
 def set_trial_config(experiment_config, port, config_file_name):
