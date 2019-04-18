017f08d03234bc1902e14f4c097ccc65e3f76899
Shinai Yang (FA TALENT) <t-shya@microsoft.com>
2019-04-18 16:48:54 +0800

    remove unused code

--- tools/nni_cmd/launcher.py
+++ tools/nni_cmd/launcher.py
@@ -305,9 +305,6 @@ def set_experiment(experiment_config, mode, port, config_file_name):
             {'key': 'trial_config', 'value': experiment_config['trial']})
 
     response = rest_post(experiment_url(port), json.dumps(request_data), REST_TIME_OUT, show_error=True)
-    print(response)
-    print(response.status_code)
-    print(response.text)
     if check_response(response):
         return response
     else:
