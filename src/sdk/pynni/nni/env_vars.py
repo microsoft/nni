62a7982859192f4c9828739f37f2ee810c7880e6
chicm-ms <38930155+chicm-ms@users.noreply.github.com>
2019-04-19 11:28:26 +0800

    Refactor env var (#993)
    * Refactoring environment variables

--- src/sdk/pynni/nni/env_vars.py
+++ src/sdk/pynni/nni/env_vars.py
@@ -18,16 +18,31 @@
 # OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # ==================================================================================================
 
+import os
+from collections import namedtuple
 
-# pylint: disable=wildcard-import
 
-from ..env_vars import trial_env_vars
+_trial_env_var_names = [
+    'NNI_PLATFORM',
+    'NNI_TRIAL_JOB_ID',
+    'NNI_SYS_DIR',
+    'NNI_OUTPUT_DIR',
+    'NNI_TRIAL_SEQ_ID',
+    'MULTI_PHASE'
+]
 
-if trial_env_vars.NNI_PLATFORM is None:
-    from .standalone import *
-elif trial_env_vars.NNI_PLATFORM == 'unittest':
-    from .test import *
-elif trial_env_vars.NNI_PLATFORM in ('local', 'remote', 'pai', 'kubeflow', 'frameworkcontroller'):
-    from .local import *
-else:
-    raise RuntimeError('Unknown platform %s' % trial_env_vars.NNI_PLATFORM)
+_dispatcher_env_var_names = [
+    'NNI_MODE',
+    'NNI_CHECKPOINT_DIRECTORY',
+    'NNI_LOG_DIRECTORY',
+    'NNI_LOG_LEVEL',
+    'NNI_INCLUDE_INTERMEDIATE_RESULTS'
+]
+
+def _load_env_vars(env_var_names):
+    env_var_dict = {k: os.environ.get(k) for k in env_var_names}
+    return namedtuple('EnvVars', env_var_names)(**env_var_dict)
+
+trial_env_vars = _load_env_vars(_trial_env_var_names)
+
+dispatcher_env_vars = _load_env_vars(_dispatcher_env_var_names)
