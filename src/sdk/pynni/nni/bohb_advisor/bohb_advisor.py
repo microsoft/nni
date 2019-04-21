256f5f781f8063e93189d2384bccf7ae57bae4e8
ShufanHuang <purityfan@163.com>
2019-04-21 22:12:56 +0800

    fix bug in import data function

--- src/sdk/pynni/nni/bohb_advisor/bohb_advisor.py
+++ src/sdk/pynni/nni/bohb_advisor/bohb_advisor.py
@@ -589,7 +589,7 @@ class BOHB(MsgDispatcherBase):
         """
         _completed_num = 0
         for trial_info in data:
-            logger.info("Importing data, current processing progress %s / %s" %(_completed_num), len(data))
+            logger.info("Importing data, current processing progress %s / %s" %(_completed_num, len(data)))
             _completed_num += 1
             assert "parameter" in trial_info
             _params = trial_info["parameter"]
