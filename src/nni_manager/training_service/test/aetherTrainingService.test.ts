fe133327dfbfb77d2faa434551af117a5f05ec12
Yan Ni <yann@microsoft.com>
2019-04-29 13:55:23 +0800

    setup ut for aether TS

---
+++ src/nni_manager/training_service/test/aetherTrainingService.test.ts
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) Microsoft Corporation
+ * All rights reserved.
+
+ * MIT License
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+`use strict`;
+
+import * as chaiAsPromised from 'chai-as-promised';
+import { prepareUnitTest, cleanupUnitTest } from 'common/utils';
+import * as component from '../../common/component';
+import { AetherTrainingService } from 'training_service/aether/aetherTrainingService';
+
+describe(`Unit Test for Aether Training Service`, () => {
+
+    let aetherTrainingService: AetherTrainingService;    
+    before(() => {
+        chai.should();
+        chai.use(chaiAsPromised);
+        prepareUnitTest();
+    })
+
+    after(() => {
+        cleanupUnitTest();
+    })
+
+    beforeEach(() => {
+        aetherTrainingService = component.get(AetherTrainingService);
+        aetherTrainingService.run();
+    })
+
+    afterEach(() => {
+        aetherTrainingService.cleanUp();
+    })
+
+    it('Submit job and collect reward', () => {}).timeout(20000);
+
+    it('Submit job and cancel', () => {}).timeout(20000);
+})
