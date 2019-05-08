776f37d458c4fa531a384204714cb4fdb5ef1224
demianzhang <jgzhang1994@163.com>
2019-05-08 15:38:22 +0800

    Upgrade docker pip

--- test/remote_docker.py
+++ test/remote_docker.py
@@ -53,6 +53,8 @@ def start_container_windows(image, name):
     run_cmds = ['docker', 'run', '-d', '-p', str(port) + ':22', '--name', name, '--mount', 'type=bind,source=' + source_dir + ',target=/tmp/nni', image]
     output = check_output(run_cmds)
     commit_id = output.decode('utf-8')
+    pip_cmds = ['docker', 'exec', name, 'python3', '-m', 'pip', 'install', '--upgrade', 'pip']
+    check_call(pip_cmds)
     sdk_cmds = ['docker', 'exec', name, 'bash', '-c', 'cd /tmp/nni/nni-remote/ && yes | source install.sh']
     check_call(sdk_cmds)
     with open(source_dir + '/port', 'w') as file:
