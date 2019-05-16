4b8d43b0fe76a7dc6fc9425636333fd44f145e39
demianzhang <jgzhang1994@163.com>
2019-05-16 13:30:11 +0800

    Add aether to pypi package

--- deployment/pypi/clean.ps1
+++ deployment/pypi/clean.ps1
@@ -5,3 +5,6 @@ Remove-Item $CWD\dist -Recurse -Force
 Remove-Item $CWD\nni -Recurse -Force
 Remove-Item $CWD\nni.egg-info -Recurse -Force
 Remove-Item $CWD\node-$OS_SPEC -Recurse -Force
+Remove-Item $CWD\nni-yarn -Recurse -Force
+Remove-Item $CWD\nni-nuget -Recurse -Force
+Remove-Item $CWD\extract.py
