bd6eb6bd576fb99fdf3071262ef2393d005be70f
demianzhang <jgzhang1994@163.com>
2019-06-13 14:12:03 +0800

    update

--- src/nni_manager/training_service/kubernetes/frameworkcontroller/frameworkcontrollerApiClient.ts
+++ src/nni_manager/training_service/kubernetes/frameworkcontroller/frameworkcontrollerApiClient.ts
@@ -22,21 +22,22 @@
 import * as fs from 'fs';
 import { GeneralK8sClient, KubernetesCRDClient } from '../kubernetesApiClient';
 
-// tslint:disable:completed-docs
+// tslint:disable:completed-docs function-name no-use-before-declare
 abstract class FrameworkControllerClient extends KubernetesCRDClient {
     /**
      * Factory method to generate operator client
      */
-    // tslint:disable-next-line:function-name
     public static generateFrameworkControllerClient(): KubernetesCRDClient {
         return new FrameworkControllerClientV1();
     }
 }
+// tslint:enable: function-name no-use-before-declare
 
 class FrameworkControllerClientV1 extends FrameworkControllerClient {
     /**
      * constructor, to initialize frameworkcontroller CRD definition
      */
+    // tslint:disable: no-unsafe-any no-any
     public constructor() {
         super();
         this.crdSchema = JSON.parse(fs.readFileSync('./config/frameworkcontroller/frameworkcontrollerjob-crd-v1.json', 'utf8'));
@@ -46,6 +47,7 @@ class FrameworkControllerClientV1 extends FrameworkControllerClient {
     protected get operator(): any {
         return this.client.apis['frameworkcontroller.microsoft.com'].v1.namespaces('default').frameworks;
     }
+    // tslint:enable: no-unsafe-any no-any
 
     public get containerName(): string {
         return 'framework';
