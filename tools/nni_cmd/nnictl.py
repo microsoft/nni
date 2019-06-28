3379411e095073599e44af886286462f65c62432
SparkSnail <shinaiyang@pku.edu.cn>
2019-06-28 09:28:25 +0700

    Fix nnictl commands (#1216)

--- tools/nni_cmd/nnictl.py
+++ tools/nni_cmd/nnictl.py
@@ -119,7 +119,7 @@ def parse_args():
     parser_experiment_status.add_argument('id', nargs='?', help='the id of experiment')
     parser_experiment_status.set_defaults(func=experiment_status)
     parser_experiment_list = parser_experiment_subparsers.add_parser('list', help='list all of running experiment ids')
-    parser_experiment_list.add_argument('all', nargs='?', help='list all of experiments')
+    parser_experiment_list.add_argument('--all', action='store_true', default=False, help='list all of experiments')
     parser_experiment_list.set_defaults(func=experiment_list)
     parser_experiment_clean = parser_experiment_subparsers.add_parser('delete', help='clean up the experiment data')
     parser_experiment_clean.add_argument('id', nargs='?', help='the id of experiment')
