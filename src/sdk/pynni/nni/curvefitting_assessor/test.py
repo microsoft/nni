1ac0fdfb55e09908bf64aa29036f830fd5cf1834
Chengmin Chi <chec@microsoft.com>
2019-07-03 16:48:40 +0800

    Fix flake8 scaned python errors

--- src/sdk/pynni/nni/curvefitting_assessor/test.py
+++ src/sdk/pynni/nni/curvefitting_assessor/test.py
@@ -15,9 +15,11 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+import numpy as np
 import unittest
 
 from .curvefitting_assessor import CurvefittingAssessor
+from .model_factory import CurveModel
 from nni.assessor import AssessResult
 
 class TestCurveFittingAssessor(unittest.TestCase):
