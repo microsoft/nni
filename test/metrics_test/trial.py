afe26b72844af4641603d0a48ea530b01530bb4e
Chengmin Chi <chec@microsoft.com>
2019-07-10 21:59:19 +0800

    updates

--- test/metrics_test/trial.py
+++ test/metrics_test/trial.py
@@ -2,9 +2,10 @@ import time
 import nni
 
 if __name__ == '__main__':
+    nni.get_next_parameter()
     for i in range(10):
         nni.report_intermediate_result(0.1*(i+1))
         time.sleep(2)
-    if nni.get_sequence_id() % 2 == 0:
+    if int(nni.get_sequence_id()) % 2 == 0:
         print('test metrics not at line start.', end='')
     nni.report_final_result(1.0)
