cb7a1b80e6ac60b81377fd9c19784fec615c2f49
v-liguo <v-liguo@microsoft.com>
2019-07-24 16:28:39 +0800

    delete the changes of default metric

--- src/webui/src/components/public-child/IntermediateVal.tsx
+++ src/webui/src/components/public-child/IntermediateVal.tsx
@@ -32,8 +32,7 @@ class IntermediateVal extends React.Component<IntermediateValProps, {}> {
             if (status === 'SUCCEEDED') {
                 result = `${result} (FINAL)`;
             } else {
-                // result = `${result} (LATEST)`;
-                result = `${result} (#${interArr.length})`;
+                result = `${result} (LATEST)`;
             }
         } else {
             result = '--';
--- src/webui/src/components/trial-detail/TableList.tsx
+++ src/webui/src/components/trial-detail/TableList.tsx
@@ -391,7 +391,7 @@ class TableList extends React.Component<TableListProps, TableListState> {
                     break;
                 case 'Default':
                     showColumn.push({
-                        title: 'Default metric (latest seq)',
+                        title: 'Default metric',
                         className: 'leftTitle',
                         dataIndex: 'acc',
                         key: 'acc',
