4b26e05f51689d6ca545de9a35028398345ac35d
Xiaohui Xue <xuehui@microsoft.com>
2019-10-29 14:06:51 +0800

    fix pylint in metis_tuner

--- src/sdk/pynni/nni/metis_tuner/Regression_GP/OutlierDetection.py
+++ src/sdk/pynni/nni/metis_tuner/Regression_GP/OutlierDetection.py
@@ -16,15 +16,18 @@
 # BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+"""
+OutlierDectection.py
+"""
 
-
-import argparse, json, os, sys
+import os
+import sys
 from multiprocessing.dummy import Pool as ThreadPool
 
 import nni.metis_tuner.Regression_GP.CreateModel as gp_create_model
 import nni.metis_tuner.Regression_GP.Prediction as gp_prediction
-import nni.metis_tuner.lib_data as lib_data
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
@@ -34,15 +37,17 @@ def _outlierDetection_threaded(inputs):
     Detect the outlier
     '''
     [samples_idx, samples_x, samples_y_aggregation] = inputs
-    sys.stderr.write("[%s] DEBUG: Evaluating %dth of %d samples\n"\
-                        % (os.path.basename(__file__), samples_idx + 1, len(samples_x)))
+    sys.stderr.write("[%s] DEBUG: Evaluating %dth of %d samples\n"
+                     % (os.path.basename(__file__), samples_idx + 1, len(samples_x)))
     outlier = None
 
-    # Create a diagnostic regression model which removes the sample that we want to evaluate
-    diagnostic_regressor_gp = gp_create_model.create_model(\
-                                    samples_x[0:samples_idx] + samples_x[samples_idx + 1:],\
-                                    samples_y_aggregation[0:samples_idx] + samples_y_aggregation[samples_idx + 1:])
-    mu, sigma = gp_prediction.predict(samples_x[samples_idx], diagnostic_regressor_gp['model'])
+    # Create a diagnostic regression model which removes the sample that we
+    # want to evaluate
+    diagnostic_regressor_gp = gp_create_model.create_model(
+        samples_x[0:samples_idx] + samples_x[samples_idx + 1:],
+        samples_y_aggregation[0:samples_idx] + samples_y_aggregation[samples_idx + 1:])
+    mu, sigma = gp_prediction.predict(
+        samples_x[samples_idx], diagnostic_regressor_gp['model'])
 
     # 2.33 is the z-score for 98% confidence level
     if abs(samples_y_aggregation[samples_idx] - mu) > (2.33 * sigma):
@@ -52,16 +57,18 @@ def _outlierDetection_threaded(inputs):
                    "difference": abs(samples_y_aggregation[samples_idx] - mu) - (2.33 * sigma)}
     return outlier
 
+
 def outlierDetection_threaded(samples_x, samples_y_aggregation):
     '''
     Use Multi-thread to detect the outlier
     '''
     outliers = []
 
-    threads_inputs = [[samples_idx, samples_x, samples_y_aggregation]\
-                            for samples_idx in range(0, len(samples_x))]
+    threads_inputs = [[samples_idx, samples_x, samples_y_aggregation]
+                      for samples_idx in range(0, len(samples_x))]
     threads_pool = ThreadPool(min(4, len(threads_inputs)))
-    threads_results = threads_pool.map(_outlierDetection_threaded, threads_inputs)
+    threads_results = threads_pool.map(
+        _outlierDetection_threaded, threads_inputs)
     threads_pool.close()
     threads_pool.join()
 
@@ -69,21 +76,21 @@ def outlierDetection_threaded(samples_x, samples_y_aggregation):
         if threads_result is not None:
             outliers.append(threads_result)
         else:
-            print("error here.")
+            sys.stderr.write("Error: threads_result is None.")
 
     outliers = None if len(outliers) == 0 else outliers
     return outliers
 
+
 def outlierDetection(samples_x, samples_y_aggregation):
-    '''
-    '''
     outliers = []
     for samples_idx in range(0, len(samples_x)):
-        #sys.stderr.write("[%s] DEBUG: Evaluating %d of %d samples\n"
-        #  \ % (os.path.basename(__file__), samples_idx + 1, len(samples_x)))
-        diagnostic_regressor_gp = gp_create_model.create_model(\
-                                        samples_x[0:samples_idx] + samples_x[samples_idx + 1:],\
-                                        samples_y_aggregation[0:samples_idx] + samples_y_aggregation[samples_idx + 1:])
+        sys.stderr.write("[%s] DEBUG: Evaluating %d of %d samples\n" \
+            % (os.path.basename(__file__), samples_idx + 1, len(samples_x)))
+
+        diagnostic_regressor_gp = gp_create_model.create_model(
+            samples_x[0:samples_idx] + samples_x[samples_idx + 1:],
+            samples_y_aggregation[0:samples_idx] + samples_y_aggregation[samples_idx + 1:])
         mu, sigma = gp_prediction.predict(samples_x[samples_idx],
                                           diagnostic_regressor_gp['model'])
         # 2.33 is the z-score for 98% confidence level
@@ -91,7 +98,8 @@ def outlierDetection(samples_x, samples_y_aggregation):
             outliers.append({"samples_idx": samples_idx,
                              "expected_mu": mu,
                              "expected_sigma": sigma,
-                             "difference": abs(samples_y_aggregation[samples_idx] - mu) - (2.33 * sigma)})
+                             "difference": \
+                                abs(samples_y_aggregation[samples_idx] - mu) - (2.33 * sigma)})
 
     outliers = None if len(outliers) == 0 else outliers
     return outliers
