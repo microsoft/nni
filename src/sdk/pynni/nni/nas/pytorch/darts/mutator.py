818f8ab3e162d7d0ac366849c65235161b86d39e
Chi Song <27178119+squirrelsc@users.noreply.github.com>
2019-11-11 12:52:57 +0800

    roll back changes

--- src/sdk/pynni/nni/nas/pytorch/darts/mutator.py
+++ src/sdk/pynni/nni/nas/pytorch/darts/mutator.py
@@ -10,12 +10,9 @@ class DartsMutator(PyTorchMutator):
 
     def before_build(self, model):
         self.choices = nn.ParameterDict()
-        self.register_on_init_hook(LayerChoice, self.on_init_layer_choice)
-        self.register_on_forward_hook(LayerChoice, self.on_forward_layer_choice)
 
     def on_init_layer_choice(self, mutable: LayerChoice):
-        self.choices[mutable.key] = nn.Parameter(
-            1.0E-3 * torch.randn(mutable.length))
+        self.choices[mutable.key] = nn.Parameter(1.0E-3 * torch.randn(mutable.length))
 
     def on_calc_layer_choice_mask(self, mutable: LayerChoice):
         return F.softmax(self.choices[mutable.key], dim=-1)
