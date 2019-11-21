d1c6ac04ff77766e7ac513201b97aeb585e04718
Xiaohui Xue <xuehui@microsoft.com>
2019-11-21 16:08:07 +0800

    update by comments

--- src/sdk/pynni/nni/feature_engineering/gradient_selector/fgtrain.py
+++ src/sdk/pynni/nni/feature_engineering/gradient_selector/fgtrain.py
@@ -18,9 +18,6 @@
 # OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # ==================================================================================================
 
-"""
-fgtrain.py
-"""
 
 import time
 
