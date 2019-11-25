31b2b8867e2f6f230684081e56639ea16ffd9715
liuzhe-lz <40699903+liuzhe-lz@users.noreply.github.com>
2019-11-25 14:00:55 +0800

    Fix bug introduced in customized trial (#1779)

--- test/config_test/multi_thread/multi_thread_tuner.py
+++ test/config_test/multi_thread/multi_thread_tuner.py
@@ -1,3 +1,4 @@
+import logging
 import time
 from nni.tuner import Tuner
 
@@ -7,14 +8,18 @@ class MultiThreadTuner(Tuner):
         self.parent_done = False
 
     def generate_parameters(self, parameter_id, **kwargs):
+        logging.debug('generate_parameters: %s %s', parameter_id, kwargs)
         if parameter_id == 0:
             return {'x': 0}
         else:
             while not self.parent_done:
+                logging.debug('parameter_id %s sleeping', parameter_id)
                 time.sleep(2)
+            logging.debug('parameter_id %s waked up', parameter_id)
             return {'x': 1}
 
     def receive_trial_result(self, parameter_id, parameters, value, **kwargs):
+        logging.debug('receive_trial_result: %s %s %s %s', parameter_id, parameters, value, kwargs)
         if parameter_id == 0:
             self.parent_done = True
 
