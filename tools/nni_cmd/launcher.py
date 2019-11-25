29ea868e6e5d7d44ff467401202f7662fbe1eb6d
quanlu <zhangql08hit@gmail.com>
2019-11-25 19:42:07 +0800

    update

--- tools/nni_cmd/launcher.py
+++ tools/nni_cmd/launcher.py
@@ -495,7 +495,6 @@ def create_experiment(args):
         print_error('Please set correct config path!')
         exit(1)
     experiment_config = get_yml_content(config_path)
-    
     validate_all_content(experiment_config, config_path)
 
     nni_config.set_config('experimentConfig', experiment_config)
