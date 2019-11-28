45db02788a41891c3967f71fd0c06284e05b7c03
zhangyuge <v-yugzh@microsoft.com>
2019-11-28 11:57:42 +0800

    detect trial status

--- test/naive_test/naive_trial.py
+++ test/naive_test/naive_trial.py
@@ -10,7 +10,9 @@ print('params:', params)
 x = params['x']
 
 for i in range(1, 10):
+    print(x, i)
     nni.report_intermediate_result(x ** i)
     time.sleep(0.5)
 
+print('final')
 nni.report_final_result(x ** 10)
