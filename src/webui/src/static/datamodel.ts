10e682ed067338260a33c6a9f6456095dc7df1b1
v-liguo <v-liguo@microsoft.com>
2019-12-02 18:35:50 +0800

    delete all old code

--- src/webui/src/static/datamodel.ts
+++
@@ -1,7 +0,0 @@
-import { Experiment } from './model/experiment';
-import { TrialManager } from './model/trialmanager';
-
-const EXPERIMENT = new Experiment();
-const TRIALS = new TrialManager();
-
-export { EXPERIMENT, TRIALS };
