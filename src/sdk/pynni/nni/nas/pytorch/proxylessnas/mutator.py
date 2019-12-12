d69c30c31afc822abd5aedbfbd12aece76e2ebeb
quanlu <zhangql08hit@gmail.com>
2019-12-12 18:53:03 +0800

    update

--- src/sdk/pynni/nni/nas/pytorch/proxylessnas/mutator.py
+++ src/sdk/pynni/nni/nas/pytorch/proxylessnas/mutator.py
@@ -25,168 +25,6 @@ import numpy as np
 
 from nni.nas.pytorch.base_mutator import BaseMutator
 
-def detach_variable(inputs):
-    if isinstance(inputs, tuple):
-        return tuple([detach_variable(x) for x in inputs])
-    else:
-        x = inputs.detach()
-        x.requires_grad = inputs.requires_grad
-        return x
-
-class ArchGradientFunction(torch.autograd.Function):
-
-    @staticmethod
-    def forward(ctx, x, binary_gates, run_func, backward_func):
-        ctx.run_func = run_func
-        ctx.backward_func = backward_func
-
-        detached_x = detach_variable(x)
-        with torch.enable_grad():
-            output = run_func(detached_x)
-        ctx.save_for_backward(detached_x, output)
-        return output.data
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        detached_x, output = ctx.saved_tensors
-
-        grad_x = torch.autograd.grad(output, detached_x, grad_output, only_inputs=True)
-        # compute gradients w.r.t. binary_gates
-        binary_grads = ctx.backward_func(detached_x.data, output.data, grad_output.data)
-
-        return grad_x[0], binary_grads, None, None
-
-class MixedOp(nn.Module):
-    """
-    This class is to instantiate and manage info of one LayerChoice
-    """
-    def __init__(self, mutable):
-        """
-        Parameters
-        ----------
-        mutable : LayerChoice
-            A LayerChoice in user model
-        """
-        super(MixedOp, self).__init__()
-        #self.mutable = mutable
-        self.AP_path_alpha = nn.Parameter(torch.Tensor(mutable.length))
-        self.AP_path_wb = nn.Parameter(torch.Tensor(mutable.length))
-        self.active_index = [0]
-        self.inactive_index = None
-        self.log_prob = None
-        self.current_prob_over_ops = None
-        self.n_choices = mutable.length
-
-    def get_AP_path_alpha(self):
-        return self.AP_path_alpha
-
-    def forward(self, mutable, x):
-        # only full_v2
-        def run_function(key, candidate_ops, active_id):
-            def forward(_x):
-                return candidate_ops[active_id](_x)
-            return forward
-
-        def backward_function(key, candidate_ops, active_id, binary_gates):
-            def backward(_x, _output, grad_output):
-                binary_grads = torch.zeros_like(binary_gates.data)
-                with torch.no_grad():
-                    for k in range(len(candidate_ops)):
-                        if k != active_id:
-                            out_k = candidate_ops[k](_x.data)
-                        else:
-                            out_k = _output.data
-                        grad_k = torch.sum(out_k * grad_output)
-                        binary_grads[k] = grad_k
-                return binary_grads
-            return backward
-        output = ArchGradientFunction.apply(
-            x, self.AP_path_wb, run_function(mutable.key, mutable.choices, self.active_index[0]),
-            backward_function(mutable.key, mutable.choices, self.active_index[0], self.AP_path_wb))
-        return output
-
-    '''def forward(self, mutable, x):
-        out = mutable.choices[0](x)
-        for choice in mutable.choices[1:]:
-            out += choice(x)
-        return out'''
-
-    @property
-    def probs_over_ops(self):
-        """
-        Apply softmax on alpha to generate probability distribution
-
-        Returns
-        -------
-        pytorch tensor
-            probability distribution
-        """
-        probs = F.softmax(self.AP_path_alpha, dim=0)  # softmax to probability
-        return probs
-
-    @property
-    def chosen_index(self):
-        """ choose the max one """
-        probs = self.probs_over_ops.data.cpu().numpy()
-        index = int(np.argmax(probs))
-        return index, probs[index]
-
-    def active_op(self, mutable):
-        """ assume only one path is active """
-        return mutable.choices[self.active_index[0]]
-
-    @property
-    def active_op_index(self):
-        """ return active op's index """
-        return self.active_index[0]
-
-    def set_chosen_op_active(self):
-        """ set chosen index, active and inactive indexes """
-        chosen_idx, _ = self.chosen_index
-        self.active_index = [chosen_idx]
-        self.inactive_index = [_i for _i in range(0, chosen_idx)] + \
-                              [_i for _i in range(chosen_idx + 1, self.n_choices)]
-
-    def binarize(self, mutable):
-        """
-        Sample based on alpha, and set binary weights accordingly
-        """
-        self.log_prob = None
-        # reset binary gates
-        self.AP_path_wb.data.zero_()
-        probs = self.probs_over_ops
-        sample = torch.multinomial(probs, 1)[0].item()
-        self.active_index = [sample]
-        self.inactive_index = [_i for _i in range(0, sample)] + \
-                              [_i for _i in range(sample + 1, len(mutable.choices))]
-        self.log_prob = torch.log(probs[sample])
-        self.current_prob_over_ops = probs
-        self.AP_path_wb.data[sample] = 1.0
-        # avoid over-regularization
-        for choice in mutable.choices:
-            for _, param in choice.named_parameters():
-                param.grad = None
-
-    def _delta_ij(self, i, j):
-        if i == j:
-            return 1
-        else:
-            return 0
-
-    def set_arch_param_grad(self, mutable):
-        """
-        Calculate alpha gradient for this LayerChoice
-        """
-        binary_grads = self.AP_path_wb.grad.data
-        if self.active_op(mutable).is_zero_layer():
-            self.AP_path_alpha.grad = None
-            return
-        if self.AP_path_alpha.grad is None:
-            self.AP_path_alpha.grad = torch.zeros_like(self.AP_path_alpha.data)
-        probs = self.probs_over_ops.data
-        for i in range(len(mutable.choices)):
-            for j in range(len(mutable.choices)):
-                self.AP_path_alpha.grad.data[i] += binary_grads[j] * probs[j] * (self._delta_ij(i, j) - probs[i])
 
 
 class ProxylessNasMutator(BaseMutator):
@@ -203,8 +41,8 @@ class ProxylessNasMutator(BaseMutator):
         #self.mixed_ops = {}
         self.cnt = 0
         for _, mutable, _ in self.named_mutables(distinct=False):
-            mo = MixedOp(mutable)
-            mutable.register_module(mo)
+            #mo = MixedOp(mutable)
+            #mutable.register_module(mo)
             #self.mixed_ops[mutable.key] = mo
             self.cnt += 1
 
@@ -265,7 +103,7 @@ class ProxylessNasMutator(BaseMutator):
         for _, mutable, _ in self.named_mutables(distinct=False):
             #k = mutable.key
             #self.mixed_ops[k].set_arch_param_grad(mutable)
-            mutable.registered_module.set_arch_param_grad(mutable)
+            mutable.registered_module.set_arch_param_grad(len(mutable))
 
     def get_architecture_parameters(self):
         """
