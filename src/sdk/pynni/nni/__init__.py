1177f263dd2728da85d87fba581b240d9e7aad2a
Chengmin Chi <chec@microsoft.com>
2019-12-23 14:06:15 +0800

    updates

--- src/sdk/pynni/nni/__init__.py
+++ src/sdk/pynni/nni/__init__.py
@@ -3,15 +3,15 @@
 
 from .env_vars import dispatcher_env_vars
 
-if dispatcher_env_vars.SDK_PROCESS == 'dispatcher':
-    class NoMoreTrialError(Exception):
-        def __init__(self, ErrorInfo):
-            super().__init__(self)
-            self.errorinfo = ErrorInfo
-
-        def __str__(self):
-            return self.errorinfo
-else:
+if dispatcher_env_vars.SDK_PROCESS != 'dispatcher':
     from .trial import *
     from .smartparam import *
     from .nas_utils import training_update
+
+class NoMoreTrialError(Exception):
+    def __init__(self, ErrorInfo):
+        super().__init__(self)
+        self.errorinfo = ErrorInfo
+
+    def __str__(self):
+        return self.errorinfo
