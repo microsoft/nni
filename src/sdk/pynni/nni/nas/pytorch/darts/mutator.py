21fc5450773ba87c5ddee9b2f5a9080c23df0f3a
zhangyuge <v-yugzh@microsoft.com>
2020-01-13 10:29:37 +0800

    refine tests

--- src/sdk/pynni/nni/nas/pytorch/darts/mutator.py
+++ src/sdk/pynni/nni/nas/pytorch/darts/mutator.py
@@ -63,18 +63,21 @@ class DartsMutator(Mutator):
                 edges_max[mutable.key] = max_val
                 result[mutable.key] = F.one_hot(index, num_classes=mutable.length).view(-1).bool()
         for mutable in self.mutables:
-            if isinstance(mutable, InputChoice) and mutable.n_chosen is not None:
-                weights = []
-                for src_key in mutable.choose_from:
-                    if src_key not in edges_max:
-                        _logger.warning("InputChoice.NO_KEY in '%s' is weighted 0 when selecting inputs.", mutable.key)
-                    weights.append(edges_max.get(src_key, 0.))
-                weights = torch.tensor(weights)  # pylint: disable=not-callable
-                _, topk_edge_indices = torch.topk(weights, mutable.n_chosen)
-                selected_multihot = []
-                for i, src_key in enumerate(mutable.choose_from):
-                    if i not in topk_edge_indices and src_key in result:
-                        result[src_key] = torch.zeros_like(result[src_key])  # clear this choice to optimize calc graph
-                    selected_multihot.append(i in topk_edge_indices)
-                result[mutable.key] = torch.tensor(selected_multihot, dtype=torch.bool, device=self.device())  # pylint: disable=not-callable
+            if isinstance(mutable, InputChoice):
+                if mutable.n_chosen is not None:
+                    weights = []
+                    for src_key in mutable.choose_from:
+                        if src_key not in edges_max:
+                            _logger.warning("InputChoice.NO_KEY in '%s' is weighted 0 when selecting inputs.", mutable.key)
+                        weights.append(edges_max.get(src_key, 0.))
+                    weights = torch.tensor(weights)  # pylint: disable=not-callable
+                    _, topk_edge_indices = torch.topk(weights, mutable.n_chosen)
+                    selected_multihot = []
+                    for i, src_key in enumerate(mutable.choose_from):
+                        if i not in topk_edge_indices and src_key in result:
+                            result[src_key] = torch.zeros_like(result[src_key])  # clear this choice to optimize calc graph
+                        selected_multihot.append(i in topk_edge_indices)
+                    result[mutable.key] = torch.tensor(selected_multihot, dtype=torch.bool, device=self.device())  # pylint: disable=not-callable
+                else:
+                    result[mutable.key] = torch.ones(mutable.n_candidates, dtype=torch.bool, device=self.device())  # pylint: disable=not-callable
         return result
--- src/sdk/pynni/tests/test_nas.py
+++ src/sdk/pynni/tests/test_nas.py
@@ -4,45 +4,64 @@
 from unittest import TestCase, main
 
 import torch
-from nni.nas.pytorch.enas import EnasMutator
 from nni.nas.pytorch.darts import DartsMutator
+from nni.nas.pytorch.enas import EnasMutator
+from nni.nas.pytorch.fixed import apply_fixed_architecture
 from nni.nas.pytorch.random import RandomMutator
+from nni.nas.pytorch.utils import reset_global_mutable_counting
 
 from .models.pytorch import NaiveSearchSpace, NestedSpace, SpaceWithMutableScope
 
 
 class NasTestCase(TestCase):
-    def setUp(self):
-        self.input_size = [3, 32, 32]
 
-    def iterative_sample_and_forward(self, model, mutator, input_size, n_iters=20, test_backward=True,
-                                     cuda_only=False, parallel_mode=None):
+    def setUp(self):
+        self.default_input_size = [3, 32, 32]
+        self.default_cls = [NaiveSearchSpace, SpaceWithMutableScope]
+
+    def iterative_sample_and_forward(self, model, mutator=None, input_size=None, n_iters=20, test_backward=True):
+        if input_size is None:
+            input_size = self.default_input_size
         # support pytorch only
         input_size = [2] + input_size  # 2 samples to enable batch norm
         for _ in range(n_iters):
-            mutator.reset()
+            for param in model.parameters():
+                param.grad = None
+            if mutator is not None:
+                mutator.reset()
             x = torch.randn(input_size)
             y = torch.sum(model(x))
             if test_backward:
                 y.backward()
 
-    def test_random_mutator(self):
-        for model_cls in [NaiveSearchSpace, SpaceWithMutableScope]:
+    def default_mutator_test_pipeline(self, mutator_cls):
+        for model_cls in self.default_cls:
+            reset_global_mutable_counting()
             model = model_cls(self)
-            mutator = RandomMutator(model)
-            self.iterative_sample_and_forward(model, mutator, self.input_size)
+            mutator = mutator_cls(model)
+            self.iterative_sample_and_forward(model, mutator)
+            reset_global_mutable_counting()
+            model_fixed = model_cls(self)
+            with torch.no_grad():
+                arc = mutator.export()
+                arc = {k: v.cpu().numpy().tolist() for k, v in arc.items()}
+            apply_fixed_architecture(model_fixed, arc)
+            self.iterative_sample_and_forward(model, n_iters=1)
+
+    def test_random_mutator(self):
+        self.default_mutator_test_pipeline(RandomMutator)
 
     def test_enas_mutator(self):
-        for model_cls in [NaiveSearchSpace, SpaceWithMutableScope]:
-            model = model_cls(self)
-            mutator = EnasMutator(model)
-            self.iterative_sample_and_forward(model, mutator, self.input_size)
+        self.default_mutator_test_pipeline(EnasMutator)
 
     def test_darts_mutator(self):
-        for model_cls in [NaiveSearchSpace, SpaceWithMutableScope]:
-            model = model_cls(self)
-            mutator = DartsMutator(model)
-            self.iterative_sample_and_forward(model, mutator, self.input_size)
+        self.default_mutator_test_pipeline(DartsMutator)
+
+    def test_apply_twice(self):
+        model = NaiveSearchSpace(self)
+        with self.assertRaises(RuntimeError):
+            for _ in range(2):
+                RandomMutator(model)
 
     def test_nested_space(self):
         model = NestedSpace(self)
