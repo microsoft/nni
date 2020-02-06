d11a54aa5a26379b895b08e2ca819c2a0f051506
QuanluZhang <quzha@v100test.j54c4fvz4ikexg1bdsarwjfncg.ix.internal.cloudapp.net>
2020-02-06 06:58:12 +0000

    add comments

--- src/sdk/pynni/nni/compression/speedup/torch/compress_modules.py
+++ src/sdk/pynni/nni/compression/speedup/torch/compress_modules.py
@@ -14,11 +14,23 @@ replace_module = {
 
 def no_replace(module, mask):
     """
+    No need to replace
     """
     return module
 
 def replace_linear(linear, mask):
     """
+    Parameters
+    ----------
+    linear : torch.nn.Linear
+        The linear module to be replace
+    mask : ModuleMasks
+        The masks of this module
+
+    Returns
+    -------
+    torch.nn.Linear
+        The new linear module
     """
     assert isinstance(mask, ModuleMasks)
     assert mask.input_mask is not None
@@ -32,22 +44,24 @@ def replace_linear(linear, mask):
                                  out_features=linear.out_features,
                                  bias=linear.bias is not None)
     new_linear.to(linear.weight.device)
-    #print(linear.weight.data.size())
-    #print(new_linear.weight.data.size())
-    #print(linear.weight.t().size())
-    #print(new_linear.weight.t().size())
     new_linear.weight.data = torch.index_select(linear.weight.data, -1, index.to(linear.weight.device))
-    #print(new_linear.weight.data.size())
     if linear.bias is not None:
-        #print(linear.bias.data.size())
-        #new_linear.bias.data = torch.index_select(linear.bias.data, 0, index.to('cuda:0'))
         new_linear.bias.data.copy_(linear.bias.data)
-        #print(new_linear.bias.data.size())
-    #print("last print: ", new_linear.weight.t().size())
     return new_linear
 
 def replace_batchnorm2d(norm, mask):
     """
+    Parameters
+    ----------
+    norm : torch.nn.BatchNorm2d
+        The batchnorm module to be replace
+    mask : ModuleMasks
+        The masks of this module
+
+    Returns
+    -------
+    torch.nn.BatchNorm2d
+        The new batchnorm module
     """
     assert isinstance(mask, ModuleMasks)
     assert 'weight' in mask.param_masks and 'bias' in mask.param_masks
@@ -62,8 +76,6 @@ def replace_batchnorm2d(norm, mask):
     # assign weights
     new_norm.weight.data = torch.index_select(norm.weight.data, 0, index)
     new_norm.bias.data = torch.index_select(norm.bias.data, 0, index)
-    #print('new_norm weight data: ', new_norm.weight.data)
-    #print('new_norm bias data: ', new_norm.bias.data)
     if norm.track_running_stats:
         new_norm.running_mean.data = torch.index_select(norm.running_mean.data, 0, index)
         new_norm.running_var.data = torch.index_select(norm.running_var.data, 0, index)
@@ -71,28 +83,29 @@ def replace_batchnorm2d(norm, mask):
 
 def replace_conv2d(conv, mask):
     """
+    Parameters
+    ----------
+    conv : torch.nn.Conv2d
+        The conv2d module to be replaced
+    mask : ModuleMasks
+        The masks of this module
+
+    Returns
+    -------
+    torch.nn.Conv2d
+        The new conv2d module
     """
-    # fine-grained tensor sparse
-    #...
-    # coarse-grained shape sparse
-    #...
     assert isinstance(mask, ModuleMasks)
     if mask.input_mask is None:
         in_channels = conv.in_channels
-        print('in_channels: ', in_channels)
     else:
         in_channels_index = mask.input_mask.mask_index[1]
-        print('in_channels_index: ', in_channels_index)
         in_channels = in_channels_index.size()[0]
-        print('in_channels: ', in_channels)
     if mask.output_mask is None:
         out_channels = conv.out_channels
-        print('out_channels: ', out_channels)
     else:
         out_channels_index = mask.output_mask.mask_index[1]
-        print('out_channels_index: ', out_channels_index)
         out_channels = out_channels_index.size()[0]
-        print('out_channels: ', out_channels)
     new_conv = torch.nn.Conv2d(in_channels=in_channels,
                                out_channels=out_channels,
                                kernel_size=conv.kernel_size,
@@ -102,20 +115,14 @@ def replace_conv2d(conv, mask):
                                groups=1, # currently only support groups is 1
                                bias=conv.bias is not None,
                                padding_mode=conv.padding_mode)
-    #print('weight: ', conv.weight.get_device())
-    #print('bias', conv.bias.get_device())
-    #print('conv2d weight: ', conv.weight.data.size(), conv.weight.data)
     new_conv.to(conv.weight.device)
     tmp_weight_data = tmp_bias_data = None
     if mask.output_mask is not None:
-        print('mask output_mask is not None')
         tmp_weight_data = torch.index_select(conv.weight.data, 0, out_channels_index)
         if conv.bias is not None:
-            print('bias is not None')
             tmp_bias_data = torch.index_select(conv.bias.data, 0, out_channels_index)
     # NOTE: does not support group
     if mask.input_mask is not None:
-        print('mask input_mask is not None')
         tmp_weight_data = torch.index_select(conv.weight.data if tmp_weight_data is None else tmp_weight_data,
                                              1, in_channels_index)
     assert tmp_weight_data is not None
@@ -123,8 +130,4 @@ def replace_conv2d(conv, mask):
     if conv.bias is not None:
         print('final conv.bias is not None')
         new_conv.bias.data.copy_(conv.bias.data if tmp_bias_data is None else tmp_bias_data)
-    #new_conv.weight.to('cuda:0')
-    #new_conv.bias.to('cuda:0')
-    #print(new_conv.weight.get_device(), new_conv.bias.data, new_conv.bias.get_device())
-    #print('new conv2d weight: ', new_conv.weight.data.size(), new_conv.weight.data)
     return new_conv
--- src/sdk/pynni/nni/compression/speedup/torch/compressor.py
+++ src/sdk/pynni/nni/compression/speedup/torch/compressor.py
@@ -469,6 +469,8 @@ class ModelSpeedup:
             if g_node.type == 'module':
                 super_module, leaf_module = get_module_by_name(self.bound_model, module_name)
                 m_type = g_node.op_type
+                if not m_type in replace_module:
+                    raise RuntimeError("Has not supported replacing the module: `{}`".format(m_type))
                 compressed_module = replace_module[m_type](leaf_module, self.inferred_masks[module_name])
                 setattr(super_module, module_name.split('.')[-1], compressed_module)
             elif g_node.type == 'func':
