9755275243783b991bff09122de303ba4d6ad267
chicm-ms <38930155+chicm-ms@users.noreply.github.com>
2020-03-18 21:50:11 +0800

    Model compression test (#2175)

---
+++ test/scripts/model_compression.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+CWD=${PWD}
+
+echo ""
+echo "===========================Testing: pruning and speedup==========================="
+cd ${CWD}/../examples/model_compress
+
+echo "testing slim pruning and speedup..."
+python3 model_prune_torch.py --pruner_name slim --pretrain_epochs 1 --prune_epochs 1
+python3 model_speedup.py --example_name slim --model_checkpoint ./checkpoints/pruned_vgg19_cifar10_slim.pth \
+    --masks_file ./checkpoints/mask_vgg19_cifar10_slim.pth
+
+echo "testing l1 pruning and speedup..."
+python3 model_prune_torch.py --pruner_name l1 --pretrain_epochs 1 --prune_epochs 1
+python3 model_speedup.py --example_name l1filter --model_checkpoint ./checkpoints/pruned_vgg16_cifar10_l1.pth \
+    --masks_file ./checkpoints/mask_vgg16_cifar10_l1.pth
+
+echo "testing apoz pruning and speedup..."
+python3 model_prune_torch.py --pruner_name apoz --pretrain_epochs 1 --prune_epochs 1
+python3 model_speedup.py --example_name apoz --model_checkpoint ./checkpoints/pruned_vgg16_cifar10_apoz.pth \
+    --masks_file ./checkpoints/mask_vgg16_cifar10_apoz.pth
+
+for name in level fpgm mean_activation
+do
+    echo "testing $name pruning..."
+    python3 model_prune_torch.py --pruner_name $name --pretrain_epochs 1 --prune_epochs 1
+done
+
+echo "testing lottery ticket pruning..."
+python3 lottery_torch_mnist_fc.py
+
+echo ""
+echo "===========================Testing: quantizers==========================="
+cd ${CWD}/../examples/model_compress
+
+echo "testing QAT quantizer..."
+python3 QAT_torch_quantizer.py
+
+echo "testing DoReFa quantizer..."
+python3 DoReFaQuantizer_torch_mnist.py
+
+echo "testing BNN quantizer..."
+python3 BNN_quantizer_cifar10.py
