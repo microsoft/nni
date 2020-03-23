6a16cd710ee081f02d7cff5cfd0d808b68b8fa39
colorjam <im.cqyan@gmail.com>
2020-03-23 17:04:53 +0800

    update

--- examples/model_compress/run_densenet40.sh
+++ examples/model_compress/run_densenet40.sh
@@ -1,5 +1,9 @@
 ### Config for pruners excluding SlimPruner
-# python main.py --config configs/pruner/densenet40_pruner.yaml --save_name Task_01 --random_seed 2222 --pruner_name GradientWeightRankFilterPruner --pretrain ./experiments/pretrained_models/densenet40_pretrain_cifar10_seed_2333.pth
+CUDA_VISIBLE_DEVICES=2 python main.py --config configs/pruner/densenet40_pruner.yaml --save_name Task_01 --random_seed 2333 --pruner_name TaylorFOWeightFilterGlobalPruner --pretrain ./experiments/pretrained_models/densenet40_pretrain_cifar10_seed_2333.pth
+
+CUDA_VISIBLE_DEVICES=2 python main.py --config configs/pruner/densenet40_pruner.yaml --save_name Task_01 --random_seed 2222 --pruner_name TaylorFOWeightFilterGlobalPruner --pretrain ./experiments/pretrained_models/densenet40_pretrain_cifar10_seed_2222.pth
+
+CUDA_VISIBLE_DEVICES=2 python main.py --config configs/pruner/densenet40_pruner.yaml --save_name Task_01 --random_seed 1000 --pruner_name TaylorFOWeightFilterGlobalPruner --pretrain ./experiments/pretrained_models/densenet40_pretrain_cifar10_seed_1000.pth
 
 ### Config for SlimPruner
 # python main.py --config configs/pruner/densenet40_slim_pruner.yaml --save_name Task_01--random_seed 2333 --pruner_name SlimPruner --pretrain ./experiments/pretrained_models/densenet40_pretrain_cifar10_seed_2333.pth
--- examples/model_compress/run_resnet56.sh
+++ examples/model_compress/run_resnet56.sh
@@ -1,5 +1,9 @@
 ### Config for pruners excluding SlimPruner
-# python main.py --config configs/pruner/resnet56_pruner.yaml --save_name Task_01 --random_seed 2333 --pruner_name ActivationAPoZRankFilterPruner --pretrain ./experiments/resnet56_cifar10_FPGMPruner/baseline_seed_2333/checkpoints/pretrain.pth
+python main.py --config configs/pruner/resnet56_pruner.yaml --save_name Task_01 --random_seed 2333 --pruner_name TaylorFOWeightFilterGlobalPruner --pretrain ./experiments/pretrained_models/resnet56_pretrain_cifar10_seed_2333.pth
+
+python main.py --config configs/pruner/resnet56_pruner.yaml --save_name Task_01 --random_seed 2222 --pruner_name TaylorFOWeightFilterGlobalPruner --pretrain ./experiments/pretrained_models/resnet56_pretrain_cifar10_seed_2222.pth
+
+python main.py --config configs/pruner/resnet56_pruner.yaml --save_name Task_01 --random_seed 1000 --pruner_name TaylorFOWeightFilterGlobalPruner --pretrain ./experiments/pretrained_models/resnet56_pretrain_cifar10_seed_1000.pth
 
 ### Config for SlimPruner
 # python main.py --config configs/pruner/resnet56_slim_pruner.yaml --save_name Task_01 --random_seed 2333 --pruner_name SlimPruner --pretrain ./experiments/pretrained_models/resnet56_pretrain_cifar10_seed_2333.pth
