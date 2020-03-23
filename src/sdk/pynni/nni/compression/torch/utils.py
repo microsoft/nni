896679c9ec59e3e34c4e49fda32dd3ae0e11f0e0
chicm-ms <chec@microsoft.com>
2020-03-23 15:06:53 +0800

    updates

--- src/sdk/pynni/nni/compression/torch/utils.py
+++ src/sdk/pynni/nni/compression/torch/utils.py
@@ -19,7 +19,7 @@ def validate_op_types(model, op_types, logger):
     for _, module in model.named_modules():
         found_types.add(type(module).__name__)
 
-    not_found_op_types = set(op_types) - found_types
+    not_found_op_types = list(set(op_types) - found_types)
     if not_found_op_types:
         logger.warning('op_types %s not found in model', not_found_op_types)
 
