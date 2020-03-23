fe8f8804479fff2a9de25725d0ed00dbfe218d7f
liuzhe <liuzhe.cs@pku.edu.cn>
2020-03-23 10:44:51 +0800

    prevent overwriting metric file in multi-progress trials

--- src/sdk/pynni/nni/platform/local.py
+++ src/sdk/pynni/nni/platform/local.py
@@ -12,9 +12,8 @@ from ..env_vars import trial_env_vars
 from ..utils import to_json
 
 _sysdir = trial_env_vars.NNI_SYS_DIR
-if not os.path.exists(os.path.join(_sysdir, '.nni')):
-    os.makedirs(os.path.join(_sysdir, '.nni'))
-_metric_file = open(os.path.join(_sysdir, '.nni', 'metrics'), 'wb')
+os.makedirs(os.path.join(_sysdir, '.nni'), exist_ok=True)
+_metric_file = None
 
 _outputdir = trial_env_vars.NNI_OUTPUT_DIR
 if not os.path.exists(_outputdir):
@@ -62,10 +61,16 @@ def get_next_parameter():
     return params
 
 def send_metric(string):
+    global _metric_file
     if _nni_platform != 'local':
         assert len(string) < 1000000, 'Metric too long'
         print("NNISDK_MEb'%s'" % (string), flush=True)
     else:
+        if _metric_file is None:
+            metric_path = os.path.join(_sysdir, '.nni', 'metrics')
+            if os.path.isfile(metric_path):
+                raise RuntimeError('Metric file exist. Only call `send_metric` in one progress.')
+            _metric_file = open(metric_path, 'wb')
         data = (string + '\n').encode('utf8')
         assert len(data) < 1000000, 'Metric too long'
         _metric_file.write(b'ME%06d%b' % (len(data), data))
