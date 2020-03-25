a45f227da447d8b6ab2fb814c5b17f9ab4495c4c
Yuge Zhang <scottyugochang@gmail.com>
2020-03-25 12:51:15 +0800

    add nas it

--- examples/nas/darts/search.py
+++ examples/nas/darts/search.py
@@ -22,12 +22,13 @@ if __name__ == "__main__":
     parser.add_argument("--batch-size", default=64, type=int)
     parser.add_argument("--log-frequency", default=10, type=int)
     parser.add_argument("--epochs", default=50, type=int)
+    parser.add_argument("--channels", default=16, type=int)
     parser.add_argument("--unrolled", default=False, action="store_true")
     args = parser.parse_args()
 
     dataset_train, dataset_valid = datasets.get_dataset("cifar10")
 
-    model = CNN(32, 3, 16, 10, args.layers)
+    model = CNN(32, 3, args.channels, 10, args.layers)
     criterion = nn.CrossEntropyLoss()
 
     optim = torch.optim.SGD(model.parameters(), 0.025, momentum=0.9, weight_decay=3.0E-4)
