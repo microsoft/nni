4e2c0aadc84353eb05681c8a7a7401f6663c089a
chicm-ms <38930155+chicm-ms@users.noreply.github.com>
2020-03-25 12:58:36 +0800

    Fix mnist-keras multi trials on same system data load (#2228)

--- examples/trials/mnist-keras/mnist-keras.py
+++ examples/trials/mnist-keras/mnist-keras.py
@@ -63,7 +63,9 @@ def load_mnist_data(args):
     '''
     Load MNIST dataset
     '''
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    mnist_path = os.path.join(os.environ.get('NNI_OUTPUT_DIR'), 'mnist.npz')
+    (x_train, y_train), (x_test, y_test) = mnist.load_data(path=mnist_path)
+    os.remove(mnist_path)
 
     x_train = (np.expand_dims(x_train, -1).astype(np.float) / 255.)[:args.num_train]
     x_test = (np.expand_dims(x_test, -1).astype(np.float) / 255.)[:args.num_test]
