546ca7344dee4e84cb4e416bf00596012cda747a
suiguoxin <suiguoxin@gmail.com>
2020-04-26 01:47:16 +0000

    revert useless modif

--- src/sdk/pynni/nni/compression/torch/pruners.py
+++ src/sdk/pynni/nni/compression/torch/pruners.py
@@ -8,9 +8,6 @@ from schema import And, Optional
 from .compressor import Pruner
 from .utils import CompressorSchema
 
-from nni.utils import OptimizeMode
-import nni.parameter_expressions as parameter_expressions
-
 __all__ = ['LevelPruner', 'AGP_Pruner', 'SlimPruner', 'LotteryTicketPruner']
 
 logger = logging.getLogger('torch pruner')
@@ -448,92 +445,3 @@ class LotteryTicketPruner(Pruner):
             self._optimizer.load_state_dict(self._optimizer_state)
             if self._lr_scheduler is not None:
                 self._lr_scheduler.load_state_dict(self._scheduler_state)
-
-class SimulatedAnnealingPruner(Pruner):
-    """
-    This is a Pytorch implementation of Simulated Annealing compression algorithm.
-    """
-
-    def __init__(self, model, config_list, optimizer=None, T=100, stop_temperature=20, cool_down_rate=0.9):
-        """
-        Parameters
-        ----------
-        model : pytorch model
-            The model to be pruned
-        config_list : list
-            Supported keys:
-                - sparsity : The final sparsity when the compression is done.
-                - op_type : The operation type to prune.
-
-        1. Randomly initialize a pruning rate distribution.
-        2. Generate perturbation 
-        3. Perform fast evaluation on the perturbation
-        4. accept the perturbation with probability
-        5. cool down, T <- T * cool_down_rate
-        6. repeat step 2~5 while T > stop_temperature
-        """
-        super().__init__(model, config_list)
-        # hyper parameters of SA algorithm
-        self.T = T
-        self.stop_temperature = stop_temperature
-        self.cool_down_rate = cool_down_rate  # cool down rate
-
-        self._optimize_mode = OptimizeMode(optimize_mode)
-        self.state = []  # the current pruning rates of the layers
-        self.current_performance = None
-        
-        # init pruning rates of the layers
-        sparsities = np.random.uniform(0, 1, len(self.modules_warpper)) 
-        sparsities = self.rescale_sparsities(model, sparsities, config_list['sparsity'])
-
-        # init current performance
-        if self._optimize_mode == OptimizeMode.Minimize:
-            self.current_performance = np.inf
-        else:
-            self.current_performance = -np.inf
-
-    def rescale_sparsities(self, model, sparsities, target_sparsity):
-        # TODO: calculate sparsity and rescale sparsities
-        # TODO: sort the sparsities
-        return sparsities
-
-
-    def validate_config(self, model, config_list):
-        """
-        Parameters
-        ----------
-        model : torch.nn.module
-            Model to be pruned
-        config_list : list
-            Supported keys:
-                - prune_iterations : The number of rounds for the iterative pruning.
-                - sparsity : The final sparsity when the compression is done.
-        """
-        schema = CompressorSchema([{
-            'sparsity': And(float, lambda n: 0 < n < 1),
-            Optional('op_types'): [str],
-        }], model, logger)
-
-        schema.validate(config_list)
-
-
-    def calc_mask(self, wrapper, **kwargs):
-        """
-        Generate mask for the given ``weight``.
-
-        Parameters
-        ----------
-        wrapper : Module
-            The layer to be pruned
-
-        Returns
-        -------
-        tensor
-            The mask for this weight, it is ```None``` because this pruner
-            calculates and assigns masks in ```prune_iteration_start```,
-            no need to do anything in this function.
-        """
-        return None
-
-    def compress(self):
-        
