78ee01a29a0d59c52d43ab060c1bdda38c1525d2
suiguoxin <suiguoxin@gmail.com>
2020-05-07 13:43:34 +0000

    fine tune

--- examples/model_compress/experiment_data/finetune_imagenet.py
+++ examples/model_compress/experiment_data/finetune_imagenet.py
@@ -1,18 +1,21 @@
+'''
+Given the config_list of LevelPruner, Prune the original model & fine tune it 
+'''
 from __future__ import print_function
 
 import argparse
 import os
+import json
+import pandas as pd
 import torch
-import torch.nn.functional as F
 import torch.utils.data
-import torch.optim as optim
 from torchvision import datasets, transforms
-from torch.optim.lr_scheduler import StepLR
+import torch.nn.functional as F
+import torchvision.models as models
 
-from models.mnist.lenet import LeNet
-from nni.compression.torch import SimulatedAnnealingPruner
+from nni.compression.torch import LevelPruner
 
-MODEL_DIR = "mnist_lenet.pt"
+MODEL_DIR = "imagenet_mobilenet.pt"
 DATA_DIR = '../data'
 
 
@@ -36,7 +39,7 @@ def test(model, device, val_loader):
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in val_loader:
+        for batch_idx, (data, target) in enumerate(val_loader):
             data, target = data.to(device), target.to(device)
             output = model(data)
             # sum up batch loss
@@ -44,6 +47,10 @@ def test(model, device, val_loader):
             # get the index of the max log-probability
             pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
+
+            if batch_idx % args.log_interval == 0:
+                print('Validating... {} / {}'.format(batch_idx *
+                                                     len(data), len(val_loader.dataset)))
 
     test_loss /= len(val_loader.dataset)
     accuracy = correct / len(val_loader.dataset)
@@ -54,11 +61,28 @@ def test(model, device, val_loader):
     return accuracy
 
 
+def get_config_list(file):
+    history = pd.read_csv(file)
+
+    idx = history['performance'].idxmax()
+    config_list = history.loc[idx, 'config_list']
+
+    return config_list
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+
+    parser.add_argument('--history-file', type=str,
+                        default='mobilenet/pruning_history_05.csv', metavar='F')
+    parser.add_argument('--data-dir', type=str,
+                        default='/datasets/imagenet/', metavar='F')
+
+    parser.add_argument('--num_classes', type=int, default=1000, metavar='N',
+                        help='number of classes (default 1000)')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+    parser.add_argument('--test-batch-size', type=int, default=128, metavar='N',
                         help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=1, metavar='N',  # TODO:14
                         help='number of epochs to train (default: 14)')
@@ -66,56 +90,60 @@ if __name__ == '__main__':
                         help='learning rate (default: 1.0)')
     parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
                         help='Learning rate step gamma (default: 0.7)')
-    parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+    parser.add_argument('--log-interval', type=int, default=50, metavar='N',
                         help='how many batches to wait before logging training status')
     parser.add_argument('--experiment-data-dir', type=str,
                         default='examples/model_compress/experiment_data/', help='For saving experiment data')
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
     args = parser.parse_args()
-
     kwargs = {'num_workers': 1, 'pin_memory': True} if torch.cuda.is_available() else {
     }
 
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
+        datasets.ImageFolder(os.path.join(args.data_dir,'/train'),
+                             transform=transforms.Compose([
+                                 transforms.Resize(256),
+                                 transforms.CenterCrop(224),
+                                 transforms.ToTensor(),
+                                 normalize,
+                             ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
+
     val_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=False, transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])),
+        datasets.ImageFolder(os.path.join(args.data_dir,'/val'),
+                             transform=transforms.Compose([
+                                 transforms.Resize(256),
+                                 transforms.CenterCrop(224),
+                                 transforms.ToTensor(),
+                                 normalize,
+                             ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
     torch.manual_seed(0)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = LeNet().to(device)
-    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
-
-    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(model, device, val_loader)
-        scheduler.step()
-
-    if args.save_model:
-        torch.save(model.state_dict(), MODEL_DIR)
-
-    # TODO: single item list here
-    configure_list = [{
-        'sparsity': 0.5,
-        'op_types': ['default']  # module types to prune
-    }]
+    model = models.mobilenet_v2(pretrained=True).to(device)
 
     def evaluator(model):
         return test(model=model, device=device, val_loader=val_loader)
 
-    pruner = SimulatedAnnealingPruner(
-        model, configure_list, evaluator=evaluator, cool_down_rate=0.5, experiment_data_dir=args.experiment_data_dir)
+    config_list = get_config_list(args.history_file)
+
+    pruner = LevelPruner(model, json.loads(config_list))
     model_masked = pruner.compress()
     evaluation_result = evaluator(model_masked)
-    print('Evaluation result : %s' % evaluation_result)
+    print('Evaluation result (pruned model): %s' % evaluation_result)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.05,
+                                momentum=0.9,
+                                weight_decay=4e-5)
+    for epoch in range(10):
+        train(args, model_masked, device,
+              train_loader, optimizer, epoch)
+        test(model, device, val_loader)
+
+    evaluation_result = evaluator(model_masked)
+    print('Evaluation result (fine tuned): %s' % evaluation_result)
---
+++ examples/model_compress/sa_torch.py
@@ -0,0 +1,191 @@
+from __future__ import print_function
+
+import argparse
+import os
+import torch
+import torch.nn.functional as F
+import torch.utils.data
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+import torchvision.models as models
+
+from models.mnist.lenet import LeNet
+from nni.compression.torch import SimulatedAnnealingPruner
+
+MODEL_DIR = "mnist_lenet.pt"
+DATA_DIR = '../data'
+
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test(model, device, val_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in val_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            # sum up batch loss
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            # get the index of the max log-probability
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(val_loader.dataset)
+    accuracy = correct / len(val_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(val_loader.dataset), 100. * accuracy))
+
+    return accuracy
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+
+    parser.add_argument('--dataset', type=str, default='mnist', metavar='DS',
+                        help='dataset to use, mnist or imagenet (default MNIST)')
+    parser.add_argument('--fine-tune', type=bool, default=True, metavar='F',
+                        help='Whether to fine-tune the pruned model')
+
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=1, metavar='N',  # TODO:14
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
+                        help='learning rate (default: 1.0)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--experiment-data-dir', type=str,
+                        default='examples/model_compress/experiment_data/', help='For saving experiment data')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+
+    args = parser.parse_args()
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if torch.cuda.is_available() else {
+    }
+
+    if args.dataset == 'mnist':
+        train_loader = torch.utils.data.DataLoader(
+            datasets.MNIST('../data', train=True, download=True,
+                           transform=transforms.Compose([
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.1307,), (0.3081,))
+                           ])),
+            batch_size=args.batch_size, shuffle=True, **kwargs)
+        val_loader = torch.utils.data.DataLoader(
+            datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,))
+            ])),
+            batch_size=args.test_batch_size, shuffle=True, **kwargs)
+    elif args.dataset == 'imagenet':
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
+        # traindir = '/datasets/imagenet/train'
+        # train_loader = torch.utils.data.DataLoader(
+        #     datasets.ImageFolder(traindir, transforms.Compose([
+        #         transforms.Resize(256),
+        #         transforms.CenterCrop(224),
+        #         transforms.ToTensor(),
+        #         normalize,
+        #     ])),
+        #     batch_size=args.batch_size, shuffle=False, **kwargs)
+        # valdir = '/datasets/imagenet/val'
+        # val_loader = torch.utils.data.DataLoader(
+        #     datasets.ImageFolder(valdir, transforms.Compose([
+        #         transforms.Resize(256),
+        #         transforms.CenterCrop(224),
+        #         transforms.ToTensor(),
+        #         normalize,
+        #     ])),
+        #     batch_size=args.test_batch_size, shuffle=False, **kwargs)
+        train_loader = torch.utils.data.DataLoader(
+            datasets.ImageFolder('/datasets/imagenet/train',
+                              transform=transforms.Compose([
+                                  transforms.Resize(256),
+                                  transforms.CenterCrop(224),
+                                  transforms.ToTensor(),
+                                  normalize,
+                              ])),
+            batch_size=args.batch_size, shuffle=True, **kwargs)
+        val_loader = torch.utils.data.DataLoader(
+            datasets.ImageFolder('/datasets/imagenet/val_back',
+                              transform=transforms.Compose([
+                                  transforms.Resize(256),
+                                  transforms.CenterCrop(224),
+                                  transforms.ToTensor(),
+                                  normalize,
+                              ])),
+            batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+    torch.manual_seed(0)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    if args.dataset == 'mnist':
+        model = LeNet().to(device)
+        optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+
+        scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+        for epoch in range(1, args.epochs + 1):
+            train(args, model, device, train_loader, optimizer, epoch)
+            # test(model, device, val_loader)
+            scheduler.step()
+        # if args.save_model:
+        #     torch.save(model.state_dict(), MODEL_DIR)
+    elif args.dataset == 'imagenet':
+        model = models.mobilenet_v2(pretrained=True).to(device)
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.05,
+                                    momentum=0.9,
+                                    weight_decay=4e-5)
+
+    # TODO: single item list here
+    config_list = [{
+        'sparsity': 0.5,
+        'op_types': ['default']  # module types to prune
+    }]
+
+    def evaluator(model):
+        return test(model=model, device=device, val_loader=val_loader)
+
+    pruner = SimulatedAnnealingPruner(
+        model, config_list, evaluator=evaluator, cool_down_rate=0.5, experiment_data_dir=args.experiment_data_dir)
+    model_masked = pruner.compress()
+    evaluation_result = evaluator(model_masked)
+    print('Evaluation result (pruned model): %s' % evaluation_result)
+
+    if args.fine_tune:
+        if args.dataset == 'mnist':
+            for epoch in range(1, args.epochs + 1):
+                train(args, model_masked, device,
+                      train_loader, optimizer, epoch)
+                test(model, device, val_loader)
+                scheduler.step()
+        elif args.dataset == 'imagenet':
+            for epoch in range(10):
+                train(args, model_masked, device,
+                      train_loader, optimizer, epoch)
+                test(model, device, val_loader)
+
+    evaluation_result = evaluator(model_masked)
+    print('Evaluation result (fine tuned): %s' % evaluation_result)
