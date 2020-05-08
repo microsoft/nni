d6e41012d92c6fd2db125279bc2c545a315cbba6
suiguoxin <suiguoxin@gmail.com>
2020-05-08 01:48:41 +0000

    add code for imagenet  integrity

---
+++ examples/model_compress/experiment_data/finetune.sh
@@ -0,0 +1,4 @@
+python finetune_imagenet.py --history-file 'mobilenet/pruning_history_05.csv'&> output05.txt
+python finetune_imagenet.py --history-file 'mobilenet/pruning_history_04.csv'&> output03.txt
+python finetune_imagenet.py --history-file 'mobilenet/pruning_history_03.csv'&> output03.txt
+python finetune_imagenet.py --history-file 'mobilenet/pruning_history_01.csv'&> output01.txt
