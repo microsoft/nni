25b0d43b0fde0045aea234d3da56bc8b8b23a654
Shinai Yang <shinyang@microsoft.com>
2020-05-25 15:49:05 +0800

    fix windows commands

--- src/nni_manager/training_service/remote_machine/extends/windowsCommands.ts
+++ src/nni_manager/training_service/remote_machine/extends/windowsCommands.ts
@@ -30,7 +30,7 @@ class WindowsCommands extends OsCommands {
             ${cudaVisibleSetting !== "" ? "set " + cudaVisibleSetting : ""}
             md %NNI_SYS_DIR%/code
             robocopy /s %NNI_CODE_DIR%/. %NNI_SYS_DIR%/code
-            cd %NNI_SYS_DIR%
+            cd %NNI_SYS_DIR%/code
             python -c "import nni" 2>nul
             if not %ERRORLEVEL% EQU 0 (
                 echo installing NNI as exit code of "import nni" is %ERRORLEVEL%
