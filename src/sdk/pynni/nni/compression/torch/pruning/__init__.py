5a911b3061751a3441f18ff69a9279b16da9bda3
chicm-ms <38930155+chicm-ms@users.noreply.github.com>
2020-05-28 17:11:42 +0800

    Refactor model compression directory structure (#2501)

---
+++ src/sdk/pynni/nni/compression/torch/pruning/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from .pruners import *
+from .weight_rank_filter_pruners import *
+from .activation_rank_filter_pruners import *
+from .apply_compression import apply_compression_results
+from .gradient_rank_filter_pruners import *
