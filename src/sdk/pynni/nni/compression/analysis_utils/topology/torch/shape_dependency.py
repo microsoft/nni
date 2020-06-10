3b9f4df63dbcd47ec63daf631e482cfec541a0ad
Ningxin <Ningxin.Zheng@microsoft.com>
2020-06-10 01:35:47 +0000

    Use TorchModuleGraph to analyze the shape dependency.
    Signed-off-by: Ningxin <Ningxin.Zheng@microsoft.com>

--- src/sdk/pynni/nni/compression/analysis_utils/topology/torch/shape_dependency.py
+++ src/sdk/pynni/nni/compression/analysis_utils/topology/torch/shape_dependency.py
@@ -2,11 +2,11 @@
 # Licensed under the MIT license.
 
 import csv
+import torch
 import logging
 
-
-from .graph_from_trace import VisualGraph
-
+import nni._graph_utils as _graph_utils
+from _graph_utils import TorchModuleGraph
 
 CONV_TYPE = 'aten::_convolution'
 ADD_TYPES = ['aten::add', 'aten::add_']
@@ -15,7 +15,7 @@ logger = logging.getLogger('Shape_Dependency')
 
 
 class ChannelDependency:
-    def __init__(self, model=None, data=None, graph=None):
+    def __init__(self, model=None, dummy_input=None, traced_model=None):
         """
         This model analyze the channel dependencis between the conv
         layers in a model.
@@ -26,17 +26,17 @@ class ChannelDependency:
             The model to be analyzed.
         data : torch.Tensor
             The example input data to trace the network architecture.
-        graph : torch._C.Graph
+        traced_model : torch._C.Graph
             if we alreay has the traced graph of the target model, we donnot
             need to trace the model again.
         """
-        self.graph_builder = VisualGraph(model, data, graph)
-        self.cnodes = list(self.graph_builder.graph.nodes())
-        self.graph = self.graph_builder.graph
-        self.forward_edge = self.graph_builder.forward_edge
-        self.c2py = self.graph_builder.c2py
-        self.dependency = {}
+        # check if the input is legal
+        if traced_model is None:
+            # user should provide model & dummy_input to trace the model or a already traced model
+            assert model is not None and dummy_input is not None
+        self.graph = TorchModuleGraph(model, dummy_input, traced_model)
         self.build_channel_dependency()
+        self.dependency = {}
 
 
     def get_parent_convs(self, node):
@@ -58,17 +58,14 @@ class ChannelDependency:
         queue.append(node)
         while queue:
             curnode = queue.pop(0)
-            if curnode in self.c2py and self.c2py[curnode].isOp \
-                    and curnode.kind() == CONV_TYPE:
+            if node.op_type == 'Conv2d':
                 # find the first met conv
-                parent_convs.append(curnode)
+                parent_convs.append(curnode.name)
                 continue
-            parents = self.c2py[curnode].parents()
+            parents = self.graph.find_predecessors(curnode.unique_name)
+            parents = [self.graph.name_to_node[name] for name in parents]
             for parent in parents:
-                if parent in self.c2py and (self.c2py[parent].isOp or 'Tensor' in str(parent.type())):
-                    # filter the scalar parameters of the functions
-                    # only consider the Tensors/ List(Tensor)
-                    queue.append(parent)
+                queue.append(parent)
         return parent_convs
 
     def build_channel_dependency(self):
@@ -76,13 +73,25 @@ class ChannelDependency:
         Build the channel dependency for the conv layers
         in the model.
         """
-        for node in self.cnodes:
+        for node in self.graph.nodes_py.nodes_op:
             parent_convs = []
-            if node.kind() in ADD_TYPES:
+            # find the node that contains aten::add
+            # or aten::cat operations
+            if node.op_type in ADD_TYPES:
                 parent_convs = self.get_parent_convs(node)
-            if node.kind() == CAT_TYPE:
-                cat_dim = list(node.inputs())[1].toIValue()
-                # N * C * H * W
+            elif node.op_type == CAT_TYPE:
+                # To determine if this cat operation will introduce channel
+                # dependency, we need the specific input parameters of the cat
+                # opertion. To get the input parameters of the cat opertion, we
+                # need to traverse all the cpp_nodes included by this NodePyGroup,
+                # because, TorchModuleGraph merges the important nodes and the adjacent
+                # unimportant nodes (nodes started with prim::attr, for example) into a
+                # NodepyGroup.
+                cat_dim = None
+                for cnode in node.node_cpps:
+                    if cnode.kind == CAT_TYPE:
+                        cat_dim = list(cnode.inputs())[1].toIValue()
+                        break
                 if cat_dim != 1:
                     parent_convs = self.get_parent_convs(node)
             dependency_set = set(parent_convs)
@@ -93,6 +102,7 @@ class ChannelDependency:
             # save the dependencies
             for _node in dependency_set:
                 self.dependency[_node] = dependency_set
+
 
     def filter_prune_check(self, ratios):
         """
@@ -116,19 +126,19 @@ class ChannelDependency:
         -------
         True/False
         """
-
-        for node in self.cnodes:
-            if node.kind() == CONV_TYPE and self.c2py[node].name in ratios:
-                if node not in self.dependency:
+        for node in self.graph.nodes_py.nodes_op:
+            if node.op_type == 'Conv2d' and node.name in ratios:
+                if node.name not in self.dependency:
                     # this layer has no dependency on other layers
                     # it's legal to set any prune ratio between 0 and 1
                     continue
-                for other in self.dependency[node]:
-                    if self.c2py[other].name not in ratios:
+                for other in self.dependency[node.name]:
+                    if other not in ratios:
                         return False
-                    elif ratios[self.c2py[node].name] != ratios[self.c2py[other].name]:
+                    elif ratios[other] != ratios[node.name]:
                         return False
         return True
+
 
     def export(self, filepath):
         """
@@ -140,18 +150,18 @@ class ChannelDependency:
         with open(filepath, 'w') as csvf:
             csv_w = csv.writer(csvf, delimiter=',')
             csv_w.writerow(header)
-            for node in self.cnodes:
-                if node.kind() != CONV_TYPE or node in visited:
+            for node in self.graph.nodes_py.nodes_op:
+                if node.op_type() != 'Conv2d' or node in visited:
                     continue
                 setid += 1
                 row = ['Set %d' % setid]
-                if node not in self.dependency:
+                if node.name not in self.dependency:
                     visited.add(node)
-                    row.append(self.c2py[node].name)
+                    row.append(node.name)
                 else:
                     for other in self.dependency[node]:
-                        visited.add(other)
-                        row.append(self.c2py[other].name)
+                        visited.add(self.graph.name_to_node[other])
+                        row.append(other)
                 csv_w.writerow(row)
 
     @property
@@ -168,16 +178,16 @@ class ChannelDependency:
         """
         d_sets = []
         visited = set()
-        for node in self.cnodes:
-            if node.kind() != CONV_TYPE or node in visited:
+        for node in self.graph.nodes_py.nodes_op:
+            if node.op_type != 'Conv2d' or node in visited:
                 continue
             tmp_set = set()
-            if node not in self.dependency:
+            if node.name not in self.dependency:
                 visited.add(node)
-                tmp_set.add(self.c2py[node].name)
+                tmp_set.add(node.name)
             else:
-                for other in self.dependency[node]:
-                    visited.add(other)
-                    tmp_set.add(self.c2py[other].name)
+                for other in self.dependency[node.name]:
+                    visited.add(self.graph.name_to_node[other])
+                    tmp_set.add(other)
             d_sets.append(tmp_set)
         return d_sets
