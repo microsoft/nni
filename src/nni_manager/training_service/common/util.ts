dc1307978fc226a455f611eeefeccea702f62900
Yuge Zhang <scottyugochang@gmail.com>
2020-06-17 11:50:39 +0800

    Check existence before mkdir

--- src/nni_manager/training_service/common/util.ts
+++ src/nni_manager/training_service/common/util.ts
@@ -105,7 +105,7 @@ export async function execCopydir(source: string, destination: string): Promise<
     for (const relPath of listDirWithIgnoredFiles(source, '', [])) {
         const sourcePath = path.join(source, relPath);
         const destPath = path.join(destination, relPath);
-        if (fs.statSync(sourcePath).isDirectory()) {
+        if (fs.statSync(sourcePath).isDirectory() && !fs.existsSync(destPath)) {
             fs.mkdirSync(destPath);
         } else {
             fs.copyFileSync(sourcePath, destPath);
