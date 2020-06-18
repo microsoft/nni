21a93df4b93e0ed151c4b2fcd157b84255a05a56
Yuge Zhang <scottyugochang@gmail.com>
2020-06-18 20:25:47 +0800

    Update script

--- examples/nas/benchmark/nasbench201.sh
+++ examples/nas/benchmark/nasbench201.sh
@@ -12,4 +12,4 @@ echo "Downloading NAS-Bench-201..."
 gdown https://drive.google.com/uc\?id\=1OOfVPpt-lA4u2HJrXbgrRd42IbfvJMyE -O a.pth
 
 echo "Generating database..."
-NASBENCHMARK_DIR=/outputs python -m nasbench201.db_gen a.pth
+NASBENCHMARK_DIR=/outputs python -m nni.nas.benchmark.nasbench201.db_gen a.pth
