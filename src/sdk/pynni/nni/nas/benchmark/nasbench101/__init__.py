435e47da6125f5def6de854ff7f0d7c8ef8fdd86
Yuge Zhang <scottyugochang@gmail.com>
2020-06-19 21:35:01 +0800

    Add API

---
+++ src/sdk/pynni/nni/nas/benchmark/nasbench101/__init__.py
@@ -0,0 +1,2 @@
+from .model import Nb101ComputedStats, Nb101IntermediateStats, Nb101RunConfig
+from .shortcuts import query_nb101_computed_stats
---
+++ src/sdk/pynni/nni/nas/benchmark/nasbench101/shortcuts.py
@@ -0,0 +1,31 @@
+import functools
+
+from peewee import fn
+from playhouse.shortcuts import model_to_dict
+from .model import Nb101ComputedStats, Nb101IntermediateStats, Nb101RunConfig
+
+
+def query_nb101_computed_stats(arch, num_epochs, reduction=None):
+    fields = []
+    if reduction == 'none':
+        reduction = None
+    if reduction == 'mean':
+        for field_name in Nb101ComputedStats._meta.sorted_field_names:
+            if field_name not in ['id', 'config']:
+                fields.append(fn.AVG(getattr(Nb101ComputedStats, field_name)).alias(field_name))
+    elif reduction is None:
+        fields.append(Nb101ComputedStats)
+    else:
+        raise ValueError('Unsupported reduction: \'%s\'' % reduction)
+    query = Nb101ComputedStats.select(*fields, Nb101RunConfig).join(Nb101RunConfig)
+    conditions = []
+    if arch is not None:
+        conditions.append(Nb101RunConfig.arch == arch)
+    if num_epochs is not None:
+        conditions.append(Nb101RunConfig.num_epochs == num_epochs)
+    if conditions:
+        query = query.where(functools.reduce(lambda a, b: a & b, conditions))
+    if reduction is not None:
+        query = query.group_by(Nb101ComputedStats.config)
+    for k in query:
+        yield model_to_dict(k)
