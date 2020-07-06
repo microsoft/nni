e0901b3e0913dbd284939666a763a0945dea941d
Yuge Zhang <scottyugochang@gmail.com>
2020-07-06 12:17:05 +0800

    using requirements.txt in nasbenchmark

--- examples/nas/benchmarks/nds.sh
+++ examples/nas/benchmarks/nds.sh
@@ -1,19 +1,9 @@
 set -e
-mkdir -p /outputs /tmp
-
-echo "Installing dependencies..."
-apt update && apt install -y wget zip
-pip install --no-cache-dir tqdm peewee
-
-echo "Installing NNI..."
-cd /nni && echo "y" | source install.sh
-
-cd /tmp
 
 echo "Downloading NDS..."
 wget https://dl.fbaipublicfiles.com/nds/data.zip -O data.zip
 unzip data.zip
 
 echo "Generating database..."
-rm -f /outputs/nds.db /outputs/nds.db-journal
-NASBENCHMARK_DIR=/outputs python -m nni.nas.benchmarks.nds.db_gen nds_data
+rm -f ${NASBENCHMARK_DIR}/nds.db ${NASBENCHMARK_DIR}/nds.db-journal
+python -m nni.nas.benchmarks.nds.db_gen nds_data
