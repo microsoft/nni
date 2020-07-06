40ab64aeade311c89ec9ad2e1c29ec7ffdb4273b
ltg001 <tabvision@bupt.icu>
2020-07-06 13:28:34 +0800

    enas test

--- examples/nas/search_space_zoo/enas_micro_example.py
+++ examples/nas/search_space_zoo/enas_micro_example.py
@@ -1,7 +1,7 @@
 import torch
 import logging
 import torch.nn as nn
-import torch.functional as F
+import torch.nn.functional as F
 
 from argparse import ArgumentParser
 from torchvision import transforms
