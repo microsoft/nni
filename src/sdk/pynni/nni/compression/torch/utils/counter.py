cb281100f7393882fd3dc8e506f2d229c6a3bfad
colorjam <im.cqyan@gmail.com>
2020-07-06 07:42:37 +0000

    remove whitespace

--- src/sdk/pynni/nni/compression/torch/utils/counter.py
+++ src/sdk/pynni/nni/compression/torch/utils/counter.py
@@ -1,7 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-import logging
 import torch
 import torch.nn as nn
 from nni.compression.torch.compressor import PrunerModuleWrapper
