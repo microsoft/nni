0d8d86cf47b3cdd95171f0e05a52c188080ea23d
colorjam <im.cqyan@gmail.com>
2020-07-07 01:56:22 +0000

    Update

--- src/sdk/pynni/nni/compression/torch/utils/counter.py
+++ src/sdk/pynni/nni/compression/torch/utils/counter.py
@@ -59,7 +59,7 @@ def count_flops_params(model: nn.Module, input_size, verbose=True):
     flops, params = profile(model, inputs=(inputs, ), custom_ops=custom_ops, verbose=verbose)
 
     for m in hook_module_list:
-        m._buffers.pop("weight_mask")    
+        m._buffers.pop("weight_mask")
     for m in model.modules():
         if 'total_ops' in m._buffers:
             m._buffers.pop("total_ops")
