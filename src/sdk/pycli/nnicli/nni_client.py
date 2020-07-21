9c8a67f06e10d77f663c9b6056d14717bf2ca952
Junwei Sun <sjw1996@bupt.edu.cn>
2020-07-21 23:07:46 +0800

    enhance nnicli, add ref to readthedoc

--- src/sdk/pycli/nnicli/nni_client.py
+++ src/sdk/pycli/nnicli/nni_client.py
@@ -7,7 +7,7 @@ Example:
 
 import nnicli as nc
 
-nc.start_nni('../../../../examples/trials/mnist/config.yml')
+nc.start_experiment('../../../../examples/trials/mnist/config.yml')
 
 nc.set_endpoint('http://localhost:8080')
 
@@ -17,7 +17,7 @@ print(nc.get_experiment_status())
 print(nc.get_job_statistics())
 print(nc.list_trial_jobs())
 
-nc.stop_nni()
+nc.stop_experiment()
 
 """
 
@@ -27,9 +27,16 @@ import subprocess
 import requests
 
 __all__ = [
-    'start_nni',
-    'stop_nni',
+    'start_experiment',
     'set_endpoint',
+    'stop_experiment',
+    'resume_experiment',
+    'view_experiment',
+    'update_searchspace',
+    'update_concurrency',
+    'update_duration',
+    'update_trailnum',
+    'stop_experiment',
     'version',
     'get_experiment_status',
     'get_experiment_profile',
@@ -53,8 +60,12 @@ API_ROOT_PATH = 'api/v1/nni'
 _api_endpoint = None
 
 def set_endpoint(endpoint):
-    """set endpoint of nni rest server for nnicli, for example:
-    http://localhost:8080
+    """set endpoint of nni rest server for nnicli, i.e., the url of Web UI.
+    everytime you want to change experiment, call this function first.
+    Parameters
+    ----------
+    endpoint: str
+        the endpoint of nni rest server for nnicli
     """
     global _api_endpoint
     _api_endpoint = endpoint
@@ -92,17 +103,139 @@ def _create_process(cmd):
             print(output.decode('utf-8').strip())
     return process.returncode
 
-def start_nni(config_file):
-    """start nni experiment with specified configuration file"""
-    cmd = 'nnictl create --config {}'.format(config_file).split(' ')
+def start_experiment(config_file, port=8080, debug=False):
+    """start an experiment with specified configuration file
+    Parameters
+    ----------
+    config_file: str
+        path to the config file
+    port: int
+        the port of restful server, greater than 1024
+    debug: boolean
+        set debug mode
+    """
+    cmd = 'nnictl create --config {} --port {} --debug {}'.format(config_file, port, debug).split(' ')
     if _create_process(cmd) != 0:
-        raise RuntimeError('Failed to start nni.')
+        raise RuntimeError('Failed to start experiment.')
 
-def stop_nni():
-    """stop nni experiment"""
-    cmd = 'nnictl stop'.split(' ')
+def resume_experiment(exp_id, port=8080, debug=False):
+    """resume a stopped experiment with specified experiment id
+    Parameters
+    ----------
+    exp_id: str
+        experiment id
+    port: int
+        the port of restful server, greater than 1024
+    debug: boolean
+        set debug mode
+    """
+    cmd = 'nnictl resume {} --port {} --debug {}'.format(exp_id, port, debug).split(' ')
     if _create_process(cmd) != 0:
-        raise RuntimeError('Failed to stop nni.')
+        raise RuntimeError('Failed to resume experiment.')
+
+def view_experiment(exp_id, port=8080):
+    """view a stopped experiment with specified experiment id
+    Parameters
+    ----------
+    exp_id: str
+        experiment id
+    port: int
+        the port of restful server, greater than 1024
+    """
+    cmd = 'nnictl view {} --port {}'.format(exp_id, port).split(' ')
+    if _create_process(cmd) != 0:
+        raise RuntimeError('Failed to resume experiment.')
+
+def update_searchspace(filename, exp_id=None):
+    """update an experiment's search space
+    Parameters
+    ----------
+    filename: str
+        path to the searchspace file
+    exp_id: str
+        experiment id
+    """
+    if not exp_id:
+        cmd = 'nnictl update searchspace --filename {}'.format(filename).split(' ')
+    else:
+        cmd = 'nnictl update searchspace {} --filename {}'.format(exp_id, filename).split(' ')
+    if _create_process(cmd) != 0:
+        raise RuntimeError('Failed to resume experiment.')
+
+def update_concurrency(value, exp_id=None):
+    """update an experiment's concurrency
+    Parameters
+    ----------
+    value: int
+        new concurrency value
+    exp_id: str
+        experiment id
+    """
+    if not exp_id:
+        cmd = 'nnictl update concurrency --value {}'.format(value).split(' ')
+    else:
+        cmd = 'nnictl update concurrency {} --value {}'.format(exp_id, value).split(' ')
+    if _create_process(cmd) != 0:
+        raise RuntimeError('Failed to resume experiment.')
+
+def update_duration(value, exp_id=None):
+    """update an experiment's duration
+    Parameters
+    ----------
+    value: int
+        new duration value
+    exp_id: str
+        experiment id
+    """
+    if not exp_id:
+        cmd = 'nnictl update duration --value {}'.format(value).split(' ')
+    else:
+        cmd = 'nnictl update duration {} --value {}'.format(exp_id, value).split(' ')
+    if _create_process(cmd) != 0:
+        raise RuntimeError('Failed to resume experiment.')
+
+def update_trailnum(value, exp_id=None):
+    """update an experiment's maxtrialnum
+    Parameters
+    ----------
+    value: int
+        new trailnum value
+    exp_id: str
+        experiment id
+    """
+    if not exp_id:
+        cmd = 'nnictl update trialnum --value {}'.format(value).split(' ')
+    else:
+        cmd = 'nnictl update duration {} --value {}'.format(exp_id, value).split(' ')
+    if _create_process(cmd) != 0:
+        raise RuntimeError('Failed to resume experiment.')
+
+def stop_experiment(exp_id=None, port=None, stop_all=False):
+    """stop an experiment
+    Parameters
+    ----------
+    exp_id: str
+        experiment id
+    port: int
+        the port of restful server
+    stop_all: boolean
+        if set to True, all the experiments will be stopped
+    note that if stop_all is set to true, exp_id and port will be ignored. other wise
+    exp_id and port must correspond to the same experiment if they are both set.
+    """
+    if stop_all:
+        cmd = 'nnictl stop --all'.split(' ')
+    else:
+        if exp_id and not port:
+            cmd = 'nnictl stop {}'.format(exp_id).split(' ')
+        elif not exp_id and port:
+            cmd = 'nnictl stop --port {}'.format(port).split(' ')
+        elif exp_id and port:
+            cmd = 'nnictl stop {} --port {}'.format(exp_id, port).split(' ')
+        else:
+            cmd = 'nnictl stop'.split(' ')
+    if _create_process(cmd) != 0:
+        raise RuntimeError('Failed to stop experiment.')
 
 def version():
     """return version of nni"""
@@ -117,7 +250,12 @@ def get_experiment_profile():
     return _nni_rest_get(EXPERIMENT_PATH)
 
 def get_trial_job(trial_job_id):
-    """return trial job information as a dict"""
+    """return trial job information as a dict
+    Parameters
+    ----------
+    trial_job_id: str
+        trial id
+    """
     assert trial_job_id is not None
     return _nni_rest_get(os.path.join(TRIAL_JOBS_PATH, trial_job_id))
 
---
+++ src/sdk/pycli/nnicli/test.py
@@ -0,0 +1,2 @@
+from nni_client import *
+start_nni('/home/v-junwsu/mnist-pytorch/config.yml', port=9090, foreground=True)
