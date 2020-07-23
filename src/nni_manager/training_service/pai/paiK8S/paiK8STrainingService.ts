c9855ca01b38d1e5688d781c9bab8d3f8526df83
Shinai Yang <shinyang@microsoft.com>
2020-07-23 17:23:45 +0800

    remove unrelated code

--- src/nni_manager/training_service/pai/paiK8S/paiK8STrainingService.ts
+++ src/nni_manager/training_service/pai/paiK8S/paiK8STrainingService.ts
@@ -118,7 +118,6 @@ class PAIK8STrainingService extends PAITrainingService {
         const trialWorkingFolder: string = path.join(this.expRootDir, 'trials', trialJobId);
         const paiJobName: string = `nni_exp_${this.experimentId}_trial_${trialJobId}`;
         const logPath: string = path.join(this.paiTrialConfig.nniManagerNFSMountPath, this.experimentId, trialJobId);
-        const paiJobDetailUrl: string = `${this.protocol}://${this.paiClusterConfig.host}/job-detail.html?username=${this.paiClusterConfig.userName}&jobName=${paiJobName}`;
         const trialJobDetail: PAITrialJobDetail = new PAITrialJobDetail(
             trialJobId,
             'WAITING',
@@ -126,8 +125,7 @@ class PAIK8STrainingService extends PAITrainingService {
             Date.now(),
             trialWorkingFolder,
             form,
-            logPath,
-            paiJobDetailUrl);
+            logPath);
 
         this.trialJobsMap.set(trialJobId, trialJobDetail);
         this.jobQueue.push(trialJobId);
