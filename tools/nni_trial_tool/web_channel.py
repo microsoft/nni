b8fbc9d3aa55279167dc5e2504ad2d4f1439c635
Shinai Yang <shinyang@microsoft.com>
2020-07-24 15:47:42 +0800

    fix typo

--- tools/nni_trial_tool/web_channel.py
+++ tools/nni_trial_tool/web_channel.py
@@ -31,7 +31,7 @@ class WebChannel(BaseChannel):
             self.client = client
             nni_log(LogType.Info, 'WebChannel: connected with info %s' % url)
         except asyncio.TimeoutError:
-            nni_log(LogType.Error, 'connect to %s timeout! Please make sure NNIManagerIP configured correclty, and accessable.' % url)
+            nni_log(LogType.Error, 'connect to %s timeout! Please make sure NNIManagerIP configured correctly, and accessable.' % url)
             os._exit(1)
 
     def _inner_close(self):
