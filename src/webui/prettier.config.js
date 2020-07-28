2de9dfca41a60a41c0eb39e08df0baa93cd560dd
Lijiao <15910218274@163.com>
2020-07-28 14:17:20 +0000

    update

---
+++ src/webui/prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 120,
+    semi: true,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    eslintIntegration: true,
+    singleQuote: true,
+    jsxSingleQuote: true
+};
