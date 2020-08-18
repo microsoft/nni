aa3dcbdb18d65838fde4b9562a1eb3c532a98fe1
Tab Zhang <tabvision@bupt.icu>
2020-08-18 13:47:28 +0800

    Update nas_bench_201.py

--- src/sdk/pynni/nni/nas/pytorch/nas_bench_201/nas_bench_201.py
+++ src/sdk/pynni/nni/nas/pytorch/nas_bench_201/nas_bench_201.py
@@ -24,7 +24,7 @@ class NASBench201Cell(nn.Module):
         stride of the convolution
     bn_affine: bool
         If set to ``True``, ``torch.nn.BatchNorm2d`` will have learnable affine parameters. Default: True
-    bn_momentun: float
+    bn_momentum: float
         the value used for the running_mean and running_var computation. Default: 0.1
     bn_track_running_stats: bool
         When set to ``True``, ``torch.nn.BatchNorm2d`` tracks the running mean and variance. Default: True
