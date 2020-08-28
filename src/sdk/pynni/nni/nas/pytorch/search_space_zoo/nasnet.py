9873aab220df7bc95e64a113bf48f3f4786c8cb2
ltg001 <tabvision@bupt.icu>
2020-08-28 22:17:44 +0800

    enas style

--- src/sdk/pynni/nni/nas/pytorch/search_space_zoo/nasnet.py
+++ src/sdk/pynni/nni/nas/pytorch/search_space_zoo/nasnet.py
@@ -9,7 +9,7 @@ from nni.nas.pytorch import mutables
 from nni.nas.pytorch.mutables import LayerChoice, InputChoice
 from torchvision import datasets, transforms
 
-from .nasnet_ops import Pool, Identity, SeparableConv2d, StdConv
+from nasnet_ops import Pool, Identity, SeparableConv2d, StdConv, FactorizedReduce
 import logging
 
 logger = logging.getLogger('nasnet')
@@ -51,13 +51,13 @@ def get_dataset(cls):
 
 
 class Cell(nn.Module):
-    def __init__(self, cell_name, prev_labels, in_ch, out_ch, stride, stem):
+    def __init__(self, cell_name, prev_labels, channels, stride, stem):
         super(Cell, self).__init__()
         self.input_choice = InputChoice(choose_from=prev_labels,
                                         n_chosen=1,
                                         return_mask=True,
                                         key=cell_name + '_input')
-        self.op_choice = LayerChoice(OPS(in_ch, out_ch, stride, stem),
+        self.op_choice = LayerChoice(OPS(channels, channels, stride, stem),
                                      key=cell_name + '_op')
 
     def forward(self, prev_layers):
@@ -67,10 +67,10 @@ class Cell(nn.Module):
 
 
 class Node(mutables.MutableScope):
-    def __init__(self, node_name, prev_node_names, in_ch, out_ch, stride, stem):
+    def __init__(self, node_name, prev_node_names, channels, stride=1, stem=False):
         super(Node, self).__init__(node_name)
-        self.cell_x = Cell(node_name + 'x', prev_node_names, in_ch, out_ch, stride, stem)
-        self.cell_y = Cell(node_name + 'y', prev_node_names, in_ch, out_ch, stride, stem)
+        self.cell_x = Cell(node_name + 'x', prev_node_names, channels, stride, stem)
+        self.cell_y = Cell(node_name + 'y', prev_node_names, channels, stride, stem)
 
     def forward(self, prev_layers):
         out_x, mask_x = self.cell_x(prev_layers)
@@ -79,123 +79,94 @@ class Node(mutables.MutableScope):
 
 
 class NasNetCell(nn.Module):
-    def __init__(self, n_hidden, in_ch, in_ch_prev, out_ch, reduction=False, reduction_p=False,
-                 stem=False, channels=None):
-        # TODO: hint: num of chosen -> user
-        # helper: json -> num
-        # hack classic nas mutator -> return json file -> dict: from mutate name to channel num
+    def __init__(self, num_nodes, in_channels_pp, in_channels_p, out_channels, reduction=False):
         super(NasNetCell, self).__init__()
-        self.n_hidden = n_hidden
         self.reduction = reduction
-        self.reduction_p = reduction_p
+        if self.reduction:
+            self.reduce0 = FactorizedReduce(in_channels_pp, out_channels, affine=False)
+            self.reduce1 = FactorizedReduce(in_channels_p, out_channels, affine=False)
+            in_channels_pp = in_channels_p = out_channels
+        self.preproc0 = StdConv(in_channels_pp, out_channels) if in_channels_pp != out_channels else nn.Identity()
+        self.preproc1 = StdConv(in_channels_p, out_channels) if in_channels_pp != out_channels else nn.Identity()
 
-        if reduction_p:
-            self.p1 = nn.Sequential(
-                nn.AvgPool2d(1, stride=2, count_include_pad=False),
-                nn.Conv2d(in_ch, out_ch // 2, 1, stride=1, bias=False)
-            )
-            self.p2 = nn.Sequential(
-                nn.ZeroPad2d((0, 1, 0, 1)),
-                nn.AvgPool2d(1, stride=2, count_include_pad=False),
-                nn.Conv2d(in_ch, (out_ch + 1) // 2, 1, stride=1, bias=False)
-            )
-            self.p_bn = nn.BatchNorm2d(out_ch, eps=0.001, momentum=0.1, affine=True)
-        else:
-            self.conv_prev_1x1 = StdConv(in_ch_prev, out_ch)
-        self.conv_1x1 = StdConv(in_ch, out_ch)
-        if channels is not None:
-            self.channels = channels
-        else:
-            self.channels = [out_ch] * (self.n_hidden + 2)
+        self.num_nodes = num_nodes
         name_prefix = "reduce" if reduction else "normal"
         self.nodes = nn.ModuleList()
         node_labels = [mutables.InputChoice.NO_KEY, mutables.InputChoice.NO_KEY]
-        for i in range(n_hidden):
+        for i in range(num_nodes):
             node_labels.append("{}_node_{}".format(name_prefix, i))
-            self.nodes.append(Node(node_name=node_labels[-1],
-                                   prev_node_names=node_labels[:-1],
-                                   in_ch=out_ch,  # TODO: change to self.channels
-                                   out_ch=out_ch,
-                                   stride=2 if reduction and out_ch < 2 else 1,
-                                   stem=stem))
-        self.final_conv_w = nn.Parameter(torch.zeros(out_ch, self.n_hidden + 2, out_ch, 1, 1),
+            self.nodes.append(Node(node_labels[-1], node_labels[:-1], out_channels))
+        self.final_conv_w = nn.Parameter(torch.zeros(out_channels, self.num_nodes + 2, out_channels, 1, 1),
                                          requires_grad=True)
+        self.bn = nn.BatchNorm2d(out_channels, affine=False)
+        self.reset_parameters()
 
-    def forward(self, x, x_prev):
-        if self.reduction_p:
-            relu_x = F.relu(x_prev)
-            x_p1 = self.p1(relu_x)
-            x_p2 = self.p2(relu_x)
-            x_prev = self.p_bn(torch.cat([x_p1, x_p2], 1))
-        else:
-            x_prev = self.conv_prev_1x1(x_prev)
-        x = self.conv_1x1(x)
-        prev_nodes_out = [x_prev, x]
-        nodes_used_mask = torch.zeros(self.n_hidden + 2, dtype=torch.bool, device=x_prev.device)
+    def reset_parameters(self):
+        nn.init.kaiming_normal_(self.final_conv_w)
+
+    def forward(self, prev, pprev):
+        if self.reduction:
+            pprev, prev = self.reduce0(pprev), self.reduce1(prev)
+        pprev_, prev_ = self.preproc0(pprev), self.preproc1(prev)
+
+        prev_nodes_out = [pprev_, prev_]
+        nodes_used_mask = torch.zeros(self.num_nodes + 2, dtype=torch.bool, device=prev.device)
         for i in range(self.num_nodes):
             node_out, mask = self.nodes[i](prev_nodes_out)
             nodes_used_mask[:mask.size(0)] |= mask.to(node_out.device)
             prev_nodes_out.append(node_out)
+
         unused_nodes = torch.cat([out for used, out in zip(nodes_used_mask, prev_nodes_out) if not used], 1)
         unused_nodes = F.relu(unused_nodes)
         conv_weight = self.final_conv_w[:, ~nodes_used_mask, :, :, :]
         conv_weight = conv_weight.view(conv_weight.size(0), -1, 1, 1)
         out = F.conv2d(unused_nodes, conv_weight)
-        return out
+        return prev, self.bn(out)
 
 
 class NasNet(nn.Module):
-    def __init__(self, num_stem_features, num_normal_cells, filters, scaling=2, n_hidden=5, num_classes=1000):
+    def __init__(self, num_blocks, num_cells, num_nodes=5, num_classes=10, in_channels=3, out_channels=24,
+                 dropout_rate=0.0):
         super(NasNet, self).__init__()
-        self.num_normal_cells = num_normal_cells
-        self.num_classes = num_classes
-        self.conv0 = nn.Sequential(OrderedDict([
-            ('conv', nn.Conv2d(3, num_stem_features, kernel_size=3, stride=2, bias=False)),
-            ('bn', nn.BatchNorm2d(num_stem_features, eps=0.001, momentum=0.1, affine=True))
-        ]))
-        filters_p = filters_pp = num_stem_features
-        self.conv0 = nn.Sequential(OrderedDict([
-            ('conv', nn.Conv2d(3, num_stem_features, kernel_size=3, stride=2, bias=False)),
-            ('bn', nn.BatchNorm2d(num_stem_features, eps=0.001, momentum=0.1, affine=True))
-        ]))
-        self.conv1 = nn.Linear(num_stem_features, filters_pp)
-        self.conv2 = nn.Linear(num_stem_features, filters_p)
-        #
-        # expand_normal = expand_normal
-        # expand_reduce = expand_reduce
-
+        self.stem = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels * 3, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels * 3)
+        )
+        self.dropout = nn.Dropout(dropout_rate)
+        c_pp = c_p = out_channels * 3
+        c_cur = out_channels
         self.layers = nn.ModuleList()
-        for block in range(3):
-            for _ in range(num_normal_cells - 1):
-                self.layers.append(NasNetCell(n_hidden, filters_p, filters_pp, filters))
-                filters_pp, filters_p = filters_p, filters
-            filters *= scaling
-            if block == 0 or block == 1:
-                self.layers.append(NasNetCell(n_hidden, filters_p, filters_pp, filters, reduction=True))
-                filters_p = filters
+        for block in range(num_blocks - 1):
+            for cell in range(num_cells - 1):
+                self.layers.append(NasNetCell(num_nodes, c_pp, c_p, c_cur))
+                c_pp, c_p = c_p, c_cur
+            self.layers.append(NasNetCell(num_nodes, c_pp, c_p, c_cur, reduction=True))
+            c_pp = c_p = c_cur
 
-        self.linear = nn.Linear(filters_p, self.num_classes)
+        for cell in range(num_cells - 1):
+            self.layers.append(NasNetCell(num_nodes, c_pp, c_p, c_cur))
+            c_pp, c_p = c_p, c_cur
+
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        self.dense = nn.Linear(c_cur, num_classes)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
 
     def forward(self, x):
-        x_conv0 = self.conv0(x)
-        x_stem_0 = self.cell_stem_0(x_conv0)
-        x_stem_1 = self.cell_stem_1(x_conv0)
+        bs = x.size(0)
+        prev = cur = self.stem(x)
 
-        prev_x, x = x_stem_0, x_stem_1
-        for block in range(3):
-            for layer in self.layers:
-                cur = layer(x, prev_x)
-                prev_x, x = x, cur
-        
-        output = self.logits(x)
-        return output
+        for layer in self.layers:
+            prev, cur = layer(prev, cur)
 
-    def logits(self, features):
-        x = F.relu(features, inplace=False)
-        x = F.avg_pool2d(x, kernel_size=x.size(2)).view(x.size(0), -1)
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = self.linear(x)
-        return x
+        cur = self.gap(F.relu(cur)).view(bs, -1)
+        cur = self.dropout(cur)
+        logits = self.dense(cur)
+        return logits
 
 
 def accuracy(output, target, topk=(1,)):
@@ -240,7 +211,7 @@ def train(model, device, train_loader, optimizer, epoch):
 
 
 if __name__ == "__main__":
-    model = NasNet(num_stem_features=32, num_normal_cells=4, filters=44, scaling=2)
+    model = NasNet(num_blocks=3, num_cells=4, dropout_rate=0.1)
     get_and_apply_next_architecture(model)
     num_epochs = 150
     criterion = nn.CrossEntropyLoss()
--- src/sdk/pynni/nni/nas/pytorch/search_space_zoo/nasnet_ops.py
+++ src/sdk/pynni/nni/nas/pytorch/search_space_zoo/nasnet_ops.py
@@ -7,23 +7,19 @@ class Pool(nn.Module):
     def __init__(self, pool_type, in_channels, out_channels, kernel_size, stride, padding,
                  affine=True, momentum=0.1, eps=0.001):
         super(Pool, self).__init__()
+        self.conv = StdConv(in_channels, out_channels)
         if pool_type.lower() == 'max':
             self.pool = nn.MaxPool2d(kernel_size=kernel_size, stride=stride, padding=padding)
         elif pool_type.lower() == 'avg':
             self.pool = nn.AvgPool2d(kernel_size=kernel_size, stride=stride, padding=padding)
         else:
             raise ValueError('Key pool_type in POol can only be \'max\' or \'avg\'')
-        self.conv, self.bn = None, None
-        if in_channels != out_channels:
-            self.conv = nn.Conv2d(in_channels, out_channels, 1, 1)
-            self.bn = nn.BatchNorm2d(out_channels, eps=eps, momentum=momentum, affine=affine)
+        self.bn = nn.BatchNorm2d(out_channels, affine=affine, momentum=momentum, eps=eps)
 
     def forward(self, x):
+        x = self.conv(x)
         x = self.pool(x)
-        if self.conv and self.bn:
-            x = self.conv(x)
-            x = self.bn(x)
-        return x
+        return self.bn(x)
 
 
 class Identity(nn.Module):
@@ -33,7 +29,7 @@ class Identity(nn.Module):
             self.op = nn.Identity()
         else:
             self.op = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, 1, stride),
+                StdConv(in_channels, out_channels),
                 nn.BatchNorm2d(out_channels, eps=eps, momentum=momentum, affine=affine)
             )
 
@@ -55,11 +51,11 @@ class StdConv(nn.Module):
     
     
 class Conv(nn.Module):
-    def __init__(self, in_channels, out_channels, dw_kernel, dw_stride, dw_padding, bias=False):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias=False):
         super(Conv, self).__init__()
-        self.depthwise_conv2d = nn.Conv2d(in_channels, in_channels, dw_kernel,
-                                          stride=dw_stride,
-                                          padding=dw_padding,
+        self.depthwise_conv2d = nn.Conv2d(in_channels, in_channels, kernel_size,
+                                          stride=stride,
+                                          padding=padding,
                                           bias=bias,
                                           groups=in_channels)
         self.pointwise_conv2d = nn.Conv2d(in_channels, out_channels, 1, stride=1, bias=bias)
@@ -76,7 +72,7 @@ class SeparableConv2d(nn.Module):
         self.in_channels = in_channels
         self.out_channels = out_channels
         mid_channels = out_channels if stem else in_channels
-        self.operation = nn.Sequential(
+        self.op = nn.Sequential(
             nn.ReLU(),
             Conv(in_channels, mid_channels, kernel_size, stride, padding, bias=bias),
             nn.BatchNorm2d(mid_channels, eps=eps, momentum=momentum, affine=affine),
@@ -85,5 +81,17 @@ class SeparableConv2d(nn.Module):
         )
 
     def forward(self, x):
-        return self.operation(x)
+        return self.op(x)
 
+
+class FactorizedReduce(nn.Module):
+    def __init__(self, C_in, C_out, affine=False):
+        super().__init__()
+        self.conv1 = nn.Conv2d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
+        self.conv2 = nn.Conv2d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
+        self.bn = nn.BatchNorm2d(C_out, affine=affine)
+
+    def forward(self, x):
+        out = torch.cat([self.conv1(x), self.conv2(x[:, :, 1:, 1:])], dim=1)
+        out = self.bn(out)
+        return out
