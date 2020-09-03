b3d1ac9b35b6ef6ec3bda576ec6921f1a1d03382
Ningxin <Ningxin.Zheng@microsoft.com>
2020-09-03 09:19:08 +0000

    update

--- src/sdk/pynni/nni/compression/torch/pruning/one_shot.py
+++ src/sdk/pynni/nni/compression/torch/pruning/one_shot.py
@@ -10,11 +10,11 @@ from ..utils.config_validation import CompressorSchema
 from ..compressor import Pruner
 
 
-__all__ = ['LevelPruner', 'SlimPruner', 'L1FilterPruner', 'L2FilterPruner', 'FPGMPruner', \
-    'TaylorFOWeightFilterPruner', 'ActivationAPoZRankFilterPruner', 'ActivationMeanRankFilterPruner', \
-    'Constrained_L1FilterPruner', 'Constrained_L2FilterPruner', 'ConstrainedActivationMeanRankFilterPruner']
+__all__ = ['LevelPruner', 'SlimPruner', 'L1FilterPruner', 'L2FilterPruner', 'FPGMPruner',
+           'TaylorFOWeightFilterPruner', 'ActivationAPoZRankFilterPruner', 'ActivationMeanRankFilterPruner']
 
 logger = logging.getLogger('torch pruner')
+
 
 class OneshotPruner(Pruner):
     """
@@ -39,7 +39,8 @@ class OneshotPruner(Pruner):
 
         super().__init__(model, config_list, optimizer)
         self.set_wrappers_attribute("if_calculated", False)
-        self.masker = MASKER_DICT[pruning_algorithm](model, self, **algo_kwargs)
+        self.masker = MASKER_DICT[pruning_algorithm](
+            model, self, **algo_kwargs)
 
     def validate_config(self, model, config_list):
         """
@@ -79,7 +80,8 @@ class OneshotPruner(Pruner):
 
         sparsity = wrapper.config['sparsity']
         if not wrapper.if_calculated:
-            masks = self.masker.calc_mask(sparsity=sparsity, wrapper=wrapper, wrapper_idx=wrapper_idx)
+            masks = self.masker.calc_mask(
+                sparsity=sparsity, wrapper=wrapper, wrapper_idx=wrapper_idx)
 
             # masker.calc_mask returns None means calc_mask is not calculated sucessfully, can try later
             if masks is not None:
@@ -87,6 +89,7 @@ class OneshotPruner(Pruner):
             return masks
         else:
             return None
+
 
 class LevelPruner(OneshotPruner):
     """
@@ -101,8 +104,10 @@ class LevelPruner(OneshotPruner):
     optimizer: torch.optim.Optimizer
             Optimizer used to train model
     """
+
     def __init__(self, model, config_list, optimizer=None):
         super().__init__(model, config_list, pruning_algorithm='level', optimizer=optimizer)
+
 
 class SlimPruner(OneshotPruner):
     """
@@ -117,6 +122,7 @@ class SlimPruner(OneshotPruner):
     optimizer: torch.optim.Optimizer
             Optimizer used to train model
     """
+
     def __init__(self, model, config_list, optimizer=None):
         super().__init__(model, config_list, pruning_algorithm='slim', optimizer=optimizer)
 
@@ -132,9 +138,32 @@ class SlimPruner(OneshotPruner):
         if len(config_list) > 1:
             logger.warning('Slim pruner only supports 1 configuration')
 
+
 class _StructuredFilterPruner(OneshotPruner):
-    def __init__(self, model, config_list, pruning_algorithm, optimizer=None, **algo_kwargs):
-        super().__init__(model, config_list, pruning_algorithm=pruning_algorithm, optimizer=optimizer, **algo_kwargs)
+    def __init__(self, model, config_list, pruning_algorithm, optimizer=None, dependency_aware=False, dummy_input=None, **algo_kwargs):
+        super().__init__(model, config_list, pruning_algorithm=pruning_algorithm,
+                         optimizer=optimizer, **algo_kwargs)
+        self.dependency_aware = dependency_aware
+        self.dummy_input = dummy_input
+        if self.dependency_aware:
+            errmsg = "When dependency_aware is set, the dummy_input should not be None"
+            assert self.dummy_input is not None, errmsg
+            # Get the TorchModuleGraph of the target model
+            # to trace the model, we need to unwrap the wrappers
+            self._unwrap_model()
+            self.graph = TorchModuleGraph(model, dummy_input)
+            self._wrap_model()
+            self.channel_depen = ChannelDependency(
+                traced_model=self.graph.trace)
+            self.group_depen = GroupDependency(traced_model=self.graph.trace)
+            self.channel_depen = self.channel_depen.dependency_sets
+            self.channel_depen = {
+                name: sets for sets in self.channel_depen for name in sets}
+            self.group_depen = self.group_depen.dependency_sets
+            # if prune the model in the dependency-aware way, the pruner will
+            # call the _dependency_update_mask rather than original update_mask
+            # function
+            setattr(self, 'update_mask', self._dependency_update_mask)
 
     def validate_config(self, model, config_list):
         schema = CompressorSchema([{
@@ -145,9 +174,69 @@ class _StructuredFilterPruner(OneshotPruner):
 
         schema.validate(config_list)
 
+    def _dependency_calc_mask(self, wrappers, channel_dsets, wrappers_idx=None):
+        """
+        calculate the masks for the conv layers in the same
+        channel dependecy set. All the layers passed in have
+        the same number of channels.
+
+        Parameters
+        ----------
+        wrappers : list
+            The list of the wrappers that in the same channel dependency
+            set.
+        wrappers_idx : list
+            The list of the indexes of wrapppers.
+        """
+        # The number of the groups for each conv layers
+        # Note that, this number may be different from its
+        # original number of groups of filters.
+        groups = [self.group_depen[_w.name] for _w in wrappers]
+        masks = self.masker.calc_mask(
+            wrappers, channel_dsets, groups, wrappers_idx=wrappers_idx)
+        if masks is not None:
+            # if masks is None, then the mask calculation fails.
+            # for example, in activation related maskers, we should
+            # pass enough batches of data to the model, so that the
+            # masks can be calculated successfully.
+            for _w in wrappers:
+                _w.if_calculated = True
+        return masks
+
+    def _dependency_update_mask(self):
+        """
+        In the original update_mask, the wraper of each layer will update its
+        mask own mask according to the sparsity specified in the config_list. However, in
+        the _dependency_update_mask, we may prune several layers at the same
+        time according the sparsities and the channel/group depedencies.
+        """
+        logger.info('Update the mask in a dependency-aware way')
+        name2wraper = {x.name: x for x in self.get_modules_wrapper()}
+        wraper2index = {x: i for i, x in enumerate(self.get_modules_wrapper())}
+        for wrapper in self.get_modules_wrapper():
+            if wrapper.if_calculated:
+                continue
+            # find all the conv layers that have channel dependecy with this layer
+            # and prune all these layers at the same time.
+            _names = [x for x in self.channel_depen[wrapper.name]]
+            logger.info('Pruning the dependent layers: %s', ','.join(_names))
+            _wrappers = [name2wraper[name]
+                         for name in _names if name in name2wraper]
+            _wrapper_idxes = [wraper2index[_w] for _w in _wrappers]
+
+            masks = self._dependency_calc_mask(
+                _wrappers, _names, wrappers_idx=_wrapper_idxes)
+            if masks is not None:
+                for layer in masks:
+                    for k in masks[layer]:
+                        assert hasattr(
+                            name2wraper[layer], k), "there is no attribute '%s' in wrapper on %s" % (k, layer)
+                        setattr(name2wraper[layer], k, masks[layer][k])
+
 class _Constrained_StructuredFilterPruner(OneshotPruner):
     def __init__(self, model, config_list, dummy_input, pruning_algorithm, optimizer=None, **algo_kwargs):
-        super().__init__(model, config_list, pruning_algorithm=pruning_algorithm, optimizer=optimizer, **algo_kwargs)
+        super().__init__(model, config_list, pruning_algorithm=pruning_algorithm,
+                         optimizer=optimizer, **algo_kwargs)
         # Get the TorchModuleGraph of the target model
         # to trace the model, we need to unwrap the wrappers
         self._unwrap_model()
@@ -156,7 +245,8 @@ class _Constrained_StructuredFilterPruner(OneshotPruner):
         self.channel_depen = ChannelDependency(traced_model=self.graph.trace)
         self.group_depen = GroupDependency(traced_model=self.graph.trace)
         self.channel_depen = self.channel_depen.dependency_sets
-        self.channel_depen = {name : sets for sets in self.channel_depen for name in sets}
+        self.channel_depen = {
+            name: sets for sets in self.channel_depen for name in sets}
         self.group_depen = self.group_depen.dependency_sets
 
     def validate_config(self, model, config_list):
@@ -186,7 +276,8 @@ class _Constrained_StructuredFilterPruner(OneshotPruner):
         # Note that, this number may be different from its
         # original number of groups of filters.
         groups = [self.group_depen[_w.name] for _w in wrappers]
-        masks = self.masker.calc_mask(wrappers, channel_dsets, groups, wrappers_idx=wrappers_idx)
+        masks = self.masker.calc_mask(
+            wrappers, channel_dsets, groups, wrappers_idx=wrappers_idx)
         if masks is not None:
             # if masks is None, then the mask calculation fails.
             # for example, in activation related maskers, we should
@@ -205,7 +296,7 @@ class _Constrained_StructuredFilterPruner(OneshotPruner):
         overwrite this function.
         """
         name2wraper = {x.name: x for x in self.get_modules_wrapper()}
-        wraper2index = {x:i for i, x in enumerate(self.get_modules_wrapper())}
+        wraper2index = {x: i for i, x in enumerate(self.get_modules_wrapper())}
         for wrapper in self.get_modules_wrapper():
             if wrapper.if_calculated:
                 continue
@@ -213,14 +304,17 @@ class _Constrained_StructuredFilterPruner(OneshotPruner):
             # and prune all these layers at the same time.
             _names = [x for x in self.channel_depen[wrapper.name]]
             logger.info('Pruning the dependent layers: %s', ','.join(_names))
-            _wrappers = [name2wraper[name] for name in _names if name in name2wraper]
+            _wrappers = [name2wraper[name]
+                         for name in _names if name in name2wraper]
             _wrapper_idxes = [wraper2index[_w] for _w in _wrappers]
 
-            masks = self.calc_mask(_wrappers, _names, wrappers_idx=_wrapper_idxes)
+            masks = self.calc_mask(
+                _wrappers, _names, wrappers_idx=_wrapper_idxes)
             if masks is not None:
                 for layer in masks:
                     for k in masks[layer]:
-                        assert hasattr(name2wraper[layer], k), "there is no attribute '%s' in wrapper on %s" % (k, layer)
+                        assert hasattr(
+                            name2wraper[layer], k), "there is no attribute '%s' in wrapper on %s" % (k, layer)
                         setattr(name2wraper[layer], k, masks[layer][k])
 
     def compress(self):
@@ -231,6 +325,7 @@ class _Constrained_StructuredFilterPruner(OneshotPruner):
         """
         self.update_mask()
         return self.bound_model
+
 
 class L1FilterPruner(_StructuredFilterPruner):
     """
@@ -244,36 +339,24 @@ class L1FilterPruner(_StructuredFilterPruner):
             - op_types : Only Conv2d is supported in L1FilterPruner.
     optimizer: torch.optim.Optimizer
             Optimizer used to train model
+    dependency_aware: bool
+        If prune the model in a dependency-aware way. If it is `True`, this pruner will
+        prune the model according to the l2-norm of weights and the channel-dependency or
+        group-dependency of the model. In this way, the pruner will force the conv layers
+        that have dependencies to prune the same channels, so the speedup module can better
+        harvest the speed benefit from the pruned model. Note that, if this flag is set True
+        , the dummy_input cannot be None, because the pruner needs a dummy input to trace the
+        dependency between the conv layers.
+    dummy_input : torch.Tensor
+        The dummy input to analyze the topology constraints. Note that, the dummy_input
+        should on the same device with the model.
     """
-    def __init__(self, model, config_list, optimizer=None):
-        print('Use L1FilterPruner')
-        # import sys
-        # sys.exit(-1)
-        super().__init__(model, config_list, pruning_algorithm='l1', optimizer=optimizer)
 
-class Constrained_L1FilterPruner(_Constrained_StructuredFilterPruner):
-    def __init__(self, model, config_list, dummy_input, optimizer=None):
-        """
-        A one-shot pruner that prunes the model according to the l2-norm
-        and the channel-dependency/group-dependency of the model. Because
-        this is a topology constraint aware pruner, so the speedup module
-        can better harvest the speed benefit from the pruned model.
+    def __init__(self, model, config_list, optimizer=None, dependency_aware=False, dummy_input=None):
+        pruning_algorithm = 'l1_constrained' if dependency_aware else 'l1'
+        super().__init__(model, config_list, pruning_algorithm=pruning_algorithm, optimizer=optimizer,
+                         dependency_aware=dependency_aware, dummy_input=dummy_input)
 
-        Paramters
-        ---------
-        model : torch.nn.Module
-            The target model to be pruned.
-        config_list : list
-            The configs that specify the sparsity for each layer.
-        dummy_input : torch.Tensor
-            The dummy input to analyze the topology constraints.
-        optimizer : torch.optim.Optimizer
-            The Optimizer used to train the model(if needed).
-        """
-        print('Use Constrained pruner')
-        # import sys
-        # sys.exit(-1)
-        super().__init__(model, config_list, dummy_input, pruning_algorithm='l1_constrained', optimizer=optimizer)
 
 class L2FilterPruner(_StructuredFilterPruner):
     """
@@ -287,9 +370,24 @@ class L2FilterPruner(_StructuredFilterPruner):
             - op_types : Only Conv2d is supported in L2FilterPruner.
     optimizer: torch.optim.Optimizer
             Optimizer used to train model
+    dependency_aware: bool
+        If prune the model in a dependency-aware way. If it is `True`, this pruner will
+        prune the model according to the l2-norm of weights and the channel-dependency or
+        group-dependency of the model. In this way, the pruner will force the conv layers
+        that have dependencies to prune the same channels, so the speedup module can better
+        harvest the speed benefit from the pruned model. Note that, if this flag is set True
+        , the dummy_input cannot be None, because the pruner needs a dummy input to trace the
+        dependency between the conv layers.
+    dummy_input : torch.Tensor
+        The dummy input to analyze the topology constraints. Note that, the dummy_input
+        should on the same device with the model.
     """
-    def __init__(self, model, config_list, optimizer=None):
-        super().__init__(model, config_list, pruning_algorithm='l2', optimizer=optimizer)
+
+    def __init__(self, model, config_list, optimizer=None, dependency_aware=False, dummy_input=None):
+        pruning_algorithm = 'l2_constrained' if dependency_aware else 'l2'
+        super().__init__(model, config_list, pruning_algorithm=pruning_algorithm, optimizer=optimizer,
+                         dependency_aware=dependency_aware, dummy_input=dummy_input)
+
 
 class Constrained_L2FilterPruner(_Constrained_StructuredFilterPruner):
     def __init__(self, model, config_list, dummy_input, optimizer=None):
@@ -311,7 +409,9 @@ class Constrained_L2FilterPruner(_Constrained_StructuredFilterPruner):
             The Optimizer used to train the model(if needed).
 
         """
-        super().__init__(model, config_list, dummy_input, pruning_algorithm='l2_constrained', optimizer=optimizer)
+        super().__init__(model, config_list, dummy_input,
+                         pruning_algorithm='l2_constrained', optimizer=optimizer)
+
 
 class FPGMPruner(_StructuredFilterPruner):
     """
@@ -326,8 +426,10 @@ class FPGMPruner(_StructuredFilterPruner):
     optimizer: torch.optim.Optimizer
             Optimizer used to train model
     """
+
     def __init__(self, model, config_list, optimizer=None):
         super().__init__(model, config_list, pruning_algorithm='fpgm', optimizer=optimizer)
+
 
 class TaylorFOWeightFilterPruner(_StructuredFilterPruner):
     """
@@ -342,8 +444,11 @@ class TaylorFOWeightFilterPruner(_StructuredFilterPruner):
     optimizer: torch.optim.Optimizer
             Optimizer used to train model
     """
+
     def __init__(self, model, config_list, optimizer=None, statistics_batch_num=1):
-        super().__init__(model, config_list, pruning_algorithm='taylorfo', optimizer=optimizer, statistics_batch_num=statistics_batch_num)
+        super().__init__(model, config_list, pruning_algorithm='taylorfo',
+                         optimizer=optimizer, statistics_batch_num=statistics_batch_num)
+
 
 class ActivationAPoZRankFilterPruner(_StructuredFilterPruner):
     """
@@ -358,9 +463,11 @@ class ActivationAPoZRankFilterPruner(_StructuredFilterPruner):
     optimizer: torch.optim.Optimizer
             Optimizer used to train model
     """
+
     def __init__(self, model, config_list, optimizer=None, activation='relu', statistics_batch_num=1):
-        super().__init__(model, config_list, pruning_algorithm='apoz', optimizer=optimizer, \
-            activation=activation, statistics_batch_num=statistics_batch_num)
+        super().__init__(model, config_list, pruning_algorithm='apoz', optimizer=optimizer,
+                         activation=activation, statistics_batch_num=statistics_batch_num)
+
 
 class ActivationMeanRankFilterPruner(_StructuredFilterPruner):
     """
@@ -375,11 +482,13 @@ class ActivationMeanRankFilterPruner(_StructuredFilterPruner):
     optimizer: torch.optim.Optimizer
             Optimizer used to train model
     """
+
     def __init__(self, model, config_list, optimizer=None, activation='relu', statistics_batch_num=1):
-        super().__init__(model, config_list, pruning_algorithm='mean_activation', optimizer=optimizer, \
-            activation=activation, statistics_batch_num=statistics_batch_num)
+        super().__init__(model, config_list, pruning_algorithm='mean_activation', optimizer=optimizer,
+                         activation=activation, statistics_batch_num=statistics_batch_num)
+
 
 class ConstrainedActivationMeanRankFilterPruner(_Constrained_StructuredFilterPruner):
     def __init__(self, model, config_list, dummy_input, optimizer=None, activation='relu', statistics_batch_num=1):
-        super().__init__(model, config_list, dummy_input, pruning_algorithm='mean_activation_constrained', optimizer=optimizer, \
-            activation=activation, statistics_batch_num=statistics_batch_num)
+        super().__init__(model, config_list, dummy_input, pruning_algorithm='mean_activation_constrained', optimizer=optimizer,
+                         activation=activation, statistics_batch_num=statistics_batch_num)
