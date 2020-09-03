cc9f336935463093df378e421281e441d8b80c14
Houwen Peng <49014385+penghouwen@users.noreply.github.com>
2020-09-03 22:06:21 +0800

    Update distributed_train.sh

--- examples/nas/cream/distributed_train.sh
+++ examples/nas/cream/distributed_train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NUM_PROC=$1
 shift
-python -m torch.distributed.launch --nproc_per_node=$NUM_PROC ./examples/nas/cream/supernet.py "$@"
+python3 -m torch.distributed.launch --nproc_per_node=$NUM_PROC ./examples/nas/cream/supernet.py "$@"
