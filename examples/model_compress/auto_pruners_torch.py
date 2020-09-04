0920efeafee658966b673b986dfe1377db2fdc7e
Ningxin <Ningxin.Zheng@microsoft.com>
2020-09-04 02:39:29 +0000

    reset the auto_pruners_torch.py

--- examples/model_compress/auto_pruners_torch.py
+++ examples/model_compress/auto_pruners_torch.py
@@ -8,28 +8,23 @@ import argparse
 import os
 import json
 import torch
-import torchvision
-from torch.optim.lr_scheduler import StepLR, MultiStepLR, CosineAnnealingLR
+from torch.optim.lr_scheduler import StepLR, MultiStepLR
 from torchvision import datasets, transforms
 
 from models.mnist.lenet import LeNet
 from models.cifar10.vgg import VGG
-from models.cifar10.mobilenetv2 import MobileNetV2
 from models.cifar10.resnet import ResNet18, ResNet50
-import nni
 from nni.compression.torch import L1FilterPruner, L2FilterPruner, FPGMPruner
-from nni.compression.torch import Constrained_L1FilterPruner, Constrained_L2FilterPruner
-from nni.compression.torch import AttentionActivationPruner
+from nni.compression.torch import SimulatedAnnealingPruner, ADMMPruner, NetAdaptPruner, AutoCompressPruner
 from nni.compression.torch import ModelSpeedup
 from nni.compression.torch.utils.counter import count_flops_params
-from nni.compression.torch.utils.shape_dependency import ChannelDependency
 
 
 def get_data(dataset, data_dir, batch_size, test_batch_size):
     '''
     get data
     '''
-    kwargs = {'num_workers': 6, 'pin_memory': True} if torch.cuda.is_available() else {
+    kwargs = {'num_workers': 1, 'pin_memory': True} if torch.cuda.is_available() else {
     }
 
     if dataset == 'mnist':
@@ -145,22 +140,7 @@ def get_trained_model_optimizer(args, device, train_loader, val_loader, criterio
             optimizer = torch.optim.Adadelta(model.parameters(), lr=1)
             scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
     elif args.model == 'vgg16':
-        if args.dataset == 'cifar10':
-            model = VGG(depth=16).to(device)
-        elif args.dataset == 'imagenet':
-            model = torchvision.models.vgg16(pretrained=True).to(device)
-        if args.load_pretrained_model:
-            model.load_state_dict(torch.load(args.pretrained_model_dir))
-            optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, momentum=0.9, weight_decay=5e-4)
-        else:
-            optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
-            scheduler = MultiStepLR(
-                optimizer, milestones=[int(args.pretrain_epochs*0.5), int(args.pretrain_epochs*0.75)], gamma=0.1)
-    elif args.model == 'mobilenet_v2':
-        if args.dataset == 'cifar10':
-            model = MobileNetV2().to(device)
-        elif args.dataset == 'imagenet':
-            model = torchvision.models.mobilenet_v2(pretrained=True).to(device)
+        model = VGG(depth=16).to(device)
         if args.load_pretrained_model:
             model.load_state_dict(torch.load(args.pretrained_model_dir))
             optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, momentum=0.9, weight_decay=5e-4)
@@ -169,10 +149,7 @@ def get_trained_model_optimizer(args, device, train_loader, val_loader, criterio
             scheduler = MultiStepLR(
                 optimizer, milestones=[int(args.pretrain_epochs*0.5), int(args.pretrain_epochs*0.75)], gamma=0.1)
     elif args.model == 'resnet18':
-        if args.dataset == 'cifar10':
-            model = ResNet18().to(device)
-        elif args.dataset == 'imagenet':
-            model = torchvision.models.resnet18(pretrained=True).to(device)
+        model = ResNet18().to(device)
         if args.load_pretrained_model:
             model.load_state_dict(torch.load(args.pretrained_model_dir))
             optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, momentum=0.9, weight_decay=5e-4)
@@ -181,10 +158,7 @@ def get_trained_model_optimizer(args, device, train_loader, val_loader, criterio
             scheduler = MultiStepLR(
                 optimizer, milestones=[int(args.pretrain_epochs*0.5), int(args.pretrain_epochs*0.75)], gamma=0.1)
     elif args.model == 'resnet50':
-        if args.dataset == 'cifar10':
-            model = ResNet50().to(device)
-        elif args.dataset == 'imagenet':
-            model = torchvision.models.resnet50(pretrained=True).to(device)
+        model = ResNet50().to(device)
         if args.load_pretrained_model:
             model.load_state_dict(torch.load(args.pretrained_model_dir))
             optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, momentum=0.9, weight_decay=5e-4)
@@ -195,10 +169,9 @@ def get_trained_model_optimizer(args, device, train_loader, val_loader, criterio
     else:
         raise ValueError("model not recognized")
 
-    if not args.load_pretrained_model and args.pretrain_epochs > 0:
+    if not args.load_pretrained_model:
         best_acc = 0
         best_epoch = 0
-        state_dict = None
         for epoch in range(args.pretrain_epochs):
             train(args, model, device, train_loader, criterion, optimizer, epoch)
             scheduler.step()
@@ -207,49 +180,32 @@ def get_trained_model_optimizer(args, device, train_loader, val_loader, criterio
                 best_acc = acc
                 best_epoch = epoch
                 state_dict = model.state_dict()
-        if state_dict:
-            model.load_state_dict(state_dict)
-    
+        model.load_state_dict(state_dict)
         print('Best acc:', best_acc)
         print('Best epoch:', best_epoch)
 
         if args.save_model:
             torch.save(state_dict, os.path.join(args.experiment_data_dir, 'model_trained.pth'))
             print('Model trained saved to %s', args.experiment_data_dir)
-    acc = test(model, device, criterion, val_loader)
-    print('Original Accuracy before the pruning', acc)
+
     return model, optimizer
 
 
 def get_dummy_input(args, device):
     if args.dataset == 'mnist':
         dummy_input = torch.randn([args.test_batch_size, 1, 28, 28]).to(device)
-    elif args.dataset == 'cifar10':
+    elif args.dataset in ['cifar10', 'imagenet']:
         dummy_input = torch.randn([args.test_batch_size, 3, 32, 32]).to(device)
-    elif args.dataset == 'imagenet':
-        dummy_input = torch.randn([args.test_batch_size, 3, 224, 224]).to(device)
     return dummy_input
 
-def only_no_dependency(model, dummy_input):
-    cd = ChannelDependency(model, dummy_input)
-    c_dsets = cd.dependency_sets
-    no_depen_layers = []
-    for dset in c_dsets:
-        if len(dset) > 1:
-            print('#'*10)
-            print('skip', dset)
-            continue
-        for layer in dset:
-            no_depen_layers.append(layer)
-    return no_depen_layers
 
 def get_input_size(dataset):
     if dataset == 'mnist':
         input_size = (1, 1, 28, 28)
-    elif dataset in ['cifar10']:
+    elif dataset == 'cifar10':
         input_size = (1, 3, 32, 32)
     elif dataset == 'imagenet':
-        input_size = (1, 3, 224, 224)
+        input_size = (1, 3, 256, 256)
     return input_size
 
 
@@ -260,9 +216,8 @@ def main(args):
     train_loader, val_loader, criterion = get_data(args.dataset, args.data_dir, args.batch_size, args.test_batch_size)
     model, optimizer = get_trained_model_optimizer(args, device, train_loader, val_loader, criterion)
 
-    def short_term_fine_tuner(model, epochs=args.short_term_finetune):
+    def short_term_fine_tuner(model, epochs=1):
         for epoch in range(epochs):
-            print('Short term finetune epoch :', epoch)
             train(args, model, device, train_loader, criterion, optimizer, epoch)
 
     def trainer(model, optimizer, criterion, epoch, callback):
@@ -287,23 +242,12 @@ def main(args):
         op_types = ['Conv2d']
     elif args.base_algo == 'level':
         op_types = ['default']
-    dummy_input = get_dummy_input(args, device)
+
     config_list = [{
         'sparsity': args.sparsity,
         'op_types': op_types
     }]
-
-    if args.only_no_dependency:
-        no_depen_layers = only_no_dependency(model, dummy_input)
-        config_list = []
-        for layer in no_depen_layers:
-            config_list.append({'sparsity': args.sparsity, 'op_types': op_types, 'op_names':[layer]})
-        print('$'*20)
-        print('Only prune the layer with no dependency')
-        print(config_list)
-    if args.sparsity_config:
-        config_list = json.load(args.sparsity_config)
-
+    dummy_input = get_dummy_input(args, device)
 
     if args.pruner == 'L1FilterPruner':
         pruner = L1FilterPruner(model, config_list)
@@ -314,12 +258,6 @@ def main(args):
     elif args.pruner == 'NetAdaptPruner':
         pruner = NetAdaptPruner(model, config_list, short_term_fine_tuner=short_term_fine_tuner, evaluator=evaluator,
                                 base_algo=args.base_algo, experiment_data_dir=args.experiment_data_dir)
-    elif args.pruner == 'AMCPruner':
-        def amc_evaluator(val_loader, model):
-            return test(model, device, criterion, val_loader) 
-        pruner = AMCPruner(model, config_list, amc_evaluator, val_loader, flops_ratio=args.sparsity)
-    elif args.pruner == 'AttentionPruner':
-        pruner = AttentionActivationPruner(model, config_list, dummy_input, optimizer, evaluator, short_term_fine_tuner)
     elif args.pruner == 'ADMMPruner':
         # users are free to change the config here
         if args.model == 'LeNet':
@@ -352,7 +290,7 @@ def main(args):
         pruner = ADMMPruner(model, config_list, trainer=trainer, num_iterations=2, training_epochs=2)
     elif args.pruner == 'SimulatedAnnealingPruner':
         pruner = SimulatedAnnealingPruner(
-            model, config_list, evaluator=evaluator, dummy_input=dummy_input,  base_algo=args.base_algo,
+            model, config_list, evaluator=evaluator, base_algo=args.base_algo,
             cool_down_rate=args.cool_down_rate, experiment_data_dir=args.experiment_data_dir)
     elif args.pruner == 'AutoCompressPruner':
         pruner = AutoCompressPruner(
@@ -363,10 +301,7 @@ def main(args):
     else:
         raise ValueError(
             "Pruner not supported.")
-    if args.constrained:
-        pruner.constrained = True
-    else:
-        pruner.constrained = False
+
     # Pruner.compress() returns the masked model
     # but for AutoCompressPruner, Pruner.compress() returns directly the pruned model
     model = pruner.compress()
@@ -390,8 +325,7 @@ def main(args):
                 model = ResNet18().to(device)
             elif args.model == 'resnet50':
                 model = ResNet50().to(device)
-            elif args.model == 'mobilenet_v2':
-                model = MobileNetV2().to(device)
+
             model.load_state_dict(torch.load(os.path.join(args.experiment_data_dir, 'model_masked.pth')))
             masks_file = os.path.join(args.experiment_data_dir, 'mask.pth')
 
@@ -411,23 +345,20 @@ def main(args):
         if args.dataset == 'mnist':
             optimizer = torch.optim.Adadelta(model.parameters(), lr=1)
             scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
-        elif args.model == 'vgg16':
-            optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-        elif args.model == 'resnet18':
-            optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-        elif args.model == 'resnet50':
-            optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-        elif args.model == 'mobilenet_v2':
-            optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-        if args.lr_decay == 'multistep':
+        elif args.dataset == 'cifar10' and args.model == 'vgg16':
+            optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
             scheduler = MultiStepLR(
-                optimizer, milestones=[int(args.fine_tune_epochs*0.25), int(args.fine_tune_epochs*0.5), int(args.fine_tune_epochs*0.75)], gamma=0.1)
-        elif args.lr_decay == 'cos':
-            scheduler = CosineAnnealingLR(optimizer, T_max=args.fine_tune_epochs)
+                optimizer, milestones=[int(args.fine_tune_epochs*0.5), int(args.fine_tune_epochs*0.75)], gamma=0.1)
+        elif args.dataset == 'cifar10' and args.model == 'resnet18':
+            optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+            scheduler = MultiStepLR(
+                optimizer, milestones=[int(args.fine_tune_epochs*0.5), int(args.fine_tune_epochs*0.75)], gamma=0.1)
+        elif args.dataset == 'cifar10' and args.model == 'resnet50':
+            optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+            scheduler = MultiStepLR(
+                optimizer, milestones=[int(args.fine_tune_epochs*0.5), int(args.fine_tune_epochs*0.75)], gamma=0.1)
         best_acc = 0
         for epoch in range(args.fine_tune_epochs):
-            acc = evaluator(model)
-            print("acc at the begining", acc)
             train(args, model, device, train_loader, criterion, optimizer, epoch)
             scheduler.step()
             acc = evaluator(model)
@@ -456,7 +387,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PyTorch Example for SimulatedAnnealingPruner')
 
     # dataset and model
-    parser.add_argument('--dataset', type=str, default='imagenet',
+    parser.add_argument('--dataset', type=str, default='cifar10',
                         help='dataset to use, mnist, cifar10 or imagenet')
     parser.add_argument('--data-dir', type=str, default='./data/',
                         help='dataset directory')
@@ -478,7 +409,7 @@ if __name__ == '__main__':
                         help='epochs to fine tune')
     parser.add_argument('--experiment-data-dir', type=str, default='./experiment_data',
                         help='For saving experiment data')
-    parser.add_argument('--sparsity_config', type=str, default=None, help='the path of the sparsity config file')
+
     # pruner
     parser.add_argument('--pruner', type=str, default='SimulatedAnnealingPruner',
                         help='pruner to use')
@@ -502,32 +433,10 @@ if __name__ == '__main__':
                         help='how many batches to wait before logging training status')
     parser.add_argument('--save-model', type=str2bool, default=True,
                         help='For Saving the current Model')
-    parser.add_argument('--constrained', type=str2bool, default=False, help='if enable the constraint-aware pruner')
-    parser.add_argument('--lr', type=float, default=0.01, help='The learning rate for the finetuning')
-    parser.add_argument('--lr_decay', type=str, default='multistep', help='lr_decay type')
-    parser.add_argument('--short_term_finetune', type=int, default=20, help='the short term finetune epochs')
-    parser.add_argument('--only_no_dependency', default=False, type=str2bool, help='If only prune the layers that have no dependency with others')
+
     args = parser.parse_args()
 
     if not os.path.exists(args.experiment_data_dir):
         os.makedirs(args.experiment_data_dir)
-    # hack the orignal L1FilterPruner
-    dummy_input = get_dummy_input(args, 'cuda')
-    class tmp_l1_constrained(Constrained_L1FilterPruner):
-        def __init__(self, model, config_list, dummy_input=dummy_input, optimizer=None):
-            super(tmp_l1_constrained, self).__init__(model, config_list, dummy_input, optimizer)
-
-    class tmp_l2_constrained(Constrained_L2FilterPruner):
-        def __init__(self, model, config_list, dummy_input=dummy_input, optimizer=None):
-            super(tmp_l2_constrained, self).__init__(model, config_list, dummy_input, optimizer)
-    from nni.compression.torch.pruning.constants_pruner import PRUNER_DICT
-    if args.constrained:
-        PRUNER_DICT['l1'] = tmp_l1_constrained
-        PRUNER_DICT['l2'] = tmp_l2_constrained
-        setattr(nni.compression.torch, 'L1FilterPruner', tmp_l1_constrained)
-        setattr(nni.compression.torch, 'L2FilterPruner', tmp_l2_constrained)
-        L1FilterPruner = tmp_l1_constrained
-        L2FilterPruner = tmp_l2_constrained
-    from nni.compression.torch import SimulatedAnnealingPruner, ADMMPruner, NetAdaptPruner, AutoCompressPruner, AMCPruner
 
     main(args)
