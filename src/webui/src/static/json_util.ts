08621ae0e0f7a9e7c1438b682d92687c4e5487eb
liuzhe <zhe.liu@microsoft.com>
2020-09-11 14:01:03 +0800

    configurable indent, and no collapse nested

--- src/webui/src/static/json_util.ts
+++ src/webui/src/static/json_util.ts
@@ -13,7 +13,8 @@ const batchThreshold = 0.5;
  *  which will add `"key": ` before each stringified object.
  *
  *  @param objects  Objects to be stringify.
- *  @param indent  Spaces that should be prepended to each line.
+ *  @param curIndent  Spaces that should be prepended to each line.
+ *  @param indent  Extra spaces to add for each level of indentation.
  *  @param width  Expected width of text block. This is only a hint, not hard limit.
  *  @param keyOrKeys  Array of keys for each object,
  *      or a single string as the same key of all objects,
@@ -21,7 +22,14 @@ const batchThreshold = 0.5;
  *
  *  @returns  Formatted string for each object, without trailing comma.
  **/
-function batchFormat(objects: any[], indent: string, width: number, keyOrKeys?: string | string[]): string[] {
+function batchFormat(
+    objects: any[],
+    curIndent: string,
+    indent: string,
+    width: number,
+    keyOrKeys?: string | string[]
+): string[] {
+
     let keys: string[];  // dict key as prefix string
     if (keyOrKeys === undefined) {
         keys = objects.map(() => '');
@@ -33,9 +41,6 @@ function batchFormat(objects: any[], indent: string, width: number, keyOrKeys?: 
 
     // try to collapse all
     const lines = objects.map((obj, i) => keys[i] + stringifySingleLine(obj));
-    if (lines.every(line => (line.length + indent.length < width))) {
-        return lines;
-    }
 
     // null values don't affect hierarchy detection
     const nonNull = objects.filter(obj => obj !== null);
@@ -43,15 +48,21 @@ function batchFormat(objects: any[], indent: string, width: number, keyOrKeys?: 
         return lines;
     }
 
+    const hasNested = nonNull.some(obj => detectNested(obj));
+
+    if (!hasNested && lines.every(line => (line.length + curIndent.length < width))) {
+        return lines;
+    }
+
     if (Array.isArray(nonNull[0])) {
         // objects are arrays, format all items in one batch
-        const elements = batchFormat(concat(nonNull), indent + '    ', width);
+        const elements = batchFormat(concat(nonNull), curIndent + indent, indent, width);
         const iter = elements[Symbol.iterator]();
         return objects.map((obj, i) => {
             if (obj === null) {
                 return keys[i] + 'null';
             } else {
-                return keys[i] + createBlock(indent, '[]', obj.map(() => iter.next().value));
+                return keys[i] + createBlock(curIndent, indent, '[]', obj.map(() => iter.next().value));
             }
         });
     }
@@ -62,13 +73,13 @@ function batchFormat(objects: any[], indent: string, width: number, keyOrKeys?: 
         const childrenKeys = concat(nonNull.map(obj => Object.keys(obj)));
         if (detectBatch(values)) {
             // these objects look like TypeScript style `Map` or `Record`, where the values have same "type"
-            const elements = batchFormat(values, indent + '    ', width, childrenKeys);
+            const elements = batchFormat(values, curIndent + indent, indent, width, childrenKeys);
             const iter = elements[Symbol.iterator]();
             return objects.map((obj, i) => {
                 if (obj === null) {
                     return keys[i] + 'null';
                 } else {
-                    return keys[i] + createBlock(indent, '{}', Object.keys(obj).map(() => iter.next().value));
+                    return keys[i] + createBlock(curIndent, indent, '{}', Object.keys(obj).map(() => iter.next().value));
                 }
             });
 
@@ -76,13 +87,13 @@ function batchFormat(objects: any[], indent: string, width: number, keyOrKeys?: 
             // these objects look like class instances, so we will try to group their fields
             const uniqueKeys = new Set(childrenKeys);
             const iters = new Map();
-            for (let key of uniqueKeys) {
+            for (const key of uniqueKeys) {
                 const fields = nonNull.map(obj => obj[key]).filter(v => v !== undefined);
                 let elements;
                 if (detectBatch(fields)) {  // look like same field of class instances
-                    elements = batchFormat(fields, indent + '    ', width, key);
+                    elements = batchFormat(fields, curIndent + indent, indent, width, key);
                 } else {  // no idea what these are, fallback to format them independently
-                    elements = fields.map(field => batchFormat([field], indent + '    ', width, key));
+                    elements = fields.map(field => batchFormat([field], curIndent + indent, indent, width, key));
                 }
                 iters.set(key, elements[Symbol.iterator]());
             }
@@ -91,7 +102,7 @@ function batchFormat(objects: any[], indent: string, width: number, keyOrKeys?: 
                     return keys[i] + 'null';
                 } else {
                     const elements = Object.keys(obj).map(key => iters.get(key).next().value);
-                    return keys[i] + createBlock(indent, '{}', elements);
+                    return keys[i] + createBlock(curIndent, indent, '{}', elements);
                 }
             });
         }
@@ -137,26 +148,30 @@ function detectBatch(objects: any[]): boolean {
     return sameType(nonNull);
 }
 
+function detectNested(obj: any): boolean {
+    return typeof(obj) == 'object' && Object.values(obj).some(child => typeof(child) == 'object');
+}
+
 function concat(arrays: any[][]): any[] {
     return ([] as any[]).concat(...arrays);
 }
 
-function createBlock(indent: string, brackets: string, elements: string[]): string {
+function createBlock(curIndent: string, indent: string, brackets: string, elements: string[]): string {
     if (elements.length === 0) {
         return brackets;
     }
-    const head = brackets[0] + '\n    ' + indent;
-    const lineSeparator = ',\n    ' + indent;
-    const tail = '\n' + indent + brackets[1];
+    const head = brackets[0] + '\n' + curIndent + indent;
+    const lineSeparator = ',\n' + curIndent + indent;
+    const tail = '\n' + curIndent + brackets[1];
     return head + elements.join(lineSeparator) + tail;
 }
 
-function sameType(objects: any[]) {
+function sameType(objects: any[]): boolean {
     const nonNull = objects.filter(obj => obj !== undefined);
     return nonNull.length > 0 ? nonNull.every(obj => (typeof obj === typeof nonNull[0])) : true;
 }
 
-function stringifySingleLine(obj: any) {
+function stringifySingleLine(obj: any): string {
     if (obj === null) {
         return 'null';
     } else if (typeof obj === 'number' || typeof obj === 'boolean') {
@@ -170,8 +185,8 @@ function stringifySingleLine(obj: any) {
     }
 }
 
-function prettyStringify(obj: any, width: number): string {
-    return batchFormat([obj], '', width)[0];
+function prettyStringify(obj: any, width: number, indent: number): string {
+    return batchFormat([obj], '', ' '.repeat(indent), width)[0];
 }
 
 export { prettyStringify };
