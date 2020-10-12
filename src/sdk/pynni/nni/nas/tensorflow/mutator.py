404669ec9f5e4b83b37294f1d0d3895c4d8e70d7
Duong Nhu <d.binhnhu@gmail.com>
2020-10-12 16:31:02 +1100

    Udpated concat axis to match image_data_format in keras

--- src/sdk/pynni/nni/nas/tensorflow/mutator.py
+++ src/sdk/pynni/nni/nas/tensorflow/mutator.py
@@ -66,7 +66,12 @@ class Mutator(BaseMutator):
         if reduction_type == 'mean':
             return sum(tensor_list) / len(tensor_list)
         if reduction_type == 'concat':
-            return tf.concat(tensor_list, axis=0)
+            image_data_format = tf.keras.backend.image_data_format()
+            if image_data_format == "channels_first":
+                axis = 0
+            else:
+                axis = -1
+            return tf.concat(tensor_list, axis=axis)
         raise ValueError('Unrecognized reduction policy: "{}'.format(reduction_type))
 
     def _get_decision(self, mutable):
