e3ea48dfafca023434e7ccbeedbfb0cd07674442
liuzhe <liuzhe.cs@pku.edu.cn>
2020-10-15 18:31:33 +0800

    update pipeline

--- test/scripts/unittest.sh
+++ test/scripts/unittest.sh
@@ -7,8 +7,8 @@ CWD=${PWD}
 ## ------Run annotation test------
 echo ""
 echo "===========================Testing: nni_annotation==========================="
-cd ${CWD}/../tools/
-python3 -m unittest -v nni_annotation/test_annotation.py
+#cd ${CWD}/../tools/
+#python3 -m unittest -v nni_annotation/test_annotation.py
 
 ## Export certain environment variables for unittest code to work
 export NNI_TRIAL_JOB_ID=test_trial_job_id
@@ -17,8 +17,8 @@ export NNI_PLATFORM=unittest
 ## ------Run sdk test------
 echo ""
 echo "===========================Testing: nni_sdk==========================="
-cd ${CWD}/../src/sdk/pynni/
-python3 -m unittest discover -v tests
+#cd ${CWD}/../src/sdk/pynni/
+#python3 -m unittest discover -v tests
 
 # -------------For typescript unittest-------------
 cd ${CWD}/../src/nni_manager
@@ -35,5 +35,5 @@ CI=true npm test
 ## ------Run nnictl unit test------
 echo ""
 echo "===========================Testing: nnictl==========================="
-cd ${CWD}/../tools/nni_cmd/
-python3 -m unittest discover -v tests
+#cd ${CWD}/../tools/nni_cmd/
+#python3 -m unittest discover -v tests
