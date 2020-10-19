73860d97f898a10f64b891e64e2f02d7271cedae
liuzhe <liuzhe.cs@pku.edu.cn>
2020-10-20 07:10:23 +0800

    update python package path in ts

--- ts/nni_manager/training_service/common/gpuData.ts
+++ ts/nni_manager/training_service/common/gpuData.ts
@@ -79,6 +79,6 @@ export function parseGpuIndices(gpuIndices?: string): Set<number> | undefined {
 export const GPU_INFO_COLLECTOR_FORMAT_WINDOWS: string =
     `
 $env:METRIC_OUTPUT_DIR="{0}"
-$app = Start-Process "python" -ArgumentList "-m nni_gpu_tool.gpu_metrics_collector" -passthru -NoNewWindow
+$app = Start-Process "python" -ArgumentList "-m nni.tools.gpu_tool.gpu_metrics_collector" -passthru -NoNewWindow
 Write $app.ID | Out-File {1} -NoNewline -encoding utf8
 `;
