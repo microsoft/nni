f73690055d0046dea93cde0605f7852d311cf5ff
Peng Wu <pengwu100@gmail.com>
2020-10-23 11:24:51 -0400

    skipping like kubeflowtest

--- src/nni_manager/training_service/test/adlTrainingService.test.ts
+++ src/nni_manager/training_service/test/adlTrainingService.test.ts
@@ -8,18 +8,22 @@ import * as chaiAsPromised from 'chai-as-promised';
 import * as fs from 'fs';
 import * as tmp from 'tmp';
 import * as component from '../../common/component';
-// import { TrialJobApplicationForm, TrialJobDetail, TrainingService } from '../../common/trainingService';
+import { TrialJobApplicationForm, TrialJobDetail, TrainingService } from '../../common/trainingService';
 import { cleanupUnitTest, prepareUnitTest } from '../../common/utils';
 import { TrialConfigMetadataKey } from '../common/trialConfigMetadataKey';
 import { AdlTrainingService } from '../kubernetes/adl/adlTrainingService';
 
-// TODO: copy mockedTrail.py to local folder
 const localCodeDir: string = tmp.dirSync().name
-const mockedTrialPath: string = './training_service/test/mockedTrial.py'
-fs.copyFileSync(mockedTrialPath, localCodeDir + '/mockedTrial.py')
 
 describe('Unit Test for AdlTrainingService', () => {
     let skip: boolean = false;
+    try {
+        const testKubeflowConfig = fs.readFileSync('/home/vsts/.kube/config', 'utf8');
+    } catch (err) {
+        console.log('Please have kubernetes cluster to enable its training service unit test.');
+        skip = true;
+    }
+
     let testAdlTrialConfig: any = JSON.stringify({
         "command": "python3 /root/apps/nni_linear_regression/main.py",
         "codeDir": ".",
@@ -76,47 +80,59 @@ describe('Unit Test for AdlTrainingService', () => {
     });
 
     beforeEach(() => {
+        if (skip) {
+            return;
+        }
         adlTrainingService = component.get(AdlTrainingService);
         adlTrainingService.run()
     });
 
     afterEach(() => {
+        if (skip) {
+            return;
+        }
         adlTrainingService.cleanUp();
     });
 
     it('Set and get cluster metadata', async () => {
+        if (skip) {
+            return;
+        }
         await adlTrainingService.setClusterMetadata(TrialConfigMetadataKey.TRIAL_CONFIG, testAdlTrialConfig2);
         await adlTrainingService.setClusterMetadata(TrialConfigMetadataKey.NNI_MANAGER_IP, testNniManagerIp);
         let data:string = await adlTrainingService.getClusterMetadata(TrialConfigMetadataKey.TRIAL_CONFIG);
         chai.expect(data).to.be.equals(testAdlTrialConfig2);
     });
 
-    // it('Submit job', async () => {
-    //     // job without given checkpoint, with resource config
-    //     await adlTrainingService.setClusterMetadata(TrialConfigMetadataKey.TRIAL_CONFIG, testAdlTrialConfig);
-    //     let form: TrialJobApplicationForm = {
-    //         sequenceId: 0,
-    //         hyperParameters: {
-    //             value: 'mock hyperparameters',
-    //             index: 0
-    //         }
-    //     };
-    //     let jobDetail: TrialJobDetail = await adlTrainingService.submitTrialJob(form);
-    //     chai.expect(jobDetail.status).to.be.equals('WAITING');
-    //     await adlTrainingService.cancelTrialJob(jobDetail.id);
-    //     chai.expect(jobDetail.status).to.be.equals('USER_CANCELED');
-    //     // job with given checkpoint
-    //     await adlTrainingService.setClusterMetadata(TrialConfigMetadataKey.TRIAL_CONFIG, testAdlTrialConfig2);
-    //     form = {
-    //         sequenceId: 0,
-    //         hyperParameters: {
-    //             value: 'mock hyperparameters',
-    //             index: 0
-    //         }
-    //     };
-    //     jobDetail = await adlTrainingService.submitTrialJob(form);
-    //     chai.expect(jobDetail.status).to.be.equals('WAITING');
-    //     await adlTrainingService.cancelTrialJob(jobDetail.id);
-    //     chai.expect(jobDetail.status).to.be.equals('USER_CANCELED');
-    // }).timeout(3000000);
+    it('Submit job', async () => {
+        if (skip) {
+            return;
+        }
+        // job without given checkpoint, with resource config
+        await adlTrainingService.setClusterMetadata(TrialConfigMetadataKey.TRIAL_CONFIG, testAdlTrialConfig);
+        let form: TrialJobApplicationForm = {
+            sequenceId: 0,
+            hyperParameters: {
+                value: 'mock hyperparameters',
+                index: 0
+            }
+        };
+        let jobDetail: TrialJobDetail = await adlTrainingService.submitTrialJob(form);
+        chai.expect(jobDetail.status).to.be.equals('WAITING');
+        await adlTrainingService.cancelTrialJob(jobDetail.id);
+        chai.expect(jobDetail.status).to.be.equals('USER_CANCELED');
+        // job with given checkpoint
+        await adlTrainingService.setClusterMetadata(TrialConfigMetadataKey.TRIAL_CONFIG, testAdlTrialConfig2);
+        form = {
+            sequenceId: 0,
+            hyperParameters: {
+                value: 'mock hyperparameters',
+                index: 0
+            }
+        };
+        jobDetail = await adlTrainingService.submitTrialJob(form);
+        chai.expect(jobDetail.status).to.be.equals('WAITING');
+        await adlTrainingService.cancelTrialJob(jobDetail.id);
+        chai.expect(jobDetail.status).to.be.equals('USER_CANCELED');
+    }).timeout(3000000);
 });
