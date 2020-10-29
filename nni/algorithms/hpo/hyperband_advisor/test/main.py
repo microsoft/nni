0933125e04fea4df20fe44c84cba6089ced8d2cc
Ning Shang <nishang@microsoft.com>
2020-10-29 11:29:55 +0800

    add copyright

--- nni/algorithms/hpo/hyperband_advisor/test/main.py
+++ nni/algorithms/hpo/hyperband_advisor/test/main.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
 """
 A test for hyperband, using nasbench201. So it need install the dependencies for nasbench201 at first.
 """
