f16046fe9918ed15d33f94253bab1433089b2c74
Lijiao <Lijiaoa@outlook.com>
2020-10-30 08:06:54 +0000

    align

--- ts/webui/src/components/overview/count/commonStyle.ts
+++ ts/webui/src/components/overview/count/commonStyle.ts
@@ -4,12 +4,12 @@ const leftProgress: React.CSSProperties = {
     top: 6
 };
 const durationItem2: React.CSSProperties = {
-    width: '52%',
+    width: '51.5%',
     paddingLeft: '15%'
 };
 
 const trialCountItem2: React.CSSProperties = {
-    width: '52%'
+    width: '51.5%'
 };
 const progressHeight = 8;
 export { leftProgress, durationItem2, trialCountItem2, progressHeight };
