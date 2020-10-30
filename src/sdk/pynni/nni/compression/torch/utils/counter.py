5bb75270d902aad0a7022fba241c83f82c6c00e2
colorjam <im.cqyan@google.com>
2020-10-30 17:24:02 +0900

    support layer info

--- src/sdk/pynni/nni/compression/torch/utils/counter.py
+++ src/sdk/pynni/nni/compression/torch/utils/counter.py
@@ -5,12 +5,107 @@ import torch
 import torch.nn as nn
 from nni.compression.torch.compressor import PrunerModuleWrapper
 
-try:
-    from thop import profile
-except Exception as e:
-    print('thop is not found, please install the python package: thop')
-    raise
+def _get_params(m):
+    return sum([p.numel() for p in m.parameters()])
 
+def count_convNd(m, _, y):
+    cin = m.in_channels
+    kernel_ops = m.weight.size()[2] * m.weight.size()[3]
+    output_size = torch.zeros(y.size()[2:]).numel()
+    cout = y.size()[1]
+
+    if hasattr(m, "weight_mask"):
+        cout = m.weight_mask.sum() // (cin * kernel_ops)
+
+    total_ops = cout * output_size * kernel_ops * cin // m.groups  # cout x oW x oH
+    m.total_ops = total_ops
+    m.total_params = _get_params(m)
+    m.num_calls += 1
+
+
+def count_linear(m, _, __):
+    out_features = m.out_features
+    if hasattr(m, "weight_mask"):
+        out_features = m.weight_mask.sum() // m.in_features
+
+    m.total_ops = out_features * m.in_features 
+    m.total_params = _get_params(m)
+    m.num_calls += 1
+
+def count_naive(m, _, __):
+    m.num_calls += 1
+
+register_hooks = {
+    nn.Conv1d: count_convNd,
+    nn.Conv2d: count_convNd,
+    nn.Conv3d: count_convNd,
+    nn.Linear: count_linear,
+}
+
+def format_results(modules):
+    name_column_width = max([len(module['name']) for module in modules]) + 4
+    has_multi_use = True if len(list(filter(lambda x: x > 1, [module['num_calls'] for module in modules]))) else False
+    module_type_column_width = max([len(module['module_type']) for module in modules]) + 4
+    headers = [
+        'Index', 
+        'Name',
+        'Module',
+        'FLOPs',
+        '#Params',
+    ]
+    if has_multi_use:
+        headers.append('#Calls')
+
+    DEFAULT_COLUMN_WIDTH = 12
+    SPACING_SIZE = 2
+    row_format_lst = [""]
+    header_sep_lst = [""]
+    line_length_lst = [-SPACING_SIZE]
+
+    def add_column(padding, text_dir='>'):
+        row_format_lst[0] += '{: ' + text_dir + str(padding) + '}' + (' ' * SPACING_SIZE)
+        header_sep_lst[0] += '-' * padding + (' ' * SPACING_SIZE)
+        line_length_lst[0] += padding + SPACING_SIZE
+
+    result = []
+    def append(s):
+        result.append(s)
+        result.append('\n')  # Yes, newline after the end as well
+        
+    add_column(DEFAULT_COLUMN_WIDTH) # index column
+    add_column(name_column_width)
+    add_column(module_type_column_width)
+    for _ in headers[3:]:
+        add_column(DEFAULT_COLUMN_WIDTH)
+    row_format = row_format_lst[0]
+    header_sep = header_sep_lst[0]
+    add_column = None  # type: ignore
+    append(header_sep)
+    append(row_format.format(*headers))
+    
+    total_ops, total_params = 0, 0
+    for i, module in enumerate(modules):
+        row_values = [
+            i,
+            module['name'],  # Name
+            module['module_type'],
+            module['flops'],
+            module['params']
+        ]
+        
+        if has_multi_use:
+            row_values.append(module['num_calls'])
+            
+        total_ops += module['num_calls'] * module['flops']
+        total_params += module['num_calls'] * module['params']
+
+        append(row_format.format(*row_values))
+
+    append(header_sep)
+    result.append(f'FLOPs total: {total_ops} \n')
+    result.append(f'#Params total: {total_params} \n')
+
+    return ''.join(result)
 
 def count_flops_params(model: nn.Module, input_size, custom_ops=None, verbose=True):
     """
@@ -40,42 +135,84 @@ def count_flops_params(model: nn.Module, input_size, custom_ops=None, verbose=Tr
     params:
         total params of the model
     """
+    handler_collection = []
 
-    assert input_size is not None
+    def add_hooks(m_):
+        if len(list(m_.children())) > 0:
+            return
 
-    device = next(model.parameters()).device
-    inputs = torch.randn(input_size).to(device)
+        m_.total_ops = m_.total_params = 0
+        m_.num_calls = 0
 
-    hook_module_list = []
-    if custom_ops is None:
-        custom_ops = {}
-    custom_mask_ops.update(custom_ops)
+        m_type = type(m_)
+        
+
+        fn = register_hooks.get(m_type, count_naive)
+
+        if fn is not None:
+            _handler = m_.register_forward_hook(fn)
+            handler_collection.append(_handler)
+
+    def remove_buffer(m_):
+        if len(list(m_.children())) > 0:
+            return
+
+        del m_.total_ops, m_.total_params, m_.num_calls
+        
+    original_device = next(model.parameters()).device
+    training = model.training
+
+    model.eval()
+    model.apply(add_hooks)
+
+    assert isinstance(input_size, tuple)
+    if torch.is_tensor(input_size[0]):
+        x = (t.to(original_device) for t in input_size)
+    else:
+        x = (torch.zeros(input_size).to(original_device), )
+
+    with torch.no_grad():
+        model(*x)
+
+    total_ops = 0
+    total_params = 0
+    results = []
+
     prev_m = None
-    for m in model.modules():
-        weight_mask = None
-        m_type = type(m)
-        if m_type in custom_mask_ops:
-            if isinstance(prev_m, PrunerModuleWrapper):
-                weight_mask = prev_m.weight_mask
+    for name, m in model.named_modules():
+        if len(list(m.children())) > 0:  # skip for non-leaf module
+            continue
+        if not m.num_calls:
+            if not verbose:
+                print(f'Module {name} of type {type(m)} is not used.')
+            continue
+        
+        if isinstance(prev_m, PrunerModuleWrapper):
+            weight_mask = prev_m.weight_mask
+            m.weight_mask = weight_mask
 
-            m.register_buffer('weight_mask', weight_mask)
-            hook_module_list.append(m)
+        total_ops += m.total_ops
+        total_params += m.total_params
+
+        if m.total_ops > 0 or m.total_params > 0:
+            results.append({
+                "name": name,
+                "module_type": type(m).__name__,
+                "flops": m.total_ops,
+                "params": m.total_params,
+                "num_calls": m.num_calls
+                }
+            )
         prev_m = m
 
-    flops, params = profile(model, inputs=(inputs, ), custom_ops=custom_mask_ops, verbose=verbose)
+    model.train(training).to(original_device)
+    for handler in handler_collection:
+        handler.remove()
+    model.apply(remove_buffer)
+    results = format_results(results)
+    # print(results)
 
-
-    for m in hook_module_list:
-        m._buffers.pop("weight_mask")
-    # Remove registerd buffer on the model, and fixed following issue:
-    # https://github.com/Lyken17/pytorch-OpCounter/issues/96
-    for m in model.modules():
-        if 'total_ops' in m._buffers:
-            m._buffers.pop("total_ops")
-        if 'total_params' in m._buffers:
-            m._buffers.pop("total_params")
-
-    return flops, params
+    return total_ops, total_params, results
 
 def count_convNd_mask(m, x, y):
     """
@@ -126,10 +263,33 @@ def count_linear_mask(m, x, y):
 
     m.total_ops += torch.DoubleTensor([int(total_ops)])
 
+    
+def test_profiler():
+    
+    class Model(nn.Module):
+        def __init__(self):
+            super(Model, self).__init__()
+            self.conv = nn.Conv2d(3, 5, 1, 1)
 
-custom_mask_ops = {
-    nn.Conv1d: count_convNd_mask,
-    nn.Conv2d: count_convNd_mask,
-    nn.Conv3d: count_convNd_mask,
-    nn.Linear: count_linear_mask,
-}
+        def forward(self, x):
+            return self.conv(x)
+
+    assert count_flops_params(Model(), (1, 3, 2, 2)) == (60, 20)
+
+    class Model2(nn.Module):
+        def __init__(self):
+            super(Model2, self).__init__()
+            self.conv = nn.Conv2d(3, 5, 1, 1)
+            self.conv2 = nn.Conv2d(5, 5, 1, 1)
+        def forward(self, x):
+            x = self.conv(x)
+            for i in range(3):
+                x = self.conv2(x)
+            return x
+
+    from torchvision.models import resnet18, resnet50
+    
+    assert count_flops_params(resnet50(), (1, 3, 224, 224)) == (4089184256, 25503912)
+    
+if __name__ == '__main__':
+    test_profiler()
