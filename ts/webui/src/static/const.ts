ed4d5acdceb39dfee2867d904ef66ff027e8849f
Lijiao <Lijiaoa@outlook.com>
2020-11-19 07:10:22 +0000

    update

--- ts/webui/src/static/const.ts
+++ ts/webui/src/static/const.ts
@@ -2,7 +2,7 @@
 const METRIC_GROUP_UPDATE_THRESHOLD = 100;
 const METRIC_GROUP_UPDATE_SIZE = 20;
 
-const MANAGER_IP = `http://13.77.78.63:8080/api/v1/nni`;
+const MANAGER_IP = `/api/v1/nni`;
 const DOWNLOAD_IP = `/logs`;
 const WEBUIDOC = 'https://nni.readthedocs.io/en/latest/Tutorial/WebUI.html';
 const trialJobStatus = [
