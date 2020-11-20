a0f7d09721f9b2562979f80df631fa6d1f2043a2
Zhenhua Han <hzhua201@gmail.com>
2020-11-20 08:45:40 +0000

    sovle merge conflict

--- nni/retiarii/operation_def/tf_op_def.py
+++ nni/retiarii/operation_def/tf_op_def.py
@@ -1,6 +1,7 @@
 from ..operation import TensorFlowOperation
 
 class Conv2D(TensorFlowOperation):
-    def to_init_code(self, field):
-        parameters = {'padding': 'same', **parameters}
-        super().__init__(type, parameters, _internal_access)
+    def __init__(self, type_name, parameters, _internal):
+        if 'padding' not in parameters:
+            parameters['padding'] = 'same'
+        super().__init__(type_name, parameters, _internal)
