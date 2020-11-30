d15a2350066d8c189d275d090c9c2aa00de1cec6
98may <98may@zju.edu.cn>
2020-11-30 10:09:25 +0800

    finish basic function, pass test with test_example.py

--- examples/nas/benchmarks/nlp.sh
+++ examples/nas/benchmarks/nlp.sh
@@ -5,6 +5,9 @@ if [ -z "${NASBENCHMARK_DIR}" ]; then
     NASBENCHMARK_DIR=~/.nni/nasbenchmark
 fi
 
+
+mkdir -p nlp_data
+cd nlp_data
 echo "Downloading NLP w_data.zip..."
 if [ -f "w_data.zip" ]; then
     echo "w_data.zip found. Skip download."
@@ -12,9 +15,10 @@ else
     wget -O w_data.zip https://github.com/98may/nas-bench-nlp-release/blob/master/train_logs_wikitext-2/logs.zip?raw=true
 fi
 unzip w_data.zip
+cd ..
 
 echo "Generating database..."
 rm -f ${NASBENCHMARK_DIR}/nlp.db ${NASBENCHMARK_DIR}/nlp.db-journal
 mkdir -p ${NASBENCHMARK_DIR}
 python3 -m nni.nas.benchmarks.nlp.db_gen nlp_data
-rm -rf w_data.zip nlp_data
+rm -rf nlp_data
