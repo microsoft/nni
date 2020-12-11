21eb93603c822fbb91eadbbb6c8f8022b08d156d
Zhenhua Han <hzhua201@gmail.com>
2020-12-11 06:53:17 +0000

    add validation in PyTorchMultiModelTrainer

--- nni/retiarii/trainer/pytorch/base.py
+++ nni/retiarii/trainer/pytorch/base.py
@@ -36,7 +36,8 @@ def get_default_transform(dataset: str) -> Any:
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
         ])
     # unsupported dataset, return None
     return None
@@ -106,7 +107,8 @@ class PyTorchImageClassificationTrainer(BaseTrainer):
     def training_step_before_model(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int):
         x, y = batch
         if self._use_cuda:
-            x, y = x.cuda(torch.device('cuda:0')), y.cuda(torch.device('cuda:0'))
+            x, y = x.cuda(torch.device('cuda:0')), y.cuda(
+                torch.device('cuda:0'))
         return x, y
 
     def training_step_after_model(self, x, y, y_hat):
@@ -156,12 +158,15 @@ class PyTorchMultiModelTrainer(BaseTrainer):
     def __init__(self, multi_model, kwargs=[]):
         self.multi_model = multi_model
         self.kwargs = kwargs
-        self._dataloaders = []
-        self._datasets = []
+        self._train_dataloaders = []
+        self._train_datasets = []
+        self._val_dataloaders = []
+        self._val_datasets = []
         self._optimizers = []
         self._trainers = []
         self._loss_fn = nn.CrossEntropyLoss()
         self.max_steps = None
+        self.n_model = len(self.kwargs['model_kwargs'])
         if 'max_steps' in self.kwargs:
             self.max_steps = self.kwargs['max_steps']
 
@@ -170,12 +175,22 @@ class PyTorchMultiModelTrainer(BaseTrainer):
                 dataset_cls = m['dataset_cls']
                 dataset_kwargs = m['dataset_kwargs']
                 dataloader_kwargs = m['dataloader_kwargs']
-                dataset = getattr(datasets, dataset_cls)(transform=get_default_transform(dataset_cls),
-                                                         **(dataset_kwargs or {}))
-                dataloader = DataLoader(dataset, **(dataloader_kwargs or {}))
-                self._datasets.append(dataset)
-                self._dataloaders.append(dataloader)
-            
+                train_dataset = getattr(datasets, dataset_cls)(train=True,
+                                                               transform=get_default_transform(
+                                                                   dataset_cls),
+                                                               **(dataset_kwargs or {}))
+                val_dataset = getattr(datasets, dataset_cls)(train=False,
+                                                               transform=get_default_transform(
+                                                                   dataset_cls),
+                                                               **(dataset_kwargs or {}))
+                train_dataloader = DataLoader(train_dataset, **(dataloader_kwargs or {}))
+                val_dataloader = DataLoader(val_dataset, **(dataloader_kwargs or {}))
+                self._train_datasets.append(train_dataset)
+                self._train_dataloaders.append(train_dataloader)
+
+                self._val_datasets.append(val_dataset)
+                self._val_dataloaders.append(val_dataloader)
+
             if m['use_output']:
                 optimizer_cls = m['optimizer_cls']
                 optimizer_kwargs = m['optimizer_kwargs']
@@ -185,27 +200,31 @@ class PyTorchMultiModelTrainer(BaseTrainer):
                     name_prefix = '_'.join(name.split('_')[:2])
                     if m_header == name_prefix:
                         one_model_params.append(param)
-                        
-                optimizer = getattr(torch.optim, optimizer_cls)(one_model_params, **(optimizer_kwargs or {}))
+
+                optimizer = getattr(torch.optim, optimizer_cls)(
+                    one_model_params, **(optimizer_kwargs or {}))
                 self._optimizers.append(optimizer)
 
     def fit(self) -> None:
         torch.autograd.set_detect_anomaly(True)
-        max_epochs = max([x['trainer_kwargs']['max_epochs'] for x in self.kwargs['model_kwargs']])
+        max_epochs = max([x['trainer_kwargs']['max_epochs']
+                          for x in self.kwargs['model_kwargs']])
         for _ in range(max_epochs):
             self._train()
+        nni.report_final_result(self._validate())
 
     def _train(self):
-        for batch_idx, multi_model_batch in enumerate(zip(*self._dataloaders)):
+        for batch_idx, multi_model_batch in enumerate(zip(*self._train_dataloaders)):
             for opt in self._optimizers:
                 opt.zero_grad()
             xs = []
             ys = []
             for idx, batch in enumerate(multi_model_batch):
-                x, y = self.training_step_before_model(batch, batch_idx, f'cuda:{idx}')
+                x, y = self.training_step_before_model(
+                    batch, batch_idx, f'cuda:{idx}')
                 xs.append(x)
                 ys.append(y)
-            
+
             y_hats = self.multi_model(*xs)
             if len(ys) != len(xs):
                 raise ValueError('len(ys) should be equal to len(xs)')
@@ -213,29 +232,32 @@ class PyTorchMultiModelTrainer(BaseTrainer):
             report_loss = {}
             for output_idx, yhat in enumerate(y_hats):
                 if len(ys) == len(y_hats):
-                    loss = self.training_step_after_model(xs[output_idx], ys[output_idx], yhat)
+                    loss = self.training_step_after_model(
+                        xs[output_idx], ys[output_idx], yhat)
                 elif len(ys) == 1:
-                    loss = self.training_step_after_model(xs[0], ys[0].to(yhat.get_device()), yhat)
+                    loss = self.training_step_after_model(
+                        xs[0], ys[0].to(yhat.get_device()), yhat)
                 else:
-                    raise ValueError('len(ys) should be either 1 or len(y_hats)')
+                    raise ValueError(
+                        'len(ys) should be either 1 or len(y_hats)')
                 losses.append(loss.to("cuda:0"))
-                report_loss[self.kwargs['model_kwargs'][output_idx]['model_id']] = loss.item()
+                report_loss[self.kwargs['model_kwargs']
+                            [output_idx]['model_id']] = loss.item()
             summed_loss = sum(losses)
             summed_loss.backward()
             for opt in self._optimizers:
                 opt.step()
-            if batch_idx % 50 == 0:
-                nni.report_intermediate_result(report_loss)
+            # if batch_idx % 50 == 0:
+            #     nni.report_intermediate_result(report_loss)
             if self.max_steps and batch_idx >= self.max_steps:
                 return
 
-    
     def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> Dict[str, Any]:
         x, y = self.training_step_before_model(batch, batch_idx)
         y_hat = self.model(x)
         return self.training_step_after_model(x, y, y_hat)
 
-    def training_step_before_model(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int, device = None):
+    def training_step_before_model(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int, device=None):
         x, y = batch
         if device:
             x, y = x.cuda(torch.device(device)), y.cuda(torch.device(device))
@@ -245,17 +267,58 @@ class PyTorchMultiModelTrainer(BaseTrainer):
         loss = self._loss_fn(y_hat, y)
         return loss
 
+    def _validate(self):
+        all_val_outputs = {idx : [] for idx in range(self.n_model)}
+        for batch_idx, multi_model_batch in enumerate(zip(*self._val_dataloaders)):
+            xs = []
+            ys = []
+            for idx, batch in enumerate(multi_model_batch):
+                x, y = self.training_step_before_model(
+                    batch, batch_idx, f'cuda:{idx}')
+                xs.append(x)
+                ys.append(y)
+            if len(ys) != len(xs):
+                raise ValueError('len(ys) should be equal to len(xs)')
+            
+            y_hats = self.multi_model(*xs)
+
+            for output_idx, yhat in enumerate(y_hats):
+                if len(ys) == len(y_hats):
+                    acc = self.validation_step_after_model(
+                        xs[output_idx], ys[output_idx], yhat)
+                elif len(ys) == 1:
+                    acc = self.validation_step_after_model(
+                        xs[0], ys[0].to(yhat.get_device()), yhat)
+                else:
+                    raise ValueError(
+                        'len(ys) should be either 1 or len(y_hats)')
+                all_val_outputs[output_idx].append(acc)
+
+                # report_loss[self.kwargs['model_kwargs']
+                #             [output_idx]['model_id']] = loss.item()
+        report_acc = {}
+        for idx in all_val_outputs:
+            avg_acc = np.mean([x['val_acc'] for x in all_val_outputs[idx]]).item()
+            report_acc[self.kwargs['model_kwargs'][idx]['model_id']] = avg_acc
+        nni.report_intermediate_result(report_acc)
+        return report_acc
+
     def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> Dict[str, Any]:
         x, y = self.validation_step_before_model(batch, batch_idx)
         y_hat = self.model(x)
         return self.validation_step_after_model(x, y, y_hat)
 
-    def validation_step_before_model(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int):
+    def validation_step_before_model(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int, device=None):
         x, y = batch
-        if self._use_cuda:
-            x, y = x.cuda(), y.cuda()
+        if device:
+            x, y = x.cuda(torch.device(device)), y.cuda(torch.device(device))
         return x, y
 
     def validation_step_after_model(self, x, y, y_hat):
         acc = self._accuracy(y_hat, y)
         return {'val_acc': acc}
+    
+    def _accuracy(self, input, target):
+        _, predict = torch.max(input.data, 1)
+        correct = predict.eq(target.data).cpu().sum().item()
+        return correct / input.size(0)
