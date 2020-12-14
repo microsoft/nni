673370e48c917dc8ac649c46c776cb39ffc449f5
Yuge Zhang <scottyugochang@gmail.com>
2020-12-14 10:50:59 +0800

    Eject importing with shortcut

--- nni/compression/pytorch/__init__.py
+++ nni/compression/pytorch/__init__.py
@@ -1,13 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from nni.runtime.importing import _shortcut
-
 from .speedup import ModelSpeedup
 from .compressor import Compressor, Pruner, Quantizer
 from .pruning import apply_compression_results
-
-_shortcut(__name__, [
-    'nni.algorithms.compression.pytorch.pruning',
-    'nni.algoirthms.compression.pytorch.quantization'
-])
--- nni/nas/pytorch/__init__.py
+++ nni/nas/pytorch/__init__.py
@@ -1,3 +1,6 @@
-from nni.runtime.importing import _shortcut
-
-_shortcut(__name__, 'nni.algorithms.nas.pytorch')
+from .base_mutator import BaseMutator
+from .base_trainer import BaseTrainer
+from .fixed import apply_fixed_architecture
+from .mutables import Mutable, LayerChoice, InputChoice
+from .mutator import Mutator
+from .trainer import Trainer
