b902100cb44d454be38bb50b517a22fff5338b6e
liuzhe <liuzhe.cs@pku.edu.cn>
2020-12-23 11:38:29 +0800

    use uploaded smac dep

--- setup.py
+++ setup.py
@@ -112,10 +112,7 @@ def _setup():
         python_requires = '>=3.6',
         install_requires = dependencies,
         extras_require = {
-            'SMAC': [
-                'ConfigSpaceNNI @ git+https://github.com/QuanluZhang/ConfigSpace.git',
-                'smac @ git+https://github.com/QuanluZhang/SMAC3.git'
-            ],
+            'SMAC': ['ConfigSpaceNNI', 'smac4nni'],
             'BOHB': ['ConfigSpace==0.4.7', 'statsmodels==0.10.0'],
             'PPOTuner': ['enum34', 'gym']
         },
