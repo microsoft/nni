42d0cb1c540753df6739cbd992a5170c658378bb
Yuge Zhang <scottyugochang@gmail.com>
2020-12-30 10:26:45 +0800

    Update retiarii init function

--- nni/retiarii/__init__.py
+++ nni/retiarii/__init__.py
@@ -2,4 +2,4 @@ from .operation import Operation
 from .graph import *
 from .execution import *
 from .mutator import *
-from .utils import blackbox_module
+from .utils import blackbox, blackbox_module, blackbox_trainer
