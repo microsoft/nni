93457237a4ec277bd6312c384597b6a885cb4178
J-shang <33053116+J-shang@users.noreply.github.com>
2021-01-13 16:10:54 +0800

    block all `nnictl package` optional arguments (#3292)

--- nni/tools/nnictl/nnictl.py
+++ nni/tools/nnictl/nnictl.py
@@ -246,8 +246,8 @@ def parse_args():
     def show_messsage_for_nnictl_package(args):
         print_error('nnictl package command is replaced by nnictl algo, please run nnictl algo -h to show the usage')
 
-    parser_package_subparsers = subparsers.add_parser('package', help='control nni tuner and assessor packages')
-    parser_package_subparsers.add_argument('args', help='all subcommands have deprecated, nnictl package command is replaced by nnictl algo, please run nnictl algo -h to show the usage', nargs=argparse.REMAINDER)
+    parser_package_subparsers = subparsers.add_parser('package', help='this argument is replaced by algo', prefix_chars='\n')
+    parser_package_subparsers.add_argument('args', nargs=argparse.REMAINDER)
     parser_package_subparsers.set_defaults(func=show_messsage_for_nnictl_package)
 
     #parse tensorboard command
