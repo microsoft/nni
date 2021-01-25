a8b1002f6dd3be2b2cb9207e507b51dd79fdf3fd
J-shang <shangning128@163.com>
2021-01-25 08:13:38 +0000

    fix import

--- nni/experiment/__init__.py
+++ nni/experiment/__init__.py
@@ -2,6 +2,4 @@
 # Licensed under the MIT license.
 
 from .config import *
-from .experiment import Experiment
-
-from .nni_client import *
+from .experiment import Experiment, TrialJob, TrialResult, TrialMetricData, TrialHyperParameters
