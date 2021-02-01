c5612989893de62d3c344b8ddd8174e5a42d62ed
v-linbin <v-linbin@microsoft.com>
2021-02-01 10:05:51 +0800

    Support mixed-precision quantization speed up by using tensorrt

---
+++ examples/model_compress/quantization/mixed_precision_example.py
@@ -0,0 +1,328 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import MultiStepLR
+import numpy as np
+from torchvision import datasets, transforms
+
+from nni.compression.pytorch.speedup.quantization_speedup.integrated_tensorrt import CalibrateType
+from nni.compression.pytorch.speedup.quantization_speedup.backend import BackendEngine
+
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(BasicBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(Bottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=10, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 norm_layer=None):
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        
+        ## CIFAR10: kernel_size 7 -> 3, stride 2 -> 1, padding 3->1
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
+        ## END
+        
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+def _resnet(arch, block, layers, pretrained, progress, device, **kwargs):
+    model = ResNet(block, layers, **kwargs)
+    return model
+
+
+def resnet18(pretrained=False, progress=True, device='cpu', **kwargs):
+    return _resnet('resnet18', BasicBlock, [2, 2, 2, 2], pretrained, progress, device,
+                   **kwargs)
+
+def get_testset():
+    test_loader = torch.utils.data.DataLoader(
+    datasets.CIFAR10('data.cifar10', train=False,download=True, transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])),
+    batch_size=10000, shuffle=False)
+    test_set, test_labels = next(iter(test_loader))[0].numpy(), next(iter(test_loader))[1].numpy()
+    return test_set, test_labels
+
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 200 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    test_loss /= len(test_loader.dataset)
+
+    print('Loss: {}  Accuracy: {}%)\n'.format(
+        test_loss, 100 * correct / len(test_loader.dataset)))
+
+def check_accuracy(preds, labels):
+    start_idx = 0
+    num_correct_all = 0
+    for pred in preds:
+        pred = np.argmax(pred.reshape(-1, 10), axis=1)
+        effective_shape = pred.shape[0]
+        num_correct = np.count_nonzero(np.equal(pred, labels[start_idx:start_idx+effective_shape]))
+        num_correct_all = num_correct_all + num_correct
+        start_idx = start_idx + effective_shape
+    print("accuracy: ", 100 * num_correct_all / 10000)
+
+def main():
+    torch.manual_seed(0)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('./data.cifar10', train=True, download=True,
+                         transform=transforms.Compose([
+                             transforms.ToTensor(),
+                             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                         ])),
+        batch_size=64, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('./data.cifar10', train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        ])),
+        batch_size=200, shuffle=False)
+    model = resnet18()
+    model.to(device)
+    train_epoch = 1
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+    scheduler = MultiStepLR(
+        optimizer, milestones=[int(train_epoch*0.5), int(train_epoch*0.75)], gamma=0.1)
+        
+    for epoch in range(train_epoch):
+        train(model, device, train_loader, optimizer, epoch)
+        scheduler.step()
+        acc = test(model, device, test_loader)
+
+    config = {
+        'conv1':8,
+        'layer1.0.conv1':8,
+        'layer1.0.conv2':8,
+        'layer1.1.conv1':32,
+        'layer1.1.conv2':8,
+        'layer2.0.conv1':8,
+        'layer2.0.conv2':32,
+        'layer2.1.conv1':8,
+        'layer2.1.conv2':8,
+        'layer3.0.conv1':32,
+        'layer3.0.conv2':32,
+        'layer3.1.conv1':8,
+        'layer3.1.conv2':8,
+        'layer4.0.conv1':32,
+        'layer4.0.conv2':32,
+        'layer4.1.conv1':8,
+        'layer4.1.conv2':8,
+        'fc':32,
+    }
+    # parameter init for torch model to onnx
+    onnx_path = "resnet18.onnx"
+
+    batch_size = 32
+    input_shape = (batch_size, 3, 32, 32)
+    input_names = ["actual_input_1"]+ [ "learned_%d" % i for i in range(16) ]
+    output_names = ["output1"]
+    calibration_cache = "cifar_calibration.cache"
+    test_set, test_labels = get_testset()
+    backend = 'tensorrt'
+
+    engine = BackendEngine(backend, model, onnx_path, input_shape, config=config, extra_layer_bit=32, 
+        strict_datatype=True, using_calibrate=True, calibrate_type=CalibrateType.ENTROPY2, calib_data=test_set, 
+        calibration_cache = calibration_cache, batchsize=batch_size, input_names=input_names, output_names=output_names)
+    engine.build()
+    output, time = engine.inference(test_set)
+
+    check_accuracy(output, test_labels)
+    print("elapsed_time: ", time)
+    
+if __name__ == '__main__':
+    main()
---
+++ nni/compression/pytorch/speedup/quantization_speedup/integrated_tensorrt.py
@@ -0,0 +1,163 @@
+import tensorrt as trt
+import nni.compression.pytorch.speedup.quantization_speedup.frontend_to_onnx as fonnx
+import nni.compression.pytorch.speedup.quantization_speedup.calibrator as calibrator
+import nni.compression.pytorch.speedup.quantization_speedup.common as common
+import time
+
+TRT_LOGGER = trt.Logger()
+
+class CalibrateType:
+    LEGACY = trt.CalibrationAlgoType.LEGACY_CALIBRATION
+    ENTROPY = trt.CalibrationAlgoType.ENTROPY_CALIBRATION
+    ENTROPY2 = trt.CalibrationAlgoType.ENTROPY_CALIBRATION_2
+    MINMAX = trt.CalibrationAlgoType.MINMAX_CALIBRATION
+
+Precision_Dict = {
+    8: trt.int8,
+    16: trt.float16,
+    32: trt.float32
+}
+
+def build_engine(model_file, calib, batch_size=32, config=None, extra_layer_bit='float32', strict_datatype=False):
+    with trt.Builder(TRT_LOGGER) as builder, builder.create_network(common.EXPLICIT_BATCH) as network, trt.OnnxParser(network, TRT_LOGGER) as parser:
+        """
+        This function builds an engine from an onnx model.
+        """
+        # Attention that, builder should be set to 1 because of the implementation of allocate_buffer
+        builder.max_batch_size = 1
+        builder.max_workspace_size = common.GiB(1)
+
+        if extra_layer_bit is 32 and config is None:
+            pass
+        elif extra_layer_bit is 8 and config is None:
+            # entire model in 8bit mode
+            builder.int8_mode = True
+            builder.int8_calibrator = calib
+        else:
+            builder.int8_mode = True
+            builder.fp16_mode = True
+            builder.int8_calibrator = calib
+            builder.strict_type_constraints = strict_datatype
+        
+        # Parse onnx model
+        with open(model_file, 'rb') as model:
+            if not parser.parse(model.read()):
+                print ('ERROR: Fail to parse the ONNX file.')
+                for error in range(parser.num_errors):
+                    print (parser.get_error(error))
+                return None
+
+        # This design may not be correct if output more than one
+        for i in range(network.num_layers):
+            if config is None:
+                break
+            layer = network.get_layer(i)
+            if layer.name in config:
+                bitset = config[layer.name]
+                layer.precision = Precision_Dict[bitset]
+                layer.set_output_type(0, Precision_Dict[bitset])
+        # network.mark_output(model_tensors.find(ModelData.OUTPUT_NAME))
+        # Build engine and do int8 calibration.
+        engine = builder.build_cuda_engine(network)
+        return engine
+
+class TensorRt:
+    def __init__(self, model, onnx_path, input_shape, config=None, extra_layer_bit=32, strict_datatype=False, using_calibrate=True, 
+    calibrate_type=None, calib_data=None, calibration_cache = None, batchsize=1, input_names=["actual_input_1"], output_names=["output1"]):
+        """
+        Parameters
+        ----------
+        model : pytorch model
+            The model to speed up by quantization.
+        onnx_path : str
+            The path user want to store onnx model which is converted from pytorch model.
+        input_shape : tuple
+            The input shape of model, shall pass it to torch.onnx.export.
+        config : dict
+            Config recording bit number and name of layers.
+        extra_layer_bit : int
+            Other layers which are not in config will be quantized to corresponding bit number.
+        strict_datatype : bool
+            Whether constrain layer bit to the number given in config or not. If true, all the layer 
+            will be set to given bit strictly. Otherwise, these layers will be set automatically by
+            tensorrt.
+        using_calibrate : bool
+            Whether calibrating during quantization or not. If true, user should provide calibration
+            dataset. If not, user should provide scale and zero_point for each layer. Current version
+            only support using calibrating.
+        calibrate_type : tensorrt.tensorrt.CalibrationAlgoType
+            The algorithm of calibrating. Please refer to https://docs.nvidia.com/deeplearning/
+            tensorrt/api/python_api/infer/Int8/Calibrator.html for detail
+        calibrate_data : numpy array
+            The data using to calibrate quantization model
+        calibration_cache : str
+            The path user want to store calibrate cache file
+        batchsize : int
+            The batch size of calibration and inference
+        input_names : list
+            Input name of onnx model providing for torch.onnx.export to generate onnx model
+        output_name : list
+            Output name of onnx model providing for torch.onnx.export to generate onnx model
+        """
+        self.model = model
+        self.onnx_path = onnx_path
+        self.input_shape = input_shape
+        self.config = config
+        self.extra_layer_bit = extra_layer_bit
+        self.strict_datatype = strict_datatype
+        self.using_calibrate = using_calibrate
+        self.calibrate_type = calibrate_type
+        self.calib_data = calib_data
+        self.calibration_cache = calibration_cache
+        self.batchsize = batchsize
+        self.input_names = input_names
+        self.output_names = output_names
+        self.context = None
+        self.onnx_config = {}
+
+    def tensorrt_build(self):
+        """
+        Get onnx config and build tensorrt engine.
+        """
+        assert self.model is not None
+        assert self.onnx_path is not None
+        assert self.input_shape is not None
+
+        # Convert pytorch model to onnx model and save onnx model in onnx_path
+        _, self.onnx_config = fonnx.torch_to_onnx(self.model, self.config, input_shape=self.input_shape, model_path=self.onnx_path, input_names=self.input_names, output_names=self.output_names)
+
+        if self.using_calibrate:
+            assert self.calibrate_type is not None
+            context = self.tensorrt_build_withcalib(self.onnx_path)
+        else:
+            raise NameError('quantized without calibrate has not been supported.')
+        self.context = context
+
+    def tensorrt_build_withcalib(self, onnx_path):
+        calib = calibrator.Calibrator(self.calib_data, self.calibration_cache, self.batchsize, self.calibrate_type)
+        engine = build_engine(onnx_path, calib, self.batchsize, self.onnx_config, self.extra_layer_bit, self.strict_datatype)
+        return engine.create_execution_context()
+
+    def inference(self, test_data):
+        """
+        Do inference by tensorrt builded engine.
+        """
+        elapsed_time = 0
+        inputs, outputs, bindings, stream = common.allocate_buffers(self.context.engine)
+        result = []
+        for start_idx in range(0, test_data.shape[0], self.batchsize):
+            # If the number of images in the test set is not divisible by the batch size, the last batch will be smaller.
+            # This logic is used for handling that case.
+            end_idx = min(start_idx + self.batchsize, test_data.shape[0])
+            effective_batch_size = end_idx - start_idx
+
+            # Do inference for every batch.
+            inputs[0].host = test_data[start_idx:start_idx + effective_batch_size]
+            t1 = time.time()
+            [output] = common.do_inference_v2(self.context, bindings=bindings, inputs=inputs, outputs=outputs, stream=stream)
+            shape = output.shape[0]
+            output = output[0:int(shape * effective_batch_size / self.batchsize)]
+            elapsed_time += time.time() - t1
+            result.append(output.copy())
+            # Use argmax to get predictions and then check accuracy
+        return result, elapsed_time
