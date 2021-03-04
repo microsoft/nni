3bb40d8b125eec7f31d954016b1280fba5a6f521
Yuge Zhang <scottyugochang@gmail.com>
2021-03-04 15:24:29 +0800

    Add support for functional serializer

--- nni/retiarii/utils.py
+++ nni/retiarii/utils.py
@@ -1,4 +1,5 @@
 import inspect
+import warnings
 from collections import defaultdict
 from typing import Any
 from pathlib import Path
@@ -27,11 +28,12 @@ def uid(namespace: str = 'default') -> int:
     return _last_uid[namespace]
 
 
-def get_module_name(cls):
-    module_name = cls.__module__
+def get_module_name(cls_or_func):
+    module_name = cls_or_func.__module__
     if module_name == '__main__':
         # infer the module name with inspect
         for frm in inspect.stack():
+            print(inspect.getmodule(frm[0]).__name__)
             if inspect.getmodule(frm[0]).__name__ == '__main__':
                 # main module found
                 main_file_path = Path(inspect.getsourcefile(frm[0]))
@@ -40,17 +42,19 @@ def get_module_name(cls):
                                        f'please launch the experiment under the directory where "{main_file_path.name}" is located.')
                 module_name = main_file_path.stem
                 break
+    if module_name == '__main__':
+        warnings.warn('Callstack exhausted but main module still not found. This will probably cause issues that the function/class cannot be imported.')
 
     # NOTE: this is hacky. As torchscript retrieves LSTM's source code to do something.
     # to make LSTM's source code can be found, we should assign original LSTM's __module__ to
     # the wrapped LSTM's __module__
     # TODO: find out all the modules that have the same requirement as LSTM
-    if f'{cls.__module__}.{cls.__name__}' == 'torch.nn.modules.rnn.LSTM':
-        module_name = cls.__module__
+    if f'{cls_or_func.__module__}.{cls_or_func.__name__}' == 'torch.nn.modules.rnn.LSTM':
+        module_name = cls_or_func.__module__
 
     return module_name
 
 
-def get_full_class_name(cls, relocate_module=False):
+def get_importable_name(cls, relocate_module=False):
     module_name = get_module_name(cls) if relocate_module else cls.__module__
     return module_name + '.' + cls.__name__
