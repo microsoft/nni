0ae338fe59f9eca4010e42554aa83a1716d85c37
Hao Ni <v-nihao@microsoft.com>
2021-03-17 15:51:35 +0800

    add shared storage test

---
+++ test/config/examples/config_sharedstorage_azureblob_trial.py
@@ -0,0 +1,19 @@
+"""
+A deep MNIST classifier using convolutional layers.
+
+This file is a modification of the official pytorch mnist example:
+https://github.com/pytorch/examples/blob/master/mnist/main.py
+"""
+
+import os
+import logging
+import nni
+
+logger = logging.getLogger('mnist_AutoML')
+
+if __name__ == '__main__':
+    try:
+        logger.info("This is a test trial")
+    except Exception as exception:
+        logger.exception(exception)
+        raise
