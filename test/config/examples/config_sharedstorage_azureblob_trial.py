f26c36bfdb0a8fa21de9dbd752838d7ae435a47f
Hao Ni <v-nihao@microsoft.com>
2021-03-18 16:39:33 +0800

    add validator method

--- test/config/examples/config_sharedstorage_azureblob_trial.py
+++ test/config/examples/config_sharedstorage_azureblob_trial.py
@@ -4,16 +4,13 @@ A deep MNIST classifier using convolutional layers.
 This file is a modification of the official pytorch mnist example:
 https://github.com/pytorch/examples/blob/master/mnist/main.py
 """
-
 import os
 import logging
 import nni
-
 logger = logging.getLogger('mnist_AutoML')
-
 if __name__ == '__main__':
     try:
-        logger.debug("This is a test trial")
+        logger.debug('This is a test trial')
     except Exception as exception:
         logger.exception(exception)
         raise
