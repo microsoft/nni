56455d39f8f55050bf46d82f3ead429a734e0a8a
J-shang <shangning128@163.com>
2021-03-19 05:36:19 +0000

    new patch

--- interim_vision_patch.py
+++ interim_vision_patch.py
@@ -3,18 +3,17 @@ import torchvision.datasets.mnist as mnist
 
 file_name = mnist.__file__
 dummy_file_name = os.path.join(os.path.dirname(file_name), 'mnist_dummy.py')
-
-with open(file_name, 'r') as fr:
-    firstline = fr.readline()
-    if firstline != 'from six.moves import urllib\n':
-        with open(dummy_file_name, 'w') as fw:
-            fw.writelines(['from six.moves import urllib\n',
-                           'opener = urllib.request.build_opener()\n',
-                           'opener.addheaders = [("User-agent", "Mozilla/5.0")]\n',
-                           'urllib.request.install_opener(opener)\n\n'])
-            fw.write(firstline)
-            for line in fr:
-                fw.write(line)
+with open(file_name, 'r') as fr, open(dummy_file_name, 'w') as fw:
+    origin_text = fr.read()
+    mnist_head = origin_text.find('class MNIST(')
+    reasource_head = origin_text.find('resources = [', mnist_head)
+    reasource_tail = origin_text.find(']\n', reasource_head)
+    top = origin_text[:reasource_head]
+    reasource = "resources = [('https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz', 'f68b3c2dcbeaaa9fbdd348bbdeb94873'),('https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz', 'd53e105ee54ea40749a09fcbcd1e9432'),('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz', '9fb629c4189551a2d022fa330f9573f3'),('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz', 'ec29112dd5afa0611ce80d1b7f02629c')]\n"
+    bottom = origin_text[reasource_tail + 2:]
+    fw.write(top)
+    fw.write(reasource)
+    fw.write(bottom)
 
 if os.path.exists(dummy_file_name):
     os.remove(file_name)
