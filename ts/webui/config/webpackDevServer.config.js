b5edb094aeebc1ec74067f58ba006c2a72aba1d4
Lijiao <Lijiaoa@outlook.com>
2021-03-28 06:45:06 +0000

    reback dev-utils version to 9.0.0

--- ts/webui/config/webpackDevServer.config.js
+++ ts/webui/config/webpackDevServer.config.js
@@ -97,7 +97,7 @@ module.exports = function(proxy, allowedHost) {
       // We do this in development to avoid hitting the production cache if
       // it used the same host and port.
       // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
-      app.use(noopServiceWorkerMiddleware(''));
+      app.use(noopServiceWorkerMiddleware());
     },
   };
 };
