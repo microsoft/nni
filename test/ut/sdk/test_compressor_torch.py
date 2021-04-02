1e16543e508bcee4f21a35e624ce3cab65721750
v-linbin <v-linbin@microsoft.com>
2021-04-02 23:31:50 +0800

    fix ut test

--- test/ut/sdk/test_compressor_torch.py
+++ test/ut/sdk/test_compressor_torch.py
@@ -241,13 +241,13 @@ class CompressorTestCase(TestCase):
         eps = 1e-7
         weight = torch.tensor([[1, 2], [3, 5]]).float()
         model.conv2.module.old_weight.data = weight
-        quantizer.quantize_weight(model.conv2)
+        quantizer.quantize_weight(weight, model.conv2)
         assert math.isclose(model.conv2.module.scale, 5 / 255, abs_tol=eps)
         assert model.conv2.module.zero_point == 0
         # range including 0
         weight = torch.tensor([[-1, 2], [3, 5]]).float()
         model.conv2.module.old_weight.data = weight
-        quantizer.quantize_weight(model.conv2)
+        quantizer.quantize_weight(weight, model.conv2)
         assert math.isclose(model.conv2.module.scale, 6 / 255, abs_tol=eps)
         assert model.conv2.module.zero_point in (42, 43)
         # test value of weight and bias after quantization
@@ -257,7 +257,7 @@ class CompressorTestCase(TestCase):
         bias_valid = torch.tensor([2.3432, 3.4342, 1.3414, 5.2341])
         model.conv2.module.old_weight.data = weight
         model.conv2.module.bias.data = bias
-        quantizer.quantize_weight(model.conv2)
+        quantizer.quantize_weight(weight, model.conv2)
         assert torch.all(torch.isclose(model.conv2.module.weight.data, weight_valid, rtol=1e-4))
         assert torch.all(torch.isclose(model.conv2.module.bias.data, bias_valid, rtol=1e-7))
 
