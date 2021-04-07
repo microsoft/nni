856717d417d4041c195a76e442bcbc94ad6bfb6c
Hao Ni <v-nihao@microsoft.com>
2021-04-07 11:33:06 +0800

    fix rebase

--- ts/nni_manager/training_service/remote_machine/shellExecutor.ts
+++ ts/nni_manager/training_service/remote_machine/shellExecutor.ts
@@ -68,7 +68,7 @@ class ShellExecutor {
 
         this.sshClient.on('ready', async () => {
             // check OS type: windows or else
-            const result = await this.execute("ver")
+            const result = await this.execute("ver");
             if (result.exitCode == 0 && result.stdout.search("Windows") > -1) {
                 this.osCommands = new WindowsCommands();
                 this.isWindows = true;
@@ -109,15 +109,6 @@ class ShellExecutor {
 
     public close(): void {
         this.sshClient.end();
-    }
-
-    public async GetIsWindows(): Promise<boolean>{
-        const result = await this.execute("ver")
-        if (result.exitCode == 0 && result.stdout.search("Windows") > -1) {
-            return true;
-        }
-
-        return false;
     }
 
     public addUsage(): boolean {
