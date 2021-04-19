dea3eb5c6e0523287cfc4855c13413d72d6d8413
J-shang <shangning128@163.com>
2021-04-19 17:27:42 +0800

    add nnimanager status VIEWED

--- ts/nni_manager/core/nnimanager.ts
+++ ts/nni_manager/core/nnimanager.ts
@@ -204,7 +204,7 @@ class NNIManager implements Manager {
         this.experimentProfile = await this.dataStore.getExperimentProfile(experimentId);
         this.readonly = readonly;
         if (readonly) {
-            this.setStatus('STOPPED');
+            this.setStatus('VIEWED');
             return Promise.resolve();
         }
 
