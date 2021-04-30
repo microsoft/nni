772e6bdf091719546666003ca65d35ef45bd444c
J-shang <shangning128@163.com>
2021-04-30 04:57:07 +0000

    temp sync

--- examples/model_compress/pruning/mixed_prune_torch.py
+++ examples/model_compress/pruning/mixed_prune_torch.py
@@ -109,7 +109,7 @@ def main(args):
     }]
 
     masker_config = {
-        'default': 'level'
+        'default': ('level', {})
     }
 
     pruner = MixedMaskerPruner(model, prune_config, masker_config, optimizer_finetune)
@@ -135,7 +135,7 @@ if __name__ == '__main__':
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+    parser.add_argument('--epochs', type=int, default=2, metavar='N',
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
                         help='learning rate (default: 1.0)')
--- nni/algorithms/compression/pytorch/pruning/mixed_masker_pruner.py
+++ nni/algorithms/compression/pytorch/pruning/mixed_masker_pruner.py
@@ -31,7 +31,7 @@ class MixedMasker(WeightMasker):
 
     def calc_mask(self, sparsity, wrapper, wrapper_idx=None, **depen_kwargs):
         masker_name = 'default' if wrapper.config.get('masker_name') is None else wrapper.config['masker_name']
-        masker = self.maskers[wrapper.config]
+        masker = self.maskers[masker_name]
         if isinstance(masker, StructuredWeightMasker):
             return masker.calc_mask(sparsity, wrapper, wrapper, **depen_kwargs)
         else:
@@ -63,40 +63,3 @@ class MixedMaskerPruner(_StructuredFilterPruner):
         }], model, _logger)
 
         schema.validate(config_list)
-
-
-if __name__ == '__main__':
-    import torch.nn as nn
-    import torch.nn.functional as F
-
-    class LeNet(nn.Module):
-        def __init__(self):
-            super(LeNet, self).__init__()
-            self.conv1 = nn.Conv2d(1, 32, 3, 1)
-            self.conv2 = nn.Conv2d(32, 64, 3, 1)
-            self.dropout1 = nn.Dropout2d(0.25)
-            self.dropout2 = nn.Dropout2d(0.5)
-            self.fc1 = nn.Linear(9216, 128)
-            self.fc2 = nn.Linear(128, 10)
-
-        def forward(self, x):
-            x = self.conv1(x)
-            x = F.relu(x)
-            x = self.conv2(x)
-            x = F.relu(x)
-            x = F.max_pool2d(x, 2)
-            x = self.dropout1(x)
-            x = torch.flatten(x, 1)
-            x = self.fc1(x)
-            x = F.relu(x)
-            x = self.dropout2(x)
-            x = self.fc2(x)
-            output = F.log_softmax(x, dim=1)
-            return output
-
-    model = LeNet()
-    pruner = MixedMaskerPruner(model, CONFIG_LIST, MASKERS_CONFIG_DICT)
-    masker = MixedMasker(model, pruner, MASKERS_CONFIG_DICT)
-
-    def test(**something):
-        print(something)
