bc317ccf0c774aecec7d36a157ca8955ce55622b
Di Wu <xiaowu200031@gmail.com>
2021-05-10 15:50:16 +0900

    Add initialization code; change hpo benchmark autorun code

--- nni/algorithms/hpo/benchmark/runbenchmark_nni.sh
+++ nni/algorithms/hpo/benchmark/runbenchmark_nni.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-outdir='results_nni'
+time=$(date "+%Y%m%d%H%M%S")
+installation='automlbenchmark'
+outdir="results_$time"
 benchmark='nnivalid'
 
 for tuner in 'NNI_TPE' 'NNI_RANDOM_SEARCH' 'NNI_ANNEAL' 'NNI_EVOLUTION' 'NNI_SMAC' 'NNI_GP' 'NNI_METIS' 'NNI_HYPERBAND' 'NNI_BOHB'; do
-    echo "python runbenchmark.py $tuner $benchmark -o $outdir -u nni"
-    python runbenchmark.py $tuner $benchmark -o $outdir -u nni
+    echo "python $installation/runbenchmark.py $tuner $benchmark -o $outdir -u nni"
+    python $installation/runbenchmark.py $tuner $benchmark -o $outdir -u nni
 done
 
 python parse_result_csv.py "$outdir/results.csv"
---
+++ nni/algorithms/hpo/benchmark/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+# setup code
+git clone https://github.com/openml/automlbenchmark.git --branch stable --depth 1
