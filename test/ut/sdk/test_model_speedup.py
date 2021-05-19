002e68e7c531b6e573af7234f6be3062b3731397
Ningxin Zheng <Ningxin.Zheng@microsoft.com>
2021-05-19 09:51:14 +0000

    use smaller model for pipeline test

--- test/ut/sdk/test_model_speedup.py
+++ test/ut/sdk/test_model_speedup.py
@@ -323,10 +323,10 @@ class SpeedupTestCase(TestCase):
 
         Gen_cfg_funcs = [generate_random_sparsity, generate_random_sparsity_v2]
 
-        for model_name in ['vgg16', 'resnet18', 'mobilenet_v2']# 'squeezenet1_1', 'densenet121',
-                           # 'inception_v3' inception is too large and may fail the pipeline
-                           # 'resnet50']:
-                            
+        # for model_name in ['vgg16', 'resnet18', 'mobilenet_v2', 'squeezenet1_1', 'densenet121',
+        #                    # 'inception_v3' inception is too large and may fail the pipeline
+        #                     'resnet50']:
+        for model_name in ['vgg16', 'resnet18', 'mobilenet_v2']:        
             for gen_cfg_func in Gen_cfg_funcs:
                 kwargs = {
                     'pretrained': True
