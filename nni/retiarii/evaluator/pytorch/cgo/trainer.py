71e9a887f8827b03ee78c1d5239dcf3444d077e4
Zhenhua Han <hzhua201@gmail.com>
2021-06-08 06:37:23 +0000

    doc string for cgo.Trainer

--- nni/retiarii/evaluator/pytorch/cgo/trainer.py
+++ nni/retiarii/evaluator/pytorch/cgo/trainer.py
@@ -7,6 +7,20 @@ from .accelerator import BypassAccelerator
 
 @serialize_cls
 class Trainer(pl.Trainer):
+    """
+    Trainer for cross-graph optimization.
+
+    Parameters
+    ----------
+    use_cgo : bool
+        Whether cross-graph optimization (CGO) is used. 
+        If it is True, CGO will manage device placement.
+        Any device placement from pytorch lightning will be bypassed. 
+        default: False
+    trainer_kwargs : dict
+        Optional keyword arguments passed to trainer. See
+        `Lightning documentation <https://pytorch-lightning.readthedocs.io/en/stable/trainer.html>`__ for details.
+    """
     def __init__(self, use_cgo=False, **trainer_kwargs):
         if use_cgo:
             if "accelerator" in trainer_kwargs:
