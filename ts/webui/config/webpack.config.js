be80e02151b66086b8f5be6c0de421c356e9f0e1
liuzhe <zhe.liu@microsoft.com>
2021-06-15 10:32:50 +0800

    fix manifest

--- ts/webui/config/webpack.config.js
+++ ts/webui/config/webpack.config.js
@@ -13,7 +13,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 //const safePostCssParser = require('postcss-safe-parser');
-const ManifestPlugin = require('webpack-manifest-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
@@ -544,20 +544,20 @@ module.exports = function(webpackEnv) {
       // Generate a manifest file which contains a mapping of all asset filenames
       // to their corresponding output file so that tools can pick it up without
       // having to parse `index.html`.
-      //new ManifestPlugin({
-      //  fileName: 'asset-manifest.json',
-      //  publicPath: publicPath,
-      //  generate: (seed, files) => {
-      //    const manifestFiles = files.reduce(function(manifest, file) {
-      //      manifest[file.name] = file.path;
-      //      return manifest;
-      //    }, seed);
+      new WebpackManifestPlugin({
+        fileName: 'asset-manifest.json',
+        publicPath: publicPath,
+        generate: (seed, files) => {
+          const manifestFiles = files.reduce(function(manifest, file) {
+            manifest[file.name] = file.path;
+            return manifest;
+          }, seed);
 
-      //    return {
-      //      files: manifestFiles,
-      //    };
-      //  },
-      //}),
+          return {
+            files: manifestFiles,
+          };
+        },
+      }),
       // Moment.js is an extremely popular library that bundles large locale files
       // by default due to how Webpack interprets its code. This is a practical
       // solution that requires the user to opt into importing specific locales.
