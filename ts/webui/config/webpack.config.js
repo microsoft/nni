716ef3685152b6dceceadc2f7d37c48c6c6e9b3a
liuzhe <zhe.liu@microsoft.com>
2021-06-25 11:26:53 +0800

    static file prefix

--- ts/webui/config/webpack.config.js
+++ ts/webui/config/webpack.config.js
@@ -58,14 +58,12 @@ module.exports = function(webpackEnv) {
   const publicPath = './';
   // Some apps do not use client-side routing with pushState.
   // For these, "homepage" can be set to "." to enable relative asset paths.
-  const shouldUseRelativeAssetPaths = publicPath === './';
+  const shouldUseRelativeAssetPaths = true;
 
   // `publicUrl` is just like `publicPath`, but we will provide it to our app
   // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
   // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
-  const publicUrl = isEnvProduction
-    ? publicPath.slice(0, -1)
-    : isEnvDevelopment && '';
+  const publicUrl = '.';
   // Get environment variables to inject into our app.
   const env = getClientEnvironment(publicUrl);
 
