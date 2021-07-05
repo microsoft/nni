398d94d8129f9238a53ce335781315ef084b5660
Zhenhua Han <hzhua201@gmail.com>
2021-07-05 06:41:29 +0000

    test of retiarii local training service placement constraint

---
+++ ts/nni_manager/training_service/test/retiariiLocalTrainingService.test.ts
@@ -0,0 +1,199 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+'use strict';
+
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as tmp from 'tmp';
+import * as component from '../../common/component';
+import { TrialJobApplicationForm, TrialJobDetail} from '../../common/trainingService';
+import { cleanupUnitTest, delay, prepareUnitTest, getExperimentRootDir } from '../../common/utils';
+import { TrialConfigMetadataKey } from '../common/trialConfigMetadataKey';
+import { RetiariiLocalTrainingService } from '../retiarii_local/retiariiLocalTrainingService';
+import { ExperimentConfig } from '../../common/experimentConfig';
+
+// TODO: copy mockedTrail.py to local folder
+const localCodeDir: string = tmp.dirSync().name.split('\\').join('\\\\');
+const mockedTrialPath: string = './training_service/test/mockedTrial.py'
+fs.copyFileSync(mockedTrialPath, localCodeDir + '/mockedTrial.py')
+
+describe('Unit Test for RetiariiLocalTrainingService', () => {
+    const config = <ExperimentConfig>{
+        trialCommand: 'sleep 1h && echo hello',
+        trialCodeDirectory: `${localCodeDir}`,
+        trialGpuNumber: 1,
+        trainingService: {
+            platform: 'retiarii_local'
+        }
+    };
+
+    const config2 = <ExperimentConfig>{
+        trialCommand: 'python3 mockedTrial.py',
+        trialCodeDirectory: `${localCodeDir}`,
+        trialGpuNumber: 0,
+        trainingService: {
+            platform: 'retiarii_local'
+        }
+    };
+
+    before(() => {
+        chai.should();
+        chai.use(chaiAsPromised);
+        prepareUnitTest();
+    });
+
+    after(() => {
+        cleanupUnitTest();
+    });
+
+    it('List empty trial jobs', async () => {
+        const localTrainingService = new RetiariiLocalTrainingService(config);
+        localTrainingService.run();
+
+        //trial jobs should be empty, since there are no submitted jobs
+        chai.expect(await localTrainingService.listTrialJobs()).to.be.empty;
+
+        localTrainingService.cleanUp();
+    });
+
+    it('Submit job and Cancel job', async () => {
+        const localTrainingService = new RetiariiLocalTrainingService(config);
+        localTrainingService.run();
+
+        // submit job
+        const form: TrialJobApplicationForm = {
+            sequenceId: 0,
+            hyperParameters: {
+                value: 'mock hyperparameters',
+                index: 0
+            }
+        };
+        const jobDetail: TrialJobDetail = await localTrainingService.submitTrialJob(form);
+        chai.expect(jobDetail.status).to.be.equals('WAITING');
+        await localTrainingService.cancelTrialJob(jobDetail.id);
+        chai.expect(jobDetail.status).to.be.equals('USER_CANCELED');
+
+        localTrainingService.cleanUp();
+    }).timeout(20000);
+
+    it('Get trial log', async () => {
+        const localTrainingService = new RetiariiLocalTrainingService(config);
+        localTrainingService.run();
+
+        // submit job
+        const form: TrialJobApplicationForm = {
+            sequenceId: 0,
+            hyperParameters: {
+                value: 'mock hyperparameters',
+                index: 0
+            }
+        };
+
+        const jobDetail: TrialJobDetail = await localTrainingService.submitTrialJob(form);
+
+        // get trial log
+        const rootDir: string = getExperimentRootDir()
+        fs.mkdirSync(path.join(rootDir, 'trials'))
+        fs.mkdirSync(jobDetail.workingDirectory)
+        fs.writeFileSync(path.join(jobDetail.workingDirectory, 'trial.log'), 'trial log')
+        fs.writeFileSync(path.join(jobDetail.workingDirectory, 'stderr'), 'trial stderr')
+        chai.expect(await localTrainingService.getTrialLog(jobDetail.id, 'TRIAL_LOG')).to.be.equals('trial log');
+        chai.expect(await localTrainingService.getTrialLog(jobDetail.id, 'TRIAL_ERROR')).to.be.equals('trial stderr');
+        fs.unlinkSync(path.join(jobDetail.workingDirectory, 'trial.log'))
+        fs.unlinkSync(path.join(jobDetail.workingDirectory, 'stderr'))
+        fs.rmdirSync(jobDetail.workingDirectory)
+        fs.rmdirSync(path.join(rootDir, 'trials'))
+
+        await localTrainingService.cancelTrialJob(jobDetail.id);
+        localTrainingService.cleanUp();
+    }).timeout(20000);
+
+    it('Read metrics, Add listener, and remove listener', async () => {
+        const localTrainingService = new RetiariiLocalTrainingService(config2);
+        localTrainingService.run();
+
+        // set meta data
+        // submit job
+        const form: TrialJobApplicationForm = {
+            sequenceId: 0,
+            hyperParameters: {
+                value: 'mock hyperparameters',
+                index: 0
+            }
+        };
+        const jobDetail: TrialJobDetail = await localTrainingService.submitTrialJob(form);
+        chai.expect(jobDetail.status).to.be.equals('WAITING');
+        localTrainingService.listTrialJobs().then((jobList)=>{
+            chai.expect(jobList.length).to.be.equals(1);
+        });
+        // Add metrics listeners
+        const listener1 = function f1(metric: any) {
+            chai.expect(metric.id).to.be.equals(jobDetail.id);
+        }
+        localTrainingService.addTrialJobMetricListener(listener1);
+        // Wait to collect metric
+        await delay(1000);
+
+        await localTrainingService.cancelTrialJob(jobDetail.id);
+        localTrainingService.removeTrialJobMetricListener(listener1);
+        localTrainingService.cleanUp();
+    }).timeout(20000);
+
+    it('Submit job with topology placement constraint', async () => {
+        const localTrainingService = new RetiariiLocalTrainingService(config);
+        localTrainingService.run();
+
+        // submit job asking for 2-GPU in one server
+        const form: TrialJobApplicationForm = {
+            sequenceId: 0,
+            hyperParameters: {
+                value: 'mock hyperparameters',
+                index: 0
+            },
+            placementConstraint: {
+                type: "Topology",
+                gpus: [2]
+            }
+        };
+        const jobDetail: TrialJobDetail = await localTrainingService.submitTrialJob(form);
+        chai.expect(jobDetail.status).to.be.equals('WAITING');
+        // const [success, resource] = localTrainingService.tryGetAvailableResource(jobDetail.form.placementConstraint!);
+        // chai.expect(success).to.be.equals(true);
+        // chai.expect(resource).to.be.equals({gpuIndices:[1,2]});
+        await localTrainingService.cancelTrialJob(jobDetail.id);
+        chai.expect(jobDetail.status).to.be.equals('USER_CANCELED');
+
+        localTrainingService.cleanUp();
+    }).timeout(20000);
+
+    it('Submit job with device placement constraint', async () => {
+        const localTrainingService = new RetiariiLocalTrainingService(config);
+        localTrainingService.run();
+
+        // submit job asking for GPU-1 and GPU-2 on Node-0
+        const form: TrialJobApplicationForm = {
+            sequenceId: 0,
+            hyperParameters: {
+                value: 'mock hyperparameters',
+                index: 0
+            },
+            placementConstraint: {
+                type: "Device",
+                gpus: [[0,1], [0,2]]
+            }
+        };
+        const jobDetail: TrialJobDetail = await localTrainingService.submitTrialJob(form);
+        chai.expect(jobDetail.status).to.be.equals('WAITING');
+        // const [success, resource] = localTrainingService.tryGetAvailableResource(jobDetail.form.placementConstraint!);
+        // chai.expect(success).to.be.equals(true);
+        // chai.expect(resource).to.deep.equals({gpuIndices:[1,2]});
+
+        await localTrainingService.cancelTrialJob(jobDetail.id);
+        chai.expect(jobDetail.status).to.be.equals('USER_CANCELED');
+
+        localTrainingService.cleanUp();
+    }).timeout(20000);
+});
