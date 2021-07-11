9fe65298078d2711e388e8b0d096824062a66202
Di Wu <xiaowu200031@gmail.com>
2021-07-11 15:55:13 +0900

    fix parallel run bug

--- examples/trials/benchmarking/automlbenchmark/runbenchmark_nni.sh
+++ examples/trials/benchmarking/automlbenchmark/runbenchmark_nni.sh
@@ -4,7 +4,7 @@ time=$(date "+%Y%m%d%H%M%S")
 installation='automlbenchmark'
 outdir="results_$time"
 benchmark='nnivalid'      # 'nnismall'  
-serialize=$true           # if false, run all experiments together in background
+serialize=true           # if false, run all experiments together in background
 
 mkdir $outdir $outdir/scorelogs $outdir/reports 
 
@@ -14,7 +14,7 @@ else
     tuner_array=( "$@" )
 fi
 
-if [ $serialize ]; then
+if [ "$serialize" = true ]; then
     # run tuners serially 
     for tuner in ${tuner_array[*]}; do
 	echo "python $installation/runbenchmark.py $tuner $benchmark -o $outdir -u nni"
