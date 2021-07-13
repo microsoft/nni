99c8fdc3d679811b0f6e33e0b0031bd0f79aac15
kalineid <noob@mail.ustc.edu.cn>
2021-07-13 13:29:32 +0800

    Move LatencyFilter to examples

--- examples/nas/oneshot/spos/multi_trial.py
+++ examples/nas/oneshot/spos/multi_trial.py
@@ -12,6 +12,8 @@ from torchvision.datasets import CIFAR10
 
 from blocks import ShuffleNetBlock, ShuffleXceptionBlock
 
+from nn_meter import get_default_config, load_latency_predictors
+
 
 class ShuffleNetV2(nn.Module):
     block_keys = [
@@ -122,6 +124,35 @@ class ShuffleNetV2(nn.Module):
                 torch.nn.init.normal_(m.weight, 0, 0.01)
                 if m.bias is not None:
                     torch.nn.init.constant_(m.bias, 0)
+
+
+class LatencyFilter:
+    def __init__(self, threshold, config=None, hardware='', reverse=False):
+        """
+        Filter the models according to predcted latency.
+
+        Parameters
+        ----------
+        threshold: `float`
+            the threshold of latency
+        config, hardware:
+            determine the targeted device
+        reverse: `bool`
+            if reverse is `False`, then the model returns `True` when `latency < threshold`,
+            else otherwisse
+        """
+        default_config, default_hardware = get_default_config()
+        if config is None:
+            config = default_config
+        if not hardware:
+            hardware = default_hardware
+
+        self.predictors = load_latency_predictors(config, hardware)
+        self.threshold = threshold
+
+    def __call__(self, ir_model):
+        latency = self.predictors.predict(ir_model, 'nni')
+        return latency < self.threshold
 
 
 @click.command()
--- nni/retiarii/strategy/utils.py
+++ nni/retiarii/strategy/utils.py
@@ -5,7 +5,6 @@
 import collections
 import logging
 from typing import Dict, Any, List
-from nn_meter import get_default_config, load_latency_predictors  # pylint: disable=import-error
 from ..graph import Model
 from ..mutator import Mutator, Sampler
 
@@ -35,35 +34,6 @@ def get_targeted_model(base_model: Model, mutators: List[Mutator], sample: dict)
     for mutator in mutators:
         model = mutator.bind_sampler(sampler).apply(model)
     return model
-
-
-class LatencyFilter:
-    def __init__(self, threshold, config=None, hardware='', reverse=False):
-        """
-        Filter the models according to predcted latency.
-
-        Parameters
-        ----------
-        threshold: `float`
-            the threshold of latency
-        config, hardware:
-            determine the targeted device
-        reverse: `bool`
-            if reverse is `False`, then the model returns `True` when `latency < threshold`,
-            else otherwisse
-        """
-        default_config, default_hardware = get_default_config()
-        if config is None:
-            config = default_config
-        if not hardware:
-            hardware = default_hardware
-
-        self.predictors = load_latency_predictors(config, hardware)
-        self.threshold = threshold
-
-    def __call__(self, ir_model):
-        latency = self.predictors.predict(ir_model, 'nni')
-        return latency < self.threshold
 
 
 def filter_model(model_filter, ir_model):
