ea683f96dd2699efffc3e76c9e39011717ec392f
liuzhe <zhe.liu@microsoft.com>
2021-07-19 11:27:12 +0800

    update test

--- ts/nni_manager/test/common/getIpv4Address.test.ts
+++ ts/nni_manager/test/common/getIpv4Address.test.ts
@@ -5,6 +5,8 @@ import * as assert from 'assert';
 import { getIPV4Address } from '../../common/utils';
 
 it('getIpv4Address', async () => {
-    const ip = await getIPV4Address();
-    assert.match(ip, /^\d+\.\d+\.\d+\.\d+$/)
+    const ip1 = await getIPV4Address();
+    const ip2 = await getIPV4Address();
+    assert.match(ip1, /^\d+\.\d+\.\d+\.\d+$/);
+    assert.equal(ip1, ip2);
 });
