67abd71e3cc5caa9e7866998daf0c009801d7e5e
Zhenhua Han <hzhua201@gmail.com>
2021-07-22 09:44:43 +0000

    change "host" to "node_id" in GPUDevice

--- nni/common/device.py
+++ nni/common/device.py
@@ -3,26 +3,26 @@ from dataclasses import dataclass
 
 @dataclass
 class GPUDevice:
-    host: str
+    node_id: str
     gpu_id: int
     status: str = 'free'
 
     def __eq__(self, o) -> bool:
-        return self.host == o.host and self.gpu_id == o.gpu_id
+        return self.node_id == o.node_id and self.gpu_id == o.gpu_id
 
     def __lt__(self, o) -> bool:
-        if self.host < o.host:
+        if self.node_id < o.node_id:
             return True
-        elif self.host > o.host:
+        elif self.node_id > o.node_id:
             return False
         else:
             return self.gpu_id < o.gpu_id
 
     def __repr__(self) -> str:
-        return "{Server-%s, GPU-%d, Status: %s}" % (self.host, self.gpu_id, self.status)
+        return "{Environment %s, GPU %d, Status %s}" % (self.node_id, self.gpu_id, self.status)
 
     def __hash__(self) -> int:
-        return hash(self.host + '_' + self.gpu_id)
+        return hash(self.node_id + '_' + self.gpu_id)
 
     def set_status(self, status):
         self.status = status
