1eef029a2c21037433d217c5c2413917796e7e47
SparkSnail <shinyang@microsoft.com>
2021-08-02 17:28:34 +0800

    fix build

--- ts/nni_manager/training_service/reusable/environments/kubernetes/kubeflowEnvironmentService.ts
+++ ts/nni_manager/training_service/reusable/environments/kubernetes/kubeflowEnvironmentService.ts
@@ -81,7 +81,9 @@ export class KubeflowEnvironmentService extends KubernetesEnvironmentService {
         if (this.createStoragePromise) {
             await this.createStoragePromise;
         }
-        environment.command = `cd ${this.CONTAINER_MOUNT_PATH}/nni/${this.experimentId} && ${environment.command}`;
+        const expFolder = `${this.CONTAINER_MOUNT_PATH}/nni/${this.experimentId}`;
+        environment.command = `cd ${expFolder} && ${environment.command} \
+        1>${expFolder}/envs/${environment.id}/trialrunner_stdout 2>${expFolder}/envs/${environment.id}/trialrunner_stderr`;
         if (this.config.deprecated && this.config.deprecated.useActiveGpu !== undefined) {
             environment.useActiveGpu = this.config.deprecated.useActiveGpu;
         }
