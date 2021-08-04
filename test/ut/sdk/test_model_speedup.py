c46e8a78913b1ef869cd148caea3ac62e2e7256a
J-shang <shangning128@163.com>
2021-08-04 13:48:30 +0800

    update

--- test/ut/sdk/test_model_speedup.py
+++ test/ut/sdk/test_model_speedup.py
@@ -349,8 +349,7 @@ class SpeedupTestCase(TestCase):
         self.speedup_integration(model_list)
 
     def test_speedup_integration_big(self):
-        model_list = ['vgg11', 'resnet34', 'squeezenet1_1',
-                      'densenet121', 'resnet50', 'wide_resnet50_2']
+        model_list = ['vgg11', 'resnet34', 'squeezenet1_1', 'densenet121']
         mem_info = psutil.virtual_memory()
         ava_gb = mem_info.available/1024.0/1024/1024
         print('Avaliable memory size: %.2f GB' % ava_gb)
