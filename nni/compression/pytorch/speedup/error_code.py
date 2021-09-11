fa95757c3a4193274cc72ec146842013c61b7799
Ningxin Zheng <Ningxin.Zheng@microsoft.com>
2021-09-11 00:06:14 +0000

    update

--- nni/compression/pytorch/speedup/error_code.py
+++ nni/compression/pytorch/speedup/error_code.py
@@ -10,22 +10,22 @@ class SpeedupError(Exception):
 
 class EmptyLayerError(SpeedupError):
     def __init__(self):
-        super(EmptyLayerError, self)("Pruning a Layer to empty is not legal")
+        super(EmptyLayerError, self).__init__("Pruning a Layer to empty is not legal")
 
 class ShapeMisMatchError(SpeedupError):
     def __init__(self):
-        super(ShapeMisMatchError, self)("Shape mismatch!")
+        super(ShapeMisMatchError, self).__init__("Shape mismatch!")
 
 class InputsNumberError(SpeedupError):
     def __init__(self):
-        super(InputsNumberError, self)("The number of the inputs of the target OP is wrong")
+        super(InputsNumberError, self).__init__("The number of the inputs of the target OP is wrong")
 
 class OutputTypeError(SpeedupError):
     def __init__(self, current_type, target_type):
         msg = f"The output type should be {str(target_type)}, but {str(current_type)} founded"
-        super(OutputTypeError, self)(msg)
+        super(OutputTypeError, self).__init__(msg)
 
 class UnBalancedGroupError(SpeedupError):
     def __init__(self):
         msg = "The number remained filters in each group is different"
-        super(UnBalancedGroupError, self)(msg)
+        super(UnBalancedGroupError, self).__init__(msg)
