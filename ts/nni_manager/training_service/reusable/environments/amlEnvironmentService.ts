bc842de6c6bdc529adacc81abab93205e6f74e95
SparkSnail <shinyang@microsoft.com>
2021-09-18 16:00:26 +0800

    init

--- ts/nni_manager/training_service/reusable/environments/amlEnvironmentService.ts
+++ ts/nni_manager/training_service/reusable/environments/amlEnvironmentService.ts
@@ -129,6 +129,11 @@ export class AMLEnvironmentService extends EnvironmentService {
         if (!amlClient) {
             throw new Error('AML client not initialized!');
         }
-        amlClient.stop();
+        const result = await amlClient.stop();
+        if (result) {
+            this.log.info(`Stop ${environment.id} success!`);
+        } else {
+            this.log.info(`Stop ${environment.id} failed!`);
+        }
     }
 }
