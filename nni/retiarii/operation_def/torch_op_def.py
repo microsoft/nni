5b47e79fd1a4075a1e5bbd96f1a3aa48e6b420e5
Zhenhua Han <hzhua201@gmail.com>
2021-09-23 03:11:03 +0000

    format

--- nni/retiarii/operation_def/torch_op_def.py
+++ nni/retiarii/operation_def/torch_op_def.py
@@ -69,7 +69,7 @@ class PrimConstant(PyTorchOperation):
         # TODO: deal with all the types
         if self.parameters['type'] in ['None', 'NoneType']:
             return f'{output} = None'
-        elif self.parameters['type'] in ('int', 'float', 'bool', 'int[]'): # 'Long()' ???
+        elif self.parameters['type'] in ('int', 'float', 'bool', 'int[]'):  # 'Long()' ???
             return f'{output} = {self.parameters["value"]}'
         elif self.parameters['type'] == 'str':
             str_val = self.parameters["value"]
@@ -507,20 +507,23 @@ class ToDevice(PyTorchOperation):
         self.overridden_device_repr = device_repr
 
     def __repr__(self):
-        if self.overridden_device_repr == None:
+        if self.overridden_device_repr is None:
             return f'to("{self.device.device_repr()}")'
         else:
             return f'to("{self.overridden_device_repr}")'
 
     def to_forward_code(self, field: str, output: str, inputs: List[str], inputs_value: List[Any]) -> str:
-        if self.overridden_device_repr == None:
+        if self.overridden_device_repr is None:
             forward_code = f'{output} = {inputs[0]}.to("{self.device.device_repr()}")'
         else:
             forward_code = f'{output} = {inputs[0]}.to("{self.overridden_device_repr}")'
         return forward_code
+
+
 class AtenDet(PyTorchOperation):
     # for torch 1.9
     # NOTE: it is not included in the above aten ops, maybe because torch.det is alias for torch.linalg.det
     _ori_type_name = ['aten::linalg_det']
+
     def to_forward_code(self, field: str, output: str, inputs: List[str], inputs_value: List[Any] = None) -> str:
         return f'{output} = torch.det({inputs[0]})'
