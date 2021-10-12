2cf80f6564c1cd08bdceb50f9bdc4673a21980d1
SparkSnail <shinyang@microsoft.com>
2021-10-13 02:33:15 +0800

    update

--- ts/nni_manager/training_service/reusable/environments/kubernetes/frameworkcontrollerEnvironmentService.ts
+++ ts/nni_manager/training_service/reusable/environments/kubernetes/frameworkcontrollerEnvironmentService.ts
@@ -97,7 +97,7 @@ export class FrameworkControllerEnvironmentService extends KubernetesEnvironment
         }
         environment.maxTrialNumberPerGpu = this.config.maxTrialNumberPerGpu;
 
-        const frameworkcontrollerJobName: string = `nni-exp-${this.experimentId}-env-${environment.id}`.toLowerCase();
+        const frameworkcontrollerJobName: string = `nniexp${this.experimentId}env${environment.id}`.toLowerCase();
         const command = this.generateCommandScript(this.config.taskRoles, environment.command);
         await fs.promises.writeFile(path.join(this.environmentLocalTempFolder, "run.sh"), command, { encoding: 'utf8' });
 
