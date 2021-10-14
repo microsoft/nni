9958de58f018179981dade205952618f4f747a9d
J-shang <shangning128@163.com>
2021-10-14 10:51:39 +0800

    update

--- nni/algorithms/compression/v2/pytorch/pruning/__init__.py
+++ nni/algorithms/compression/v2/pytorch/pruning/__init__.py
@@ -1,9 +1,3 @@
 from .basic_pruner import *
 from .basic_scheduler import PruningScheduler
-from .tools.task_generator import (
-    LinearTaskGenerator,
-    AGPTaskGenerator,
-    LotteryTicketTaskGenerator,
-    SimulatedAnnealingTaskGenerator
-)
 from .iterative_pruner import *
