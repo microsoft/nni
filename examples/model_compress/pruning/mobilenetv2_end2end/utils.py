13b4aedfe91f73575a8bfcff498b2373fb16c67d
v-linbin <v-linbin@microsoft.com>
2021-10-14 23:36:41 +0800

    update

--- examples/model_compress/pruning/mobilenetv2_end2end/utils.py
+++ examples/model_compress/pruning/mobilenetv2_end2end/utils.py
@@ -7,7 +7,8 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 import numpy as np
 from nni.compression.pytorch.utils.counter import count_flops_params
-
+from nni.compression.pytorch.utils import get_module_by_name
+from nni.compression.pytorch.speedup import ModelSpeedup
 import sys
 sys.path.append('../../models')
 from mobilenet import MobileNet
@@ -102,3 +103,62 @@ def count_flops(model, log=None, device=None):
     if log is not None:
         log.write(f"FLOPs: {flops}, params: {params}\n")
     return flops, params
+
+def align_speedup(model, dummy_input, mask_file):
+    ms = ModelSpeedup(model, dummy_input, mask_file)
+    ms.speedup_model()
+    
+    align = 16
+    for name, module in model.named_modules():
+        if isinstance(module, torch.nn.Conv2d):
+            in_channel = module.in_channels
+            out_channel = module.out_channels
+            depthwise = False
+            if in_channel == out_channel and in_channel == module.groups:
+                depthwise =  True
+            if name != "conv1.0":
+                new_in_c = in_channel + align - in_channel % align
+            else:
+                new_in_c = in_channel
+            new_out_c = out_channel + align - out_channel % align
+            print(f"{name} In Channel:{in_channel} -> {new_in_c}  Out Channel:{out_channel}->{new_out_c}")
+            new_conv = torch.nn.Conv2d(in_channels = new_in_c, out_channels=new_out_c, groups=module.groups if not depthwise else new_in_c, kernel_size=module.kernel_size, stride=module.stride, padding = module.padding, bias = module.bias is not None).to(module.weight.device)
+            new_conv.weight.data[:] = 0
+            new_conv.weight.data[:out_channel, :in_channel] = module.weight.data
+            if new_conv.bias is not None:
+                new_conv.bias.data[:] = 0
+                new_conv.bias.data[:out_channel] = module.bias.data
+            father, leaf = get_module_by_name(model, name)
+            setattr(father, name.split('.')[-1], new_conv)
+        elif isinstance(module, torch.nn.BatchNorm2d):
+            num_features = module.num_features
+            new_features = num_features + align - num_features % align
+            new_norm = torch.nn.BatchNorm2d(num_features=new_features,
+                            eps=module.eps,
+                            momentum=module.momentum,
+                            affine=module.affine,
+                            track_running_stats=module.track_running_stats).to(module.weight.device)
+            new_norm.bias.data[:] = 0
+            new_norm.bias.data[: num_features] = module.bias.data 
+            new_norm.weight.data[:] = 0
+            new_norm.weight.data[: num_features] = module.weight.data 
+
+            new_norm.running_mean.data[:] = 0
+            new_norm.running_mean.data[: num_features] = module.running_mean.data 
+
+            new_norm.running_var.data[:] = 0
+            new_norm.running_var.data[: num_features] = module.running_var.data 
+            father, leaf = get_module_by_name(model, name)
+            setattr(father, name.split('.')[-1], new_norm)
+        elif isinstance(module, torch.nn.Linear):
+            in_feat= module.in_features
+            out_feat = module.out_features
+            new_in_feat = in_feat + align - in_feat % align
+            new_linear = torch.nn.Linear(new_in_feat, out_feat, bias=module.bias is not None).to(module.weight.device)
+            new_linear.weight.data[:] = 0
+            new_linear.weight.data[:,:in_feat] = module.weight.data
+            if new_linear.bias is not None:
+                new_linear.bias.data = module.bias.data
+            father, leaf = get_module_by_name(model, name)
+            setattr(father, name.split('.')[-1], new_linear)
+    return model
