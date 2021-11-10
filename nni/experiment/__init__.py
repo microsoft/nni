2e8fa0e01dfa32478377a19f2ef71c63252721df
liuzhe <zhe.liu@microsoft.com>
2021-11-10 17:29:18 +0800

    config refactor - cleanup and base class

--- nni/experiment/__init__.py
+++ nni/experiment/__init__.py
@@ -2,5 +2,5 @@
 # Licensed under the MIT license.
 
 from .config import *
-from .experiment import Experiment
-from .data import *
+#from .experiment import Experiment
+#from .data import *
--- nni/experiment/config/__init__.py
+++ nni/experiment/config/__init__.py
@@ -1,13 +1,13 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from .common import *
-from .local import *
-from .remote import *
-from .openpai import *
-from .aml import *
-from .kubeflow import *
-from .frameworkcontroller import *
-from .adl import *
-from .dlc import *
-from .shared_storage import *
+#from .common import *
+#from .local import *
+#from .remote import *
+#from .openpai import *
+#from .aml import *
+#from .kubeflow import *
+#from .frameworkcontroller import *
+#from .adl import *
+#from .dlc import *
+#from .shared_storage import *
