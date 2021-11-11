1516035a92ad75d17e39c97ef8f784250a7caea4
quzha <Quanlu.Zhang@microsoft.com>
2021-11-11 11:30:32 +0800

    update the logic of search space generation

--- nni/retiarii/strategy/base.py
+++ nni/retiarii/strategy/base.py
@@ -13,14 +13,26 @@ class BaseStrategy(abc.ABC):
 
     @classmethod
     def report_model_space(self, base_model: Model, applied_mutators: List[Mutator]) -> None:
-        sample_space = []
+        """
+        NOTE: nested search space is not supported (currently also not supported on webui),
+        currently, only support type `choice`.
+        """
+        search_space = {}
+        auto_label_seq = 0
         new_model = base_model
         for mutator in applied_mutators:
             recorded_candidates, new_model = mutator.dry_run(new_model)
-            sample_space.extend(recorded_candidates)
-        search_space = {}
-        for i, each in enumerate(sample_space):
-            search_space[str(i)] = {'_type': 'choice', '_value': each}
+            if mutator.label is not None:
+                label = mutator.label
+            else:
+                label = f'auto_label_{auto_label_seq}'
+                auto_label_seq += 1
+            if len(recorded_candidates) > 1:
+                for i, candidate in enumerate(recorded_candidates):
+                    search_space[f'{label}_{i}'] = {'_type': 'choice', '_value': candidate}
+            else:
+                search_space[label] = {'_type': 'choice', '_value': recorded_candidates[0]}
+
         report_search_space(search_space)
 
     @abc.abstractmethod
