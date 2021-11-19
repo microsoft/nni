afdac5fe5c5e614a6cb4c47c3830853e49f0df2f
liuzhe-lz <40699903+liuzhe-lz@users.noreply.github.com>
2021-11-19 14:47:59 +0800

    config refactor stage 1 - ExperimentConfig and LocalConfig (#4313)

--- nni/experiment/config/utils/public.py
+++ nni/experiment/config/utils/public.py
@@ -6,6 +6,11 @@ Utility functions for experiment config classes.
 """
 
 import dataclasses
+import math
+from pathlib import Path
+from typing import Union
+
+PathLike = Union[Path, str]
 
 def is_missing(value):
     """
@@ -16,3 +21,35 @@ def is_missing(value):
     """
     # MISSING is not singleton and there is no official API to check it
     return isinstance(value, type(dataclasses.MISSING))
+
+def canonical_gpu_indices(indices):
+    if isinstance(indices, str):
+        return [int(idx) for idx in indices.split(',')]
+    if isinstance(indices, int):
+        return [indices]
+    return indices
+
+def validate_gpu_indices(indices):
+    if indices is None:
+        return
+    if len(set(indices)) != len(indices):
+        raise ValueError(f'Duplication detected in GPU indices {indices}')
+    if any(idx < 0 for idx in indices):
+        raise ValueError(f'Negative detected in GPU indices {indices}')
+
+def parse_time(value):
+    return _parse_unit(value, 's', _time_units)
+
+_time_units = {'d': 24 * 3600, 'h': 3600, 'm': 60, 's': 1}
+
+def _parse_unit(value, target_unit, all_units):
+    if not isinstance(value, str):
+        return value
+    value = value.lower()
+    for unit, factor in all_units.items():
+        if value.endswith(unit):
+            number = value[:-len(unit)]
+            value = float(number) * factor
+            return math.ceil(value / all_units[target_unit])
+    supported_units = ', '.join(all_units.keys())
+    raise ValueError(f'Bad unit in "{value}", supported units are {supported_units}')
