cb6c72ea0391a47c2602171696f8a79010628048
cruiseliu <cruise.pas@gmail.com>
2021-11-25 04:11:21 +0800

    Refactor Hyperopt Tuners (Stage 3) - TPE tuner (#4239)

--- nni/algorithms/hpo/random_tuner.py
+++ nni/algorithms/hpo/random_tuner.py
@@ -9,6 +9,8 @@ You can specify an integer seed to determine random result.
 
 __all__ = ['RandomTuner', 'suggest', 'suggest_parameter']
 
+import logging
+
 import numpy as np
 import schema
 
@@ -16,10 +18,15 @@ from nni import ClassArgsValidator
 from nni.common.hpo_utils import format_search_space, deformat_parameters
 from nni.tuner import Tuner
 
+_logger = logging.getLogger('nni.tuner.random')
+
 class RandomTuner(Tuner):
     def __init__(self, seed=None):
         self.space = None
+        if seed is None:  # explicitly generate a seed to make the experiment reproducible
+            seed = np.random.default_rng().integers(2 ** 31)
         self.rng = np.random.default_rng(seed)
+        _logger.info(f'Using random seed {seed}')
 
     def update_search_space(self, space):
         self.space = format_search_space(space)
