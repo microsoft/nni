5ce474bc8dbd841641d930d5e72098fd8d745f44
Panacea <48376348+Fiascolsy@users.noreply.github.com>
2021-11-26 17:13:18 +0800

    [Model Compression] V2 doc translation (#4328)

--- nni/algorithms/compression/v2/pytorch/pruning/basic_pruner.py
+++ nni/algorithms/compression/v2/pytorch/pruning/basic_pruner.py
@@ -347,8 +347,7 @@ class SlimPruner(BasicPruner):
         Supported keys:
             - sparsity : This is to specify the sparsity for each layer in this config to be compressed.
             - sparsity_per_layer : Equals to sparsity.
-            - total_sparsity : This is to specify the total sparsity for all layers in this config,
-                each layer may have different sparsity.
+            - total_sparsity : This is to specify the total sparsity for all layers in this config, each layer may have different sparsity.
             - max_sparsity_per_layer : Always used with total_sparsity. Limit the max sparsity of each layer.
             - op_types : Only BatchNorm2d is supported in SlimPruner.
             - op_names : Operation names to prune.
@@ -562,8 +561,7 @@ class TaylorFOWeightPruner(BasicPruner):
         Supported keys:
             - sparsity : This is to specify the sparsity for each layer in this config to be compressed.
             - sparsity_per_layer : Equals to sparsity.
-            - total_sparsity : This is to specify the total sparsity for all layers in this config,
-                each layer may have different sparsity.
+            - total_sparsity : This is to specify the total sparsity for all layers in this config, each layer may have different sparsity.
             - max_sparsity_per_layer : Always used with total_sparsity. Limit the max sparsity of each layer.
             - op_types : Conv2d and Linear are supported in TaylorFOWeightPruner.
             - op_names : Operation names to prune.
