8c593033b9f5fb8d1d20be14c5d9fa1d34460237
liuzhe <zhe.liu@microsoft.com>
2021-12-06 13:04:41 +0800

    comments

--- nni/runtime/config.py
+++ nni/runtime/config.py
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+from datetime import datetime
 import os
 from pathlib import Path
 import shutil
@@ -8,6 +9,8 @@ import sys
 
 import nni
 
+# the version when default config got modified
+# don't need to update if default config does not change
 latest_config_version = '2.6'
 
 def get_config_directory() -> Path:
@@ -23,11 +26,14 @@ def get_config_directory() -> Path:
         config_dir = Path(os.environ['APPDATA'], 'nni')
     else:
         config_dir = Path.home() / '.config/nni'
+
     if config_dir.exists():
         _check_and_update_config_version(config_dir)
     else:
-        config_dir.mkdir(parents=True, exist_ok=True)
-        (config_dir / f'version_{latest_config_version}').touch()
+        config_dir.mkdir(parents=True)
+        info = f'Created by NNI {nni.__version__} at {datetime.now()}'
+        (config_dir / f'version-{latest_config_version}').write_text(info)
+
     return config_dir
 
 def get_config_file(name: str) -> Path:
@@ -42,13 +48,22 @@ def get_config_file(name: str) -> Path:
     return config_file
 
 def _check_and_update_config_version(config_dir):
-    latest = config_dir / f'version_{latest_config_version}'
-    if latest.exists():
+    # nni creates a file like "version-2.6" in config dir to identify the version of config files
+    # the number means that config content was lastly updated at version 2.6, while nni's version might be higher
+
+    # version number is saved in file name because it's faster to check
+
+    version_file = config_dir / f'version-{latest_config_version}'
+    if version_file.exists():
         return
+
+    old_version = 0
     for file in config_dir.iterdir():
-        if str(file).startswith('version_'):
+        if str(file).startswith('version-'):
+            old_version = str(file)[8:]
             file.unlink()
-    latest.touch()
+    info = f'Updated by NNI {nni.__version__} from {old_version} at {datetime.now()}'
+    version_file.write_text(info)
 
     from nni.tools.package_utils import update_algo_config
     default_dir = Path(nni.__path__[0], 'runtime/default_config')
