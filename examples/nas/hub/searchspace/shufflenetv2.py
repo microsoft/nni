7669174fe745c3fc41afdc06ce4bdecedd7aeea6
Yuge Zhang <Yuge.Zhang@microsoft.com>
2021-12-23 18:01:20 +0900

    update ShuffleNet

--- examples/nas/hub/searchspace/shufflenetv2.py
+++ examples/nas/hub/searchspace/shufflenetv2.py
@@ -1,158 +1,237 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
-import os
-import pickle
-import re
-
 import torch
-import torch.nn as nn
-from nni.nas.pytorch import mutables
-
-from blocks import ShuffleNetBlock, ShuffleXceptionBlock
+import nni.retiarii.nn.pytorch as nn
+from nni.retiarii import model_wrapper
 
 
-class ShuffleNetV2OneShot(nn.Module):
-    block_keys = [
-        'shufflenet_3x3',
-        'shufflenet_5x5',
-        'shufflenet_7x7',
-        'xception_3x3',
-    ]
+class ShuffleNetBlock(nn.Module):
+    """
+    Describe the basic building block of shuffle net, as described in [shufflenet]_ .
 
-    def __init__(self, input_size=224, first_conv_channels=16, last_conv_channels=1024, n_classes=1000,
-                 op_flops_path="./data/op_flops_dict.pkl", affine=False):
+    When stride = 1, the block expects an input with ``2 * input channels``. Otherwise input channels.
+
+    References
+    ----------
+    .. [shufflenet] Zhang, Xiangyu, et al. "ShuffleNet: An Extremely Efficient Convolutional Neural Network for Mobile Devices".
+        CVPR 2018.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, mid_channels: int, *,
+                 kernel_size: int, stride: int, sequence: str = "pdp", affine: bool = True):
+        super().__init__()
+        assert stride in [1, 2]
+        assert kernel_size in [3, 5, 7]
+        self.channels = in_channels // 2 if stride == 1 else in_channels
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.mid_channels = mid_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.pad = kernel_size // 2
+        self.oup_main = out_channels - self.channels
+        self.affine = affine
+        assert self.oup_main > 0
+
+        self.branch_main = nn.Sequential(*self._decode_point_depth_conv(sequence))
+
+        if stride == 2:
+            self.branch_proj = nn.Sequential(
+                # dw
+                nn.Conv2d(self.channels, self.channels, kernel_size, stride, self.pad,
+                          groups=self.channels, bias=False),
+                nn.BatchNorm2d(self.channels, affine=affine),
+                # pw-linear
+                nn.Conv2d(self.channels, self.channels, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(self.channels, affine=affine),
+                nn.ReLU(inplace=True)
+            )
+        else:
+            # empty block to be compatible with torchscript
+            self.branch_proj = nn.Sequential()
+
+    def forward(self, x):
+        if self.stride == 2:
+            x_proj, x = self.branch_proj(x), x
+        else:
+            x_proj, x = self._channel_shuffle(x)
+        return torch.cat((x_proj, self.branch_main(x)), 1)
+
+    def _decode_point_depth_conv(self, sequence):
+        result = []
+        first_depth = first_point = True
+        pc = c = self.channels
+        for i, token in enumerate(sequence):
+            # compute output channels of this conv
+            if i + 1 == len(sequence):
+                assert token == "p", "Last conv must be point-wise conv."
+                c = self.oup_main
+            elif token == "p" and first_point:
+                c = self.mid_channels
+            if token == "d":
+                # depth-wise conv
+                assert pc == c, "Depth-wise conv must not change channels."
+                result.append(nn.Conv2d(pc, c, self.kernel_size, self.stride if first_depth else 1, self.pad,
+                                        groups=c, bias=False))
+                result.append(nn.BatchNorm2d(c, affine=self.affine))
+                first_depth = False
+            elif token == "p":
+                # point-wise conv
+                result.append(nn.Conv2d(pc, c, 1, 1, 0, bias=False))
+                result.append(nn.BatchNorm2d(c, affine=self.affine))
+                result.append(nn.ReLU(inplace=True))
+                first_point = False
+            else:
+                raise ValueError("Conv sequence must be d and p.")
+            pc = c
+        return result
+
+    def _channel_shuffle(self, x):
+        bs, num_channels, height, width = x.size()
+        # NOTE: this line is commented for torchscript
+        # assert (num_channels % 4 == 0)
+        x = x.reshape(bs * num_channels // 2, 2, height * width)
+        x = x.permute(1, 0, 2)
+        x = x.reshape(2, -1, num_channels // 2, height, width)
+        return x[0], x[1]
+
+
+class ShuffleXceptionBlock(ShuffleNetBlock):
+    """
+    The ``choice_x`` version of shuffle net block, described in [spos]_ .
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, mid_channels: int, *, stride: int, affine: bool = True):
+        super().__init__(in_channels, out_channels, mid_channels,
+                         kernel_size=3, stride=stride, sequence="dpdpdp", affine=affine)
+
+
+@model_wrapper
+class ShuffleNetSpace(nn.Module):
+    """
+    The search space proposed in [spos]_ .
+
+    The basic building block design is inspired by a state-of-the-art manually-designed network -- ShuffleNetV2 [shufflenetv2]_ .
+    There are 20 choice blocks in total. Each choice block has 4 candidates, namely ``choice 3``, ``choice 5``,
+    ``choice_7`` and ``choice_x`` respectively. They differ in kernel sizes and the number of depthwise convolutions.
+    The size of the search space is :math:`4^{20}`.
+
+    Parameters
+    ----------
+    num_labels : int
+        Number of classes for the classification head. Default: 1000.
+    channel_search : bool
+        If true, for each building block, the number of ``mid_channels``
+        (output channels of the first 1x1 conv in each building block) varies from 0.2x to 1.6x (quantized to multiple of 0.2).
+        Here, "k-x" means k times the number of default channels.
+        Otherwise, 1.0x is used by default. Default: false.
+    affine : bool
+        Apply affine to all batch norm. Default: false.
+
+    References
+    ----------
+    .. [shufflenetv2] Ma, Ningning, et al. "Shufflenet v2: Practical guidelines for efficient cnn architecture design."
+        Proceedings of the European conference on computer vision (ECCV). 2018.
+    .. [spos] Guo, Zichao, et al. "Single Path One-Shot Neural Architecture Search with Uniform Sampling".
+        Proceedings of the European conference on computer vision (ECCV). 2020.
+    """
+
+    def __init__(self,
+                 num_labels: int = 1000,
+                 channel_search: bool = False,
+                 affine: bool = False):
         super().__init__()
 
-        assert input_size % 32 == 0
-        with open(os.path.join(os.path.dirname(__file__), op_flops_path), "rb") as fp:
-            self._op_flops_dict = pickle.load(fp)
-
-        self.stage_blocks = [4, 4, 8, 4]
-        self.stage_channels = [64, 160, 320, 640]
-        self._parsed_flops = dict()
-        self._input_size = input_size
-        self._feature_map_size = input_size
-        self._first_conv_channels = first_conv_channels
-        self._last_conv_channels = last_conv_channels
-        self._n_classes = n_classes
-        self._affine = affine
+        self.num_labels = num_labels
+        self.channel_search = channel_search
+        self.affine = affine
+        self.stage_repeats = [4, 4, 8, 4]
+        self.stage_out_channels = [-1, 16, 64, 160, 320, 640, 1024]
 
         # building first layer
+        out_channels = self.stage_out_channels[1]
         self.first_conv = nn.Sequential(
-            nn.Conv2d(3, first_conv_channels, 3, 2, 1, bias=False),
-            nn.BatchNorm2d(first_conv_channels, affine=affine),
+            nn.Conv2d(3, out_channels, 3, 2, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
         )
-        self._feature_map_size //= 2
 
-        p_channels = first_conv_channels
-        features = []
-        for num_blocks, channels in zip(self.stage_blocks, self.stage_channels):
-            features.extend(self._make_blocks(num_blocks, p_channels, channels))
-            p_channels = channels
-        self.features = nn.Sequential(*features)
+        self.features = []
 
+        global_block_idx = 0
+        for stage_idx, num_repeat in enumerate(self.stage_repeats):
+            for block_idx in range(num_repeat):
+                # count global index to give names to choices
+                global_block_idx += 1
+
+                # get ready for input and output
+                in_channels = out_channels
+                out_channels = self.stage_out_channels[stage_idx + 2]
+                stride = 2 if block_idx == 0 else 1
+
+                # mid channels can be searched
+                base_mid_channels = out_channels // 2
+                if self.channel_search:
+                    k_choice_list = [int(base_mid_channels * (.2 * k)) for k in range(1, 9)]
+                    mid_channels = nn.ValueChoice(k_choice_list, label=f'channel_{global_block_idx}')
+                else:
+                    mid_channels = int(base_mid_channels)
+
+                choice_block = nn.LayerChoice([
+                    ShuffleNetBlock(in_channels, out_channels, mid_channels=mid_channels, kernel_size=3, stride=stride, affine=affine),
+                    ShuffleNetBlock(in_channels, out_channels, mid_channels=mid_channels, kernel_size=5, stride=stride, affine=affine),
+                    ShuffleNetBlock(in_channels, out_channels, mid_channels=mid_channels, kernel_size=7, stride=stride, affine=affine),
+                    ShuffleXceptionBlock(in_channels, out_channels, mid_channels=mid_channels, stride=stride, affine=affine)
+                ], label=f'layer_{global_block_idx}')
+                self.features.append(choice_block)
+
+        self.features = nn.Sequential(*self.features)
+
+        # final layers
+        last_conv_channels = self.stage_out_channels[-1]
         self.conv_last = nn.Sequential(
-            nn.Conv2d(p_channels, last_conv_channels, 1, 1, 0, bias=False),
+            nn.Conv2d(out_channels, last_conv_channels, 1, 1, 0, bias=False),
             nn.BatchNorm2d(last_conv_channels, affine=affine),
             nn.ReLU(inplace=True),
         )
-        self.globalpool = nn.AvgPool2d(self._feature_map_size)
+        self.globalpool = nn.AdaptiveAvgPool2d((1, 1))
         self.dropout = nn.Dropout(0.1)
         self.classifier = nn.Sequential(
-            nn.Linear(last_conv_channels, n_classes, bias=False),
+            nn.Linear(last_conv_channels, num_labels, bias=False),
         )
 
         self._initialize_weights()
 
-    def _make_blocks(self, blocks, in_channels, channels):
-        result = []
-        for i in range(blocks):
-            stride = 2 if i == 0 else 1
-            inp = in_channels if i == 0 else channels
-            oup = channels
-
-            base_mid_channels = channels // 2
-            mid_channels = int(base_mid_channels)  # prepare for scale
-            choice_block = mutables.LayerChoice([
-                ShuffleNetBlock(inp, oup, mid_channels=mid_channels, ksize=3, stride=stride, affine=self._affine),
-                ShuffleNetBlock(inp, oup, mid_channels=mid_channels, ksize=5, stride=stride, affine=self._affine),
-                ShuffleNetBlock(inp, oup, mid_channels=mid_channels, ksize=7, stride=stride, affine=self._affine),
-                ShuffleXceptionBlock(inp, oup, mid_channels=mid_channels, stride=stride, affine=self._affine)
-            ])
-            result.append(choice_block)
-
-            # find the corresponding flops
-            flop_key = (inp, oup, mid_channels, self._feature_map_size, self._feature_map_size, stride)
-            self._parsed_flops[choice_block.key] = [
-                self._op_flops_dict["{}_stride_{}".format(k, stride)][flop_key] for k in self.block_keys
-            ]
-            if stride == 2:
-                self._feature_map_size //= 2
-        return result
-
     def forward(self, x):
-        bs = x.size(0)
         x = self.first_conv(x)
         x = self.features(x)
         x = self.conv_last(x)
+
         x = self.globalpool(x)
 
         x = self.dropout(x)
-        x = x.contiguous().view(bs, -1)
+        x = x.contiguous().view(-1, self.stage_out_channels[-1])
         x = self.classifier(x)
         return x
-
-    def get_candidate_flops(self, candidate):
-        conv1_flops = self._op_flops_dict["conv1"][(3, self._first_conv_channels,
-                                                    self._input_size, self._input_size, 2)]
-        # Should use `last_conv_channels` here, but megvii insists that it's `n_classes`. Keeping it.
-        # https://github.com/megvii-model/SinglePathOneShot/blob/36eed6cf083497ffa9cfe7b8da25bb0b6ba5a452/src/Supernet/flops.py#L313
-        rest_flops = self._op_flops_dict["rest_operation"][(self.stage_channels[-1], self._n_classes,
-                                                            self._feature_map_size, self._feature_map_size, 1)]
-        total_flops = conv1_flops + rest_flops
-        for k, m in candidate.items():
-            parsed_flops_dict = self._parsed_flops[k]
-            if isinstance(m, dict):  # to be compatible with classical nas format
-                total_flops += parsed_flops_dict[m["_idx"]]
-            else:
-                total_flops += parsed_flops_dict[torch.max(m, 0)[1]]
-        return total_flops
 
     def _initialize_weights(self):
         for name, m in self.named_modules():
             if isinstance(m, nn.Conv2d):
                 if 'first' in name:
-                    nn.init.normal_(m.weight, 0, 0.01)
+                    torch.nn.init.normal_(m.weight, 0, 0.01)
                 else:
-                    nn.init.normal_(m.weight, 0, 1.0 / m.weight.shape[1])
+                    torch.nn.init.normal_(m.weight, 0, 1.0 / m.weight.shape[1])
                 if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
+                    torch.nn.init.constant_(m.bias, 0)
             elif isinstance(m, nn.BatchNorm2d):
-                if m.weight is not None:
-                    nn.init.constant_(m.weight, 1)
+                torch.nn.init.constant_(m.weight, 1)
                 if m.bias is not None:
-                    nn.init.constant_(m.bias, 0.0001)
-                nn.init.constant_(m.running_mean, 0)
+                    torch.nn.init.constant_(m.bias, 0.0001)
+                torch.nn.init.constant_(m.running_mean, 0)
             elif isinstance(m, nn.BatchNorm1d):
-                nn.init.constant_(m.weight, 1)
+                torch.nn.init.constant_(m.weight, 1)
                 if m.bias is not None:
-                    nn.init.constant_(m.bias, 0.0001)
-                nn.init.constant_(m.running_mean, 0)
+                    torch.nn.init.constant_(m.bias, 0.0001)
+                torch.nn.init.constant_(m.running_mean, 0)
             elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
+                torch.nn.init.normal_(m.weight, 0, 0.01)
                 if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-
-
-def load_and_parse_state_dict(filepath="./data/checkpoint-150000.pth.tar"):
-    checkpoint = torch.load(filepath, map_location=torch.device("cpu"))
-    if "state_dict" in checkpoint:
-        checkpoint = checkpoint["state_dict"]
-    result = dict()
-    for k, v in checkpoint.items():
-        if k.startswith("module."):
-            k = k[len("module."):]
-        result[k] = v
-    return result
+                    torch.nn.init.constant_(m.bias, 0)
