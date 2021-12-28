68a9c18b9105d4ddee33a6cd408c8bc77a2babcb
Yuge Zhang <Yuge.Zhang@microsoft.com>
2021-12-28 15:51:33 +0900

    Support graph engine

--- nni/retiarii/nn/pytorch/mutator.py
+++ nni/retiarii/nn/pytorch/mutator.py
@@ -1,8 +1,9 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+import collections
 import inspect
-from typing import Any, List, Optional, Tuple
+from typing import Any, List, Optional, Tuple, Dict
 
 import torch.nn as nn
 
@@ -63,6 +64,7 @@ class InputChoiceMutator(Mutator):
 
 class ValueChoiceMutator(Mutator):
     def __init__(self, nodes: List[Node], candidates: List[Any]):
+        # use nodes[0] as an example to get label
         super().__init__(label=nodes[0].operation.parameters['label'])
         self.nodes = nodes
         self.candidates = candidates
@@ -76,22 +78,50 @@ class ValueChoiceMutator(Mutator):
             target.update_operation('prim::Constant', {'type': type(chosen).__name__, 'value': chosen})
 
 
-class ParameterChoiceMutator(Mutator):
-    def __init__(self, nodes: List[Tuple[Node, str]], candidates: List[Any]):
-        # argname is the location of the argument
-        # e.g., Conv2d(out_channels=nn.ValueChoice([1, 2, 3])) => argname = "out_channels"
-        # use nodes[0] as an example to get label
-        node, argname = nodes[0]
-        super().__init__(label=node.operation.parameters[argname].label)
-        self.nodes = nodes
+class ParameterChoiceLeafMutator(Mutator):
+    # mutate the leaf node (i.e., ValueChoice) of parameter choices
+    # should be used together with ParameterChoiceMutator
+
+    def __init__(self, candidates: List[Any], label: str):
+        super().__init__(label=label)
         self.candidates = candidates
 
-    def mutate(self, model):
-        chosen = self.choice(self.candidates)
+    def mutate(self, model: Model) -> Model:
+        # leave a record here
+        # real mutations will be done in ParameterChoiceMutator
+        self.choice(self.candidates)
+
+
+class ParameterChoiceMutator(Mutator):
+    # parameter choice mutator is an empty-shell-mutator
+    # calculate all the parameter values based on previous mutations of value choice mutator
+
+    def __init__(self, nodes: List[Tuple[Node, str]]):
+        super().__init__()
+
+        self.nodes = nodes
+
+    def mutate(self, model: Model) -> Model:
+        # looks like {"label1": "cat", "label2": 123}
+        value_choice_decisions = {}
+        for mutation in model.history:
+            if isinstance(mutation.mutator, ValueChoiceMutator):
+                value_choice_decisions[mutation.mutator.label] = mutation.sample[0]
+
         for node, argname in self.nodes:
-            chosen_value = node.operation.parameters[argname].access(chosen)
+            # argname is the location of the argument
+            # e.g., Conv2d(out_channels=nn.ValueChoice([1, 2, 3])) => argname = "out_channels"
+            value_choice: ValueChoiceX = node.operation.parameters[argname]
+
+            # calculate all the values on the leaf node of ValueChoiceX computation graph
+            leaf_node_values = []
+            for choice in value_choice.inner_choices():
+                leaf_node_values.append(value_choice_decisions[choice.label])
+            result_value = value_choice.evaluate(leaf_node_values)
+
+            # update model with graph mutation primitives
             target = model.get_node_by_name(node.name)
-            target.update_operation(target.operation.type, {**target.operation.parameters, argname: chosen_value})
+            target.update_operation(target.operation.type, {**target.operation.parameters, argname: result_value})
 
 
 class RepeatMutator(Mutator):
@@ -148,18 +178,28 @@ def process_inline_mutation(model: Model) -> Optional[List[Mutator]]:
         mutator = ValueChoiceMutator(node_list, node_list[0].operation.parameters['candidates'])
         applied_mutators.append(mutator)
 
-    pc_nodes = []
+    pc_nodes: List[Tuple[Node, str, ValueChoiceX]] = []
     for node in model.get_nodes():
         for name, choice in node.operation.parameters.items():
-            if isinstance(choice, ValueChoice):
-                pc_nodes.append((node, name))
-    pc_nodes = _group_parameters_by_label(pc_nodes)
-    for node_list in pc_nodes:
-        assert _is_all_equal([node.operation.parameters[name].candidates for node, name in node_list]), \
-            'Value choice with the same label must have the same candidates.'
-        first_node, first_argname = node_list[0]
-        mutator = ParameterChoiceMutator(node_list, first_node.operation.parameters[first_argname].candidates)
-        applied_mutators.append(mutator)
+            if isinstance(choice, ValueChoiceX):
+                # e.g., (conv_node, "out_channels", ValueChoice([1, 3]))
+                pc_nodes.append((node, name, choice))
+
+    leaf_value_choices: Dict[str, List[Any]] = {}
+    for _, __, choice in pc_nodes:
+        for inner_choice in choice.inner_choices():
+            if inner_choice.label not in leaf_value_choices:
+                leaf_value_choices[inner_choice.label] = inner_choice.candidates
+            else:
+                assert leaf_value_choices[inner_choice.label] == inner_choice.candidates, \
+                    'Value choice with the same label must have the same candidates, but found ' \
+                    f'{leaf_value_choices[inner_choice.label]} vs. {inner_choice.candidates}'
+
+    for label, candidates in leaf_value_choices.items():
+        applied_mutators.append(ParameterChoiceLeafMutator(candidates, label))
+
+    # in the end, add another parameter choice mutator for "real" mutations
+    applied_mutators.append(ParameterChoiceMutator([(node, name) for node, name, _ in pc_nodes]))
 
     # apply layer choice at last as it will delete some nodes
     lc_nodes = _group_by_label(filter(lambda d: d.operation.parameters.get('mutation') == 'layerchoice',
@@ -317,14 +357,4 @@ def _group_by_label(nodes: List[Node]) -> List[List[Node]]:
         if label not in result:
             result[label] = []
         result[label].append(node)
-    return list(result.values())
-
-
-def _group_parameters_by_label(nodes: List[Tuple[Node, str]]) -> List[List[Tuple[Node, str]]]:
-    result = {}
-    for node, argname in nodes:
-        label = node.operation.parameters[argname].label
-        if label not in result:
-            result[label] = []
-        result[label].append((node, argname))
     return list(result.values())
