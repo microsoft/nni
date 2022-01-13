32ac1abedf1eb086d0c0e6d0844c0ebcbf9eeddc
Fang Dong (FA Talent) <v-fangdong@microsoft.com>
2022-01-13 11:08:57 +0800

    fix import mistake

--- nni/retiarii/oneshot/pytorch/__init__.py
+++ nni/retiarii/oneshot/pytorch/__init__.py
@@ -6,5 +6,6 @@ from .enas import EnasTrainer
 from .proxyless import ProxylessTrainer
 from .random import SinglePathTrainer, RandomTrainer
 from .utils import replace_input_choice, replace_layer_choice
-from .differentiable import MergeTrainValDataset, DartsModel, ProxylessModel
+from .differentiable import DartsModel, ProxylessModel
 from .sampling import EnasModel,RandomSampleModel
+from .base_lightning import MergeTrainValDataset
