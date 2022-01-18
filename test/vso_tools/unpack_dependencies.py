1907fca2cd85e5594c933545dfc1712c34fa9e6d
liuzhe <zhe.liu@microsoft.com>
2022-01-18 12:19:15 +0800

    fix unzip

--- test/vso_tools/unpack_dependencies.py
+++ test/vso_tools/unpack_dependencies.py
@@ -3,13 +3,14 @@ Extract an archive created by pack_dependencies.py.
 """
 
 import json
+import os
 from pathlib import Path
 import shutil
 import site
 from zipfile import ZipFile
 
 def main():
-    ZipFile('cache.zip').extractall()
+    extract_all(ZipFile('cache.zip'))
     empty_dirs = json.loads(Path('directories.json').read_text())
     symlinks = json.loads(Path('symlinks.json').read_text())
     for dir_ in empty_dirs:
@@ -23,6 +24,14 @@ def main():
     shutil.move('cache/python-dependencies', site_packages)
     shutil.move('cache/nni-manager-dependencies', 'ts/nni_manager/node_modules')
     shutil.move('cache/webui-dependencies', 'ts/webui/node_modules')
+
+def extract_all(zf: ZipFile) -> None:
+    # fix a bug in ZipFile.extractall()
+    # see https://stackoverflow.com/questions/39296101
+    for info in zf.infolist():
+        path = zf.extract(info)
+        if info.external_attr > 0xffff:
+            os.chmod(path, info.external_attr >> 16)
 
 if __name__ == '__main__':
     main()
