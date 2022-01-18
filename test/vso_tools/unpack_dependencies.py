a36a40d2927e44478e3bc9e3ab504817f840dbd7
liuzhe <zhe.liu@microsoft.com>
2022-01-18 12:49:59 +0800

    script

--- test/vso_tools/unpack_dependencies.py
+++ test/vso_tools/unpack_dependencies.py
@@ -9,7 +9,7 @@ import shutil
 import site
 from zipfile import ZipFile
 
-def main():
+def main() -> None:
     extract_all(ZipFile('cache.zip'))
     empty_dirs = json.loads(Path('directories.json').read_text())
     symlinks = json.loads(Path('symlinks.json').read_text())
@@ -21,7 +21,13 @@ def main():
     site_packages = Path(site.getusersitepackages())
     assert not site_packages.exists()
     site_packages.parent.mkdir(parents=True, exist_ok=True)
+
+    script_dir = get_user_script_directory()
+    assert not script_dir.exists()
+    script_dir.parent.mkdir(parents=True, exist_ok=True)
+
     shutil.move('cache/python-dependencies', site_packages)
+    shutil.move('cache/python-scripts', script_dir)
     shutil.move('cache/nni-manager-dependencies', 'ts/nni_manager/node_modules')
     shutil.move('cache/webui-dependencies', 'ts/webui/node_modules')
 
@@ -32,6 +38,13 @@ def extract_all(zf: ZipFile) -> None:
         path = zf.extract(info)
         if info.external_attr > 0xffff:
             os.chmod(path, info.external_attr >> 16)
+
+def get_user_script_directory() -> Path:
+    # strangely this is not a site api
+    if sys.platform == 'win32':
+        return Path(site.getuserbase(), f'Python{sys.version_info.major}{sys.version_info.minor}', 'Scripts')
+    else:
+        return Path(site.getuserbase(), 'bin')
 
 if __name__ == '__main__':
     main()
