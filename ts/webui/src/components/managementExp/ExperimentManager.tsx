852605762295f7319fdb485f37d7a905d19c5a4f
Lijiaoa <Lijiaoa@outlook.com>
2022-02-15 07:24:41 +0000

    fix lint

--- ts/webui/src/components/managementExp/ExperimentManager.tsx
+++ ts/webui/src/components/managementExp/ExperimentManager.tsx
@@ -285,7 +285,8 @@ class Experiment extends React.Component<{}, ExpListState> {
                 const searchInput = newValue.trim();
                 let result = originExperimentList.filter(
                     item =>
-                        (item.experimentName !== null && item.experimentName.toLowerCase().includes(searchInput.toLowerCase())) ||
+                        (item.experimentName !== null &&
+                            item.experimentName.toLowerCase().includes(searchInput.toLowerCase())) ||
                         item.id.toLowerCase().includes(searchInput.toLowerCase())
                 );
                 result = this.commonSelectString(result, '');
@@ -402,13 +403,14 @@ class Experiment extends React.Component<{}, ExpListState> {
         let { searchInputVal } = this.state;
         let result = JSON.parse(JSON.stringify(originExperimentList));
         searchInputVal = searchInputVal.trim();
-         // user input some value to filter trial [name, id] first...
-         if(searchInputVal !== '') {
+        // user input some value to filter trial [name, id] first...
+        if (searchInputVal !== '') {
             // reset experiments list to first filter result
             result = originExperimentList.filter(
                 item =>
-                item.id.toLowerCase().includes(searchInputVal.toLowerCase()) ||
-                (item.experimentName !== null && item.experimentName.toLowerCase().includes(searchInputVal.toLowerCase()))
+                    item.id.toLowerCase().includes(searchInputVal.toLowerCase()) ||
+                    (item.experimentName !== null &&
+                        item.experimentName.toLowerCase().includes(searchInputVal.toLowerCase()))
             );
         }
         this.setState(() => ({
