bcd3d4c585d8b44bc81591db470eeaff0fe87a9c
J-shang <shangning128@163.com>
2022-02-17 14:33:44 +0800

    update ut & extend fast test windows timeout

--- test/ut/compression/v2/test_pruner_torch.py
+++ test/ut/compression/v2/test_pruner_torch.py
@@ -25,11 +25,11 @@ from nni.algorithms.compression.v2.pytorch.utils import compute_sparsity_mask2co
 class TorchModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = torch.nn.Conv2d(1, 5, 5, 1)
-        self.bn1 = torch.nn.BatchNorm2d(5)
-        self.conv2 = torch.nn.Conv2d(5, 10, 5, 1)
-        self.bn2 = torch.nn.BatchNorm2d(10)
-        self.fc1 = torch.nn.Linear(4 * 4 * 10, 100)
+        self.conv1 = torch.nn.Conv2d(1, 10, 5, 1)
+        self.bn1 = torch.nn.BatchNorm2d(10)
+        self.conv2 = torch.nn.Conv2d(10, 20, 5, 1)
+        self.bn2 = torch.nn.BatchNorm2d(20)
+        self.fc1 = torch.nn.Linear(4 * 4 * 20, 100)
         self.fc2 = torch.nn.Linear(100, 10)
 
     def forward(self, x):
@@ -37,7 +37,7 @@ class TorchModel(torch.nn.Module):
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4 * 4 * 10)
+        x = x.view(-1, 4 * 4 * 20)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
