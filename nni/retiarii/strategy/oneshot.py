199964199899eb51008da1313fb7203df4c7f143
Yuge Zhang <Yuge.Zhang@microsoft.com>
2022-02-21 16:32:02 +0900

    update

--- nni/retiarii/strategy/oneshot.py
+++ nni/retiarii/strategy/oneshot.py
@@ -1,8 +1,14 @@
+from .base import BaseStrategy
+
 _import_failed = False
+
 try:
-    from nni.retiarii.oneshot.pytorch.strategy import (
-        DartsModule, ProxylessModule, SNASModule, EnasModule, RandomSamplingModule,
-        InterleavedTrainValDataLoader
-    )
+    from nni.retiarii.oneshot.pytorch.strategy import DartsStrategy
 except ImportError:
     _import_failed = True
+
+    class ImportFailedStrategy(BaseStrategy):
+        def run(self, base_model, applied_mutators):
+            raise
+
+    class DartsStrategy
