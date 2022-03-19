bd85615e5d91d154a0d44daaadc7ba9060e6e873
Yuge Zhang <scottyugochang@gmail.com>
2022-03-19 12:32:13 +0000

    fix lint

--- nni/algorithms/hpo/pbt_tuner.py
+++ nni/algorithms/hpo/pbt_tuner.py
@@ -175,7 +175,7 @@ class PBTTuner(Tuner):
     It's a simple asynchronous optimization algorithm which effectively utilizes a fixed computational budget to jointly optimize
     a population of models and their hyperparameters to maximize performance.
     Importantly, PBT discovers a schedule of hyperparameter settings rather than following the generally sub-optimal strategy of
-    trying to find a single fixed set to use for the whole course of training. 
+    trying to find a single fixed set to use for the whole course of training.
 
     .. image:: ../../img/pbt.jpg
 
@@ -237,7 +237,7 @@ class PBTTuner(Tuner):
 
     Examples
     --------
-    Below is an example of PBTTuner configuration in experiment config file. 
+    Below is an example of PBTTuner configuration in experiment config file.
 
     .. code-block:: yaml
 
