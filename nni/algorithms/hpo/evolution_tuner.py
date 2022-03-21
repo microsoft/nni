1c92cd11f694b8ab5bb912f68ef7d6ed891cbdb1
ChrisZRen <dragonmren@gmail.com>
2022-03-21 07:44:41 +0000

    udpate

--- nni/algorithms/hpo/evolution_tuner.py
+++ nni/algorithms/hpo/evolution_tuner.py
@@ -3,7 +3,7 @@
 
 """
 evolution_tuner.py
-"""                         
+"""
 from __future__ import annotations
 
 import copy
@@ -55,12 +55,13 @@ class EvolutionTuner(Tuner):
     """
     Naive Evolution comes from `Large-Scale Evolution of Image Classifiers <https://arxiv.org/pdf/1703.01041.pdf>`__
     It randomly initializes a population based on the search space.
-    For each generation, it chooses better ones and does some mutation (e.g., changes a hyperparameter, adds/removes one layer, etc.) on them to get the next generation. 
+    For each generation, it chooses better ones and does some mutation 
+    (e.g., changes a hyperparameter, adds/removes one layer, etc.) on them to get the next generation.
     Naive Evolution requires many trials to works but it’s very simple and it’s easily expanded with new features.
-    
+
     Examples
     --------
-    
+
     .. code-block::
 
         config.tuner.name = 'Evolution'
@@ -75,8 +76,9 @@ class EvolutionTuner(Tuner):
         Optimize mode, 'maximize' or 'minimize'.
         If 'maximize', the tuner will try to maximize metrics. If 'minimize', the tuner will try to minimize metrics.
     population_size: int
-        The initial size of the population (trial num) in the evolution tuner(default=32). The larger population size, the better evolution performance.
-        It's suggested that ``population_size`` be much larger than ``concurrency`` so users can get the most out of the algorithm. 
+        The initial size of the population (trial num) in the evolution tuner(default=32). 
+        The larger population size, the better evolution performance.
+        It's suggested that ``population_size`` be much larger than ``concurrency`` so users can get the most out of the algorithm.
         And at least ``concurrency``, or the tuner will fail on its first generation of parameters.
     """
 
