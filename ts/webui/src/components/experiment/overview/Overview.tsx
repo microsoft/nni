b0bcde8631a65ac8163970852fc5e12bef5c35f5
Lijiaoa <61399850+Lijiaoa@users.noreply.github.com>
2022-03-23 10:50:02 +0800

    Refine webui (#4548)

---
+++ ts/webui/src/components/experiment/overview/Overview.tsx
@@ -0,0 +1,212 @@
+import * as React from 'react';
+import { Stack, Icon, Dropdown, DefaultButton } from '@fluentui/react';
+import { EXPERIMENT, TRIALS } from '@static/datamodel';
+import { Trial } from '@static/model/trial';
+import { AppContext } from '@/App';
+import { Title } from './Title';
+import SuccessTable from './table/SuccessTable';
+import DefaultPoint from '../trialdetail/chart/DefaultMetricPoint';
+import { BasicInfo } from './params/BasicInfo';
+import { ExpDuration } from './count/ExpDuration';
+import { ExpDurationContext } from './count/ExpDurationContext';
+import { TrialCount } from './count/TrialCount';
+import { Command1 } from './command/Command1';
+import { Command2 } from './command/Command2';
+import { TitleContext } from './TitleContext';
+import { itemStyleSucceed, entriesOption } from './overviewConst';
+import '@style/experiment/overview/overview.scss';
+import '@style/experiment/overview/topTrial.scss';
+import '@style/logPath.scss';
+
+/**
+ * single experiment
+ * overview page
+ */
+
+interface OverviewState {
+    trialConcurrency: number;
+}
+
+export const BestMetricContext = React.createContext({
+    bestAccuracy: 0
+});
+
+class Overview extends React.Component<{}, OverviewState> {
+    static contextType = AppContext;
+    context!: React.ContextType<typeof AppContext>;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            trialConcurrency: EXPERIMENT.trialConcurrency
+        };
+    }
+
+    clickMaxTop = (event: React.SyntheticEvent<EventTarget>): void => {
+        event.stopPropagation();
+        // #999 panel active bgcolor; #b3b3b3 as usual
+        const { changeMetricGraphMode } = this.context;
+        changeMetricGraphMode('max');
+    };
+
+    clickMinTop = (event: React.SyntheticEvent<EventTarget>): void => {
+        event.stopPropagation();
+        const { changeMetricGraphMode } = this.context;
+        changeMetricGraphMode('min');
+    };
+
+    updateEntries = (event: React.FormEvent<HTMLDivElement>, item: any): void => {
+        if (item !== undefined) {
+            this.context.changeEntries(item.key);
+        }
+    };
+
+    render(): React.ReactNode {
+        const bestTrials = this.findBestTrials();
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const bestAccuracy = bestTrials.length > 0 ? bestTrials[0].accuracy! : NaN;
+        const execDuration = EXPERIMENT.profile.execDuration;
+
+        return (
+            <AppContext.Consumer>
+                {(value): React.ReactNode => {
+                    const {
+                        metricGraphMode,
+                        bestTrialEntries,
+                        maxDurationUnit,
+                        expandRowIDs,
+                        updateOverviewPage,
+                        changeMaxDurationUnit,
+                        changeExpandRowIDs
+                    } = value;
+                    const maxActive = metricGraphMode === 'max' ? 'active' : '';
+                    const minActive = metricGraphMode === 'min' ? 'active' : '';
+                    return (
+                        <div className='overview'>
+                            <div className='overviewWrapper'>
+                                {/* exp params */}
+                                <div className='overviewBasicInfo'>
+                                    <TitleContext.Provider value={{ text: 'Experiment', icon: 'AutoRacing' }}>
+                                        <Title />
+                                    </TitleContext.Provider>
+                                    <BestMetricContext.Provider value={{ bestAccuracy: bestAccuracy }}>
+                                        <BasicInfo />
+                                    </BestMetricContext.Provider>
+                                </div>
+                                {/* duration & trial numbers */}
+                                <div className='duration'>
+                                    <TitleContext.Provider value={{ text: 'Duration', icon: 'Timer' }}>
+                                        <Title />
+                                    </TitleContext.Provider>
+                                    <ExpDurationContext.Provider
+                                        value={{
+                                            maxExecDuration: EXPERIMENT.maxExperimentDurationSeconds,
+                                            execDuration,
+                                            updateOverviewPage,
+                                            maxDurationUnit,
+                                            changeMaxDurationUnit
+                                        }}
+                                    >
+                                        <ExpDuration />
+                                    </ExpDurationContext.Provider>
+                                </div>
+                                <div className='trialCount'>
+                                    <TitleContext.Provider value={{ text: 'Trial numbers', icon: 'NumberSymbol' }}>
+                                        <Title />
+                                    </TitleContext.Provider>
+                                    <ExpDurationContext.Provider
+                                        value={{
+                                            maxExecDuration: EXPERIMENT.maxExperimentDurationSeconds,
+                                            execDuration,
+                                            updateOverviewPage,
+                                            maxDurationUnit,
+                                            changeMaxDurationUnit
+                                        }}
+                                    >
+                                        <TrialCount />
+                                    </ExpDurationContext.Provider>
+                                </div>
+                                {/* table */}
+                                <div className='overviewBestMetric'>
+                                    <Stack horizontal>
+                                        <div style={itemStyleSucceed}>
+                                            <TitleContext.Provider value={{ text: 'Top trials', icon: 'BulletedList' }}>
+                                                <Title />
+                                            </TitleContext.Provider>
+                                        </div>
+                                        <div className='topTrialTitle'>
+                                            <Stack horizontal horizontalAlign='end'>
+                                                <DefaultButton
+                                                    onClick={this.clickMaxTop}
+                                                    className={maxActive}
+                                                    styles={{ root: { minWidth: 70, padding: 0 } }}
+                                                >
+                                                    <Icon iconName='Market' />
+                                                    <span className='max'>Max</span>
+                                                </DefaultButton>
+                                                <div className='mincenter'>
+                                                    <DefaultButton
+                                                        onClick={this.clickMinTop}
+                                                        className={minActive}
+                                                        styles={{ root: { minWidth: 70, padding: 0 } }}
+                                                    >
+                                                        <Icon iconName='MarketDown' />
+                                                        <span className='max'>Min</span>
+                                                    </DefaultButton>
+                                                </div>
+                                                <div>
+                                                    <Stack horizontal>
+                                                        <div className='chooseEntry'>Display top</div>
+                                                        <div>
+                                                            <Dropdown
+                                                                selectedKey={bestTrialEntries}
+                                                                options={entriesOption}
+                                                                onChange={this.updateEntries}
+                                                                styles={{ root: { width: 70 } }}
+                                                            />
+                                                        </div>
+                                                    </Stack>
+                                                </div>
+                                            </Stack>
+                                        </div>
+                                    </Stack>
+                                    <div className='overviewChart'>
+                                        <DefaultPoint
+                                            trialIds={bestTrials.map(trial => trial.info.trialJobId)}
+                                            chartHeight={300}
+                                            hasBestCurve={false}
+                                            changeExpandRowIDs={changeExpandRowIDs}
+                                        />
+                                        <SuccessTable
+                                            trialIds={bestTrials.map(trial => trial.info.trialJobId)}
+                                            updateOverviewPage={updateOverviewPage}
+                                            expandRowIDs={expandRowIDs}
+                                            changeExpandRowIDs={changeExpandRowIDs}
+                                        />
+                                    </div>
+                                </div>
+                                <Stack className='overviewCommand' horizontal>
+                                    <Command2 />
+                                    <Command1 />
+                                </Stack>
+                            </div>
+                        </div>
+                    );
+                }}
+            </AppContext.Consumer>
+        );
+    }
+
+    private findBestTrials(): Trial[] {
+        const bestTrials = TRIALS.sort();
+        const { bestTrialEntries, metricGraphMode } = this.context;
+        if (metricGraphMode === 'max') {
+            bestTrials.reverse().splice(JSON.parse(bestTrialEntries));
+        } else {
+            bestTrials.splice(JSON.parse(bestTrialEntries));
+        }
+        return bestTrials;
+    }
+}
+
+export default Overview;
---
+++ ts/webui/src/components/experiment/trialdetail/table/tableFunction/Compare.tsx
@@ -0,0 +1,293 @@
+import * as React from 'react';
+import { renderToString } from 'react-dom/server';
+import { Stack, Modal, IconButton, IDragOptions, ContextualMenu, Dropdown, IDropdownOption } from '@fluentui/react';
+import ReactEcharts from 'echarts-for-react';
+import { TooltipForIntermediate, TableObj, SingleAxis } from '@static/interface';
+import { contentStyles, iconButtonStyles } from '@components/fluent/ModalTheme';
+import { convertDuration, parseMetrics } from '@static/function';
+import { EXPERIMENT, TRIALS } from '@static/datamodel';
+import '@style/experiment/trialdetail/compare.scss';
+
+/***
+ * Compare file is designed for [each trial intermediate result, trials compare model]
+ * if trial has dict intermediate result,
+ * graph only supports to show all keys that type is number
+ */
+
+function _getWebUIWidth(): number {
+    return window.innerWidth;
+}
+
+const dragOptions: IDragOptions = {
+    moveMenuItemText: 'Move',
+    closeMenuItemText: 'Close',
+    menu: ContextualMenu
+};
+
+// TODO: this should be refactored to the common modules
+// copied from trial.ts
+function _parseIntermediates(trial: TableObj, key: string): number[] {
+    const intermediates: number[] = [];
+    for (const metric of trial.intermediates) {
+        if (metric === undefined) {
+            break;
+        }
+        const parsedMetric = parseMetrics(metric.data);
+        if (typeof parsedMetric === 'object') {
+            // TODO: should handle more types of metric keys
+            intermediates.push(parsedMetric[key]);
+        } else {
+            intermediates.push(parsedMetric);
+        }
+    }
+    return intermediates;
+}
+
+interface Item {
+    id: string;
+    sequenceId: number;
+    duration: string;
+    parameters: Map<string, any>;
+    metrics: Map<string, any>;
+    intermediates: number[];
+}
+
+interface CompareProps {
+    trials: TableObj[];
+    title: string;
+    showDetails: boolean;
+    intermediateKeyList?: string[];
+    onHideDialog: () => void;
+    changeSelectTrialIds?: () => void;
+}
+
+interface CompareState {
+    intermediateKey: string; // default, dict other keys
+}
+
+class Compare extends React.Component<CompareProps, CompareState> {
+    constructor(props: CompareProps) {
+        super(props);
+
+        this.state = {
+            // intermediate result maybe don't have the 'default' key...
+            intermediateKey:
+                this.props.intermediateKeyList !== undefined ? this.props.intermediateKeyList[0] : 'default'
+        };
+    }
+
+    private _generateTooltipSummary = (row: Item, value: string): string =>
+        renderToString(
+            <div className='tooldetailAccuracy'>
+                <div>Trial No.: {row.sequenceId}</div>
+                <div>Trial ID: {row.id}</div>
+                <div>Intermediate metric: {value}</div>
+            </div>
+        );
+
+    private _intermediates(items: Item[]): React.ReactNode {
+        // Precondition: make sure `items` is not empty
+        const xAxisMax = Math.max(...items.map(item => item.intermediates.length));
+        const xAxis = Array(xAxisMax)
+            .fill(0)
+            .map((_, i) => i + 1); // [1, 2, 3, ..., xAxisMax]
+        const dataForEchart = items.map(item => ({
+            name: item.id,
+            data: item.intermediates,
+            type: 'line'
+        }));
+        const legend = dataForEchart.map(item => item.name);
+        const option = {
+            tooltip: {
+                trigger: 'item',
+                enterable: true,
+                confine: true,
+                formatter: (data: TooltipForIntermediate): string => {
+                    const item = items.find(k => k.id === data.seriesName) as Item;
+                    return this._generateTooltipSummary(item, data.data);
+                }
+            },
+            grid: {
+                left: '5%',
+                top: 40,
+                containLabel: true
+            },
+            legend: {
+                type: 'scroll',
+                right: 40,
+                left: legend.length > 6 ? '15%' : null,
+                data: legend
+            },
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: xAxis
+            },
+            yAxis: {
+                type: 'value',
+                name: 'Metric',
+                scale: true
+            },
+            series: dataForEchart
+        };
+        return (
+            <div className='graph'>
+                <ReactEcharts
+                    option={option}
+                    style={{ width: '100%', height: 418, margin: '0 auto' }}
+                    notMerge={true} // update now
+                />
+            </div>
+        );
+    }
+
+    private _renderRow(
+        key: string,
+        rowName: string,
+        className: string,
+        items: Item[],
+        formatter: (item: Item) => string
+    ): React.ReactNode {
+        return (
+            <tr key={key}>
+                <td className='column'>{rowName}</td>
+                {items.map(item => (
+                    <td className={className} key={item.id}>
+                        {formatter(item) || '--'}
+                    </td>
+                ))}
+            </tr>
+        );
+    }
+
+    private _overlapKeys(s: Map<string, any>[]): string[] {
+        // Calculate the overlapped keys for multiple
+        const intersection: string[] = [];
+        for (const i of s[0].keys()) {
+            let inAll = true;
+            for (const t of s) {
+                if (!Array.from(t.keys()).includes(i)) {
+                    inAll = false;
+                    break;
+                }
+            }
+            if (inAll) {
+                intersection.push(i);
+            }
+        }
+        return intersection;
+    }
+
+    // render table column ---
+    private _columns(items: Item[]): React.ReactNode {
+        // Precondition: make sure `items` is not empty
+        const width = _getWebUIWidth();
+        let scrollClass: string = '';
+        if (width > 1200) {
+            scrollClass = items.length > 3 ? 'flex' : '';
+        } else if (width < 700) {
+            scrollClass = items.length > 1 ? 'flex' : '';
+        } else {
+            scrollClass = items.length > 2 ? 'flex' : '';
+        }
+        const parameterKeys = this._overlapKeys(items.map(item => item.parameters));
+        const metricKeys = this._overlapKeys(items.map(item => item.metrics));
+
+        return (
+            <table className={`compare-modal-table ${scrollClass}`}>
+                <tbody>
+                    {this._renderRow('id', 'ID', 'value idList', items, item => item.id)}
+                    {this._renderRow('trialnum', 'Trial No.', 'value', items, item => item.sequenceId.toString())}
+                    {this._renderRow('duration', 'Duration', 'value', items, item => item.duration)}
+                    {parameterKeys.map(k =>
+                        this._renderRow(`space_${k}`, k, 'value', items, item => item.parameters.get(k))
+                    )}
+                    {metricKeys !== undefined
+                        ? metricKeys.map(k =>
+                              this._renderRow(`metrics_${k}`, `Metric: ${k}`, 'value', items, item =>
+                                  item.metrics.get(k)
+                              )
+                          )
+                        : null}
+                </tbody>
+            </table>
+        );
+    }
+
+    private closeCompareModal = (): void => {
+        const { showDetails, changeSelectTrialIds, onHideDialog } = this.props;
+        if (showDetails === true) {
+            // eslint-disable-next-line  @typescript-eslint/no-non-null-assertion
+            changeSelectTrialIds!();
+        }
+        onHideDialog();
+    };
+
+    private selectOtherKeys = (_event: React.FormEvent<HTMLDivElement>, item?: IDropdownOption): void => {
+        if (item !== undefined) {
+            this.setState(() => ({ intermediateKey: item.text }));
+        }
+    };
+
+    render(): React.ReactNode {
+        const { trials, title, showDetails, intermediateKeyList } = this.props;
+        const { intermediateKey } = this.state;
+        const intermediateAllKeysList: string[] = intermediateKeyList !== undefined ? intermediateKeyList : [];
+        const flatten = (m: Map<SingleAxis, any>): Map<string, any> => {
+            return new Map(Array.from(m).map(([key, value]) => [key.baseName, value]));
+        };
+        const inferredSearchSpace = TRIALS.inferredSearchSpace(EXPERIMENT.searchSpaceNew);
+        const items: Item[] = trials.map(trial => ({
+            id: trial.id,
+            sequenceId: trial.sequenceId,
+            duration: convertDuration(trial.duration),
+            parameters: flatten(trial.parameters(inferredSearchSpace)),
+            metrics: flatten(trial.metrics(TRIALS.inferredMetricSpace())),
+            intermediates: _parseIntermediates(trial, intermediateKey)
+        }));
+
+        return (
+            <Modal
+                isOpen={true}
+                containerClassName={contentStyles.container}
+                className='compare-modal'
+                allowTouchBodyScroll={true}
+                dragOptions={dragOptions}
+                onDismiss={this.closeCompareModal}
+            >
+                <div>
+                    <div className={contentStyles.header}>
+                        <span>{title}</span>
+                        <IconButton
+                            styles={iconButtonStyles}
+                            iconProps={{ iconName: 'Cancel' }}
+                            ariaLabel='Close popup modal'
+                            onClick={this.closeCompareModal}
+                        />
+                    </div>
+                    {intermediateAllKeysList.length > 1 ||
+                    (intermediateAllKeysList.length === 1 && intermediateAllKeysList !== ['default']) ? (
+                        <Stack horizontalAlign='end' className='selectKeys'>
+                            <Dropdown
+                                className='select'
+                                selectedKey={intermediateKey}
+                                options={intermediateAllKeysList.map((key, item) => ({
+                                    key: key,
+                                    text: intermediateAllKeysList[item]
+                                }))}
+                                onChange={this.selectOtherKeys}
+                            />
+                        </Stack>
+                    ) : null}
+                    <Stack className='compare-modal-intermediate'>
+                        {this._intermediates(items)}
+                        <Stack className='compare-yAxis'># Intermediate result</Stack>
+                    </Stack>
+                    {showDetails && <Stack>{this._columns(items)}</Stack>}
+                </div>
+            </Modal>
+        );
+    }
+}
+
+export default Compare;
