5acd03cc65941f535677cdb33d2bcae2617176bd
liuzhe <zhliu1@microsoft.com>
2022-03-26 08:21:10 +0800

    fix lint

--- nni/runtime/protocol.py
+++ nni/runtime/protocol.py
@@ -1,5 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+# pylint: disable=unused-import
+
 from .tuner_command_channel.command_type import CommandType
 from .tuner_command_channel.shim import send, receive
