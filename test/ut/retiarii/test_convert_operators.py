d83ca7d19ed42e9f092b7bce62c87367760406d8
Ningxin Zheng <Ningxin.Zheng@microsoft.com>
2022-04-05 13:55:20 +0000

    remove ut

--- test/ut/retiarii/test_convert_operators.py
+++ test/ut/retiarii/test_convert_operators.py
@@ -1387,29 +1387,5 @@ class TestOperators(unittest.TestCase, ConvertMixin):
         x = torch.randn(1, 2, 3, 1, requires_grad=False).int()
         self.checkExportImport(SimpleOp(), (x, ))
 
-    def test_basic_instancenorm_2d(self):
-        class SimpleOp(nn.Module):
-            def __init__(self):
-                super().__init__()
-                
-            def forward(self, x):
-                out = torch.nn.InstanceNorm2d(x)
-                return out
-
-        x = torch.randn(2, 2, 3, 1, requires_grad=False).int()
-        self.checkExportImport(SimpleOp(), (x,))
-
-    def test_basic_pixelshuffle(self):
-        class SimpleOp(nn.Module):
-            def __init__(self):
-                super().__init__()
-
-            def forward(self, x):
-                out = torch.nn.PixelShuffle(x)
-                return out
-
-        x = torch.randn(4, 9, 4, 4, requires_grad=False).int()
-        self.checkExportImport(SimpleOp(), (x,))
-
 class TestOperatorsWithShape(TestOperators, ConvertWithShapeMixin):
     pass
