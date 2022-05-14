df5d169d33e98ad00fe474bddd35d6f588b0a586
Yuge Zhang <scottyugochang@gmail.com>
2022-05-14 14:19:23 +0000

    .

--- ts/nni_manager/training_service/reusable/shared_storages/azureblobStorageService.ts
+++ ts/nni_manager/training_service/reusable/shared_storages/azureblobStorageService.ts
@@ -35,7 +35,8 @@ version=$(lsb_release -r | cut -c9- | sed s/[[:space:]]//g)
 if [ "$id" = "Ubuntu" ]
 then
     wget https://packages.microsoft.com/config/ubuntu/$version/packages-microsoft-prod.deb
-    sudo DEBIAN_FRONTEND=noninteractive dpkg -i packages-microsoft-prod.deb
+    sudo dpkg-reconfigure debconf -f noninteractive
+    sudo dpkg -i packages-microsoft-prod.deb
     sudo apt-get update
     sudo apt-get install -y blobfuse fuse
 elif [ "$id" = "CentOS" ] || [ "$id" = "RHEL" ]
