6e9ca3533719b3d597c064256bc9b7290fddcf7a
quzha <Quanlu.Zhang@microsoft.com>
2022-05-23 12:29:09 +0800

    add comment

--- ts/nni_manager/core/nnimanager.ts
+++ ts/nni_manager/core/nnimanager.ts
@@ -303,6 +303,9 @@ class NNIManager implements Manager {
         }
 
         this.trainingService.removeTrialJobMetricListener(this.trialJobMetricListener);
+        // NOTE: this sending TERMINATE should be out of the if clause,
+        // because when python dispatcher is started before nnimanager
+        // this.dispatcherPid would not have a valid value (i.e., not >0).
         this.dispatcher.sendCommand(TERMINATE);
         if (this.dispatcherPid > 0) {
             // gracefully terminate tuner and assessor here, wait at most 30 seconds.
