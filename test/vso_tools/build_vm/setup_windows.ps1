4d073a3e81c7446af32ff3eb11a2973ad32f012d
Yuge Zhang <scottyugochang@gmail.com>
2022-05-25 03:05:56 +0000

    .

--- test/vso_tools/build_vm/setup_windows.ps1
+++ test/vso_tools/build_vm/setup_windows.ps1
@@ -1,18 +1,23 @@
-Set-PSDebug -Trace 1
 $ErrorActionPreference = "Stop"
 
 # Choco.
 # https://docs.chocolatey.org/en-us/choco/setup
 # Community version can't customize output directory.
+Write-Host "Installing Choco..."
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+Set-PSDebug -Trace 1
+
 # Nuget.
 # Doesn't have azcopy.
+Write-Host "Installing Nuget..."
 $NugetDir = "C:\nuget"
-New-Item "$NugetDir" -ItemType Directory -Force
+New-Item "$NugetDir" -ItemType Directory -Force | Out-Null
 Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile "${NugetDir}\nuget.exe"
 $env:path = "$env:path;$NugetDir"
+
+Write-Host "Installing utilities..."
 
 # Install azcopy for cache download.
 # Something wrong with the latest (10.15.0) checksum.
@@ -25,6 +30,7 @@ choco install -y --force swig --version=3.0.12
 swig -version
 
 # Install SSH.
+Write-Host "Installing SSH..."
 # https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse
 Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
 # Install the OpenSSH Client
@@ -32,8 +38,11 @@ Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 # Install the OpenSSH Server
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 # Start the sshd service
+Set-PSDebug -Trace 0
+Write-Host "Starting SSH service..."
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
+Write-Host "Configure firewall for SSH..."
 # Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
 if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
     Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
@@ -42,13 +51,19 @@ if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyCon
     Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
 }
 
+Set-PSDebug -Trace 1
+
 # Install python.
+Write-Host "Installing Python..."
 $PythonDir = "C:\Python"
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.12/python-3.9.12-amd64.exe" -OutFile "python-installer.exe"
-Start-Process -FilePath .\python-installer.exe -NoNewWindow -Wait \
-    -ArgumentList "/quiet InstallAllUsers=1 TargetDir=C:\Python\ Include_launcher=0"
+Start-Process -FilePath .\python-installer.exe -NoNewWindow -Wait -ArgumentList "/quiet InstallAllUsers=1 TargetDir=C:\Python\ Include_launcher=0"
+$env:path = "$env:path;$PythonDir"
+Remove-Item python-installer.exe
+Write-Host "Verify Python installation..."
+python --version
 
-# Here are some other comments that might be useful when installing python without elevation.
+# Here are some other comments, which might be useful when installing python without elevation.
 #
 # Originally I tried to install the python by downloading from official, and run the installation.
 #
@@ -82,5 +97,6 @@ Start-Process -FilePath .\python-installer.exe -NoNewWindow -Wait \
 
 # Permanently update the PATHs
 # https://codingbee.net/powershell/powershell-make-a-permanent-change-to-the-path-environment-variable
+Write-Host "Prepare PATHs..."
 Write-Host $env:path
 Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $env:path
