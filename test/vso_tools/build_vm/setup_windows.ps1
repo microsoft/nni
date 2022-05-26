2fcd34862c2ced3c69cfabaabbea63484c185a8a
Yuge Zhang <scottyugochang@gmail.com>
2022-05-26 11:40:56 +0800

    .

--- test/vso_tools/build_vm/setup_windows.ps1
+++ test/vso_tools/build_vm/setup_windows.ps1
@@ -37,6 +37,12 @@ Write-Host "Installing utilities..."
 # Visual Studio C++ Build tools (for Cython)
 Invoke-WebRequest "https://aka.ms/vs/17/release/vs_BuildTools.exe" -OutFile "vs_BuildTools.exe"
 Start-Process -FilePath "vs_BuildTools.exe" -ArgumentList "--quiet --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended" -Wait
+Remove-Item "vs_BuildTools.exe"
+
+# Microsoft Visual C++ Redistributable (for PyTorch)
+Invoke-WebRequest "https://aka.ms/vs/16/release/vc_redist.x64.exe" -OutFile "vc_redist.x64.exe"
+Start-Process -FilePath ".\vc_redist.x64.exe" -ArgumentList "/q /norestart" -Wait
+Remove-Item "vc_redist.x64.exe"
 
 # Install azcopy for cache download.
 # Something wrong with the latest (10.15.0) checksum.
