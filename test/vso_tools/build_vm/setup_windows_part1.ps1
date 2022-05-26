d676e2556c42c6a13c79cec3b4118e6b65ef1da4
Yuge Zhang <scottyugochang@gmail.com>
2022-05-26 11:13:31 +0000

    .

--- test/vso_tools/build_vm/setup_windows_part1.ps1
+++ test/vso_tools/build_vm/setup_windows_part1.ps1
@@ -2,13 +2,3 @@
 $ErrorActionPreference = "Stop"
 
 Set-PSDebug -Trace 1
-
-# Visual Studio C++ Build tools (for Cython)
-Invoke-WebRequest "https://aka.ms/vs/17/release/vs_BuildTools.exe" -OutFile "vs_BuildTools.exe"
-Start-Process -FilePath "vs_BuildTools.exe" -ArgumentList "--quiet --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended" -Wait
-Remove-Item "vs_BuildTools.exe"
-
-# Microsoft Visual C++ Redistributable (for PyTorch)
-Invoke-WebRequest "https://aka.ms/vs/16/release/vc_redist.x64.exe" -OutFile "vc_redist.x64.exe"
-Start-Process -FilePath ".\vc_redist.x64.exe" -ArgumentList "/q /norestart" -Wait
-Remove-Item "vc_redist.x64.exe"
