513a81c11047d626751048168db29c72c18aab39
Yuge Zhang <scottyugochang@gmail.com>
2022-05-27 13:17:17 +0000

    .

--- test/vso_tools/build_vm/setup_windows.ps1
+++ test/vso_tools/build_vm/setup_windows.ps1
@@ -8,32 +8,35 @@ Write-Host "Installing Choco..."
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-Set-PSDebug -Trace 1
+# Install dotnetfx (and restart)
+choco install -y --no-progress dotnetfx
 
-# Nuget.
-# Doesn't have azcopy.
-Write-Host "Installing Nuget..."
-$NugetDir = "$env:ProgramData\nuget"
-New-Item "$NugetDir" -ItemType Directory -Force | Out-Null
-Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile "${NugetDir}\nuget.exe"
-$env:path = "$env:path;$NugetDir"
+# Set-PSDebug -Trace 1
 
-# These installation seems not working.
+# # Nuget.
+# # Doesn't have azcopy.
+# Write-Host "Installing Nuget..."
+# $NugetDir = "$env:ProgramData\nuget"
+# New-Item "$NugetDir" -ItemType Directory -Force | Out-Null
+# Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile "${NugetDir}\nuget.exe"
+# $env:path = "$env:path;$NugetDir"
 
-# Visual Studio C++ Build tools (for Cython)
-# Invoke-WebRequest "https://aka.ms/vs/17/release/vs_BuildTools.exe" -OutFile "vs_BuildTools.exe"
-# Start-Process -FilePath "vs_BuildTools.exe" -ArgumentList "--quiet --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended" -Wait
-# Remove-Item "vs_BuildTools.exe"
+# # These installation seems not working.
 
-# Microsoft Visual C++ Redistributable (for PyTorch)
-# Invoke-WebRequest "https://aka.ms/vs/16/release/vc_redist.x64.exe" -OutFile "vc_redist.x64.exe"
-# Start-Process -FilePath ".\vc_redist.x64.exe" -ArgumentList "/q /norestart" -Wait
-# Remove-Item "vc_redist.x64.exe"
+# # Visual Studio C++ Build tools (for Cython)
+# # Invoke-WebRequest "https://aka.ms/vs/17/release/vs_BuildTools.exe" -OutFile "vs_BuildTools.exe"
+# # Start-Process -FilePath "vs_BuildTools.exe" -ArgumentList "--quiet --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended" -Wait
+# # Remove-Item "vs_BuildTools.exe"
 
-# Use choco instead.
-choco install -y --force visualstudio2019buildtools --no-progress
-choco install -y --force visualstudio2019-workload-vctools --no-progress
-choco install -y --force vcredist-all --no-progress
+# # Microsoft Visual C++ Redistributable (for PyTorch)
+# # Invoke-WebRequest "https://aka.ms/vs/16/release/vc_redist.x64.exe" -OutFile "vc_redist.x64.exe"
+# # Start-Process -FilePath ".\vc_redist.x64.exe" -ArgumentList "/q /norestart" -Wait
+# # Remove-Item "vc_redist.x64.exe"
+
+# # Use choco instead.
+# choco install -y --no-progress visualstudio2019buildtools
+# choco install -y --no-progress visualstudio2019-workload-vctools
+# choco install -y --no-progress vcredist2012 vcredist2013 vcredist2015 vcredist2017
 
 # # Install CUDA.
 # Write-Host "Installing CUDA..."
@@ -89,38 +92,38 @@ choco install -y --force vcredist-all --no-progress
 # choco install -y --force swig --version=3.0.12 --no-progress
 # swig -version
 
-# Install python.
-# Originally I tried to install the python by downloading from official, and run the installation.
-#
-#     Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.12/python-3.9.12-amd64.exe" -OutFile "python-installer.exe"
-#     Start-Process -FilePath .\python-installer.exe -NoNewWindow -Wait \
-#       -ArgumentList "/quiet InstallAllUsers=1 TargetDir=$(Agent.ToolsDirectory)\Python\3.9.12\x64 Include_launcher=0"
-#     New-Item -Path $(Agent.ToolsDirectory)\Python\3.9.12\x64.complete -ItemType file -Force
-#
-# But ``Start-Process`` fails with mysterious reasons (exit code is not zero and no error message).
-# I tried with -PassThru, -NoNewWindow, -Wait, /quiet, /passive, InstallAllUsers and some other flags, but none works.
-# (InstallAllUsers is the key to make it work on my local, but not on pipeline).
-# I guess it's related to lack of adminstrative privileges.
-# I kept this attempt here in case any one can make it work.
-#
-# Other two workarounds.
-# 1) choco install python. The community verison can't customize output directory,
-#    and the output directory is only a guess (e.g., C:\Python310).
-# 2) nuget install python. This seems working.
-#
-# Can't move to the installed python to $PythonDir\3.9.12\x64 because,
-# 1. If we copy it, Python path will complain in the next few steps.
-# 2. If we try to create a symlink, it will tell us that we don't have adminstrative rights.
-#
-# After all this struggle, the workaround here is simple:
-# to install with nuget, then don't use `UsePythonVersion` in the next step.
-# The workaround works because we actually never needs multiple python versions on windows.
-Write-Host "Installing Python..."
-$PythonDir = "$env:ProgramData\Python"
-nuget install python -Version 3.9.12 -OutputDirectory "$PythonDir"
-$env:path = "$env:path;$PythonDir\python.3.9.12\tools\;$PythonDir\python.3.9.12\tools\Scripts"
-Write-Host "Verify Python installation..."
-python --version
+# # Install python.
+# # Originally I tried to install the python by downloading from official, and run the installation.
+# #
+# #     Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.12/python-3.9.12-amd64.exe" -OutFile "python-installer.exe"
+# #     Start-Process -FilePath .\python-installer.exe -NoNewWindow -Wait \
+# #       -ArgumentList "/quiet InstallAllUsers=1 TargetDir=$(Agent.ToolsDirectory)\Python\3.9.12\x64 Include_launcher=0"
+# #     New-Item -Path $(Agent.ToolsDirectory)\Python\3.9.12\x64.complete -ItemType file -Force
+# #
+# # But ``Start-Process`` fails with mysterious reasons (exit code is not zero and no error message).
+# # I tried with -PassThru, -NoNewWindow, -Wait, /quiet, /passive, InstallAllUsers and some other flags, but none works.
+# # (InstallAllUsers is the key to make it work on my local, but not on pipeline).
+# # I guess it's related to lack of adminstrative privileges.
+# # I kept this attempt here in case any one can make it work.
+# #
+# # Other two workarounds.
+# # 1) choco install python. The community verison can't customize output directory,
+# #    and the output directory is only a guess (e.g., C:\Python310).
+# # 2) nuget install python. This seems working.
+# #
+# # Can't move to the installed python to $PythonDir\3.9.12\x64 because,
+# # 1. If we copy it, Python path will complain in the next few steps.
+# # 2. If we try to create a symlink, it will tell us that we don't have adminstrative rights.
+# #
+# # After all this struggle, the workaround here is simple:
+# # to install with nuget, then don't use `UsePythonVersion` in the next step.
+# # The workaround works because we actually never needs multiple python versions on windows.
+# Write-Host "Installing Python..."
+# $PythonDir = "$env:ProgramData\Python"
+# nuget install python -Version 3.9.12 -OutputDirectory "$PythonDir"
+# $env:path = "$env:path;$PythonDir\python.3.9.12\tools\;$PythonDir\python.3.9.12\tools\Scripts"
+# Write-Host "Verify Python installation..."
+# python --version
 
 # Permanently update the PATHs
 # https://codingbee.net/powershell/powershell-make-a-permanent-change-to-the-path-environment-variable
