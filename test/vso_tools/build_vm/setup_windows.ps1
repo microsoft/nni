ac9f94d985cf114ea0da52d055459a0c56fd571d
Yuge Zhang <scottyugochang@gmail.com>
2022-05-27 02:36:03 +0000

    .

--- test/vso_tools/build_vm/setup_windows.ps1
+++ test/vso_tools/build_vm/setup_windows.ps1
@@ -41,20 +41,20 @@ Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 # Install the OpenSSH Server
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 # Start the sshd service
-# Set-PSDebug -Trace 0
+Set-PSDebug -Trace 0
 Write-Host "Starting SSH service..."
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
-# Write-Host "Configure firewall for SSH..."
-# # Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
-# if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
-#     Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
-#     New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-# } else {
-#     Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
-# }
+Write-Host "Configure firewall for SSH..."
+# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+} else {
+    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+}
 
-# Set-PSDebug -Trace 1
+Set-PSDebug -Trace 1
 
 # # Create a new user (for SSH login).
 # $Password = ConvertTo-SecureString "P@ssW0rD!" -AsPlainText -Force
