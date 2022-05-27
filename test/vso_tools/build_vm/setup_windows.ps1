dd73ade7e0f62f4e335586c97d9c08c7d0128d96
Yuge Zhang <scottyugochang@gmail.com>
2022-05-27 03:02:16 +0000

    .

--- test/vso_tools/build_vm/setup_windows.ps1
+++ test/vso_tools/build_vm/setup_windows.ps1
@@ -1,36 +1,36 @@
 #Requires -RunAsAdministrator
 $ErrorActionPreference = "Stop"
 
-# # Choco.
-# # https://docs.chocolatey.org/en-us/choco/setup
-# # Community version can't customize output directory.
-# Write-Host "Installing Choco..."
-# [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-# iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Choco.
+# https://docs.chocolatey.org/en-us/choco/setup
+# Community version can't customize output directory.
+Write-Host "Installing Choco..."
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Set-PSDebug -Trace 1
+Set-PSDebug -Trace 1
 
-# # Nuget.
-# # Doesn't have azcopy.
-# Write-Host "Installing Nuget..."
-# $NugetDir = "$env:ProgramData\nuget"
-# New-Item "$NugetDir" -ItemType Directory -Force | Out-Null
-# Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile "${NugetDir}\nuget.exe"
-# $env:path = "$env:path;$NugetDir"
+# Nuget.
+# Doesn't have azcopy.
+Write-Host "Installing Nuget..."
+$NugetDir = "$env:ProgramData\nuget"
+New-Item "$NugetDir" -ItemType Directory -Force | Out-Null
+Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile "${NugetDir}\nuget.exe"
+$env:path = "$env:path;$NugetDir"
 
-# # Install CUDA.
-# Write-Host "Installing CUDA..."
-# $CudaUrl = "https://developer.download.nvidia.com/compute/cuda/11.7.0/network_installers/cuda_11.7.0_windows_network.exe"
-# Invoke-WebRequest $CudaUrl -OutFile "cuda_installer.exe"
-# Start-Process -FilePath "cuda_installer.exe" -ArgumentList "/s /n" -Wait
-# Remove-Item "cuda_installer.exe"
-# # Verify CUDA.
-# Write-Host "Verify CUDA installation..."
-# $CudaDir = "$env:ProgramFiles\NVIDIA GPU Computing Toolkit\CUDA\v11.7\bin"
-# # GPU driver can't be installed without a hardware
-# # Get-Command nvidia-smi
-# Get-ChildItem $CudaDir
-# $env:path = "$env:path;$CudaDir"
+# Install CUDA.
+Write-Host "Installing CUDA..."
+$CudaUrl = "https://developer.download.nvidia.com/compute/cuda/11.7.0/network_installers/cuda_11.7.0_windows_network.exe"
+Invoke-WebRequest $CudaUrl -OutFile "cuda_installer.exe"
+Start-Process -FilePath "cuda_installer.exe" -ArgumentList "/s /n" -Wait
+Remove-Item "cuda_installer.exe"
+# Verify CUDA.
+Write-Host "Verify CUDA installation..."
+$CudaDir = "$env:ProgramFiles\NVIDIA GPU Computing Toolkit\CUDA\v11.7\bin"
+# GPU driver can't be installed without a hardware
+# Get-Command nvidia-smi
+Get-ChildItem $CudaDir
+$env:path = "$env:path;$CudaDir"
 
 # Install SSH.
 Write-Host "Installing SSH..."
@@ -45,14 +45,14 @@ Set-PSDebug -Trace 0
 Write-Host "Starting SSH service..."
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
-Write-Host "Configure firewall for SSH..."
-# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
-if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
-    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
-    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-} else {
-    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
-}
+# Write-Host "Configure firewall for SSH..."
+# # Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+# if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+#     Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+#     New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+# } else {
+#     Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+# }
 
 Set-PSDebug -Trace 1
 
