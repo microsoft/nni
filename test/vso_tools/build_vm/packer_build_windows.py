96d4af7cb078acf9438e2b90b77919d4da563f75
Yuge Zhang <scottyugochang@gmail.com>
2022-05-28 17:44:22 +0000

    .

--- test/vso_tools/build_vm/packer_build_windows.py
+++ test/vso_tools/build_vm/packer_build_windows.py
@@ -1,9 +1,21 @@
+"""Start and monitor a packer building process, and change WinRM ports at an appropriate time.
+
+It first catches the name of the VM. It's a randomly generated string by packer.
+It then catches the signal of packer's waiting for WinRM ready.
+Actually, it will be ready soon, but packer is waiting on the wrong port.
+Invoking a command here to change the port of WinRM so that packer could conect to it.
+
+The monitor is designed to be as robust as possible, so that packer won't easily crash.
+It's painful to manually clean up the resources that packer has created,
+as we are reusing an existing resource group.
+"""
+
 import re
 import subprocess
 import sys
 import time
 
-BUILD_COMMAND = 'PACKER_LOG=1 packer build packer_test.json'
+BUILD_COMMAND = 'PACKER_LOG=1 packer build packer_windows.json'
 RESOURCE_GROUP = 'nni'
 
 
@@ -32,7 +44,7 @@ def main():
 
             # Find VM name
             vm_name_grp = re.search(r'pkrvm[a-z0-9]{10,}', line)
-            if vm_name_grp is not None:
+            if vm_name is None and vm_name_grp is not None:
                 vm_name = vm_name_grp.group(0)
                 monitor_print('VM name found:', vm_name)
 
@@ -42,10 +54,11 @@ def main():
                     monitor_print('VM name not found. This is not normal.')
                 else:
                     winrm_start_waiting = time.time()
-                    monitor_print('Start waiting for WinRM.')
+                    monitor_print('Waiting for WinRM detected. You might see some errors. No worry.')
 
+            # After WinRM has a waiting signal, wait another minute to make sure it's ready.
             if winrm_start_waiting is not None and time.time() - winrm_start_waiting > 60:
-                monitor_print('WinRM waits time has exceeded 60 seconds. Starting to invoke commands.')
+                monitor_print('WinRM waits time has exceeded 60 seconds. Starting to invoke command to change its port.')
                 result = subprocess.run(
                     'az vm run-command invoke --command-id RunPowerShellScript '
                     f'--name {vm_name} -g {RESOURCE_GROUP} '
@@ -55,7 +68,8 @@ def main():
                 if result.returncode != 0:
                     monitor_print('Return code of command invoking is non-zero:', result.returncode)
 
-                # WinRM set to true regardless of subprocess status.
+                # To make the packer resource cleanup robust,
+                # WinRM is always finished regardless of subprocess status.
                 winrm_finished = True
 
         if retcode is not None:
