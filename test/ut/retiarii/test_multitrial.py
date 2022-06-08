811e44ec1e0f79733b47a509e009df6b7c6c3d77
quzha <Quanlu.Zhang@microsoft.com>
2022-06-08 15:04:21 +0800

    add unittest

---
+++ test/ut/retiarii/test_multitrial.py
@@ -0,0 +1,44 @@
+import argparse
+import torch.nn.functional as F
+
+from nni.retiarii import strategy
+from nni.retiarii.experiment.pytorch import RetiariiExeConfig, RetiariiExperiment
+from .test_oneshot import _mnist_net
+
+
+def test_multi_trial():
+    evaluator_kwargs = {
+        'max_epochs': 1
+    }
+
+    to_test = [
+        # (model, evaluator)
+        _mnist_net('simple', evaluator_kwargs),
+        _mnist_net('simple_value_choice', evaluator_kwargs),
+        _mnist_net('value_choice', evaluator_kwargs),
+        _mnist_net('repeat', evaluator_kwargs),
+        _mnist_net('custom_op', evaluator_kwargs),
+    ]
+
+    for base_model, evaluator in to_test:
+        search_strategy = strategy.Random()
+        exp = RetiariiExperiment(base_model, evaluator, strategy=search_strategy)
+        exp_config = RetiariiExeConfig('local')
+        exp_config.experiment_name = 'mnist_unittest'
+        exp_config.trial_concurrency = 2
+        exp_config.max_trial_number = 2
+        exp_config.training_service.use_active_gpu = False
+        exp.run(exp_config, 8081)
+        assert isinstance(exp.export_top_models()[0], dict)
+        exp.stop()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp', type=str, default='all', metavar='E',
+                        help='experiment to run, default = all')
+    args = parser.parse_args()
+
+    if args.exp == 'all':
+        test_multi_trial()
+    else:
+        globals()[f'test_{args.exp}']()
