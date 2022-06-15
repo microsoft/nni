17d6f4694f15aaaa67f5ce7807389446369088b3
Yuge Zhang <scottyugochang@gmail.com>
2022-06-15 15:32:03 +0000

    .

--- test/algo/__init__.py
+++ test/algo/__init__.py
@@ -2,7 +2,7 @@
 # Licensed under the MIT license.
 
 """
-Extra tests for "algorithms", complementary to ut.
+Extra tests for "algorithms", complementary to UT.
 
 If the test satisfies one of the following conditions, it should be put here:
 
@@ -10,9 +10,9 @@ If the test satisfies one of the following conditions, it should be put here:
 2. The test doesn't work on the free agent. It needs accelerators like GPUs.
 3. The test is dedicated for a specific replacable module, which doesn't involve core functionalities.
 
-Note that if a core function (something, without which NNI doesn't work at all) were to be tested against,
-it's still highly recommended to write at least a simple test in UT.
-If comprehensive tests are expensive, they can belong here.
+Note that if a test is to ensure the correctness of a "core function", without which NNI doesn't work at all,
+it's still highly recommended to include at least a simple test in UT.
+If a set of exhaustive tests were to be expensive, they can still belong here.
 """
 
 # Import ut to set environment variables
