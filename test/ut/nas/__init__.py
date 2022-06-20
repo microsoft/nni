394c145cedd044b11f679ef1d9900a46b423f560
J-shang <shangning128@163.com>
2022-06-20 12:49:33 +0800

    fix broken link

--- test/ut/nas/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
-import os
-
-os.environ['NNI_PLATFORM'] = 'unittest'
-os.environ['NNI_TRIAL_JOB_ID'] = 'test_trial_job_id'
