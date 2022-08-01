aa3675a64cfaed47bf03ba0cb55bfdc3832a35e7
Yuge Zhang <Yuge.Zhang@microsoft.com>
2022-08-01 18:43:47 +0900

    init

---
+++ examples/tutorials/darts.py
@@ -0,0 +1,19 @@
+"""
+Searching on DARTS search space
+===============================
+
+In this tutorial, we demonstrate how to search on the famous model space proposed in `DARTS <https://arxiv.org/abs/1806.09055>`__.
+
+Through this process, you will learn:
+
+* How to use the built-in model spaces from NNI's model space hub.
+* How to use one-shot exploration strategies to explore a model space.
+* How to customize evaluators to achieve the best performance.
+
+In the end, we get a strong-performing model on CIFAR-10 dataset, which achieves xx.xx% accuracy.
+
+Use the model space
+-------------------
+"""
+
+
