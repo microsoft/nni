e4bee3f74ddfb2a161e8e08c0f32bc53320332de
Yuge Zhang <Yuge.Zhang@microsoft.com>
2022-08-02 13:46:17 +0900

    update

--- examples/tutorials/darts.py
+++ examples/tutorials/darts.py
@@ -56,6 +56,9 @@ during the search phase, and increase them back when training the final searched
 # %%
 # In the following example, we initialize a DARTS model space, with only 16 initial filters and 8 stacked cells.
 # The network is specialized for CIFAR-10 dataset with 32x32 input resolution.
+#
+# The DARTS model space here is provided by :doc:`model space hub <./space_hub>`,
+# where we have supported multiple popular model spaces for plug-and-play.
 
 from nni.retiarii.hub.pytorch import DARTS
 
@@ -65,4 +68,57 @@ model_space = DARTS(16, 8, 'cifar')
 # Search on the model space
 # -------------------------
 #
+# To begin exploring the model space, one firstly need to have an evaluator to provide the criterion of a "good model".
+# As we are searching on CIFAR-10 dataset, one can easily use the :class:`~nni.retiarii.evaluator.pytorch.Classification`
+# as a starting point.
 #
+# Note that for a typical setup of NAS, the model search should be on validation set, and the evaluation of the final searched model
+# should be on test set. However, as CIFAR-10 dataset only has a training set of 50k images and a validation set (10k images),
+# we have to split the original training set into a training set and a validation set.
+# As we are going to use the provided by DARTS paper, the recommended train/val split is 1:1.
+
+import nni
+import numpy as np
+from nni.retiarii.evaluator.pytorch import (
+    Classification,
+    DataLoader  # might also use torch.utils.data.DataLoader if not using multi-trial strategy
+)
+from torch.utils.data import SubsetRandomSampler
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+
+CIFAR_MEAN = [0.49139968, 0.48215827, 0.44653124]
+CIFAR_STD = [0.24703233, 0.24348505, 0.26158768]
+
+transform = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+])
+
+# If you are to use multi-trial strategies, please wrap CIFAR10 with :func:`nni.trace`.
+train_data = nni.trace(CIFAR10)(root='./data', train=True, download=True, transform=transform)
+
+num_samples = len(train_data)
+indices = np.random.permutation(num_samples)
+split = num_samples // 2
+
+train_loader = DataLoader(
+    train_data, batch_size=64,
+    sampler=SubsetRandomSampler(indices[:split]),
+)
+
+valid_loader = DataLoader(
+    train_data, batch_size=64,
+    sampler=SubsetRandomSampler(indices[split:]),
+)
+
+evaluator = Classification(
+    learning_rate=0.01,
+    weight_decay=1e-4,
+    train_dataloaders=train_loader,
+    val_dataloaders=valid_loader
+)
+
+# %%
