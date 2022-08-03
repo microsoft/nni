725bae899b20fff13196667b130a6197adb0c89e
J-shang <shangning128@163.com>
2022-08-03 11:13:10 +0800

    rm files

--- test/ut/sdk/models/pytorch_models/__init__.py
+++ test/ut/sdk/models/pytorch_models/__init__.py
@@ -1,6 +1,2 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
-
-from .mutable_scope import SpaceWithMutableScope
-from .naive import NaiveSearchSpace
-from .nested import NestedSpace
