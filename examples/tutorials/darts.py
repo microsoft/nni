59a684a5e0cc54bd44f1fd2cdf6eeefecc7a62d5
Yuge Zhang <scottyugochang@gmail.com>
2022-08-04 03:37:44 +0000

    .

--- examples/tutorials/darts.py
+++ examples/tutorials/darts.py
@@ -244,3 +244,71 @@ evaluator = Classification(
 )
 
 evaluator.fit(final_model)
+
+# %%
+#
+# Reproduce results in DARTS paper
+# --------------------------------
+#
+# You might notice there's still a gap between our results and the results in the DARTS paper.
+# This is because we didn't introduce some extra training tricks, including `DropPath <https://arxiv.org/pdf/1605.07648v4.pdf>`__,
+# Auxiliary loss, gradient clipping and augmentations like `Cutout <https://arxiv.org/pdf/1708.04552v2.pdf>`__.
+# They also train the networks for longer time (i.e., 600 epochs).
+#
+# To implement these tricks, we need to rewrite a few parts of evaluator.
+# Firstly, few more info about evaluator...
+# We inherit the corresponding lightning module of :class:`~nni.retiarii.evaluator.pytorch.Classification` evaluator and override the following methods.
+# This could look intimidating at first, but most of them are just plug-and-play tricks which you don't need to know details about.
+
+from nni.retiarii.evaluator.pytorch import ClassificationModule
+
+class DartsClassificationModule(ClassificationModule):
+    """Several customization for the training of DARTS, based on default Classification."""
+    model: DARTS
+
+    def __init__(self,
+                 learning_rate: float = 0.001,
+                 weight_decay: float = 0.,
+                 auxiliary_loss_weight: float = 0.4,
+                 max_epochs: int = 600):
+        self.auxiliary_loss_weight = auxiliary_loss_weight
+        self.max_epochs = max_epochs
+        super().__init__(learning_rate=learning_rate, weight_decay=weight_decay, export_onnx=False)
+
+    def configure_optimizers(self):
+        """Customized optimizer with momentum, as well as a scheduler."""
+        optimizer = torch.optim.SGD(
+            self.parameters(),
+            momentum=0.9,
+            lr=self.hparams.learning_rate,  # type: ignore
+            weight_decay=self.hparams.weight_decay  # type: ignore
+        )
+        return {
+            'optimizer': optimizer,
+            'lr_scheduler': torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, self.max_epochs, eta_min=1e-3)
+        }
+
+    def training_step(self, batch, batch_idx):
+        """Training step, customized with auxiliary loss."""
+        x, y = batch
+        if self.auxiliary_loss_weight:
+            y_hat, y_aux = self(x)
+            loss_main = self.criterion(y_hat, y)
+            loss_aux = self.criterion(y_aux, y)
+            self.log('train_loss_main', loss_main)
+            self.log('train_loss_aux', loss_aux)
+            loss = loss_main + self.auxiliary_loss_weight * loss_aux
+        else:
+            y_hat = self(x)
+            loss = self.criterion(y_hat, y)
+        self.log('train_loss', loss, prog_bar=True)
+        for name, metric in self.metrics.items():
+            self.log('train_' + name, metric(y_hat, y), prog_bar=True)
+        return loss
+
+    def on_train_epoch_start(self):
+        """Set drop path probability before every epoch. This has no effect if drop path is not enabled in model."""
+        self.model.set_drop_path_prob(self.model.drop_path_prob * self.current_epoch / self.max_epochs)
+
+        # Logging learning rate at the beginning of every epoch
+        self.log('lr', self.trainer.optimizers[0].param_groups[0]['lr'])
