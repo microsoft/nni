e92e5392a736c743e744bd12fe6b947b3ff81172
Yuge Zhang <Yuge.Zhang@microsoft.com>
2022-08-11 17:51:08 +0900

    extend time limit

--- test/algo/nas/test_space_hub_oneshot.py
+++ test/algo/nas/test_space_hub_oneshot.py
@@ -196,6 +196,18 @@ def test_hub_oneshot(space_type, strategy_type):
     if strategy_type in ['darts', 'gumbel'] and space_type == 'mobilenetv3':
         pytest.skip('Skip as it consumes too much memory.')
 
+    WINDOWS_SPACES = [
+        # Skip some spaces as Windows platform is slow.
+        'nasbench201',
+        'mobilenetv3',
+        'proxylessnas',
+        'shufflenet',
+        'autoformer',
+        'darts',
+    ]
+    if sys.platform == 'win32' and space_type not in WINDOWS_SPACES:
+        pytest.skip('Skip as Windows is too slow.')
+
     model_space = _hub_factory(space_type)
 
     dataset_type = 'cifar10'
