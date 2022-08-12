d8a3bca1b207197b34240d2ebf939eedfcfe9561
Ubuntu <core@lijiao-vm.blbee4diapvedlj52zlxs1ydub.phxx.internal.cloudapp.net>
2022-08-12 07:40:02 +0000

    update

--- ts/webui/src/components/experiment/overview/count/ExpDuration.tsx
+++ ts/webui/src/components/experiment/overview/count/ExpDuration.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Stack, ProgressIndicator, TooltipHost, DirectionalHint } from '@fluentui/react';
 import { EXPERIMENT } from '@static/datamodel';
-import { CONTROLTYPE, TOOLTIP_BACKGROUND_COLOR } from '@static/const';
+import { CONTROLTYPE } from '@static/const';
 import { convertDuration, convertTimeAsUnit } from '@static/function';
 import { EditExperimentParam } from './EditExperimentParam';
 import { ExpDurationContext } from './ExpDurationContext';
+import ProgressBar from './ProgressBar';
 import { EditExpeParamContext } from './context';
-import { leftProgress, rightEidtParam, progressHeight } from './commonStyle';
 import '@style/experiment/overview/count.scss';
 
 export const ExpDuration = (): any => (
@@ -18,35 +17,14 @@ export const ExpDuration = (): any => (
             const execDurationStr = convertDuration(execDuration);
             const maxExecDurationStr = convertTimeAsUnit(maxDurationUnit, maxExecDuration).toString();
             return (
-                <Stack horizontal className='ExpDuration'>
-                    <div style={leftProgress}>
-                        <TooltipHost
-                            content={`${convertDuration(tooltip)} remaining`}
-                            directionalHint={DirectionalHint.bottomCenter}
-                            tooltipProps={{
-                                calloutProps: {
-                                    styles: {
-                                        beak: { background: TOOLTIP_BACKGROUND_COLOR },
-                                        beakCurtain: { background: TOOLTIP_BACKGROUND_COLOR },
-                                        calloutMain: { background: TOOLTIP_BACKGROUND_COLOR }
-                                    }
-                                }
-                            }}
-                        >
-                            <ProgressIndicator
-                                className={EXPERIMENT.status}
-                                percentComplete={percent}
-                                barHeight={progressHeight}
-                            />
-                        </TooltipHost>
-                        {/* execDuration / maxDuration: 20min / 1h */}
-                        <div className='exp-progress'>
-                            <span className={`${EXPERIMENT.status} bold`}>{execDurationStr}</span>
-                            <span className='joiner'>/</span>
-                            <span>{`${maxExecDurationStr} ${maxDurationUnit}`}</span>
-                        </div>
-                    </div>
-                    <div style={rightEidtParam}>
+                <React.Fragment>
+                    <ProgressBar
+                        tooltip={`${convertDuration(tooltip)} remaining`}
+                        percent={percent}
+                        latestVal={execDurationStr}
+                        presetVal={`${maxExecDurationStr} ${maxDurationUnit}`}
+                    />
+                    <div className='editExpDuration'>
                         <EditExpeParamContext.Provider
                             value={{
                                 editType: CONTROLTYPE[0],
@@ -61,7 +39,7 @@ export const ExpDuration = (): any => (
                             <EditExperimentParam />
                         </EditExpeParamContext.Provider>
                     </div>
-                </Stack>
+                </React.Fragment>
             );
         }}
     </ExpDurationContext.Consumer>
