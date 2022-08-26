ec3f37b0f4584c33f3ba52cfed3823885ad9d20b
J-shang <shangning128@163.com>
2022-08-26 15:08:30 +0800

    fix comments

--- examples/model_compress/pruning/taylorfo_transformers_evaluator.py
+++ examples/model_compress/pruning/taylorfo_transformers_evaluator.py
@@ -1,9 +1,19 @@
-from datasets import load_dataset
+import numpy as np
+
+from datasets import load_dataset, load_metric
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    Trainer,
+    TrainingArguments
+)
+
+import nni
+from nni.algorithms.compression.v2.pytorch import TransformersEvaluator
+from nni.algorithms.compression.v2.pytorch.pruning import TaylorFOWeightPruner
+
 
 dataset = load_dataset('yelp_review_full')
-
-from transformers import AutoTokenizer
-
 tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
 
 def tokenize_function(examples):
@@ -14,16 +24,9 @@ tokenized_datasets = dataset.map(tokenize_function, batched=True)
 small_train_dataset = tokenized_datasets['train'].shuffle(seed=42).select(range(1000))
 small_eval_dataset = tokenized_datasets['test'].shuffle(seed=42).select(range(1000))
 
-from transformers import AutoModelForSequenceClassification
-
 model = AutoModelForSequenceClassification.from_pretrained('bert-base-cased', num_labels=5)
 
-from transformers import TrainingArguments
-
 training_args = TrainingArguments(output_dir='test_trainer')
-
-import numpy as np
-from datasets import load_metric
 
 metric = load_metric('accuracy')
 
@@ -32,8 +35,6 @@ def compute_metrics(eval_pred):
     predictions = np.argmax(logits, axis=-1)
     return metric.compute(predictions=predictions, references=labels)
 
-from transformers import TrainingArguments, Trainer
-
 training_args = TrainingArguments(
     output_dir='./log',
     evaluation_strategy='epoch',
@@ -41,12 +42,6 @@ training_args = TrainingArguments(
     num_train_epochs=3,
     max_steps=-1
 )
-
-
-#################################################################################################
-import nni
-from nni.algorithms.compression.v2.pytorch import TransformersEvaluator
-from nni.algorithms.compression.v2.pytorch.pruning import TaylorFOWeightPruner
 
 trainer = nni.trace(Trainer)(
     model=model,
