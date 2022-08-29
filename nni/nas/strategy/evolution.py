99400a068ac0af923f5c3714e7fbb0f31bdd6f36
Yuge Zhang <Yuge.Zhang@microsoft.com>
2022-08-29 11:55:48 +0900

    Dedup for evolution search

--- nni/nas/strategy/evolution.py
+++ nni/nas/strategy/evolution.py
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+from __future__ import annotations
+
 import collections
 import dataclasses
 import logging
@@ -8,7 +10,7 @@ import random
 import time
 
 from nni.nas.execution import query_available_resources, submit_models
-from nni.nas.execution.common import ModelStatus
+from nni.nas.execution.common import Model, ModelStatus
 from .base import BaseStrategy
 from .utils import dry_run_for_search_space, get_targeted_model, filter_model
 
@@ -43,6 +45,10 @@ class RegularizedEvolution(BaseStrategy):
         The number of individuals that should participate in each tournament. Default: 25.
     mutation_prob : float
         Probability that mutation happens in each dim. Default: 0.05
+    dedup : bool
+        Do not try the same configuration twice. When variational is true, deduplication is not supported. Default: true.
+    dedup_retries : int
+        If dedup is true, retry the same configuration up to dedup_retries times. Default: 500.
     on_failure : str
         Can be one of "ignore" and "worst". If "ignore", simply give up the model and find a new one.
         If "worst", mark the model as -inf (if maximize, inf if minimize), so that the algorithm "learns" to avoid such model.
@@ -52,7 +58,7 @@ class RegularizedEvolution(BaseStrategy):
     """
 
     def __init__(self, optimize_mode='maximize', population_size=100, sample_size=25, cycles=20000,
-                 mutation_prob=0.05, on_failure='ignore', model_filter=None):
+                 mutation_prob=0.05, dedup=False, dedup_retries=500, on_failure='ignore', model_filter=None):
         assert optimize_mode in ['maximize', 'minimize']
         assert on_failure in ['ignore', 'worst']
         assert sample_size < population_size
@@ -61,13 +67,16 @@ class RegularizedEvolution(BaseStrategy):
         self.sample_size = sample_size
         self.cycles = cycles
         self.mutation_prob = mutation_prob
+        self.dedup = dedup
+        self.dedup_retries = dedup_retries
         self.on_failure = on_failure
 
         self._worst = float('-inf') if self.optimize_mode == 'maximize' else float('inf')
 
         self._success_count = 0
-        self._population = collections.deque()
-        self._running_models = []
+        self._history_configs: set[dict] = set()  # for dedup
+        self._population: list[Individual] = collections.deque()
+        self._running_models: list[tuple[dict, Model]] = []
         self._polling_interval = 2.
         self.filter = model_filter
 
@@ -117,7 +126,18 @@ class RegularizedEvolution(BaseStrategy):
         while self._success_count + len(self._running_models) <= self.cycles:
             # try to submit new models
             while query_available_resources() > 0 and self._success_count + len(self._running_models) < self.cycles:
-                config = self.mutate(self.best_parent(), search_space)
+                if self.dedup:
+                    config = None
+                    for _ in range(self.dedup_retries):
+                        new_config = self.mutate(self.best_parent(), search_space)
+                        if new_config not in self._history_configs:
+                            config = new_config
+                            break
+                    if config is None:
+                        _logger.warning('Deduplication failed. Generating an arbitrary config.')
+                        config = self.mutate(self.best_parent(), search_space)
+                else:
+                    config = self.mutate(self.best_parent(), search_space)
                 self._submit_config(config, base_model, applied_mutators)
             # collect results
             self._move_succeeded_models_to_population()
@@ -129,6 +149,7 @@ class RegularizedEvolution(BaseStrategy):
 
     def _submit_config(self, config, base_model, mutators):
         _logger.debug('Model submitted to running queue: %s', config)
+        self._history_configs.add(config)
         model = get_targeted_model(base_model, mutators, config)
         if not filter_model(self.filter, model):
             if self.on_failure == "worst":
