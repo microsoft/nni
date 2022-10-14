c209b83226e63f43ebcd9117f27e3625c6e2c584
Ubuntu <core@lijiao-vm.blbee4diapvedlj52zlxs1ydub.phxx.internal.cloudapp.net>
2022-10-14 07:27:49 +0000

    add localstorage expire time

--- ts/webui/src/components/experiment/trialdetail/ChangeColumnComponent.tsx
+++ ts/webui/src/components/experiment/trialdetail/ChangeColumnComponent.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Dialog, DialogType, DialogFooter, Checkbox, PrimaryButton, DefaultButton } from '@fluentui/react';
 import { EXPERIMENT } from '@static/datamodel';
+import { Storage } from '@model/localStorage';
+
 /**
  * changeColumnComponent file is for [customized table column, customized hyper-parameter graph yAxis]
  * and currently it uses localstorage to store the customized results
@@ -67,9 +69,19 @@ class ChangeColumnComponent extends React.Component<ChangeColumnProps, ChangeCol
         const selectedColumns = allColumns.map(column => column.key).filter(key => currentSelected.includes(key));
         onSelectedChange(selectedColumns);
         if (whichComponent === 'table') {
-            localStorage.setItem(`${EXPERIMENT.profile.id}_columns`, JSON.stringify(selectedColumns));
+            const storage = new Storage(
+                `${EXPERIMENT.profile.id}_columns`,
+                JSON.stringify(selectedColumns),
+                30 * 24 * 60 * 60 * 1000
+            );
+            storage.setValue();
         } else {
-            localStorage.setItem(`${EXPERIMENT.profile.id}_paraColumns`, JSON.stringify(selectedColumns));
+            const storage = new Storage(
+                `${EXPERIMENT.profile.id}_paraColumns`,
+                JSON.stringify(selectedColumns),
+                30 * 24 * 60 * 60 * 1000
+            );
+            storage.setValue();
         }
         this.hideDialog();
     };
