19328990531844a7249deac49be7aec2fd522c9b
Liu Zhe <zhe.liu@microsoft.com>
2022-10-28 03:11:43 +0800

    fix ws reconnect ut

--- ts/nni_manager/test/core/tuner_command_channel.test.ts
+++ ts/nni_manager/test/core/tuner_command_channel.test.ts
@@ -9,71 +9,66 @@ import WebSocket from 'ws';
 import { getWebSocketChannel, serveWebSocket } from 'core/tuner_command_channel';
 import { UnitTestHelpers } from 'core/tuner_command_channel/websocket_channel';
 
-UnitTestHelpers.setHeartbeatInterval(10);  // for testError, must be set before serveWebSocket()
+const heartbeatInterval: number = 10;
+
+// for testError, must be set before serveWebSocket()
+UnitTestHelpers.setHeartbeatInterval(heartbeatInterval);
 
 /* test cases */
 
 // Start serving and let a client connect.
 async function testInit(): Promise<void> {
+    const channel = getWebSocketChannel();
+    channel.onCommand(command => { serverReceived.push(command); });
+    channel.onError(error => { catchedError = error; });
+
     server.on('connection', serveWebSocket);
-    startClient();
-    await getWebSocketChannel().init();
+    client1 = new Client('client1');
+    await channel.init();
 }
 
 // Send commands from server to client.
-async function testSend(): Promise<void> {
+async function testSend(client: Client): Promise<void> {
     const channel = getWebSocketChannel();
 
     channel.sendCommand(command1);
     channel.sendCommand(command2);
-    await setTimeout(10);
+    await setTimeout(heartbeatInterval);
 
-    assert.equal(clientReceived.length, 2);
-    assert.equal(clientReceived[0], command1);
-    assert.equal(clientReceived[1], command2);
+    assert.deepEqual(client.received, [command1, command2]);
 }
 
 // Send commands from client to server.
-async function testReceive(): Promise<void> {
-    const channel = getWebSocketChannel();
-    channel.onCommand(command => { serverReceived.push(command); });
+async function testReceive(client: Client): Promise<void> {
+    serverReceived.length = 0;
 
-    client.send(command1);
-    client.send(command2);
-    await setTimeout(10);
+    client.ws.send(command2);
+    client.ws.send(command1);
+    await setTimeout(heartbeatInterval);
 
-    assert.equal(serverReceived.length, 2);
-    assert.deepEqual(serverReceived[0], command1);
-    assert.deepEqual(serverReceived[1], command2);
+    assert.deepEqual(serverReceived, [command2, command1]);
 }
 
 // Simulate client side crash.
 async function testError(): Promise<void> {
-    const channel = getWebSocketChannel();
+    if (process.platform !== 'darwin') {
+        // we have set heartbeat interval to 10ms, so pause for 30ms should make it timeout
+        client1.ws.pause();
+        await setTimeout(heartbeatInterval * 3);
+        client1.ws.resume();
 
-    if (process.platform === 'darwin') {
+    } else {
         // macOS does not raise the error in 30ms
         // not a big problem and don't want to debug. ignore it.
-        channel.shutdown();
-        return;
+        client1.ws.terminate();
     }
 
-    channel.onError(error => { catchedError = error; });
-
-    // we have set heartbeat interval to 10ms, so pause for 30ms should make it timeout
-    client.pause();
-    await setTimeout(30);
-
     assert.notEqual(catchedError, undefined);
-    client.resume();
 }
 
-// WebSocket might get broken in long experiments. Simulate reconnect.
+// If the client losses connection by accident but not crashed, it will reconnect.
 async function testReconnect(): Promise<void> {
-    client.close();
-    startClient();
-    testInit();
-    testSend();
+    client2 = new Client('client2');
 }
 
 // Clean up.
@@ -81,21 +76,24 @@ async function testShutdown(): Promise<void> {
     const channel = getWebSocketChannel();
     await channel.shutdown();
 
-    try {
-        client.close();
-    } catch (error) {
-        console.log('Error on clean up:', error);
-    }
+    client1.ws.close();
+    client2.ws.close();
     server.close();
 }
 
 /* register */
 describe('## tuner_command_channel ##', () => {
     it('init', testInit);
-    it('send', testSend);
-    it('receive', testReceive);
-    it('catch error', testError);
+
+    it('send', () => testSend(client1));
+    it('receive', () => testReceive(client1));
+
+    it('mock timeout', testError);
     it('reconnect', testReconnect);
+
+    it('send after reconnect', () => testSend(client2));
+    it('receive after reconnect', () => testReceive(client2));
+
     it('shutdown', testShutdown);
 });
 
@@ -103,17 +101,25 @@ describe('## tuner_command_channel ##', () => {
 
 const command1 = 'T_hello world';
 const command2 = 'T_你好';
-const commandPing = 'PI';
 
 const server = new WebSocket.Server({ port: 0 });
-let client!: WebSocket;
+let client1!: Client;
+let client2!: Client;
 
 const serverReceived: string[] = [];
-const clientReceived: string[] = [];
 let catchedError: Error | undefined;
 
-function startClient() {
-    const port = (server.address() as any).port;
-    client = new WebSocket(`ws://localhost:${port}`);
-    client.on('message', message => { clientReceived.push(message.toString()); });
+class Client {
+    name: string;
+    received: string[] = [];
+    ws!: WebSocket;
+
+    constructor(name: string) {
+        this.name = name;
+        const port = (server.address() as any).port;
+        this.ws = new WebSocket(`ws://localhost:${port}`);
+        this.ws.on('message', (data, _isBinary) => {
+            this.received.push(data.toString());
+        });
+    }
 }
