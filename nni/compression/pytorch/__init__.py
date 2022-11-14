d3b312d71f2558d15576c01b6a63a68d1ecb944d
J-shang <shangning128@163.com>
2022-11-14 18:25:04 +0800

    rm v1 example & mv quantization to compression

--- nni/compression/pytorch/__init__.py
+++ nni/compression/pytorch/__init__.py
@@ -4,4 +4,3 @@
 from nni.algorithms.compression.v2.pytorch import TorchEvaluator, LightningEvaluator, TransformersEvaluator
 from .speedup import ModelSpeedup
 from .compressor import Compressor, Pruner, Quantizer
-from .utils.apply_compression import apply_compression_results
