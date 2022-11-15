0fdb9476efc547c35f17daf1b9ea5d4a7bb079f7
Liu Zhe <zhe.liu@microsoft.com>
2022-11-16 07:52:54 +0800

    trial keeper

---
+++ ts/nni_manager/common/trial_keeper/keeper.ts
@@ -0,0 +1,157 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { EventEmitter } from 'events';
+import fs from 'fs/promises';
+import path from 'path';
+
+import { HttpChannelServer } from 'common/command_channel/http';
+import globals from 'common/globals';
+import { GpuScheduler } from 'common/gpu_scheduler';
+import { Logger, getLogger } from 'common/log';
+import { TrialProcess } from './process';
+
+export declare namespace TrialKeeper {
+    export interface TrialOptions {
+        id: string;
+        command: string;
+        codeDirectoryName: string;
+        sequenceId?: number;
+        gpuNumber?: number;
+        gpuRestrictions?: GpuRestrictions;
+    }
+
+    export interface GpuRestrictions {
+        onlyUseIndices?: number[];
+        rejectActive?: boolean;
+        rejectComputeActive?: boolean;
+    }
+}
+
+export class TrialKeeper {
+    private envId: string;
+    private channels: HttpChannelServer;
+    private dirs: Map<string, string> = new Map();
+    private emitter: EventEmitter = new EventEmitter();
+    private gpuScheduler: GpuScheduler | null = null;
+    private log: Logger;
+    private platform: string;
+    private trials: Map<string, TrialProcess> = new Map();
+
+    constructor(environmentId: string, platform: string, enableGpuScheduler: boolean) {
+        this.envId = environmentId;
+        this.platform = platform;
+        this.log = getLogger(`TrialKeeper.${environmentId}`);
+
+        this.channels = new HttpChannelServer(this.envId, `/env/${this.envId}`);
+
+        if (enableGpuScheduler) {
+            this.gpuScheduler = new GpuScheduler();
+        }
+
+        this.channels.onReceive((trialId, command) => {
+            if (command.type === 'request_parameter') {
+                this.emitter.emit('request-parameter', trialId);
+            } else if (command.type === 'metric') {
+                this.emitter.emit('metric', trialId, command.metric);
+            } else {
+                this.log.error('Received bad command:', trialId, command);
+            }
+        });
+    }
+
+    public async start(): Promise<void> {
+        await this.channels.start();
+
+        if (this.gpuScheduler !== null) {
+            await this.gpuScheduler.init();
+        }
+    }
+
+    public async shutdown(): Promise<void> {
+        await this.channels.shutdown();
+    }
+
+    public registerDirectory(name: string, path: string): void {
+        this.dirs.set(name, path);
+    }
+
+    public async createTrial(options: TrialKeeper.TrialOptions): Promise<string | null> {
+        const trialId = options.id;
+
+        let gpus: number[] | null = null;
+        if (options.gpuNumber) {
+            if (this.gpuScheduler === null) {
+                this.log.error('GPU scheduler is not enabled');
+                return null;
+            }
+            gpus = await this.gpuScheduler.schedule(
+                globals.args.experimentId,
+                trialId,
+                options.gpuNumber,
+                options.gpuRestrictions
+            );
+            if (gpus === null) {
+                this.log.info('No GPU available');
+                return null;
+            }
+        } else if (options.gpuNumber === 0) {
+            gpus = [];  // hide all gpus
+        }
+
+        // TODO: move this to globals.paths
+        const outputDir = path.join(globals.paths.experimentRoot, 'environments', this.envId, 'trials', trialId);
+        await fs.mkdir(outputDir, { recursive: true });
+
+        const trial = new TrialProcess(trialId);
+        trial.onStart(timestamp => {
+            this.emitter.emit('trial-start', trialId, timestamp);
+        });
+        trial.onStop((timestamp, exitCode, _signal) => {
+            this.emitter.emit('trial-stop', trialId, timestamp, exitCode);
+        });
+
+        const procOptions = {
+            command: options.command,
+            codeDirectory: this.dirs.get(options.codeDirectoryName)!,
+            outputDirectory: outputDir,
+            commandChannelUrl: this.channels.getChannelUrl(trialId),
+            gpuIndices: gpus ?? undefined,  // change null to undefined
+            platform: this.platform,
+            sequenceId: options.sequenceId,
+        }
+
+        const success = await trial.spawn(procOptions);
+        if (success) {
+            this.trials.set(trialId, trial);
+            return trialId;
+        } else {
+            return null;
+        }
+    }
+
+    public async stopTrial(trialId: string): Promise<void> {
+        await this.trials.get(trialId)!.kill();
+    }
+
+    public async sendParameter(trialId: string, parameter: any): Promise<void> {
+        const command = { type: 'parameter', parameter };
+        this.channels.send(trialId, command);
+    }
+
+    public onTrialStart(callback: (trialId: string, timestamp: number) => void): void {
+        this.emitter.on('trial-start', callback);
+    }
+
+    public onTrialStop(callback: (trialId: string, timestamp: number, exitCode: number | null) => void): void {
+        this.emitter.on('trial-stop', callback);
+    }
+
+    public onRequestParameter(callback: (trialId: string) => void): void {
+        this.emitter.on('request-parameter', callback);
+    }
+
+    public onMetric(callback: (trialId: string, metric: any) => void): void {
+        this.emitter.on('metric', callback);
+    }
+}
---
+++ ts/nni_manager/common/trial_keeper/process.ts
@@ -0,0 +1,206 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+/**
+ *  Manage the lifecycle of a trial process.
+ *
+ *  This is trial keeper's internal helper.
+ *  Should not be used elsewhere.
+ **/
+
+import child_process, { ChildProcess, SpawnOptions } from 'child_process';
+import events from 'events';
+import fs from 'fs';
+import path from 'path';
+
+import { Deferred } from 'common/deferred';
+import globals from 'common/globals';
+import { Logger, getLogger } from 'common/log';
+
+export interface TrialProcessOptions {
+    command: string;
+    codeDirectory: string;
+    outputDirectory: string;
+    commandChannelUrl: string;
+    gpuIndices?: number[];
+    platform: string;
+    sequenceId?: number;
+}
+
+interface TrialProcessInfo {
+    startSuccess?: boolean;  // it is true when the shell successfully launched, even if the trial command is ill-formed
+    startTime?: number;
+    stopTime?: number;
+    stopCode?: number | null;
+    stopSignal?: string | null;
+}
+
+export class TrialProcess {
+    private id: string;
+    private info: TrialProcessInfo = {};
+    private log: Logger;
+    private proc: ChildProcess | null = null;
+    private started: Deferred<void> = new Deferred();
+    private stopped: Deferred<void> = new Deferred();
+
+    constructor(id: string) {
+        this.id = id;
+        this.log = getLogger(`TrialProcess.${id}`);
+    }
+
+    public async spawn(options: TrialProcessOptions): Promise<boolean> {
+        // might change for log collection
+        const stdout = fs.createWriteStream(path.join(options.outputDirectory, 'trial.stdout'));
+        const stderr = fs.createWriteStream(path.join(options.outputDirectory, 'trial.stderr'));
+        await Promise.all([ events.once(stdout, 'open'), events.once(stderr, 'open') ]);
+
+        const spawnOptions: SpawnOptions = {
+            cwd: options.codeDirectory,
+            env: this.buildEnv(options),
+            stdio: [ 'ignore', stdout, stderr ],
+            shell: (process.platform === 'win32' ? true : '/bin/bash'),
+        };
+
+        this.proc = child_process.spawn(options.command, spawnOptions);
+
+        this.proc.on('spawn', () => { this.resolveStart('spawn'); });
+        this.proc.on('exit', (code, signal) => { this.resolveStop('exit', code, signal); });
+        this.proc.on('error', (err) => { this.handleError(err); });
+        this.proc.on('close', (code, signal) => { this.resolveStop('close', code, signal); });
+
+        await this.started.promise;
+        return Boolean(this.info.startSuccess);
+    }
+
+    /**
+     *  Kill the trial with SIGINT (not SIGTERM) and wait for `timeout` milliseconds.
+     *  Then force kill with SIGKILL if it is still running.
+     *
+     *  If the trial is not running, silently do nothing.
+     *
+     *  (FIXME) On Windows it always do force kill.
+     *
+     *  The returned promise is resolved together with onStop() callback.
+     **/
+    public async kill(timeout?: number): Promise<void> {
+        this.log.trace('kill');
+
+        if (!this.started.settled) {
+            this.log.error('Killing a not started trial');
+            return;
+        }
+        if (this.stopped.settled) {
+            return;
+        }
+
+        if (process.platform === 'win32') {
+            this.proc!.kill();  // FIXME
+
+        } else {
+            this.proc!.kill('SIGINT');
+            setTimeout(() => {
+                if (!this.stopped.settled) {
+                    this.log.info(`Failed to terminate in ${timeout ?? 5000} ms, force kill`);
+                    this.proc!.kill('SIGKILL');
+                }
+            }, timeout ?? 5000);
+        }
+
+        await this.stopped.promise;
+    }
+
+    /**
+     *  Register an on trial start callback.
+     *
+     *  The callback will always get invoked when spawn() returns true,
+     *  even if this method is called after start complete.
+     **/
+    public onStart(callback: (timestamp: number) => void): void {
+        this.started.promise.then(() => {
+            if (this.info.startSuccess) {
+                callback(this.info.startTime!);
+            }
+        });
+    }
+
+    public onStop(callback: (timestamp: number, exitCode: number | null, signal: string | null) => void): void {
+        this.stopped.promise.then(() => {
+            if (this.info.startSuccess) {
+                callback(this.info.stopTime!, this.info.stopCode!, this.info.stopSignal!);
+            }
+        });
+    }
+
+    private buildEnv(opts: TrialProcessOptions): Record<string, string> {
+        const env: Record<string, string> = Object.assign({}, process.env as any);
+        env['NNI_CODE_DIR'] = opts.codeDirectory;
+        env['NNI_EXP_ID'] = globals.args.experimentId;
+        env['NNI_OUTPUT_DIR'] = opts.outputDirectory;
+        env['NNI_PLATFORM'] = opts.platform;
+        env['NNI_SYS_DIR'] = opts.outputDirectory;
+        env['NNI_TRIAL_COMMAND_CHANNEL'] = opts.commandChannelUrl;
+        env['NNI_TRIAL_JOB_ID'] = this.id;
+        env['NNI_TRIAL_SEQ_ID'] = String(opts.sequenceId ?? -1);
+        if (opts.gpuIndices !== undefined) {
+            env['CUDA_VISIBLE_DEVICES'] = opts.gpuIndices.join(',');
+        }
+        this.log.trace('Env:', env);
+        return env;
+    }
+
+    private resolveStart(event: string): void {
+        this.log.trace('Start', event);
+
+        if (this.started.settled) {
+            this.log.warning(`Receive ${event} event after started`);
+            return;
+        }
+
+        this.info.startTime = Date.now();
+
+        if (this.stopped.settled) {
+            this.log.error(`Receive ${event} event after stopped`);
+            this.info.startSuccess = false;
+        } else {
+            this.info.startSuccess = true;
+        }
+
+        this.started.resolve();
+    }
+
+    private resolveStop(event: string, code: number | null, signal: string | null, timestamp?: number): void {
+        this.log.trace('Stop', event, code, signal, timestamp);
+
+        if (event === 'close') {
+            this.log.debug('Stopped and cleaned');
+            this.proc = null;
+        }
+
+        if (this.stopped.settled) {
+            if (event !== 'close' && timestamp === undefined) {
+                this.log.warning(`Receive ${event} event after stopped`);
+            }
+            return;
+        }
+
+        this.info.stopTime = timestamp ?? Date.now();
+        this.info.stopCode = code;
+        this.info.stopSignal = signal;
+
+        if (!this.started.settled) {
+            this.log.error(`Receive ${event} event before starting`);
+            this.info.startSuccess = false;
+            this.started.resolve();
+        }
+
+        this.stopped.resolve();
+    }
+
+    private handleError(err: Error): void {
+        this.log.error('Error:', err);
+        if (!this.stopped.settled) {
+            const time = Date.now();
+            setTimeout(() => { this.resolveStop('error', null, null, time); }, 1000);
+        }
+    }
+}
