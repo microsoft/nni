740c530e458597c579faa43e6fabf17cab61af27
J-shang <shangning128@163.com>
2022-11-17 10:34:16 +0800

    fix comments

---
+++ nni/algorithms/compression/pytorch/quantization/native_quantizer.py
@@ -0,0 +1,4 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from nni.compression.pytorch.quantization.native_quantizer import *
---
+++ nni/algorithms/compression/pytorch/quantization/observer_quantizer.py
@@ -0,0 +1,4 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from nni.compression.pytorch.quantization.observer_quantizer import *
