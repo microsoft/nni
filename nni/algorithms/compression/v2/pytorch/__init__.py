b2929b32ebe26f9a115ab23080d44445efbbcaa8
J-shang <shangning128@163.com>
2022-11-17 10:31:59 +0800

    fix comments

--- nni/algorithms/compression/v2/pytorch/__init__.py
+++ nni/algorithms/compression/v2/pytorch/__init__.py
@@ -1,9 +1,4 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-import logging
-
-_logger = logging.getLogger(__name__)
-warn_msg = 'import path `nni.algorithms.compression.v2.pytorch` has been removed in v3.0, ' +\
-           'please import from `nni.compression.pytorch`.'
-_logger.warning(warn_msg)
+from nni.compression.pytorch import *
--- nni/algorithms/compression/v2/pytorch/base/pruner.py
+++ nni/algorithms/compression/v2/pytorch/base/pruner.py
@@ -1,4 +1,4 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from nni.compression import *
+from nni.compression.pytorch.base.pruner import *
