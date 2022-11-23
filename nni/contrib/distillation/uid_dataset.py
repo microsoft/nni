3c9612ccdacd201bf5655f57fd1e479622a649c0
J-shang <shangning128@163.com>
2022-11-23 13:20:22 +0800

    fix comments

--- nni/contrib/distillation/uid_dataset.py
+++ nni/contrib/distillation/uid_dataset.py
@@ -22,7 +22,7 @@ class _UidDataset(Dataset):
         return len(self._dataset)  # type: ignore
 
     def __getitem__(self, index):
-        return None, self._dataset.__getitem__(index)
+        raise NotImplementedError()
 
     def get_origin_dataset(self):
         return self._dataset
@@ -68,6 +68,7 @@ class HashedDataset(_UidDataset):
 
 class AugmentationDataset(_UidDataset):
     """
+    Each sample will be given a integer random seed to apply transform.
     Return (aux_uid/seed, origin_sample).
 
     Parameters
@@ -78,7 +79,22 @@ class AugmentationDataset(_UidDataset):
     transform
         The transform function for each sample from dataset.
     seed
-        This seed controls the random generators used in transform.
+        This seed controls the random generator used in transform.
+    get_rngs_state
+        () -> states. A callable function to get all the random generator states in current environment.
+        By default, the random generator states of ``built-in random``, ``numpy.random``, ``torch.random`` will be got,
+        used to recover the random state after transform to before tranform.
+        If other random library is used, please customize ``get_rngs_state``.
+    set_rngs_state
+        (states) -> None. A callable function to set all the random generators in current environment with given states.
+        By default, the random generator states of ``built-in random``, ``numpy.random``, ``torch.random`` will be set,
+        used to recover the random state after transform to before tranform.
+        If other random library is used, please customize ``set_rngs_state``.
+        The input of ``set_rngs_state`` is the state got from ``get_rngs_state``.
+    manual_seed
+        (int) -> None. A callable function to manual seed for all random library.
+        By default, ``built-in random``, ``numpy.random``, ``torch.random`` will be set seed.
+        In ``__getitem__``, the calling sequence is ``get_rngs_state -> manual_seed -> transform -> set_rngs_state``.
     aux_dataset_cls
         `aux_dataset_cls` is used to wrap the dataset to generate the aux_uid.
     aux_args
@@ -87,6 +103,8 @@ class AugmentationDataset(_UidDataset):
         Additional keyword arguments for `aux_dataset_cls` initialization.
     """
     def __init__(self, dataset: Dataset, transform: Callable[[Any], Any], seed: int | None = None,
+                 get_rngs_state: Callable | None = None, set_rngs_state: Callable | None = None,
+                 manual_seed: Callable[[int]] | None = None,
                  aux_dataset_cls: Type[_UidDataset] | None = None, *aux_args, **aux_kwargs):
         if isinstance(dataset, _UidDataset) and aux_dataset_cls is None:
             _dataset = dataset
@@ -100,6 +118,9 @@ class AugmentationDataset(_UidDataset):
         self._rng = torch.Generator()
         if seed is not None:
             self._rng.manual_seed(seed=seed)
+        self.get_rngs_state = get_rngs_state if get_rngs_state else _default_get_rngs_state
+        self.set_rngs_state = set_rngs_state if set_rngs_state else _default_set_rngs_state
+        self.manual_seed = manual_seed if manual_seed else _default_manual_seed
 
         self._replay_mode = False
         self._suid_seed = defaultdict(list)
@@ -107,7 +128,7 @@ class AugmentationDataset(_UidDataset):
     def __getitem__(self, index):
         suid, sample = self._dataset.__getitem__(index)
 
-        rngs_state = _default_get_rngs_state()
+        rngs_state = self.get_rngs_state()
 
         if self._replay_mode:
             seed = self._suid_seed[str(suid)].pop(-1)
@@ -115,10 +136,10 @@ class AugmentationDataset(_UidDataset):
         else:
             seed = self._generate_seed()
             self._suid_seed[str(suid)].append(seed)
-        _default_manual_seed(seed)
+        self.manual_seed(seed)
         sample = self._transform(sample)
 
-        _default_set_rngs_state(rngs_state)
+        self.set_rngs_state(rngs_state)
 
         return f'{suid}/{seed}', sample
 
