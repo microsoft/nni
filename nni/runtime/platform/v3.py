d41c289e3151bd8a83278b3009641dd436e27619
liuzhe <zhliu1@microsoft.com>
2022-12-01 22:45:11 +0800

    lazy channel

--- nni/runtime/platform/v3.py
+++ nni/runtime/platform/v3.py
@@ -8,20 +8,34 @@ import os
 from ..command_channel.http import HttpChannel
 from ..env_vars import trial_env_vars
 
-_channel = HttpChannel(os.environ['NNI_TRIAL_COMMAND_CHANNEL'])
+_logger = logging.getLogger(__name__)
+
+_channel = None
+
+def _init_channel():
+    global _channel
+    url = os.environ['NNI_TRIAL_COMMAND_CHANNEL']
+    _logger.info(f'Connect to trial command channel {url}')
+    _channel = HttpChannel(url)
 
 def get_next_parameter():
+    if _channel is None:
+        _init_channel()
+
     req = {'type': 'request_parameter'}
     _channel.send(req)
 
     res = _channel.receive()
     if res is None:
-        logging.getLogger(__name__).error('Command channel is closed')
+        _logger.error('Command channel is closed')
         return None
     assert res['type'] == 'parameter'
     return json.loads(res['parameter'])
 
 def send_metric(string):
+    if _channel is None:
+        _init_channel()
+
     command = {'type': 'metric', 'metric': string}
     _channel.send(command)
 
