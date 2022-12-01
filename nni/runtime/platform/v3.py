3f4a8320dd495437d30cb955fa070fdc0214ae89
liuzhe <zhliu1@microsoft.com>
2022-12-01 23:02:07 +0800

    fix lint

--- nni/runtime/platform/v3.py
+++ nni/runtime/platform/v3.py
@@ -4,21 +4,22 @@
 import json
 import logging
 import os
+from typing import Any
 
 from ..command_channel.http import HttpChannel
 from ..env_vars import trial_env_vars
 
 _logger = logging.getLogger(__name__)
 
-_channel = None
+_channel: HttpChannel = None  # type: ignore
 
-def _init_channel():
+def _init_channel() -> None:
     global _channel
     url = os.environ['NNI_TRIAL_COMMAND_CHANNEL']
     _logger.info(f'Connect to trial command channel {url}')
     _channel = HttpChannel(url)
 
-def get_next_parameter():
+def get_next_parameter() -> Any:
     if _channel is None:
         _init_channel()
 
@@ -32,18 +33,18 @@ def get_next_parameter():
     assert res['type'] == 'parameter'
     return json.loads(res['parameter'])
 
-def send_metric(string):
+def send_metric(string: str) -> None:
     if _channel is None:
         _init_channel()
 
     command = {'type': 'metric', 'metric': string}
     _channel.send(command)
 
-def get_experiment_id():
+def get_experiment_id() -> str:
     return trial_env_vars.NNI_EXP_ID
 
-def get_trial_id():
+def get_trial_id() -> str:
     return trial_env_vars.NNI_TRIAL_JOB_ID
 
-def get_sequence_id():
+def get_sequence_id() -> int:
     return int(trial_env_vars.NNI_TRIAL_SEQ_ID)
