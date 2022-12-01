313b60f55327e8fcd25dbfc20856d1da5a47dbbe
Yuge Zhang <scottyugochang@gmail.com>
2022-12-01 14:40:08 +0800

    fix tests

--- test/ut/sdk/test_trial_command_channel.py
+++ test/ut/sdk/test_trial_command_channel.py
@@ -1,6 +1,7 @@
 import pytest
 import nni
 
+from nni.trial import overwrite_intermediate_seq
 from nni.runtime.trial_command_channel import (
     TrialCommandChannel, set_default_trial_command_channel,
     get_default_trial_command_channel
@@ -8,6 +9,8 @@ from nni.runtime.trial_command_channel import (
 
 
 def test_standalone(caplog):
+    overwrite_intermediate_seq(0)
+
     with pytest.warns(RuntimeWarning) as record:
         nni.get_next_parameter()
 
@@ -33,10 +36,12 @@ def test_customized_channel():
                 assert kwargs.get('type') == 'FINAL'
                 assert kwargs.get('parameter_id') == 123
                 assert kwargs.get('value') == 456
+                self._received = True
 
         set_default_trial_command_channel(TestChannel())
         assert nni.get_next_parameter()['x'] == 0
         nni.report_final_result(456)
+        assert get_default_trial_command_channel()._received
 
     finally:
         set_default_trial_command_channel(_default_channel)
