badf49dfee4776a5cf7d00a3f5d2406175a295da
J-shang <33053116+J-shang@users.noreply.github.com>
2023-02-16 18:54:14 +0800

    [Compression] v2.5 stage 5: add QAT quantizer (#5296)

--- nni/contrib/compression/base/target_space.py
+++ nni/contrib/compression/base/target_space.py
@@ -289,11 +289,11 @@ class QuantizationTargetSpace(TargetSpace):
         self._tensor_setter_helper(self._zero_point_name, val)
 
     @property
-    def qmax(self) -> Tensor | None:
+    def qmax(self) -> int | None:
         return self._get_wrapper_attr(self._qmax_name)
 
     @property
-    def qmin(self) -> Tensor | None:
+    def qmin(self) -> int | None:
         return self._get_wrapper_attr(self._qmin_name)
 
     @property
