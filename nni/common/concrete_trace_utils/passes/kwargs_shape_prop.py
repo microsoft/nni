2bd1c70384ef461d6ff35ac6fac9ac1fcd56d466
Juntao <junesnow0806@gmail.com>
2023-03-12 09:12:50 +0800

    update kwargs_shape_prop with reusing codes of torch.fx.passes.shape_prop

--- nni/common/concrete_trace_utils/passes/kwargs_shape_prop.py
+++ nni/common/concrete_trace_utils/passes/kwargs_shape_prop.py
@@ -1,64 +1,9 @@
 import torch
 import traceback
 from torch.fx.node import Node, map_aggregate
-from typing import Optional, Union, NamedTuple, Tuple, Any, Dict
+from torch.fx.passes.shape_prop import _extract_tensor_metadata
+from typing import Union, Tuple, Any, Dict
 from ..kwargs_interpreter import KwargsInterpreter
-
-class TensorMetadata(NamedTuple):
-    # TensorMetadata is a structure containing pertinent information
-    # about a tensor within a PyTorch program.
-
-    # General Tensor metadata
-    shape : torch.Size
-    dtype : torch.dtype
-    requires_grad : bool
-    stride : Tuple[int]
-    memory_format : Optional[torch.memory_format]
-
-    # Quantization metadata
-    is_quantized : bool
-    qparams: Dict[str, Any]
-
-def _extract_tensor_metadata(result : torch.Tensor) -> TensorMetadata:
-    """
-    Extract a TensorMetadata NamedTuple describing `result`.
-    """
-    shape = result.shape
-    dtype = result.dtype
-    requires_grad = result.requires_grad
-    stride = result.stride()
-
-    memory_formats = {
-        torch.contiguous_format,
-        torch.channels_last,
-        torch.channels_last_3d,
-    }
-
-    memory_format = None
-
-    for query_format in memory_formats:
-        if result.is_contiguous(memory_format=query_format):
-            memory_format = query_format
-            break
-
-    is_quantized = result.is_quantized
-    qparams: Dict[str, Any] = {}
-    if is_quantized:
-        qscheme = result.qscheme()
-        qparams["qscheme"] = qscheme
-        if qscheme in {torch.per_tensor_affine, torch.per_tensor_symmetric}:
-            qparams["scale"] = result.q_scale()  # type: ignore[assignment]
-            qparams["zero_point"] = result.q_zero_point()  # type: ignore[assignment]
-        elif qscheme in {torch.per_channel_affine, torch.per_channel_affine_float_qparams, torch.per_channel_symmetric}:
-            # In this branch, scale and zero_point are expected to be tensors,
-            # we store the values as immutable_list in TensorMetadata for
-            # easier serialization downstream
-            qparams["scale"] = result.q_per_channel_scales().tolist()  # type: ignore[assignment]
-            qparams["zero_point"] = result.q_per_channel_zero_points().tolist()  # type: ignore[assignment]
-            qparams["axis"] = result.q_per_channel_axis()  # type: ignore[assignment]
-
-    return TensorMetadata(
-        shape, dtype, requires_grad, stride, memory_format, is_quantized, qparams)
 
 class KwargsShapeProp(KwargsInterpreter):
     def run_node(self, n: Node):
