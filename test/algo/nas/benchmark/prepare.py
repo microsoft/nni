38186bc2f49a4699f322e23a3798d102147487cb
Yuge Zhang <scottyugochang@gmail.com>
2023-03-17 12:25:08 +0800

    fix full tests

--- test/algo/nas/benchmark/prepare.py
+++ test/algo/nas/benchmark/prepare.py
@@ -1,8 +1,14 @@
 import pytest
 
+import nni
 from nni.nas.benchmark import download_benchmark
 
 @pytest.fixture(autouse=True, scope='session')
 def prepare_benchmark():
     for benchmark in ['nasbench101', 'nasbench201']:
         download_benchmark(benchmark)
+
+@pytest.fixture(autouse=True)
+def reset_cached_parameter():
+    nni.trial._params = None
+    nni.trial.overwrite_intermediate_seq(0)
