9b96e567cce2ca341d21cf90737e385b4913bceb
Yuge Zhang <scottyugochang@gmail.com>
2023-03-17 17:21:41 +0800

    update pipeline

--- test/algo/nas/benchmark/prepare.py
+++ test/algo/nas/benchmark/prepare.py
@@ -4,14 +4,16 @@ import pytest
 import nni
 from nni.nas.benchmark import download_benchmark
 
-@pytest.fixture(autouse=True, scope='session')
 def prepare_benchmark():
-    if sys.platform != 'linux':
-        pytest.skip('Benchmark tests are too slow on Windows.')
     for benchmark in ['nasbench101', 'nasbench201']:
         download_benchmark(benchmark)
 
 @pytest.fixture(autouse=True)
 def reset_cached_parameter():
+    if sys.platform != 'linux':
+        pytest.skip('Benchmark tests are too slow on Windows.')
     nni.trial._params = None
     nni.trial.overwrite_intermediate_seq(0)
+
+if __name__ == '__main__':
+    prepare_benchmark()
