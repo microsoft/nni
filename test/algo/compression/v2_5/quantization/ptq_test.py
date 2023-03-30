e017ae98b754825021d69500802be807f1f24be6
Bonytu <47250017+Bonytu@users.noreply.github.com>
2023-03-30 13:13:11 +0800

    [Bugbash] Bug fix (#5467)

--- test/algo/compression/v2_5/quantization/ptq_test.py
+++ test/algo/compression/v2_5/quantization/ptq_test.py
@@ -32,11 +32,11 @@ def test_ptq_forward_with_torch_model():
     torch.manual_seed(0)
     model = SimpleTorchModel().to(device)
     configure_list = [{
-        'target_names':['_input_', 'weight', '_output_'],
+        'target_names':['weight'],
         'op_names': ['fc1', 'fc2'],
         'quant_dtype': 'int8',
         'quant_scheme': 'affine',
-        'granularity': 'default',
+        'granularity': 'in_channel',
     },
     {
         'target_names':['_input_', 'weight', '_output_'],
@@ -54,11 +54,11 @@ def test_ptq_forward_with_torch_model():
 def test_ptq_forward_with_lighting_model():
     torch.manual_seed(0)
     configure_list = [{
-        'target_names':['_input_', 'weight', '_output_'],
+        'target_names':['weight'],
         'op_names': ['model.fc1', 'model.fc2'],
         'quant_dtype': 'int8',
         'quant_scheme': 'affine',
-        'granularity': 'default',
+        'granularity': 'in_channel',
     },
     {
         'target_names':['_input_', 'weight', '_output_'],
