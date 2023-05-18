corrected shape formula, added unit test

nni/nas/profiler/pytorch/utils/shape_formula.py
@@ -180,8 +180,8 @@ def avgpool2d_formula(module: nn.AvgPool2d , input: ShapeTensor) -> MutableShape
     )
  
     # H_out and W_out
-    shape[-2] = (shape[-2] + 2 * padding[0] -  (kernel_size[0] - 1) - 1) // stride[0] + 1
-    shape[-1] = (shape[-1] + 2 * padding[1] -  (kernel_size[1] - 1) - 1) // stride[1] + 1
+    shape[-2] = (shape[-2] + 2 * padding[0] -  (kernel_size[0])) // stride[0] + 1
+    shape[-1] = (shape[-1] + 2 * padding[1] -  (kernel_size[1])) // stride[1] + 1
     return MutableShape(*shape)
 
 def multihead_attention_formula(module: nn.MultiheadAttention | nas_nn.MutableMultiheadAttention,