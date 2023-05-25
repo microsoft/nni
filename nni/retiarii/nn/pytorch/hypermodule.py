ab5e21fc19d8d3f6fef5c1faf8268da3b75ec165
Yuge Zhang <scottyugochang@gmail.com>
2023-05-25 11:44:57 +0800

    Remove Retiarii and legacy examples

--- nni/retiarii/nn/pytorch/hypermodule.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
-# pylint: disable=wildcard-import,unused-wildcard-import,unused-import
-
-from nni.nas.hub.pytorch.modules import AutoActivation
