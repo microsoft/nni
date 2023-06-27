364a9e10a2adab9793680395b9059896ed9de965
xinzhang3 <xinzhang3@microsoft.com>
2023-06-27 07:06:28 +0000

    fix full test

--- test/vso_tools/build_vm/setup_linux.sh
+++ test/vso_tools/build_vm/setup_linux.sh
@@ -75,3 +75,15 @@ sudo systemctl disable apt-daily-upgrade.service
 # Deprovision
 sudo /usr/sbin/waagent -force -deprovision
 sudo HISTSIZE=0 sync
+
+# Cuda
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.4.1/local_installers/cuda-repo-ubuntu2004-11-4-local_11.4.1-470.57.02-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-4-local_11.4.1-470.57.02-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-4-local/7fa2af80.pub
+sudo apt-get update
+
+sudo apt-get -y install cuda
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
